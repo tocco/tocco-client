acec8de82c59210f3d2f4bd0600265b52913d8dd
Roland Züger <rzueger@tocco.ch>
2016-04-27 21:38:27 +0200

    Remove `text-center` class from CoreLayout

--- src/layouts/CoreLayout/CoreLayout.js
+++ src/layouts/CoreLayout/CoreLayout.js
@@ -2,7 +2,7 @@ import React from 'react'
 import '../../styles/core.scss'
 
 export const CoreLayout = ({ children }) => (
-  <div className='container text-center'>
+  <div className='container'>
     {children}
   </div>
 )
