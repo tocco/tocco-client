7381af163316acca63d5b4de8f9771cddfa2294a
Urs Wolfer <uwolfer@tocco.ch>
2016-09-16 14:12:06 +0200

    simplify sendDwrRequest: nice2.netui.dwr.RemoteService returns a promise now

--- packages/merge-action/src/utils/Dwr.js
+++ packages/merge-action/src/utils/Dwr.js
@@ -1,10 +1,7 @@
 export default function sendDwrRequest(remoteService, method, args) {
-  return new Promise((resolve) => {
-    nice2.netui.dwr.RemoteService.call({
-      remoteService: remoteService,
-      method: method,
-      args: [args],
-      success: res => resolve(res)
-    })
+  return nice2.netui.dwr.RemoteService.call({
+    remoteService: remoteService,
+    method: method,
+    args: [args]
   })
 }
