1ea97393907a1d01c95fa9cd49d9cfb4aee4c39e
Daniel Keller <dkeller@tocco.ch>
2016-09-19 15:33:59 +0200

    refact(tocco-util): intl stuff to util
    - moved intl initializing method from merge package to tocco-util

---
+++ packages/tocco-util/src/Intl.js
@@ -0,0 +1,30 @@
+import {updateIntl} from 'react-intl-redux'
+
+export default class Intl {
+  static initIntl(store, moduleName) {
+    return Intl.getUserInfo()
+      .then(userInfo => Intl.loadTextResources(userInfo.locale, moduleName)
+        .then(textResources => store.dispatch(updateIntl({
+            locale: userInfo.locale.replace('_', '-'),
+            messages: textResources
+          }))
+        ))
+  }
+
+  static getUserInfo() {
+    return fetch(`${__BACKEND_URL__}/nice2/username`)
+      .then(response => response.json())
+  }
+
+  static loadTextResources(locale, moduleName) {
+    return fetch(`${__BACKEND_URL__}/nice2/textresource?locale=${locale}${moduleName ? `&module=${moduleName}` : ''}`)
+      .then(response => response.json())
+  }
+}
+
+
+
+
+
+
+
--- packages/tocco-util/src/main.js
+++ packages/tocco-util/src/main.js
@@ -1,9 +1,11 @@
 import StoreFactory from './store/storeFactory'
 import ExternalEvents from './ExternalEvents'
+import Intl from './Intl'
 
 var exports = {
   StoreFactory,
-  ExternalEvents: new ExternalEvents()
+  ExternalEvents: new ExternalEvents(),
+  Intl
 }
 
 module.exports = exports
