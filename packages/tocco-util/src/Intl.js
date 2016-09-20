ed97482ac1164ed2c9ac4d027294e91d47eccd15
Roli Züger <rzueger@tocco.ch>
2016-09-20 10:16:32 +0200

    feat(tocco-util): Mock text resource requests in dev env

--- packages/tocco-util/src/Intl.js
+++ packages/tocco-util/src/Intl.js
@@ -14,11 +14,23 @@ export default class Intl {
   }
 
   static getUserInfo() {
+    if (__DEV__) {
+      console.log('would fetch userinfo')
+      return new Promise(resolve => resolve({
+        locale: 'de-CH'
+      }))
+    }
     return fetch(`${__BACKEND_URL__}/nice2/username`)
       .then(response => response.json())
   }
 
   static loadTextResources(locale, moduleName) {
+    if (__DEV__) {
+      console.log('would fetch textresources')
+      return new Promise(resolve => resolve({
+        testKey: 'das ist ein test'
+      }))
+    }
     return fetch(`${__BACKEND_URL__}/nice2/textresource?locale=${locale}${moduleName ? `&module=${moduleName}` : ''}`)
       .then(response => response.json())
   }
