462dcf8cc2c8ea4b3e675d0c4c4a7780e1dbabbd
Daniel Keller <dkeller@tocco.ch>
2016-09-28 16:29:44 +0200

    fix(tocco-ui-showcase): workaround loading highlight.js styles from node_modules folder
    - Localy addressing the module without node_modules in path worked fine but travis failed to load it that way :
        Module not found: Error: Cannot resolve module 'highlight.js/styles/Github.css' in /home/travis/build/tocco/tocco-client/packages/tocco-ui-showcase/src/components

--- packages/tocco-ui-showcase/src/components/Example.js
+++ packages/tocco-ui-showcase/src/components/Example.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Highlight from 'react-highlight'
-import 'highlight.js/styles/Github.css'
+import '../../node_modules/highlight.js/styles/Github.css'
 import {extractExampleCode, extractRealImports, removeIndent} from './../util/ExampleParser'
 
 const Example = (props) => {
