e0f75a9a10538cc3a05d19ef286e47902e453ba2
Daniel Keller <dkeller@tocco.ch>
2016-10-14 14:30:05 +0200

    reafctor(login): dev response default fail, linting
    - dev login response now retruns failt login as default
    - small refactoring and fix lint error

--- packages/login/src/dev/loginResponseMocks.js
+++ packages/login/src/dev/loginResponseMocks.js
@@ -1,33 +1,27 @@
 export function getResponse(data) {
+  const headers = {'Content-Type': 'application/json; charset=utf-8'}
+
   var response
   var type = getResponseType(data)
   switch (type) {
     case 'success':
       response = new Response('{"success":true}', {
-        headers: {'Content-Type': 'application/json; charset=utf-8'}
-      })
-      break
-    case 'auth_fail':
-      response = new Response('{"success":false}', {
-        headers: {'Content-Type': 'application/json; charset=utf-8'},
-        ok: false,
-        status: 401,
-        statusText: 'Unauthorized'
+        headers
       })
       break
     case 'two_factor':
       response = new Response('{"TWOSTEPLOGIN":true,"success":false,"REQUESTEDCODE":"A8","timeout":30}', {
-        headers: {'Content-Type': 'application/json; charset=utf-8'}
+        headers
       })
       break
     case 'reset_password':
       response = new Response('{"RESET_PASSWORD_REQUIRED":true,"success":false,"principal_id":"12345","timeout":30}', {
-        headers: {'Content-Type': 'application/json; charset=utf-8'}
+        headers
       })
       break
     case 'before_blocked':
       response = new Response('{"ONE_TILL_BLOCK":true,"success":false}', {
-        headers: {'Content-Type': 'application/json; charset=utf-8'},
+        headers,
         ok: false,
         status: 401,
         statusText: 'Unauthorized'
@@ -35,15 +29,18 @@ export function getResponse(data) {
       break
     case 'blocked':
       response = new Response('{"LOGIN_BLOCKED":true,"success":false}', {
-        headers: {'Content-Type': 'application/json; charset=utf-8'},
+        headers,
         ok: false,
         status: 401,
         statusText: 'Unauthorized'
       })
       break
     default:
-      response = new Response('', {
-        headers: {'Content-Type': 'application/json; charset=utf-8'}
+      response = new Response('{"success":false}', {
+        headers,
+        ok: false,
+        status: 401,
+        statusText: 'Unauthorized'
       })
   }
 
