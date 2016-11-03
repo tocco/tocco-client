dea69910a57cfc35599c090ab2fff9ca0113f470
Daniel Keller <dkeller@tocco.ch>
2016-11-03 14:05:20 +0100

    chore(tocco-client): rewrite compile function to promise

--- bin/compile.js
+++ bin/compile.js
@@ -6,24 +6,29 @@ import {packagesExists} from './packages'
 
 const debug = _debug('app:bin:compile')
 
-  ;
-(async function () {
-  try {
-    debug('Run compiler')
-    var packageName = process.env.npm_config_package
+const compile = () => {
+  debug('Run compiler')
 
-    if (!packageName || !packagesExists(packageName)) {
-      debug('Please select a valid package with --package={PACKAGE_NAME} parameter.')
-      process.exit(1)
-    }
-
-    const stats = await webpackCompiler(webpackConfig)
-    if (stats.warnings.length && config.compiler_fail_on_warning) {
-      debug('Config set to fail on warning, exiting with status code "1".')
-      process.exit(1)
-    }
-  } catch (e) {
-    debug('Compiler encountered an error.', e)
+  var packageName = process.env.npm_config_package
+  if (!packageName || !packagesExists(packageName)) {
+    debug('Please select a valid package with --package={PACKAGE_NAME} parameter.')
     process.exit(1)
   }
-})()
+
+  return Promise.resolve()
+    .then(() => webpackCompiler(webpackConfig))
+    .then(stats => {
+      if (stats.warnings.length && config.compiler_fail_on_warning) {
+        throw new Error('Config set to fail on warning, exiting with status code "1".')
+      }
+    })
+    .then(() => {
+      debug('Compilation completed successfully.')
+    })
+    .catch(err => {
+      debug('Compiler encountered an error.', err)
+      process.exit(1)
+    })
+}
+
+compile()
