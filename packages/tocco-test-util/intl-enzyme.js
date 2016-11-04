651946c8ca6d34228c0db1632e8e85870413e031
Daniel Keller <dkeller@tocco.ch>
2016-11-04 13:35:18 +0100

    refactor(tocco-test-util): remove unused enzyme intlprovider wrapper

--- packages/tocco-test-util/intl-enzyme.js
+++
@@ -1,8 +0,0 @@
-import {mountWithIntlProvider, shallowWithIntlProvider} from './src/intl-enzyme'
-
-const exports = {
-  mountWithIntlProvider,
-  shallowWithIntlProvider
-}
-
-module.exports = exports
