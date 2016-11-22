1e70859143c9471316c8dc8045d3d73d0aca811e
Daniel Keller <dkeller@tocco.ch>
2016-11-22 09:35:18 +0100

    refactor(tocco-ui): table component various review fixes
    - misspellings in docs
    - use english labels in showcase
    - refactor a function to shorter form

--- packages/tocco-ui-showcase/src/uiComponentsTree.js
+++ packages/tocco-ui-showcase/src/uiComponentsTree.js
@@ -1,22 +1,46 @@
-import ToccoLogoRaw from '!raw!../../tocco-ui/src/ToccoLogo/ToccoLogo'
-import ToccoLogoExample from '../../tocco-ui/src/ToccoLogo/example'
-import ToccoLogoExampleRaw from '!raw!../../tocco-ui/src/ToccoLogo/example'
-
-import ButtonRaw from '!raw!../../tocco-ui/src/Button/Button'
-import ButtonExample from '../../tocco-ui/src/Button/example'
-import ButtonExampleRaw from '!raw!../../tocco-ui/src/Button/example'
+import TableRaw from '!raw!../../tocco-ui/src/Table/Table'
+import TableExample from '../../tocco-ui/src/Table/example'
+import TableExampleRaw from '!raw!../../tocco-ui/src/Table/example'
 
 import LoadMaskRaw from '!raw!../../tocco-ui/src/LoadMask/LoadMask'
 import LoadMaskExample from '../../tocco-ui/src/LoadMask/example'
 import LoadMaskExampleRaw from '!raw!../../tocco-ui/src/LoadMask/example'
 
+import ButtonRaw from '!raw!../../tocco-ui/src/Button/Button'
+import ButtonExample from '../../tocco-ui/src/Button/example'
+import ButtonExampleRaw from '!raw!../../tocco-ui/src/Button/example'
+
+import ToccoLogoRaw from '!raw!../../tocco-ui/src/ToccoLogo/ToccoLogo'
+import ToccoLogoExample from '../../tocco-ui/src/ToccoLogo/example'
+import ToccoLogoExampleRaw from '!raw!../../tocco-ui/src/ToccoLogo/example'
+
 const categories = {
-  CORPORATE_IDENTITY: 'Corporate identity',
+  CONTENT: 'Content',
   FORMS: 'From Controls',
-  LAYOUT: 'Layout'
+  CORPORATE_IDENTITY: 'Corporate identity'
 }
 
 export default [
+  {
+    category: categories.CONTENT,
+    components: [
+      {
+        name: 'Table',
+        raw: TableRaw,
+        example: {
+          component: TableExample,
+          raw: TableExampleRaw
+        }
+      },
+      {
+        name: 'LoadMask',
+        raw: LoadMaskRaw,
+        example: {
+          component: LoadMaskExample,
+          raw: LoadMaskExampleRaw
+        }
+      }]
+  },
   {
     category: categories.FORMS,
     components: [
@@ -38,27 +62,6 @@ export default [
         example: {
           component: ToccoLogoExample,
           raw: ToccoLogoExampleRaw
-        }
-      },
-      {
-        name: 'ToccoLogo1',
-        raw: ToccoLogoRaw,
-        example: {
-          component: ToccoLogoExample,
-          raw: ToccoLogoExampleRaw
-        }
-      }
-    ]
-  },
-  {
-    category: categories.LAYOUT,
-    components: [
-      {
-        name: 'LoadMask',
-        raw: LoadMaskRaw,
-        example: {
-          component: LoadMaskExample,
-          raw: LoadMaskExampleRaw
         }
       }
     ]
---
+++ packages/tocco-ui/src/Table/Table.spec.js
@@ -0,0 +1,149 @@
+import React from 'react'
+import Table from './Table'
+import {shallow} from 'enzyme'
+
+describe('tocco-ui', function() {
+  describe('Table', function() {
+    it('should render a html table', () => {
+      const wrapper = shallow(
+        <Table records={[]} columnDefinitions={[]}/>
+      )
+      expect(wrapper.find('table')).to.have.length(1)
+    })
+
+    it('should render the right amount of records', () => {
+      const recordsAmount = 100
+      const records = []
+      for (let i = 0; i < recordsAmount; i++) {
+        records.push({a: {value: i.toString()}})
+      }
+
+      const wrapper = shallow(
+        <Table records={records} columnDefinitions={[{value: 'a'}]}/>
+      )
+      expect(wrapper.find('tr')).to.have.length(recordsAmount + 1) // +1 for header row
+    })
+
+    it('should show a label if provided and default value if not', () => {
+      const records = [
+        {
+          a: {value: 'a1'},
+          b: {value: 'b1'},
+          c: {value: 'c1'}
+        }
+      ]
+
+      const columnDefinitions = [
+        {
+          value: 'a'
+        },
+        {
+          value: 'c',
+          label: 'CLabel',
+          order: 1
+        }
+      ]
+
+      const wrapper = shallow(
+        <Table records={records} columnDefinitions={columnDefinitions}/>
+      )
+
+      expect(wrapper.contains(<th>a</th>)).to.be.true
+      expect(wrapper.contains(<th>CLabel</th>)).to.be.true
+    })
+
+    it('should render the right amount of columns and order them ', () => {
+      const records = [
+        {
+          a: {value: 'a1'},
+          b: {value: 'b1'},
+          c: {value: 'c1'}
+        },
+        {
+          a: {value: 'a2'},
+          b: {value: 'b2'},
+          c: {value: 'c2'}
+        },
+        {
+          a: {value: 'a3'},
+          b: {value: 'b3'},
+          c: {value: 'c3'}
+        }
+      ]
+
+      const columnDefinitions = [
+        {
+          value: 'a',
+          label: 'ALabel',
+          order: 30
+        },
+        {
+          value: 'c',
+          label: 'CLabel',
+          order: 1
+        }
+      ]
+
+      const wrapper = shallow(
+        <Table records={records} columnDefinitions={columnDefinitions}/>
+      )
+
+      expect(wrapper.contains(
+        <thead>
+          <tr>
+            <th>CLabel</th>
+            <th>ALabel</th>
+          </tr>
+        </thead>
+      )).to.be.true
+
+      expect(wrapper.contains(<th>BLabel</th>)).to.be.false
+
+      expect(wrapper.contains('a1')).to.be.true
+      expect(wrapper.contains('a2')).to.be.true
+      expect(wrapper.contains('a3')).to.be.true
+
+      expect(wrapper.contains('b1')).to.be.false
+      expect(wrapper.contains('b2')).to.be.false
+      expect(wrapper.contains('b3')).to.be.false
+
+      expect(wrapper.contains('c1')).to.be.true
+      expect(wrapper.contains('c2')).to.be.true
+      expect(wrapper.contains('c3')).to.be.true
+    })
+
+    it('should add class attribute to table', () => {
+      const wrapper = shallow(
+        <Table className="table-striped abc" records={[]} columnDefinitions={[]}/>
+      )
+      expect(wrapper.find('table.table-striped.abc')).to.have.length(1)
+    })
+
+    it('should add a row click handler', () => {
+      const record = {a: {value: 'a'}}
+
+      const onClick = sinon.spy()
+      const wrapper = shallow(
+        <Table onRowClick={onClick} records={[record]} columnDefinitions={[{value: 'a'}]}/>
+      )
+
+      wrapper.find('tr').last().simulate('click')
+
+      expect(onClick).to.have.calledOnce
+      expect(onClick).to.have.been.calledWith(record)
+    })
+
+    it('should use the cell renderer', () => {
+      const record = {a: {value: 'a'}, b: {value: 'b'}}
+
+      const cellRenderer = sinon.spy()
+      shallow(
+        <Table cellRenderer={cellRenderer} records={[record]} columnDefinitions={[{value: 'a'}, {value: 'b'}]}/>
+      )
+
+      expect(cellRenderer).to.have.calledTwice
+      expect(cellRenderer).to.have.been.calledWith(record.a, record)
+      expect(cellRenderer).to.have.been.calledWith(record.b, record)
+    })
+  })
+})
