406616d5844a57dd2ddd037e21a03048686de4e8
Daniel Keller <dkeller@tocco.ch>
2016-12-21 14:51:25 +0100

    test(entity-browser): add searchform components test

---
+++ packages/entity-browser/src/components/SearchForm/SearchForm.spec.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import SearchField from './SearchField'
+import SearchForm from './'
+import {mount} from 'enzyme'
+
+const EMPTY_FUNC = () => {}
+
+describe('entity-browser', () => {
+  describe('components', () => {
+    describe('SearchForm', () => {
+      it('should render needed components', () => {
+        const entityModel = require('../../dev/test-data/userModel.json')
+        const searchFormDefinition = require('../../dev/test-data/searchFormDefinition.json')
+
+        const wrapper = mount(<SearchForm
+          entityModel={entityModel}
+          searchFormDefinition={searchFormDefinition}
+          setSearchInput={EMPTY_FUNC}
+          relationEntities={{}}
+          searchInputs={{}}
+          reset={EMPTY_FUNC}
+        />)
+
+        expect(wrapper.find(SearchField)).to.have.length(searchFormDefinition.length)
+      })
+    })
+  })
+})
