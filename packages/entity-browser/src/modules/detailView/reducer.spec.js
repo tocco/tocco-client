9b77632f84ef6c1be86a495c41896b31a29b7966
Daniel Keller <dkeller@tocco.ch>
2017-01-17 16:32:57 +0100

    test(entity-browser): add detail view reducer tests

---
+++ packages/entity-browser/src/modules/detailView/reducer.spec.js
@@ -0,0 +1,32 @@
+import reducer from './index'
+import * as actions from './actions'
+
+const EXPECTED_INITIAL_STATE = {
+  entityName: '',
+  formDefinition: [],
+  entity: {}
+}
+
+describe('entity-browser', () => {
+  describe('modules', () => {
+    describe('listView', () => {
+      describe('reducer', () => {
+        it('should create a valid initial state', () => {
+          expect(reducer(undefined, {})).to.deep.equal(EXPECTED_INITIAL_STATE)
+        })
+
+        it('should handle an action', () => {
+          const stateBefore = {
+            entity: ''
+          }
+
+          const expectedStateAfter = {
+            entity: 'User'
+          }
+
+          expect(reducer(stateBefore, actions.setEntity('User'))).to.deep.equal(expectedStateAfter)
+        })
+      })
+    })
+  })
+})
