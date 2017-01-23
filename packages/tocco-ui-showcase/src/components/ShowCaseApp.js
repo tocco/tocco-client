29d04e8df1fd496c58b643c265521424960120a7
Daniel Keller <dkeller@tocco.ch>
2017-01-23 10:01:14 +0100

    feat(tocco-ui-showcase): add global locale switcher

--- packages/tocco-ui-showcase/src/components/ShowCaseApp.js
+++ packages/tocco-ui-showcase/src/components/ShowCaseApp.js
@@ -1,29 +1,53 @@
 import React from 'react'
+import {addLocaleData, IntlProvider} from 'react-intl'
 
 import Navigation from './Navigation'
+import LocaleSwitcher from './LocaleSwitcher'
 import ShowCaseList from './ShowCaseList'
 import Affix from './Affix'
 
+import de from 'react-intl/locale-data/de'
+import en from 'react-intl/locale-data/en'
+import fr from 'react-intl/locale-data/fr'
+import it from 'react-intl/locale-data/it'
+
 export default class ShowCaseApp extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      locale: 'en'
+    }
+
+    addLocaleData([...de, ...en, ...fr, ...it])
+  }
+
   static propTypes = {
     componentsTree: React.PropTypes.array.isRequired
   }
 
+  localeChange(locale) {
+    console.log('Change locale to: ', locale)
+    this.setState({locale})
+  }
+
   render() {
     return (
-      <div className="show-case-app">
-        <div className="col title">
-          <span>Tocco UI</span>
+      <IntlProvider locale={this.state.locale}>
+        <div className="show-case-app">
+          <div className="col title">
+            <span>Tocco UI</span>
+          </div>
+          <div className="col-md-9">
+            <ShowCaseList componentsTree={this.props.componentsTree}/>
+          </div>
+          <div className="col-md-2 hidden-xs hidden-sm">
+            <Affix className="navi-affix" offset={80}>
+              <LocaleSwitcher onLocaleChange={this.localeChange.bind(this)}/>
+              <Navigation componentsTree={this.props.componentsTree}/>
+            </Affix>
+          </div>
         </div>
-        <div className="col-md-9">
-          <ShowCaseList componentsTree={this.props.componentsTree}/>
-        </div>
-        <div className="col-md-2 hidden-xs hidden-sm">
-          <Affix className="navi-affix" offset={80}>
-            <Navigation componentsTree={this.props.componentsTree}/>
-          </Affix>
-        </div>
-      </div>
+      </IntlProvider>
     )
   }
 }
