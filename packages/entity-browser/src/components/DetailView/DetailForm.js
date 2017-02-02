d67d92a84c8b5ddf25ab88a68e1716927996652f
Daniel Keller <dkeller@tocco.ch>
2017-02-02 16:51:31 +0100

    refactor(entity-browser): remove unused import

--- packages/entity-browser/src/components/DetailView/DetailForm.js
+++ packages/entity-browser/src/components/DetailView/DetailForm.js
@@ -2,7 +2,6 @@ import React from 'react'
 import LabeledField from './LabeledField'
 import {Field, reduxForm} from 'redux-form'
 import {Button, LayoutBox} from 'tocco-ui'
-import {asyncValidate} from '../../util/reduxForms'
 
 export const DetailForm = props => {
   if (!props.entity.paths) {
