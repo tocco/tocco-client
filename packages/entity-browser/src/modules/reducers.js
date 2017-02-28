b63d298d74a1035b28bed546c990a35be74ce506
Roland Züger <rzueger@tocco.ch>
2017-02-28 13:26:04 +0100

    refactor(entity-browser): Inject module `searchForm` from route

--- packages/entity-browser/src/modules/reducers.js
+++ packages/entity-browser/src/modules/reducers.js
@@ -3,22 +3,18 @@ import {reducer as toastr} from 'react-redux-toastr'
 
 import entityBrowser from '../routes/entity-browser/modules'
 import detail from '../routes/detail/modules'
-import searchForm from '../routes/list/modules/searchForm'
 
 import entityBrowserSagas from '../routes/entity-browser/modules/sagas'
 import detailViewSagas from '../routes/detail/modules/sagas'
-import searchFormSagas from '../routes/list/modules/searchForm/sagas'
 
 export default {
   toastr,
   form,
   entityBrowser,
-  detail,
-  searchForm
+  detail
 }
 
 export const sagas = [
   entityBrowserSagas,
-  detailViewSagas,
-  searchFormSagas
+  detailViewSagas
 ]
--- packages/entity-browser/src/routes/list/index.js
+++ packages/entity-browser/src/routes/list/index.js
@@ -3,14 +3,20 @@ import {storeFactory} from 'tocco-util'
 
 export default store => props => {
   const ListViewContainer = require('./containers/ListViewContainer').default
+
   const reducer = require('./modules').default
+  const searchFormReducer = require('./modules/searchForm').default
+
   const sagas = require('./modules/sagas').default
+  const searchFormSagas = require('./modules/searchForm/sagas').default
 
   storeFactory.hotReloadReducers(store, {
-    list: reducer
+    list: reducer,
+    searchForm: searchFormReducer
   })
 
   storeFactory.injectSaga(sagas)
+  storeFactory.injectSaga(searchFormSagas)
 
   return <ListViewContainer router={props}/>
 }
