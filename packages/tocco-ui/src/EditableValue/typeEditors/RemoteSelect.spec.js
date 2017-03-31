2aae8132a06c6a92700abfba7302696a9f99fc9f
Daniel Keller <dkeller@tocco.ch>
2017-03-31 09:22:39 +0200

    test(tocco-ui): add remote editablevalue tests
    Signed-off-by: Daniel Keller <dkeller@tocco.ch>

---
+++ packages/tocco-ui/src/EditableValue/typeEditors/RemoteSelect.spec.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import {shallow} from 'enzyme'
+import Select from 'react-select'
+
+import RemoteSelect from './RemoteSelect'
+
+describe('tocco-ui', () => {
+  describe('EditableValue', () => {
+    describe('typeEditors', () => {
+      describe('RemoteSelect ', () => {
+        it('should render a Async Select component', () => {
+          const wrapper = shallow(<RemoteSelect options={{}} value={{key: 2, display: 'Two'}} onChange={() => {}}/>)
+          expect(wrapper.find(Select.Async)).to.have.length(1)
+        })
+
+        it('should call onChange ', () => {
+          const newValue = {key: 1, display: 'label1'}
+          const spy = sinon.spy()
+          const wrapper = shallow(<RemoteSelect onChange={spy} options={{}}/>)
+          wrapper.find(Select.Async).simulate('change', newValue)
+          expect(spy).to.have.been.calledWith(newValue)
+        })
+      })
+    })
+  })
+})
