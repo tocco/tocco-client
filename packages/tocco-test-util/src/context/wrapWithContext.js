1c4cc4abccb3ca6931a6e92c19773488bd13579b
Roland Züger <rzueger@tocco.ch>
2017-04-12 10:52:56 +0200

    test(tocco-test-util): Refactor wrapWithContext: no React.createClass
    `React.createClass()` is deprecated since v15.5.0:
    https://github.com/facebook/react/releases/tag/v15.5.0

--- packages/tocco-test-util/src/context/wrapWithContext.js
+++ packages/tocco-test-util/src/context/wrapWithContext.js
@@ -1,16 +1,13 @@
 import React from 'react'
 
 const wrapWithContext = (context, contextTypes, children) => {
-  const wrapperWithContext = React.createClass({
-    childContextTypes: contextTypes,
-    getChildContext() {
-      return context
-    },
-    render() {
-      return children
-    }
-  })
-  return React.createElement(wrapperWithContext)
+  class WrapperWithContext extends React.Component {
+    getChildContext = () => context
+    render = () => children
+  }
+  WrapperWithContext.childContextTypes = contextTypes
+
+  return React.createElement(WrapperWithContext)
 }
 
 export default wrapWithContext
