3e51222ab8ab23e0f545ceb83a2c3447a14c0842
Daniel Keller <dkeller@tocco.ch>
2017-04-21 09:12:24 +0200

    chore(entity-browser): add hot reloading

--- packages/entity-browser/src/routes/detail/index.js
+++ packages/entity-browser/src/routes/detail/index.js
@@ -1,5 +1,12 @@
-import {route} from 'tocco-util'
+import {route, storeFactory} from 'tocco-util'
 
-export default (store, input) => (
-  route.loadRoute(store, input, () => (import('./route')))
-)
+export default (store, input) => {
+  if (module.hot) {
+    module.hot.accept('./route', () => {
+      const reducers = require('./route').default.reducers
+      storeFactory.hotReloadReducers(store, reducers)
+    })
+  }
+
+  return route.loadRoute(store, input, () => (import('./route')))
+}
