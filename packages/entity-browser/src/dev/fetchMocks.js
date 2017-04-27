4f9096a9dd1495315337dd00e124aa85beb38491
Daniel Keller <dkeller@tocco.ch>
2017-04-27 12:18:17 +0200

    refactor(entity-browser): mock data enhancements
    - delete unused mock data
    - implement dummy entity factory

--- packages/entity-browser/src/dev/fetchMocks.js
+++ packages/entity-browser/src/dev/fetchMocks.js
@@ -23,18 +23,21 @@ export default function setupFetchMock(fetchMock) {
   fetchMock.get(new RegExp('^.*?/nice2/rest/forms/User_detail'), require('./rest-responses/form_user_detail.json'))
   fetchMock.get(new RegExp('^.*?/nice2/rest/forms/User_list'), require('./rest-responses/form_user_list.json'))
   fetchMock.get(new RegExp('^.*?/nice2/rest/forms/UserSearch_list'), require('./rest-responses/form_user_list.json'))
-  fetchMock.get(new RegExp('^.*?/nice2/rest/entities/Affiliation.*'), require('./rest-responses/affiliation.json'))
   fetchMock.get(new RegExp('^.*?/nice2/rest/entities/User_code1.*'), require('./rest-responses/user_code1.json'))
-  fetchMock.get(new RegExp('^.*?/nice2/rest/entities/Dummy_entity.*'), require('./rest-responses/dummy_entity.json'))
-  fetchMock.get(new RegExp('^.*?/nice2/rest/entities/Gender.*'), require('./rest-responses/gender.json'))
   fetchMock.get(new RegExp('^.*?/nice2/rest/entities/User/model.*'), require('./rest-responses/model_user.json'))
-  fetchMock.get(new RegExp('^.*?/nice2/rest/entities/User/count?.*'), createCountResponse)
+
+  fetchMock.get(new RegExp('^.*?/nice2/rest/entities/User/count?.*'), createCountResponse('user'))
   fetchMock.get(new RegExp('^.*?/nice2/rest/entities/User/[0-9]?.*'), createEntityResponse)
-  fetchMock.get(new RegExp('^.*?/nice2/rest/entities/User?.*'), createEntitiesResponse)
+  fetchMock.get(new RegExp('^.*?/nice2/rest/entities/User?.*'), createEntitiesResponse('user'))
 
   fetchMock.post(new RegExp('^.*?/nice2/rest/entities/User/[0-9]'), createEntityUpdateResponse)
   fetchMock.patch(new RegExp('^.*?/nice2/rest/entities/User/[0-9]\\?_validate=true'), createValidateResponse)
   fetchMock.patch(new RegExp('^.*?/nice2/rest/entities/User/[0-9]'), createEntityUpdateResponse)
+
+  fetchMock.get(new RegExp('^.*?/nice2/rest/entities/Dummy_entity/model.*'),
+    require('./rest-responses/model_user.json'))
+  fetchMock.get(new RegExp('^.*?/nice2/rest/entities/Dummy_entity/count?.*'), createCountResponse('dummy'))
+  fetchMock.get(new RegExp('^.*?/nice2/rest/entities/Dummy_entity?.*'), createEntitiesResponse('dummy'))
 
   fetchMock.spy()
 }
