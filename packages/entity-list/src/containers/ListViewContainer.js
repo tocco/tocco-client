a645b847acebf3e74c993e449dc017683ea0e60d
Roland Züger <rzueger@tocco.ch>
2017-04-27 09:43:37 +0200

    feat(entity-list): Introduce external event `onRowClick`

--- packages/entity-list/src/containers/ListViewContainer.js
+++ packages/entity-list/src/containers/ListViewContainer.js
@@ -2,12 +2,14 @@ import {connect} from 'react-redux'
 import {injectIntl} from 'react-intl'
 import ListView from '../components/ListView'
 import {initialize, changePage, setOrderBy, refresh} from '../modules/list/actions'
+import {externalEvents} from 'tocco-util'
 
 const mapActionCreators = {
   initialize,
   changePage,
   setOrderBy,
-  refresh
+  refresh,
+  onRowClick: id => externalEvents.fireExternalEvent('onRowClick', { id })
 }
 
 const mapStateToProps = (state, props) => {
