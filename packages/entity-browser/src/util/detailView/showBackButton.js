b0009c8b3a28cebfbf31db517ed76b1e24ba7cc9
Robin Ott <rott@tocco.ch>
2017-05-08 16:11:03 +0200

    feat(entity-browser): add new util function showBackButton

---
+++ packages/entity-browser/src/util/detailView/showBackButton.js
@@ -0,0 +1,8 @@
+const showBackButton = (initialKey, modelPaths) => {
+  if (modelPaths.length === 0) {
+    return (initialKey === '' || isNaN(initialKey))
+  }
+  return true
+}
+
+export default showBackButton
---
+++ packages/entity-browser/src/util/detailView/showBackButton.spec.js
@@ -0,0 +1,31 @@
+import showBackButton from './showBackButton'
+
+describe('entity-browser', () => {
+  describe('util', () => {
+    describe('detailView', () => {
+      describe('showBackButton', () => {
+        it('should return `true` if modelPaths is not empty', () => {
+          const initialKey = '12'
+          const modelPaths = ['some input']
+          const result = showBackButton(initialKey, modelPaths)
+          expect(result).to.be.true
+        })
+
+        it('should return `true` if initialKey is not a number', () => {
+          const initialKey = 'not a number'
+          const modelPaths = []
+          const result = showBackButton(initialKey, modelPaths)
+          expect(result).to.be.true
+        })
+
+        it('should return `false` if modelPaths is empty and initialKey is a number', () => {
+          const initialKey = '123'
+          const modelPaths = []
+          const result = showBackButton(initialKey, modelPaths)
+          expect(result).to.be.false
+          expect(result).to.be.false
+        })
+      })
+    })
+  })
+})
