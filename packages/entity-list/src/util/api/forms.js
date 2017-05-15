e2a2a5c9f7868e3d19d1131d3e0bb8f828f199f4
Daniel Keller <dkeller@tocco.ch>
2017-05-15 11:20:28 +0200

    refactor(entity-list): remove unused code vom entity-browser

--- packages/entity-list/src/util/api/forms.js
+++ packages/entity-list/src/util/api/forms.js
@@ -1,39 +1,9 @@
-import _startsWith from 'lodash/startsWith'
 import {request} from 'tocco-util/src/rest'
 
-const NO_FIELD_TYPES = [
+const IGNORED_FIELD_TYPES = [
   'ch.tocco.nice2.model.form.components.simple.DescriptionField',
   'ch.tocco.nice2.model.form.components.simple.DisplayExpressionField'
 ]
-
-export const getFieldsOfDetailForm = formDefinition => {
-  return getFieldsOfChildren(formDefinition.children)
-}
-
-const getFieldsOfChildren = children => {
-  const result = []
-
-  const subIterationTypes = [
-    'ch.tocco.nice2.model.form.components.navigation.IteratorComponent',
-    'ch.tocco.nice2.model.form.components.table.Table'
-  ]
-
-  for (let i = 0; i < children.length; i++) {
-    if (children[i].children) {
-      const childType = children[i].type
-      if (!subIterationTypes.includes(childType)) {
-        result.push(...getFieldsOfChildren(children[i].children))
-      }
-    }
-
-    const fieldType = children[i].type
-    if (_startsWith(fieldType, 'ch.tocco.nice2.model.form.components.simple') && !NO_FIELD_TYPES.includes(fieldType)) {
-      result.push(children[i].name)
-    }
-  }
-
-  return result
-}
 
 const defaultFormTransformer = json => (json.form)
 
@@ -80,7 +50,7 @@ export const getFieldsOfColumnDefinition = columnDefinition => {
   let fields = []
 
   columnDefinition
-    .filter(column => !column.values.some(field => NO_FIELD_TYPES.includes(field.type)))
+    .filter(column => !column.values.some(field => IGNORED_FIELD_TYPES.includes(field.type)))
     .forEach(column => {
       fields = fields.concat(column.values.map(field => field.name))
     })
