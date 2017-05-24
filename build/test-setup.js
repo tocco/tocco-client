174ee7ffb2fe5db19467520f98a044552d574b10
Urs Wolfer <uwolfer@tocco.ch>
2017-05-24 08:47:13 +0200

    chore: intercept prop-type log-errors in tests in order to fail them

--- build/test-setup.js
+++ build/test-setup.js
@@ -10,3 +10,11 @@ global.chai = chai
 global.sinon = sinon
 global.expect = chai.expect
 global.should = chai.should()
+
+const error = console.error // eslint-disable-line no-console
+console.error = function(warning, ...args) { // eslint-disable-line no-console
+  if (/(Invalid prop|Failed prop type)/.test(warning)) {
+    throw new Error(warning)
+  }
+  error.apply(console, [warning, ...args])
+}
