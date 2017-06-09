595e989cf036124af21f6fcb01cf340418394e48
Daniel Keller <dkeller@tocco.ch>
2017-06-09 15:42:10 +0200

    refactor(tocco-util): improve externalEvents
    - simpler api (index.js exports)
    - no more singleton container for events
    - usage with actions only

--- packages/tocco-util/src/externalEvents/externalEvents.spec.js
+++ packages/tocco-util/src/externalEvents/externalEvents.spec.js
@@ -1,67 +1,25 @@
 import * as externalEvents from './externalEvents'
-import {call} from 'redux-saga/effects'
 
 describe('tocco-util', () => {
   describe('externalEvents', () => {
-    describe('fireExternalEvent', () => {
-      it('should create a FIRE_EXTERNAL_EVENT action', () => {
-        const eventName = 'success'
-        const payload = {
-          prop1: 'value1',
-          prop2: 'value2'
-        }
-
-        const action = externalEvents.fireExternalEvent(eventName, payload)
-
-        expect(action).to.deep.equal({
-          type: externalEvents.FIRE_EXTERNAL_EVENT,
-          payload: {
-            name: eventName,
-            payload
-          }
-        })
-      })
-    })
-
-    describe('registerEvents', () => {
-      it('should register event handlers', () => {
-        externalEvents.registerEvents({success: () => {}})
-        expect(externalEvents.getEvents()).to.deep.equal(['success'])
-      })
-    })
-
     describe('invokeExternalEvent', () => {
       it('should invoke event handler', () => {
         const handler = sinon.spy()
-        externalEvents.registerEvents({success: handler})
-        externalEvents.invokeExternalEvent('success')
+        const events = {success: handler}
+        externalEvents.invokeExternalEvent(events, 'success')
         expect(handler).to.have.property('callCount', 1)
       })
 
       it('should pass arguments to event handler', () => {
         const handler = sinon.spy()
-        externalEvents.registerEvents({success: handler})
-        externalEvents.invokeExternalEvent('success', 'arg1', 'arg2', 'arg3')
+        const events = {success: handler}
+        externalEvents.invokeExternalEvent(events, 'success', 'arg1', 'arg2', 'arg3')
         expect(handler).to.have.been.calledWith('arg1', 'arg2', 'arg3')
       })
 
       it('should ignore unknown events', done => {
         externalEvents.invokeExternalEvent('unknown')
         done()
-      })
-    })
-
-    describe('fireExternalEventSaga', () => {
-      it('should call invokeExternalEvent', () => {
-        const eventName = 'success'
-        const payload = {
-          prop1: 'value1',
-          prop2: 'value2'
-        }
-        const action = externalEvents.fireExternalEvent(eventName, payload)
-        const generator = externalEvents.fireExternalEventSaga(action)
-        expect(generator.next().value).to.deep.equal(call(externalEvents.invokeExternalEvent, eventName, payload))
-        expect(generator.next().done).to.equal(true)
       })
     })
   })
---
+++ packages/tocco-util/src/externalEvents/sagas.js
@@ -0,0 +1,14 @@
+import {fork, takeEvery, call, all} from 'redux-saga/effects'
+import * as actions from './actions'
+import {invokeExternalEvent} from './externalEvents'
+
+export default function* sagas(events) {
+  yield all([
+    fork(takeEvery, actions.FIRE_EXTERNAL_EVENT, fireExternalEvent, events)
+  ])
+}
+
+export function* fireExternalEvent(events, action) {
+  const {name, payload} = action.payload
+  yield call(invokeExternalEvent, events, name, payload)
+}
