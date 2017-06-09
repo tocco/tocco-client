4ce4d090f4cccfa590eb47837890027f04cca4d6
Daniel Keller <dkeller@tocco.ch>
2017-06-09 15:01:32 +0200

    refactor(tocco-util): remove dependencies from storeFactory
    - externalEvent and errorLogging are not added to store automatically
      they each get new methods to decorate the store

--- packages/tocco-util/src/storeFactory/sagaHelpers.js
+++ packages/tocco-util/src/storeFactory/sagaHelpers.js
@@ -1,12 +1,13 @@
 import {call, put, all} from 'redux-saga/effects'
+import errorLogging from '../errorLogging'
 
-export const autoRestartSaga = (generator, logError) => {
+export const autoRestartSaga = generator => {
   return function* autoRestarting(...args) {
     while (true) {
       try {
         yield call(generator, ...args)
       } catch (error) {
-        yield put(logError('client.common.unexpectedError', 'client.common.unexpectedError', error))
+        yield put(errorLogging.logError('client.common.unexpectedError', 'client.common.unexpectedError', error))
       }
     }
   }
