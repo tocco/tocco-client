bd0133296c504ec9a7a1eb80ee59f582aebd6c02
Daniel Keller <dkeller@tocco.ch>
2017-10-06 13:07:16 +0200

    feat(resource-scheduler): create packages

---
+++ packages/scheduler/src/main.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import {appFactory} from 'tocco-util'
+
+import reducers, {sagas} from './modules/reducers'
+
+const packageName = 'scheduler'
+
+const initApp = (id, input, events, publicPath) => {
+  const content = <div>Scheduler</div>
+
+  const store = appFactory.createStore(reducers, sagas, undefined)
+
+  return appFactory.createApp(
+    packageName,
+    content,
+    store,
+    input,
+    events,
+    [],
+    publicPath
+  )
+}
+
+(() => {
+  if (__DEV__) {
+    require('tocco-theme/src/ToccoTheme/theme.scss')
+    const input = require('./dev/input.json')
+
+    const fetchMock = require('fetch-mock')
+    const setupFetchMocks = require('./dev/fetchMocks')
+    setupFetchMocks(fetchMock)
+
+    const app = initApp('id', input)
+
+    if (module.hot) {
+      module.hot.accept('./modules/reducers', () => {
+        const reducers = require('./modules/reducers').default
+        appFactory.hotReloadReducers(app.store, reducers)
+      })
+    }
+
+    appFactory.renderApp(app.renderComponent())
+  } else {
+    appFactory.registerAppInRegistry(packageName, initApp)
+  }
+})()
