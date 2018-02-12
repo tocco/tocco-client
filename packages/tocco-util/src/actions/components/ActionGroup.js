4a1d5cfa4efb5e64878dceaf1edf7db15c7597ea
Daniel Keller <dkeller@tocco.ch>
2018-02-12 16:01:46 +0100

    feat(tocco-util): action selection range
    - consider minSelection and maxSelection attribute on action definition
      and disable action if selection count is not in range

--- packages/tocco-util/src/actions/components/ActionGroup.js
+++ packages/tocco-util/src/actions/components/ActionGroup.js
@@ -2,22 +2,43 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import {DropdownButton, Dropdown, Button as ButtonBS} from 'react-bootstrap'
 import GroupElement from './GroupElement'
+import {isValidSelection, selectionText} from './selectionHelper'
+import {intlShape} from 'react-intl'
 
-const ActionGroup = ({definition, onClick}) => {
+const MainAction = ({definition, selectedCount, onClick}, context) => {
+  const validSelection = isValidSelection(selectedCount, definition)
+  const title = selectionText(selectedCount, definition, context.intl)
+
+  const disabled = definition.readonly === true || !validSelection
+
+  return <ButtonBS onClick={() => { onClick(definition) }} disabled={disabled}>
+    <span title={title}>
+      {definition.icon && <i className={'fa ' + definition.icon}/>} {definition.label}
+    </span>
+  </ButtonBS>
+}
+
+MainAction.contextTypes = {
+  intl: intlShape
+}
+
+MainAction.propTypes = {
+  definition: PropTypes.object.isRequired,
+  onClick: PropTypes.func.isRequired,
+  selectedCount: PropTypes.number
+}
+
+const ActionGroup = ({definition, onClick, selectedCount}, context) => {
   const hasMainAction = (definition.action && Object.keys(definition.action).length > 0)
 
   if (hasMainAction) {
-    const mainActionDefinition = definition.action
-
     return (
-      <Dropdown id={'action-' + definition.name} disabled={definition.readOnly === true}>
-        <ButtonBS onClick={() => { onClick(mainActionDefinition) }}>
-          {mainActionDefinition.icon && <i className={'fa ' + mainActionDefinition.icon}/>} {mainActionDefinition.label}
-        </ButtonBS>
+      <Dropdown id={'action-' + definition.name} disabled={definition.readonly}>
+        <MainAction definition={definition.action} selectedCount={selectedCount} onClick={onClick}/>
         <Dropdown.Toggle/>
         <Dropdown.Menu>
           {definition.children.map((actionDefinition, idx) =>
-            <GroupElement key={idx} definition={actionDefinition} onClick={onClick}/>
+            <GroupElement key={idx} definition={actionDefinition} onClick={onClick} selectedCount={selectedCount}/>
           )}
         </Dropdown.Menu>
       </Dropdown>
@@ -27,19 +48,24 @@ const ActionGroup = ({definition, onClick}) => {
       <DropdownButton
         id={'action-' + definition.name}
         title={definition.label}
-        disabled={definition.readOnly === true}
+        disabled={definition.readonly === true}
       >
         {definition.children.map((actionDefinition, idx) =>
-          <GroupElement key={idx} definition={actionDefinition} onClick={onClick}/>
+          <GroupElement key={idx} definition={actionDefinition} onClick={onClick} selectedCount={selectedCount}/>
         )}
       </DropdownButton>
     )
   }
 }
 
+ActionGroup.contextTypes = {
+  intl: intlShape
+}
+
 ActionGroup.propTypes = {
   definition: PropTypes.object.isRequired,
-  onClick: PropTypes.func.isRequired
+  onClick: PropTypes.func.isRequired,
+  selectedCount: PropTypes.number
 }
 
 export default ActionGroup
--- packages/tocco-util/src/actions/components/GroupElement.js
+++ packages/tocco-util/src/actions/components/GroupElement.js
@@ -2,28 +2,42 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import {MenuItem} from 'react-bootstrap'
 import actionTypes from '../actionTypes'
+import {isValidSelection, selectionText} from './selectionHelper'
+import {intlShape} from 'react-intl'
 
-const GroupElement = ({definition, onClick, onSelect}) => {
+const GroupElement = ({definition, onClick, onSelect, selectedCount}, context) => {
   if (definition.actionType === actionTypes.DIVIDER) {
     return <MenuItem divider/>
   }
 
+  const validSelection = isValidSelection(selectedCount, definition)
+  const title = selectionText(selectedCount, definition, context.intl)
+
+  const disabled = definition.readonly === true || !validSelection
+
   return (
-    <MenuItem disabled={definition.readonly === true} onClick={() => {
-      if (!definition.readonly) {
+    <MenuItem disabled={disabled} onClick={() => {
+      if (!disabled) {
         setTimeout(() => onSelect(), 100)
         onClick(definition)
       }
     }}>
-      {definition.icon && <i className={'fa ' + definition.icon}/>} {definition.label}
+      <span title={title}>
+        {definition.icon && <i className={'fa ' + definition.icon}/>} {definition.label}
+      </span>
     </MenuItem>
   )
+}
+
+GroupElement.contextTypes = {
+  intl: intlShape
 }
 
 GroupElement.propTypes = {
   definition: PropTypes.object.isRequired,
   onClick: PropTypes.func.isRequired,
-  onSelect: PropTypes.func
+  onSelect: PropTypes.func,
+  selectedCount: PropTypes.number
 }
 
 export default GroupElement
