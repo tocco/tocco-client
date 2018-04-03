b813dd0317b88678288b36eba94766f13ec53be6
Niklaus Hug <nhug@tocco.ch>
2018-04-03 13:40:26 +0200

    feat(tocco-ui): establish ButtonLink component
    - ButtonLink inherit ButtonStyles of Button component

---
+++ packages/tocco-ui/src/ButtonLink/ButtonLink.js
@@ -0,0 +1,78 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+import Icon from '../Icon'
+import {ButtonStyles} from '../Button'
+
+const ButtonLinkStyles = ButtonStyles.withComponent('a').extend`
+  && {
+    :hover,
+    :focus {
+      text-decoration: none;
+    }
+  }
+`
+
+const ButtonLink = props => {
+  return (
+    <ButtonLinkStyles
+      alt={props.alt}
+      dense={props.dense}
+      download={props.download}
+      href={props.href}
+      ink={props.ink}
+      look={props.look}
+      title={props.title}
+    >
+      {props.icon && <Icon dense={props.dense} icon={props.icon} position="before"/>}
+      {props.label}
+    </ButtonLinkStyles>
+  )
+}
+
+ButtonLink.defaultProps = {
+  look: 'flat',
+  href: '#',
+  ink: 'base'
+}
+
+ButtonLink.propTypes = {
+  /**
+   * Define text for screenreaders and crawlers.
+   */
+  alt: PropTypes.string,
+  /**
+   * If true, compress button to occupy less space.
+   */
+  dense: PropTypes.bool,
+  /**
+   * Indicate download by attribute. Add string to suggest filename.
+   */
+  download: PropTypes.string,
+  /**
+   * Set an Url (absolute/relative) or a contact with prefix (mailto, tel). Default is '#'
+   */
+  href: PropTypes.string,
+  /**
+   * Add an icon to the link. Set the specific class only from
+   * https://getbootstrap.com/docs/3.3/components/#glyphicons or https://fontawesome.com/v4.7.0/icons/
+   */
+  icon: PropTypes.string,
+  /**
+   * Define color palette. Default value is 'base'. Possible values: base|primary
+   */
+  ink: PropTypes.oneOf(['base', 'primary']),
+  /**
+   * Visible text. Default is an empty string.
+   */
+  label: PropTypes.node,
+  /**
+   * Button Link style. Default value is 'plain'. Possible values: plain|flat|raised
+   */
+  look: PropTypes.oneOf(['flat', 'raised']),
+  /**
+   * Popover title to be shown on mouse over.
+   */
+  title: PropTypes.string
+}
+
+export default ButtonLink
