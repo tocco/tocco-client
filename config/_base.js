67067e9c846f7a6ec8c9fb86efaf472eab2855e0
Daniel Keller <dkeller@tocco.ch>
2018-04-04 09:03:59 +0200

    chore: remove 2.11 legacy from build process

--- config/_base.js
+++ config/_base.js
@@ -70,7 +70,6 @@ config.globals = {
   '__DEV__'      : config.env === 'development',
   '__PROD__'     : config.env === 'production',
   '__STANDALONE__': config.env === 'standalone',
-  '__NICE2_11_LEGACY__': config.env === 'nice2_11_legacy',
   '__TEST__'     : config.env === 'test',
   '__DEBUG__'    : config.env === 'development' && !argv.no_debug,
   '__BACKEND_URL__': JSON.stringify(''),
