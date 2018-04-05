cf9fb8416e7c0d6523488acef4e3de05606e4e3c
Niklaus Hug <nhug@tocco.ch>
2018-04-05 14:28:24 +0200

    refactor(tocco-ui): Upload as styled component
    - refactor as styled component
    - remove spare html elements
    - utilize Icon, Button and ButtonLink component
    - sort props alphabetically
    - revise tests

--- packages/tocco-ui/src/Upload/Upload.spec.js
+++ packages/tocco-ui/src/Upload/Upload.spec.js
@@ -12,17 +12,12 @@ const sampleValue = {
 
 describe('tocco-ui', () => {
   describe('Upload', () => {
-    it('should render', () => {
-      const wrapper = mount(<Upload onUpload={EMPTY_FUNC}/>)
-      expect(wrapper.find('.tocco-upload')).to.have.length(1)
-    })
-
     it('should show input if no value set', () => {
       const wrapper = mount(<Upload onUpload={EMPTY_FUNC}/>)
       expect(wrapper.find('UploadInput')).to.have.length(1)
     })
 
-    it('should show view if  value set', () => {
+    it('should show view if value set', () => {
       const wrapper = mount(<Upload onUpload={EMPTY_FUNC} value={sampleValue}/>)
       expect(wrapper.find('View')).to.have.length(1)
     })
