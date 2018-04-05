bc7913b511e0954989c1387e39b05ca586ec369b
Niklaus Hug <nhug@tocco.ch>
2018-04-05 14:28:24 +0200

    refactor(tocco-ui): Upload as styled component
    - refactor as styled component
    - remove spare html elements
    - utilize Icon, Button and ButtonLink component
    - sort props alphabetically
    - revise tests

--- packages/tocco-ui/src/Upload/UploadInput.js
+++ packages/tocco-ui/src/Upload/UploadInput.js
@@ -1,20 +1,42 @@
 import PropTypes from 'prop-types'
-import classNames from 'classnames'
 import React from 'react'
 import Dropzone from 'react-dropzone'
+import styled from 'styled-components'
+import {theme} from 'styled-system'
+
+import Icon from '../Icon'
+
+const UploadInputStyles = styled.div`
+  border: dashed 1px ${theme('colors.base.fill.2')};
+  border-radius: ${theme('radii.2')};
+  padding: ${theme('space.3')} ${theme('space.4')};
+  cursor: pointer;
+
+  &[aria-disabled="true"] {
+    cursor: no-drop;
+    text-decoration: line-through;
+  }
+`
 
 const UploadInput = props => {
   const onDrop = files => {
     props.onDrop(files[0])
   }
 
-  const classes = classNames('dragzone', {'disabled': props.readOnly})
-
-  return <div>
-    <Dropzone disabled={props.readOnly} className={classes} multiple={false} onDrop={onDrop}>
-      <i className="fa fa-upload" aria-hidden="true"></i><span>{props.text || 'drag and drop or click'}</span>
+  return <UploadInputStyles>
+    <Dropzone
+      className="dropzone"
+      disabled={props.readOnly}
+      multiple={false}
+      onDrop={onDrop}
+    >
+      <Icon
+        icon="fa-upload"
+        position="before"
+      />
+      {props.text || 'drag and drop or click'}
     </Dropzone>
-  </div>
+  </UploadInputStyles>
 }
 
 UploadInput.propTypes = {
