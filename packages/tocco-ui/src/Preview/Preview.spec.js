12ae79814292760f1351fb9716e46ebb3c47a93a
Niklaus Hug <nhug@tocco.ch>
2018-04-05 14:25:44 +0200

    refactor(tocco-ui): combine Figure and Preview
    - refactor as styled component
    - combine Figure and Preview
    - utilize Icon component
    - remove spare html elements
    - extend examples
    - sort props alphabetically
    - revise tests
    - wrap image inside link if no click event function and all necessary props are provided
    - signal interactivity if a click event function is provided or the image is wrapped by a link

--- packages/tocco-ui/src/Preview/Preview.spec.js
+++ packages/tocco-ui/src/Preview/Preview.spec.js
@@ -1,61 +1,139 @@
 import React from 'react'
 import Preview from './Preview'
-import Figure from './Figure'
 import {shallow} from 'enzyme'
 
 describe('tocco-ui', () => {
   describe('Preview', () => {
-    it('should render', () => {
-      const wrapper = shallow(<Preview srcUrl="link/to/source" thumbnailUrl="link/to/thumbnail"/>)
-      expect(wrapper.find('.tocco-preview')).to.have.length(1)
-      expect(wrapper.find('Figure')).to.have.length(1)
-    })
-
-    it('should wrap thumbnail in link', () => {
+    it('render all elements and attributes', () => {
       const wrapper = shallow(
         <Preview
-          srcUrl="link/to/source"
-          thumbnailUrl="link/to/thumbnail"
-          downloadOnClick/>
-      )
+          alt="alternative text"
+          caption="caption text"
+          downloadOnClick={true}
+          srcUrl="/link-to-source"
+          thumbnailUrl="/link-to-thumbnail"
+        />
+      ).dive()
 
-      expect(wrapper.find('a')).to.have.length(1)
+      const figure = wrapper.find('figure')
+      const a = wrapper.find('a')
+      const img = wrapper.find('img')
+      const figcaption = wrapper.find('figcaption')
+
+      expect(figure).to.have.length(1)
+      expect(figure).to.have.attr('class')
+      expect(a).to.have.length(1)
+      expect(a).to.have.attr('href', '/link-to-source')
+      expect(a).to.have.attr('alt', 'alternative text')
+      expect(a).to.have.attr('download', 'caption text')
+      expect(img).to.have.length(1)
+      expect(img).to.have.attr('alt', 'alternative text')
+      expect(img).to.have.attr('src', '/link-to-thumbnail')
+      expect(figcaption).to.have.length(1)
+      expect(figcaption).to.have.text('caption text')
     })
 
-    it('should show Figure if thumbnailUrl is given', () => {
+    it('display image or icon depending on thumbnailUrl', () => {
+      let wrapper = shallow(
+        <Preview
+          srcUrl="/link-to-source"
+          thumbnailUrl="/link-to-thumbnail"
+        />
+      ).dive()
+
+      expect(wrapper.find('img')).to.have.length(1)
+      expect(wrapper.find('Icon')).to.have.length(0)
+
+      wrapper = shallow(
+        <Preview
+          srcUrl="/link-to-source"
+          thumbnailUrl=""
+        />
+      ).dive()
+
+      const Icon = wrapper.find('Icon')
+
+      expect(wrapper.find('img')).to.have.length(0)
+      expect(Icon).to.have.length(1)
+      expect(Icon.prop('animation')).to.equal('none')
+      expect(Icon.prop('icon')).to.equal('fa-file-text-o')
+      expect(Icon.prop('position')).to.equal('solely')
+    })
+
+    it('add figcaption only if caption in provided', () => {
+      let wrapper = shallow(
+        <Preview
+          caption="caption text"
+          srcUrl="/link-to-source"
+        />
+      ).dive()
+
+      let figcaption = wrapper.find('figcaption')
+
+      expect(figcaption).to.have.length(1)
+      expect(figcaption).to.have.text('caption text')
+
+      wrapper = shallow(
+        <Preview
+          caption=""
+          srcUrl="/link-to-source"
+        />
+      ).dive()
+
+      figcaption = wrapper.find('figcaption')
+
+      expect(figcaption).to.have.length(0)
+    })
+
+    it('link image if demanded and allowed', () => {
+      let wrapper = shallow(
+        <Preview
+          downloadOnClick={false}
+          srcUrl="/link-to-source"
+        />
+      ).dive()
+
+      expect(wrapper.find('a')).to.have.length(0)
+
+      wrapper = shallow(
+        <Preview
+          downloadOnClick={true}
+          srcUrl="/link-to-source"
+        />
+      ).dive()
+
+      const a = wrapper.find('a')
+
+      expect(a).to.have.length(1)
+      expect(a).to.have.attr('href', '/link-to-source')
+
+      wrapper = shallow(
+        <Preview
+          downloadOnClick={true}
+          onClick={(srcUrl, thumbnailUrl) => alert(srcUrl + '\n' + thumbnailUrl)}
+          srcUrl="/link-to-source"
+        />
+      ).dive()
+
+      expect(wrapper.find('a')).to.have.length(0)
+    })
+
+    it('trigger callback on click', () => {
+      const srcUrl = '/link-to-source'
+      const thumbnailUrl = '/link-to-thumbnail'
+      const onClickFunction = sinon.spy()
+
       const wrapper = shallow(
         <Preview
-          srcUrl="link/to/source"
-          thumbnailUrl="link/to/thumbnail"
+          downloadOnClick={true}
+          onClick={onClickFunction}
+          srcUrl={srcUrl}
+          thumbnailUrl={thumbnailUrl}
         />
       )
 
-      expect(wrapper.find(Figure)).to.have.length(1)
-    })
-
-    it('should show Figure if thumbnailUrl is given', () => {
-      const wrapper = shallow(
-        <Preview
-          srcUrl="link/to/source"
-          caption="Test.pdf"
-        />
-      )
-
-      expect(wrapper.find('.no-thumbnail')).to.have.length(1)
-    })
-
-    it('should add className', () => {
-      const customClassName = 'customClass'
-      const wrapper = shallow(
-        <Preview
-          srcUrl="link/to/source"
-          thumbnailUrl="link/to/thumbnail"
-          className={customClassName}
-        />
-      )
-
-      expect(wrapper.find(`.${customClassName}`)).to.have.length(1)
-      expect(wrapper.find('.tocco-preview')).to.have.length(1)
+      wrapper.find('img').simulate('click')
+      expect(onClickFunction).to.be.calledWith(srcUrl, thumbnailUrl)
     })
   })
 })
