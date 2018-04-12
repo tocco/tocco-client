e140ff96422bb5d3d48b201076fb68bed481f366
Daniel Keller <dkeller@tocco.ch>
2018-04-12 11:19:09 +0200

    refactor(entity-detail): small adaptations in ErrorBox
    - add classes and adjust tests
    - destruct props
    - add necessary intlprovider to tests

--- packages/entity-detail/src/components/ErrorBox/ErrorBox.spec.js
+++ packages/entity-detail/src/components/ErrorBox/ErrorBox.spec.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ErrorBox from './ErrorBox'
-import {shallow} from 'enzyme'
+import {mount} from 'enzyme'
+import {IntlProvider} from 'react-intl'
 
 const EMPTY_FUNC = () => {}
 
@@ -28,25 +29,33 @@ describe('entity-detail', () => {
                 }
               }
             }]}}
-        const wrapper = shallow(<ErrorBox
-          formErrors={formErrors}
-          showErrors={EMPTY_FUNC}
-        />)
+        const wrapper = mount(
+          <IntlProvider locale="en">
+            <ErrorBox
+              formErrors={{...formErrors}}
+              showErrors={EMPTY_FUNC}
+            />
+          </IntlProvider>
+        )
 
         expect(wrapper.find('.alert')).to.have.length(1)
-        expect(wrapper.find('li')).to.have.length(3)
+        expect(wrapper.find('.relationEntityError')).to.have.length(2)
       })
 
       it('should show field', () => {
         const formErrors = {_error: {}, firstname: {mandatory: ['mandatory!']}}
-        const wrapper = shallow(<ErrorBox
-          formErrors={formErrors}
-          showErrors={EMPTY_FUNC}
-        />)
+        const wrapper = mount(
+          <IntlProvider locale="en">
+            <ErrorBox
+              formErrors={{...formErrors}}
+              showErrors={EMPTY_FUNC}
+            />
+          </IntlProvider>
+        )
 
         expect(wrapper.find('.alert')).to.have.length(1)
         expect(wrapper.find('a')).to.have.length(1)
-        expect(wrapper.find('li')).to.have.length(1)
+        expect(wrapper.find('.fieldError')).to.have.length(1)
       })
 
       it('should show field', () => {
@@ -59,13 +68,17 @@ describe('entity-detail', () => {
           }
         }
 
-        const wrapper = shallow(<ErrorBox
-          formErrors={formErrors}
-          showErrors={EMPTY_FUNC}
-        />)
+        const wrapper = mount(
+          <IntlProvider locale="en">
+            <ErrorBox
+              formErrors={{...formErrors}}
+              showErrors={EMPTY_FUNC}
+            />
+          </IntlProvider>
+        )
 
         expect(wrapper.find('.alert')).to.have.length(1)
-        expect(wrapper.find('li')).to.have.length(4)
+        expect(wrapper.find('.validationError')).to.have.length(3)
       })
     })
   })
