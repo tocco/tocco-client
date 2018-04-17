8a5a62b715ee9cd27d38c00584c4da9de49b8d77
Daniel Keller <dkeller@tocco.ch>
2018-04-17 09:39:52 +0200

    feat(scheduler): show conflicts
    - Show border on existing conflicts
    - Show localized text in event popover for existing and resolved conflicts
    - Add mock data
    - Load scheduler mock in resource-scheduler

---
+++ packages/scheduler/src/components/Conflict/Conflict.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {intlShape} from 'react-intl'
+import {conflicts} from './utils'
+
+const Conflict = ({conflictStatus, intl}) => {
+  if (!(conflictStatus === conflicts.ACCEPTED || conflictStatus === conflicts.EXISTING)) {
+    return null
+  }
+
+  const accepted = conflictStatus === conflicts.ACCEPTED
+
+  const icon = accepted ? 'fa-check' : 'fa-times'
+  const textResource = `client.scheduler.conflict${accepted ? 'Accepted' : 'Existing'}`
+  const style = {
+    ...(accepted ? {} : {color: '#8b0000'})
+  }
+
+  return <span style={style}>
+    <i className={`fa ${icon}`} style={{paddingRight: '10px'}} aria-hidden="true"></i>
+    {intl.formatMessage({id: textResource})}
+  </span>
+}
+
+Conflict.propTypes = {
+  intl: intlShape.isRequired,
+  conflictStatus: PropTypes.string
+}
+
+export default Conflict
---
+++ packages/scheduler/src/components/Conflict/utils.js
@@ -0,0 +1,5 @@
+export const conflicts = {
+  EXISTING: 'existing',
+  ACCEPTED: 'accepted',
+  NONE: 'none'
+}
