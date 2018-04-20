138d7bd86a93c176f991e431f7acae5802a677c6
Niklaus Hug <nhug@tocco.ch>
2018-04-20 13:28:55 +0200

    feat(tocco-ui): establish Panel

---
+++ packages/tocco-ui/src/Panel/Panel.spec.js
@@ -0,0 +1,43 @@
+
+import React from 'react'
+import Panel from './Panel'
+import {shallow} from 'enzyme'
+
+describe('tocco-ui', function() {
+  describe('Panel', function() {
+    it('body should be closed initially', () => {
+      const wrapper = shallow(<Panel/>)
+      expect(wrapper.state('isOpen')).to.equal(false)
+    })
+
+    it('body should be opened initially', () => {
+      const wrapper = shallow(<Panel isOpen={true}/>)
+      expect(wrapper.state('isOpen')).to.equal(true)
+    })
+
+    it('open state should be toggleable', () => {
+      const wrapper = shallow(<Panel/>)
+      expect(wrapper.state('isOpen')).to.equal(false)
+      wrapper.instance().toogleOpenState()
+      expect(wrapper.state('isOpen')).to.equal(true)
+      wrapper.instance().toogleOpenState()
+      expect(wrapper.state('isOpen')).to.equal(false)
+    })
+
+    it('should render parent and children', () => {
+      const wrapper = shallow(<Panel><span>text-1</span><span>text-2</span></Panel>)
+      expect(wrapper.find('div')).to.have.length(1)
+      expect(wrapper.find('span')).to.have.length(2)
+      expect(wrapper.find('span').first().text()).to.equal('text-1')
+      expect(wrapper.find('span').last().text()).to.equal('text-2')
+    })
+
+    it('should pass props to child', () => {
+      const wrapper = shallow(<Panel><span>text-1</span></Panel>)
+      const {isOpen, isToggleable, toogleOpenState} = wrapper.find('span').props()
+      expect(isOpen).to.equal(false)
+      expect(isToggleable).to.equal(true)
+      expect(typeof toogleOpenState).to.equal('function')
+    })
+  })
+})
---
+++ packages/tocco-ui/src/Panel/index.js
@@ -0,0 +1,12 @@
+import Panel from './Panel'
+import PanelBody, {StyledPanelBody} from './PanelBody'
+import PanelHeaderFooter, {StyledPanelHeaderFooter} from './PanelHeaderFooter'
+
+export {
+  Panel as default,
+  PanelBody,
+  StyledPanelBody,
+  PanelHeaderFooter as PanelFooter,
+  PanelHeaderFooter as PanelHeader,
+  StyledPanelHeaderFooter
+}
