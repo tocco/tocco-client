631dea2be8196ee35d9fce9add8b7ab7a232c379
Niklaus Hug <nhug@tocco.ch>
2018-04-24 16:36:15 +0200

    feat(tocco-ui): animate Panel expansion and collapse

--- packages/tocco-ui/src/Panel/PanelBody.js
+++ packages/tocco-ui/src/Panel/PanelBody.js
@@ -2,8 +2,18 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import styled from 'styled-components'
 
+/**
+ * Using property max-height for slide-out animation is not perfect due two circumstances.
+ * Firstly the collapse animation is slightly delayed and secondly the value for expanded
+ * panels is never exact. Declaration "overflow-y: scroll;" ensure accessibility of
+ * overflowing content Value "fit-content" would be a promising choice but not yet
+ * supported well. The max-height solution still exceed any solution with properties like
+ * height and transform and keyframes because of simplicity.
+ */
 const StyledPanelBody = styled.div`
-  display: ${props => props.isOpen ? 'block' : 'none'};
+  max-height: ${props => props.isOpen ? '2000px' : '0'};
+  overflow-y: scroll;
+  transition: max-height 300ms ease-in-out;
 `
 
 class PanelBody extends React.Component {
