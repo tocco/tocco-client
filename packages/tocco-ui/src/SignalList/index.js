055e38ab04b67284f9417cb23d082129e2937f94
Niklaus Hug <nhug@tocco.ch>
2018-04-25 13:11:10 +0200

    feat(tocco-ui): establish SignalList
    - signalize conditions by icon, color and label in a list
    - define singal colors
    - define styling conditions

---
+++ packages/tocco-ui/src/SignalList/index.js
@@ -0,0 +1,9 @@
+import SignalList, {StyledSignalList} from './SignalList'
+import SignalListItem, {StyledSignalListItem} from './SignalListItem'
+
+export {
+  SignalList as default,
+  StyledSignalList,
+  SignalListItem,
+  StyledSignalListItem
+}
