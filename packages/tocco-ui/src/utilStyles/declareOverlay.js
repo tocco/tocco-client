803f576e67bbfa1e520988caab0c1cb6638bda1a
Niklaus Hug <nhug@tocco.ch>
2018-05-24 15:54:16 +0200

    style(tocco-ui): overlay disabled buttons
    - overlay disabled buttons with transparent color instead of making them transparent
    - this ensures same behaviour on all backgrounds
    - this prevents background shining through

---
+++ packages/tocco-ui/src/utilStyles/declareOverlay.js
@@ -0,0 +1,24 @@
+/**
+ * overlay an element by a pseudo element completely to colorize and make it inaccessible
+ * @param  {color}      css color
+ * @param  {integer}    opacity
+ * @return {string}     declaration
+ */
+
+const declareOverlay = (backgroundColor, opacity) => {
+  return `
+    &:after {
+      background-color: ${backgroundColor};
+      border-radius: inherit;
+      bottom: 0;
+      content: '';
+      left: 0;
+      opacity: ${opacity};
+      position: absolute;
+      right: 0;
+      top: 0;
+    }
+  `
+}
+
+export default declareOverlay
