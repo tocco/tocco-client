8e9929a9ae49b06634c7f8e418fcc76a73e95060
Niklaus Hug <nhug@tocco.ch>
2018-05-29 10:21:00 +0200

    style(tocco-ui): coherent stlying for flyout without label
    - ensure that dropdown menu items without label has same height like items with label by utilizing standard typo for Icon
    - ensure that buttons without label has correct padding independently of prop iconPosition

--- packages/tocco-ui/src/Icon/Icon.js
+++ packages/tocco-ui/src/Icon/Icon.js
@@ -24,6 +24,7 @@ const getSpacing = props => {
   let right = 0
 
   const space = (props.dense) ? theme('space.1')(props) : theme('space.3')(props)
+  const lineHeight = (props.dense) ? theme('lineHeights.0')(props) : theme('lineHeights.1')(props)
 
   if (props.position === stylingPosition.AFTER || props.position === stylingPosition.BETWEEN) {
     left = space
@@ -35,11 +36,12 @@ const getSpacing = props => {
 
   return `
     margin: 0 ${right} 0 ${left};
+    line-height: ${lineHeight};
   `
 }
 
 const StyledIcon = styled.i`
-  && {
+  &&& {
     padding: 0;
     ${props => getSpacing(props)}
   }
