be7be8a37984bcfd0d1bb62956378ff853ae8c71
Niklaus Hug <nhug@tocco.ch>
2018-06-01 14:19:59 +0200

    test(tocco-ui): add and extend tests

---
+++ packages/tocco-ui/src/utilStyles/declareInteractionColors.spec.js
@@ -0,0 +1,111 @@
+import {
+  declareFlatBaseColors,
+  declareFlatPrimaryColors,
+  declareInteractionColors,
+  declareRaisedBaseColors,
+  declareRaisedPrimaryColors
+} from '../utilStyles'
+
+const props = {
+  theme: {
+    colors: {
+      base: {
+        fill: [
+          'base_fill_0',
+          'base_fill_1',
+          'base_fill_2'
+        ],
+        line: [
+          'base_line_0',
+          'base_line_1',
+          'base_line_2'
+        ],
+        paper: 'base_paper',
+        text: 'base_text'
+      },
+      primary: {
+        fill: [
+          'primary_fill_0',
+          'primary_fill_1',
+          'primary_fill_2'
+        ],
+        line: [
+          'primary_line_0',
+          'primary_line_1',
+          'primary_line_2'
+        ],
+        fillContrast: [
+          'primary_fillContrast_0',
+          'primary_fillContrast_1',
+          'primary_fillContrast_2'
+        ]
+      }
+    }
+  }
+}
+
+const colorSet = {
+  defaultBackground: 'defaultBackground',
+  defaultColor: 'defaultColor',
+  focusBackground: 'focusBackground',
+  focusColor: 'focusColor',
+  activeBackground: 'activeBackground',
+  activeColor: 'activeColor'
+}
+
+describe('tocco-ui', () => {
+  describe('utilStyles', () => {
+    describe('declareElevation', () => {
+      it('should be color set for flat base', () => {
+        const colors = declareFlatBaseColors(props)
+        expect(Object.keys(colors)).to.have.lengthOf(6)
+        expect(colors.defaultColor).to.equal('base_text')
+        expect(colors.defaultBackground).to.equal('base_paper')
+        expect(colors.focusColor).to.equal('base_line_1')
+        expect(colors.focusBackground).to.equal('base_fill_0')
+        expect(colors.activeColor).to.equal('base_line_2')
+        expect(colors.activeBackground).to.equal('base_fill_1')
+      })
+
+      it('should be color set for flat primary', () => {
+        const colors = declareFlatPrimaryColors(props)
+        expect(Object.keys(colors)).to.have.lengthOf(6)
+        expect(colors.defaultColor).to.equal('primary_line_0')
+        expect(colors.defaultBackground).to.equal('base_paper')
+        expect(colors.focusColor).to.equal('primary_line_1')
+        expect(colors.focusBackground).to.equal('base_fill_0')
+        expect(colors.activeColor).to.equal('primary_line_2')
+        expect(colors.activeBackground).to.equal('base_fill_1')
+      })
+
+      it('should be color set for raised base', () => {
+        const colors = declareRaisedBaseColors(props)
+        expect(Object.keys(colors)).to.have.lengthOf(6)
+        expect(colors.defaultColor).to.equal('base_line_0')
+        expect(colors.defaultBackground).to.equal('base_fill_0')
+        expect(colors.focusColor).to.equal('base_line_1')
+        expect(colors.focusBackground).to.equal('base_fill_1')
+        expect(colors.activeColor).to.equal('base_line_2')
+        expect(colors.activeBackground).to.equal('base_fill_2')
+      })
+
+      it('should be color set for raised primary', () => {
+        const colors = declareRaisedPrimaryColors(props)
+        expect(Object.keys(colors)).to.have.lengthOf(6)
+        expect(colors.defaultColor).to.equal('primary_fillContrast_0')
+        expect(colors.defaultBackground).to.equal('primary_fill_0')
+        expect(colors.focusColor).to.equal('primary_fillContrast_1')
+        expect(colors.focusBackground).to.equal('primary_fill_1')
+        expect(colors.activeColor).to.equal('primary_fillContrast_2')
+        expect(colors.activeBackground).to.equal('primary_fill_2')
+      })
+
+      it('should declare color and background for default state, :focus, :hover and :active', () => {
+        const css = declareInteractionColors(colorSet)
+        expect(css).to.match(/background-color: defaultBackground;[\n\s]*color: defaultColor;/)
+        expect(css).to.match(/&:focus,[\n\s]*&:hover {[\n\s]*background-color: focusBackground;[\n\s]*color: focusColor;[\n\s]*}/)  // eslint-disable-line
+        expect(css).to.match(/&:active {[\n\s]*background-color: activeBackground;[\n\s]*color: activeColor;[\n\s]*}/)
+      })
+    })
+  })
+})
--- packages/tocco-ui/src/utilStyles/declareOverlay.js
+++ packages/tocco-ui/src/utilStyles/declareOverlay.js
@@ -1,11 +1,22 @@
 /**
- * overlay an element by a pseudo element completely to colorize and make it inaccessible
+ * overlay an element completely by a pseudo element to colorize it and make it inaccessible
  * @param  {color}      css color
  * @param  {integer}    opacity
  * @return {string}     declaration
  */
 
 const declareOverlay = (backgroundColor, opacity) => {
+  // inspired by https://gist.github.com/sethlopezme/d072b945969a3cc2cc11
+  // adapt regex by utilizing https://regex101.com/r/nNI4US/3
+  const regexHexRgbRgba = /(^#[a-fA-F0-9]{3}$)|(^#[a-fA-F0-9]{6}$)|(^rgba\((0|255|25[0-4]|2[0-4]\d|1\d\d|0?\d?\d),\s*(0|255|25[0-4]|2[0-4]\d|1\d\d|0?\d?\d),\s*(0|255|25[0-4]|2[0-4]\d|1\d\d|0?\d?\d),\s*(0?\.\d|1(\.0)?)\)$)|(^rgb\((0|255|25[0-4]|2[0-4]\d|1\d\d|0?\d?\d),\s*(0|255|25[0-4]|2[0-4]\d|1\d\d|0?\d?\d),\s*(0|255|25[0-4]|2[0-4]\d|1\d\d|0?\d?\d)\)$)/  // eslint-disable-line
+  if (!regexHexRgbRgba.test(backgroundColor)) {
+    throw new Error(`Error in declareOverlay: ${backgroundColor} is an invalid color`)
+  }
+
+  if (!(typeof opacity === 'number' && opacity >= 0 && opacity <= 1)) {
+    throw new Error(`Error in declareOverlay: ${opacity} is an invalid opacity`)
+  }
+
   return `
     &:after {
       background-color: ${backgroundColor};
