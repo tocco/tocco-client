cfc5fe0b1f5262dd1afc9f11bb6163575f5b5ba6
Daniel Keller <dkeller@tocco.ch>
2018-06-04 11:11:04 +0200

    refactor(tocco-ui): document formattedValue improvement
    - add document-compact for a small display of the file (e.g. list view)
    - pass along option to FormattedValue
    - Reuse Upload component in DocumentFormatter

---
+++ packages/tocco-ui/src/FormattedValue/typeFormatters/DocumentCompactFormatter.js
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+
+const DocumentFormatter = props => (
+  <span className="form-control-static document-compact">
+    <a
+      download="Test.pdf"
+      href={props.value.binaryLink}
+      title={props.options.downloadTitle || 'Download'}
+      onClick={e => e.stopPropagation()}
+      className="action btn btn-default"
+      target="_blank"
+    >
+      <i className="fa fa-download" aria-hidden="true"></i>
+    </a>
+  </span>
+)
+
+DocumentFormatter.propTypes = {
+  value: PropTypes.shape({
+    fileName: PropTypes.string.isRequired,
+    binaryLink: PropTypes.string.isRequired,
+    thumbnailLink: PropTypes.string.isRequired
+  }).isRequired,
+  options: PropTypes.shape({
+    downloadTitle: PropTypes.string
+  })
+}
+
+export default DocumentFormatter
