632e50f8a144949eb7a5eaee8b8cce4b156e9035
Daniel Keller <dkeller@tocco.ch>
2018-06-05 16:31:28 +0200

    feat(tocco-util): integrate PhoneEdit to Form

--- packages/tocco-util/src/form/validators/type.js
+++ packages/tocco-util/src/form/validators/type.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {FormattedMessage} from 'react-intl'
 import isUrl from 'validator/lib/isURL'
+import {isValidNumber} from 'libphonenumber-js'
 
 export const urlValidator = value => {
   if (!isUrl(value)) {
@@ -16,6 +17,28 @@ export const urlValidator = value => {
   return null
 }
 
+const DEFAULT_DEFAUL_COUNTRY = 'CH'
+export const phoneValidator = (value, fieldModel = {}) => {
+  const isValidPhoneNumber = isValidNumber(value, (fieldModel.defaultCountry) || DEFAULT_DEFAUL_COUNTRY)
+
+  const invalidPhoneError = {
+    format: [<FormattedMessage
+      key="invalidUrl"
+      id="client.component.form.invalidPhoneNumber"
+    />
+    ]
+  }
+
+  if (fieldModel.customPhoneRegex) {
+    return value.match(fieldModel.customPhoneRegex) ? null : invalidPhoneError
+  } else if (!isValidPhoneNumber) {
+    return invalidPhoneError
+  }
+
+  return null
+}
+
 export default {
-  url: urlValidator
+  url: urlValidator,
+  phone: phoneValidator
 }
