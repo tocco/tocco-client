e12f9218df440571dbfd8c0c849b86066e3753b1
Niklaus Hug <nhug@tocco.ch>
2018-06-07 17:10:15 +0200

    style(tocco-ui): enable primary color for ItemAccordion and ItemFlyout

--- packages/tocco-ui/src/Menu/ItemAccordion.js
+++ packages/tocco-ui/src/Menu/ItemAccordion.js
@@ -1,15 +1,23 @@
+import PropTypes from 'prop-types'
 import React from 'react'
 
 import Button from '../Button'
 import Item from './Item'
 import StyledItemAccordion from './StyledItemAccordion'
-import {stylingPosition} from '../utilStyles'
+import {stylingInk, stylingPosition} from '../utilStyles'
 
 /**
  * Wrap <MenuStack> in <ItemAccordion> if it should be toggleable. Submenu expand and
  * collapse alternating on click and push subsequent content down.
  */
 class ItemAccordion extends Item {
+  getChildren = () => {
+    // eslint-disable-next-line
+    return React.Children.map(this.props.children, child => {
+      return React.cloneElement(child, {look: this.props.look, ink: this.props.ink})
+    })
+  }
+
   render() {
     return (
       <StyledItemAccordion
@@ -18,6 +26,7 @@ class ItemAccordion extends Item {
         isToggleable={this.props.isToggleable}
       >
         <Button
+          ink={this.props.ink}
           icon={this.state.isOpen ? 'fa-caret-up' : 'fa-caret-down'}
           iconPosition={stylingPosition.AFTER}
           label={this.props.label}
@@ -34,5 +43,10 @@ ItemAccordion.defaultProps = {
   isOpen: true,
   isToggleable: true
 }
+
+/**
+ * Specify color palette. Default value is 'base'.
+ */
+ItemAccordion.propTypes['ink'] = PropTypes.oneOf([stylingInk.BASE, stylingInk.PRIMARY])
 
 export default ItemAccordion
--- packages/tocco-ui/src/Menu/ItemFlyout.js
+++ packages/tocco-ui/src/Menu/ItemFlyout.js
@@ -1,15 +1,23 @@
+import PropTypes from 'prop-types'
 import React from 'react'
 
 import Button from '../Button'
 import Item from './Item'
 import StyledItemFlyout from './StyledItemFlyout'
-import {stylingPosition} from '../utilStyles'
+import {stylingInk, stylingPosition} from '../utilStyles'
 
 /**
  * Wrap <MenuStack> in <ItemFlyout> if it should be toggleable. Submenu fly out on
  * click and cover subsequent content.
  */
 class ItemFlyout extends Item {
+  getChildren = () => {
+    // eslint-disable-next-line
+    return React.Children.map(this.props.children, child => {
+      return React.cloneElement(child, {look: this.props.look, ink: this.props.ink})
+    })
+  }
+
   render() {
     return (
       <StyledItemFlyout
@@ -18,6 +26,7 @@ class ItemFlyout extends Item {
         isToggleable={this.props.isToggleable}
       >
         <Button
+          ink={this.props.ink}
           icon={this.state.isOpen ? 'fa-caret-up' : 'fa-caret-down'}
           iconPosition={stylingPosition.AFTER}
           label={this.props.label}
@@ -34,5 +43,10 @@ ItemFlyout.defaultProps = {
   isOpen: false,
   isToggleable: true
 }
+
+/**
+ * Specify color palette. Default value is 'base'.
+ */
+ItemFlyout.propTypes['ink'] = PropTypes.oneOf([stylingInk.BASE, stylingInk.PRIMARY])
 
 export default ItemFlyout
