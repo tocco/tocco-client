317bfa11908313e3a52acbfaf268d6a62df79af2
Niklaus Hug <nhug@tocco.ch>
2018-06-08 15:54:59 +0200

    chore(tocco-ui): revise declareDensity

--- packages/tocco-ui/src/utilStyles/declareDensity.js
+++ packages/tocco-ui/src/utilStyles/declareDensity.js
@@ -1,17 +1,10 @@
 import objectToCss from './objectToCss'
 
 const declareDensity = props => {
-  if (props.dense) {
-    return objectToCss({
-      'line-height': ['lineHeights', 0],
-      'padding': ['space', 2]
-    }, props)
-  } else {
-    return objectToCss({
-      'line-height': ['lineHeights', 1],
-      'padding': ['space', 3, 4]
-    }, props)
-  }
+  return objectToCss({
+    'line-height': ['lineHeights', props.dense ? 0 : 1],
+    'padding': ['space', ...(props.dense ? [2] : [3, 4])]
+  }, props)
 }
 
 export default declareDensity
