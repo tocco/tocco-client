2ccd01df979036681479562f048ef3f1534a77e0
Niklaus Hug <nhug@tocco.ch>
2018-06-12 10:28:46 +0200

    chore(tocco-ui): remove unnecessary return statement

--- packages/tocco-ui/src/utilStyles/declareElevation.js
+++ packages/tocco-ui/src/utilStyles/declareElevation.js
@@ -1,7 +1,6 @@
 import {theme} from 'styled-system'
 
-const declareElevation = (props, elevation) => {
-  return `box-shadow: ${theme(`shadows.levels.${elevation}`)(props)} ${theme('shadows.color')(props)};`
-}
+const declareElevation = (props, elevation) =>
+  `box-shadow: ${theme(`shadows.levels.${elevation}`)(props)} ${theme('shadows.color')(props)};`
 
 export default declareElevation
