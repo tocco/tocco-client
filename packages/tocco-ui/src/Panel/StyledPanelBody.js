7f014d795b21a26967d73c8e49f25845927702ef
Niklaus Hug <nhug@tocco.ch>
2018-06-13 17:28:02 +0200

    style(tocco-ui): make panel body animation smoother
    - open panel body for an unnoticeable amount of time to get real height
    - respect window resize event
    - add optional fallback if measured height is incorrect

--- packages/tocco-ui/src/Panel/StyledPanelBody.js
+++ packages/tocco-ui/src/Panel/StyledPanelBody.js
@@ -1,16 +1,8 @@
 import styled from 'styled-components'
 
-/**
- * Using property max-height for slide-out animation is not perfect due two circumstances.
- * Firstly the collapse animation is slightly delayed and secondly the value for expanded
- * panels is never exact. Declaration "overflow-y: scroll;" ensure accessibility of
- * overflowing content Value "fit-content" would be a promising choice but not yet
- * supported well. The max-height solution still exceed any solution with properties like
- * height and transform and keyframes because of simplicity.
- */
 const StyledPanelBody = styled.div`
-  max-height: ${props => props.isOpen ? '2000px' : '0'};
-  overflow-y: scroll;
-  transition: max-height 300ms ease-in-out;
+  height: ${props => props.isOpen ? props.heightIfOpen : '0'};
+  overflow-y: auto;  /* if calculation does not work properly content is still accessible */
+  transition: height 300ms ease-in-out;
 `
 export default StyledPanelBody
