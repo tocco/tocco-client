26782d789d6e1f07a8c068dc1309afddb776b522
Roland Züger <rzueger@tocco.ch>
2018-06-26 09:28:14 +0200

    feat(entity-detail): Allow sub grid page limit to be set in model
    The previously hardcoded page limit of 5 now acts as a default value.

--- packages/entity-detail/src/components/SubGrid/SubGrid.spec.js
+++ packages/entity-detail/src/components/SubGrid/SubGrid.spec.js
@@ -6,20 +6,33 @@ import EntityListApp from 'tocco-entity-list/src/main'
 describe('entity-detail', () => {
   describe('components', () => {
     describe('SubGrid', () => {
-      it('should render', () => {
-        const wrapper = shallow(
-          <SubGrid
-            detailFormName="User"
-            gridName="relFoo"
-            modelField={{targetEntity: 'Foo'}}
-            relationName="relFoo"
-            onNavigateToCreate={() => {}}
-            showSubGridsCreateButton={false}
-            dispatchEmittedAction={() => {}}
-          />
-        )
+      const testProps = {
+        detailFormName: 'User',
+        gridName: 'relFoo',
+        modelField: {targetEntity: 'Foo'},
+        relationName: 'relFoo',
+        onNavigateToCreate: () => {},
+        showSubGridsCreateButton: false,
+        dispatchEmittedAction: () => {}
+      }
 
+      it('should render', () => {
+        const wrapper = shallow(<SubGrid {...testProps} />)
         expect(wrapper.find(EntityListApp)).to.have.length(1)
+      })
+
+      it('should render with default limit 5', () => {
+        const wrapper = shallow(<SubGrid {...testProps} />)
+
+        const listApp = wrapper.find(EntityListApp)
+        expect(listApp.props().limit).to.equal(5)
+      })
+
+      it('should render with custom limit', () => {
+        const wrapper = shallow(<SubGrid {...testProps} limit={10} />)
+
+        const listApp = wrapper.find(EntityListApp)
+        expect(listApp.props().limit).to.equal(10)
       })
     })
   })
