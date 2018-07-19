628289a75a787c4a3fcc5f374ce992f5f8281909
Daniel Keller <dkeller@tocco.ch>
2018-07-19 11:32:04 +0200

    refactor(tocco-util): rename table componentType
    - Adapt API changes. Table is no longer of componentType layout.

--- packages/entity-list/src/components/ListView/ListView.spec.js
+++ packages/entity-list/src/components/ListView/ListView.spec.js
@@ -12,7 +12,7 @@ const props = {
   formDefinition: {
     children: [
       {
-        componentType: 'layout',
+        componentType: 'table',
         layoutType: 'table',
         children: []
       },
--- packages/tocco-util/src/form/enums/layoutTypes.js
+++ packages/tocco-util/src/form/enums/layoutTypes.js
@@ -1,5 +1,4 @@
 export default Object.freeze({
-  TABLE: 'table',
   VERTICAL_BOX: 'vertical-box',
   HORIZONTAL_BOX: 'horizontal-box'
 })
