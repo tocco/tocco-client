c9a13dba38813ddc3676bfa0f2a2359dba2d17f3
Daniel Keller <dkeller@tocco.ch>
2018-07-30 14:05:33 +0200

    refactor(tocco-ui): add child property to button
    - add test and example
    - clean up button test and remove irrelevant theme tags

--- packages/tocco-ui/src/Button/Button.spec.js
+++ packages/tocco-ui/src/Button/Button.spec.js
@@ -4,115 +4,68 @@ import {ThemeProvider} from 'styled-components'
 
 import Button from './Button'
 
-const theme = {
-  overlays: {
-    disabled: {
-      color: '#f00',
-      opacity: 0.8
-    }
-  }
-}
-
 describe('tocco-ui', function() {
   describe('Button', function() {
     it('should handle click events', () => {
       const onButtonClick = sinon.spy()
-      const wrapper = mount(
-        <ThemeProvider theme={theme}>
-          <Button onClick={onButtonClick}/>
-        </ThemeProvider>
-      )
+      const wrapper = mount(<Button onClick={onButtonClick}/>)
       wrapper.find('button').simulate('click')
       expect(onButtonClick).to.have.property('callCount', 1)
     })
 
     it('should show label', () => {
-      const wrapper = mount(
-        <ThemeProvider theme={theme}>
-          <Button label="test"/>
-        </ThemeProvider>
-      )
+      const wrapper = mount(<Button label="test"/>)
       expect(wrapper.find('button').text()).to.equal('test')
     })
 
+    it('should show children if no label is provided', () => {
+      const child = 'test123'
+      const wrapper = mount(<Button>{child}</Button>)
+      expect(wrapper.find('button').text()).to.equal(child)
+    })
+
     it('should be disabled and hidden', () => {
-      let wrapper = mount(
-        <ThemeProvider theme={theme}>
-          <Button/>
-        </ThemeProvider>
-      )
+      let wrapper = mount(<Button/>)
       expect(wrapper.find('button')).to.not.have.property('disabled')
 
-      wrapper = mount(
-        <ThemeProvider theme={theme}>
-          <Button disabled={false}/>
-        </ThemeProvider>
-      )
+      wrapper = mount(<Button disabled={false}/>)
       expect(wrapper.find('button')).to.not.have.property('disabled')
 
-      wrapper = mount(
-        <ThemeProvider theme={theme}>
-          <Button disabled/>
-        </ThemeProvider>
-      )
+      wrapper = mount(<Button disabled/>)
       expect(wrapper.find('button')).to.be.disabled()
     })
 
     it('should show pending spinner', () => {
-      let wrapper = mount(
-        <ThemeProvider theme={theme}>
-          <Button/>
-        </ThemeProvider>
-      )
+      let wrapper = mount(<Button/>)
       expect(wrapper.find('Icon')).to.have.length(0)
 
-      wrapper = mount(
-        <ThemeProvider theme={theme}>
-          <Button pending={false}/>
-        </ThemeProvider>
-      )
+      wrapper = mount(<Button pending={false}/>)
       expect(wrapper.find('Icon')).to.have.length(0)
 
-      wrapper = mount(
-        <ThemeProvider theme={theme}>
-          <Button pending/>
-        </ThemeProvider>
-      )
+      wrapper = mount(<Button pending/>)
       expect(wrapper.find('Icon').prop('animation')).to.equal('spin')
       expect(wrapper.find('Icon').prop('icon')).to.equal('fa-spinner')
     })
 
     it('should show icon', () => {
-      const wrapper = mount(
-        <ThemeProvider theme={theme}>
-          <Button icon="icon"/>
-        </ThemeProvider>
-      )
+      const wrapper = mount(<Button icon="icon"/>)
       expect(wrapper.find('Icon')).to.have.length(1)
     })
 
     it('should set default type to button', () => {
-      const wrapper = mount(
-        <ThemeProvider theme={theme}>
-          <Button/>
-        </ThemeProvider>
-      )
+      const wrapper = mount(<Button/>)
       expect(wrapper.find('button').prop('type')).to.equal('button')
     })
 
     it('should set type', () => {
-      const wrapper = mount(
-        <ThemeProvider theme={theme}>
-          <Button type="submit"/>
-        </ThemeProvider>
-      )
+      const wrapper = mount(<Button type="submit"/>)
       expect(wrapper.find('button').prop('type')).to.equal('submit')
     })
 
     it('should have four defaultProps', () => {
       /* wrapper.props() and wrapper.instance().props does not list label and iconPosition */
       const wrapper = shallow(
-        <ThemeProvider theme={theme}>
+        <ThemeProvider theme={{}}>
           <Button/>
         </ThemeProvider>
       )
