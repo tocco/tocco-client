f033d9ea26578f813154d41970f4a58a0811c751
Niklaus Hug <nhug@tocco.ch>
2018-08-17 17:45:06 +0200

    refactor(tocco-ui): utilize react-svg version of FontAwesome 5.1
    - package react-fontawesome as chunk
    - load react-fontawesome asynchronously
    - remove css classes
    - remove animation prop and icon prop (those are handled directly by react-fontawesome)
    - remove unicode prop to separate concerns
    - drop styled components and handle margin as inline style (an additional wrapper would have unwanted side effects)

--- packages/tocco-ui/src/Icon/Icon.spec.js
+++ packages/tocco-ui/src/Icon/Icon.spec.js
@@ -1,63 +1,23 @@
 import React from 'react'
+import {ThemeProvider} from 'styled-components'
+import {shallow, mount} from 'enzyme'
+
 import Icon from './Icon'
-import {shallow} from 'enzyme'
 
 describe('tocco-ui', function() {
   describe('Icon', function() {
-    it('should have 2 defaultProps', () => {
-      const wrapper = shallow(<Icon icon="fa-bar"/>)
+    it('should have 1 defaultProps', () => {
+      const wrapper = shallow(<Icon />)
       expect(wrapper.props().position).to.equal('sole')
-      expect(Icon.defaultProps.animation).to.equal('none')
     })
 
-    it('should pass 4 props to StyledIcon', () => {
-      const wrapper = shallow(
-        <Icon
-          animation="none"
-          dense={true}
-          icon="fa-bar"
-          position="between"
-        />
+    it('should receive theme', () => {
+      const wrapper = mount(
+        <ThemeProvider theme={{key: 'value'}}>
+          <Icon />
+        </ThemeProvider>
       )
-      const props = wrapper.props()
-      const {
-        animation,
-        className,
-        dense,
-        position
-      } = props
-      expect(Object.keys(props)).to.have.lengthOf(5)
-      expect(animation).to.equal('none')
-      expect(className).to.equal('fa fa-bar icon')
-      expect(dense).to.be.true
-      expect(position).to.equal('between')
-    })
-
-    it('should set classnames for Glyphicon', () => {
-      const wrapper = shallow(<Icon icon="glyphicon-plus"/>)
-      expect(wrapper.props().className).to.equal('glyphicon glyphicon-plus icon')
-    })
-
-    it('should set classnames for Font Awesome', () => {
-      const wrapper = shallow(<Icon icon="fa-bar"/>)
-      expect(wrapper.props().className).to.equal('fa fa-bar icon')
-    })
-
-    it('should animate Font Awesome', () => {
-      const wrapper = shallow(<Icon animation="spin" icon="fa-bar"/>)
-      expect(wrapper.props().className).to.equal('fa fa-spin fa-bar icon')
-    })
-
-    it('should render unicode and set classname', () => {
-      const wrapper = shallow(<Icon unicode={'\u2022'}/>)
-      expect(wrapper.dive().text()).to.equal('•')
-      expect(wrapper.props().className).to.equal('fa')
-    })
-
-    it('should render unicode and set classname', () => {
-      const wrapper = shallow(<Icon unicode={'\u2022'}/>)
-      expect(wrapper.dive().text()).to.equal('•')
-      expect(wrapper.props().className).to.equal('fa')
+      expect(wrapper.prop('theme')).to.be.object
     })
   })
 })
