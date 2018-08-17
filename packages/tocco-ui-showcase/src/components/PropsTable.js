b7cbcf0c4b5b0966bf51b0a16eac84bbb7d1dfb0
Niklaus Hug <nhug@tocco.ch>
2018-08-17 17:53:20 +0200

    refactor(tocco-ui-showcase): utilize Icon

--- packages/tocco-ui-showcase/src/components/PropsTable.js
+++ packages/tocco-ui-showcase/src/components/PropsTable.js
@@ -1,6 +1,8 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 
+import {Icon} from 'tocco-ui'
+
 const PropsTable = props => {
   const rows = []
 
@@ -11,7 +13,7 @@ const PropsTable = props => {
         <tr key={propName}>
           <td>{propName}</td>
           <td>{prop.description}</td>
-          <td>{prop.required && <span className="glyphicon glyphicon-exclamation-sign required-prop"/>}</td>
+          <td>{prop.required && <Icon icon="exclamation"/>}</td>
           <td>{prop.type.name}</td>
         </tr>
       )
