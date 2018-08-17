89314b45b77927f5ce3a4b32587fcd58eb5a25be
Niklaus Hug <nhug@tocco.ch>
2018-08-17 17:53:34 +0200

    refactor(tocco-util): utilize Icon

--- packages/tocco-util/src/errorLogging/handlers/toastrHandler.js
+++ packages/tocco-util/src/errorLogging/handlers/toastrHandler.js
@@ -3,6 +3,6 @@ import notifier from '../../notifier'
 import {put, call} from 'redux-saga/effects'
 
 export default function* toastr(title, description, error) {
-  const action = yield call(notifier.info, 'error', title, description, 'exclamation-circle', 0)
+  const action = yield call(notifier.info, 'error', title, description, 'exclamation-triangle', 0)
   yield put(action)
 }
