9d608f220baa2193cf84150dcd659974d601663c
Daniel Keller <dkeller@tocco.ch>
2018-08-21 16:17:44 +0200

    chore: remove saucelab browser
    - safari and edge were constantly failing.
      Attempts to fix the problem were not successful.
      Migrating to jest probably helps.

--- build/karma-saucelabs.conf.js
+++ build/karma-saucelabs.conf.js
@@ -15,18 +15,6 @@ module.exports = cfg => {
       browserName: 'firefox',
       platform: 'Windows 10',
       version: 'latest'
-    },
-    sl_osx_safari: {
-      base: 'SauceLabs',
-      browserName: 'safari',
-      platform: 'macOS 10.12',
-      version: 'latest'
-    },
-    sl_edge: {
-      base: 'SauceLabs',
-      browserName: 'microsoftedge',
-      platform: 'Windows 10',
-      version: 'latest'
     }
   }
 
