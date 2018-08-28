27c1ac0712962d39ebc0e2b6920555d050246aac
Daniel Keller <dkeller@tocco.ch>
2018-08-28 13:13:58 +0200

    refactor(tocco-ui): improve lazyComponent

--- packages/tocco-ui/src/util/lazyComponent.spec.js
+++ packages/tocco-ui/src/util/lazyComponent.spec.js
@@ -9,8 +9,7 @@ describe('tocco-ui', () => {
       const InnerComp = () => <div>test</div>
 
       it('should render nothing before loaded', () => {
-        const promise = new Promise(() => {
-        }) // never resolved
+        const promise = new Promise(() => {}) // never resolved
 
         const AsyncComp = lazyComponent(() => promise)
         const wrapper = mount(<AsyncComp/>)
@@ -19,7 +18,7 @@ describe('tocco-ui', () => {
       })
 
       it('should render component when loaded', done => {
-        const promise = Promise.resolve({default: InnerComp})
+        const promise = Promise.resolve(InnerComp)
 
         const AsyncComp = lazyComponent(() => promise)
         const wrapper = mount(<AsyncComp/>)
@@ -27,6 +26,43 @@ describe('tocco-ui', () => {
         promise.then(() => {
           wrapper.update()
           expect(wrapper.find(InnerComp)).to.have.length(1)
+          done()
+        })
+      })
+
+      it('should respect selector param', done => {
+        const selector = 'xyz'
+        const promise = Promise.resolve({[selector]: InnerComp})
+        const AsyncComp = lazyComponent(() => promise, selector)
+        const wrapper = mount(<AsyncComp/>)
+
+        promise.then(() => {
+          wrapper.update()
+          expect(wrapper.find(InnerComp)).to.have.length(1)
+          done()
+        })
+      })
+
+      it('should respect default component param', () => {
+        const Default = () => <div>loading...</div>
+        const promise = new Promise(() => {})
+
+        const AsyncComp = lazyComponent(() => promise, null, <Default/>)
+        const wrapper = mount(<AsyncComp/>)
+        wrapper.update()
+        expect(wrapper.find(Default)).to.have.length(1)
+        expect(wrapper.html()).to.contains('loading...')
+      })
+
+      it('should call onLoaded', done => {
+        const onLoadedSpy = sinon.spy()
+        const promise = Promise.resolve(InnerComp)
+        const AsyncComp = lazyComponent(() => promise, null, null, onLoadedSpy)
+        const wrapper = mount(<AsyncComp/>)
+        expect(onLoadedSpy).not.to.be.called
+        promise.then(() => {
+          wrapper.update()
+          expect(onLoadedSpy).to.be.called
           done()
         })
       })
