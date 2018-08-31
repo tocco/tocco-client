13724f749a7dcc1c229ac2a19d61db611b5194ef
Niklaus Hug <nhug@tocco.ch>
2018-08-31 15:34:04 +0200

    style(tocco-ui): style buttons in fullcalender custom header

---
+++ packages/scheduler/src/components/FullCalendar/StyledFullCalendar.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components'
+import {theme} from 'styled-system'
+
+import {StyledMenuButton, StyledItem} from 'tocco-ui/src/Menu'
+
+const StyledFullCalendar = styled.div`
+  && {
+    margin-bottom: ${props => theme('space.3')};
+
+    > ${StyledMenuButton} {
+      display: flex;
+
+      > ${StyledItem}:nth-child(3) {
+        flex-grow: 1;
+
+        > h3 {
+          text-align: center;
+          width: 100%;
+        }
+      }
+    }
+  }
+`
+
+export default StyledFullCalendar
