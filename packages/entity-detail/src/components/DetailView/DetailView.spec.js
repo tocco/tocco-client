c32bda971eeadb1f16cb4666481287455acca582
Cyril Khan <ckhan@tocco.ch>
2018-09-06 14:53:28 +0200

    feat(entity-detail): implement DetailViewContainer

--- packages/entity-detail/src/components/DetailView/DetailView.spec.js
+++ packages/entity-detail/src/components/DetailView/DetailView.spec.js
@@ -3,7 +3,7 @@ import {shallow} from 'enzyme'
 import {IntlStub} from 'tocco-test-util'
 
 import DetailView from './DetailView'
-import DetailForm from '../DetailForm'
+import DetailFormContainer from '../../containers/DetailFormContainer'
 
 const EMPTY_FUNC = () => {
 }
@@ -24,28 +24,21 @@ describe('entity-detail', () => {
           intl={IntlStub}
           router={routerProp}
           loadDetailView={EMPTY_FUNC}
-          formDefinition={{children: []}}
           closeEntityDetail={EMPTY_FUNC}
-          entity={{}}
           entityModel={{}}
           entityName="User"
           entityId="21"
           modelPaths={[]}
           unloadDetailView={EMPTY_FUNC}
-          loadRelationEntities={EMPTY_FUNC}
-          uploadDocument={EMPTY_FUNC}
           loadRemoteEntity={EMPTY_FUNC}
-          loadTooltip={EMPTY_FUNC}
-          relationEntities={{}}
           remoteEntities={{}}
           saveEntity={EMPTY_FUNC}
           submitForm={EMPTY_FUNC}
           logError={EMPTY_FUNC}
-          fireTouched={EMPTY_FUNC}
         />)
 
         expect(wrapper.find('.detail-view')).to.have.length(1)
-        expect(wrapper.find(DetailForm)).to.have.length(1)
+        expect(wrapper.find(DetailFormContainer)).to.have.length(1)
       })
     })
   })
