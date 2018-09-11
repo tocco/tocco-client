42d7a15ca624719f612521710df8709e430b8de0
Daniel Keller <dkeller@tocco.ch>
2018-09-11 14:36:43 +0200

    test(resource-scheduler): delete unused test

--- packages/resource-scheduler/src/components/SearchPanel/SearchPanel.specs.js
+++ packages/resource-scheduler/src/components/SearchPanel/SearchPanel.specs.js
@@ -18,39 +18,6 @@ describe('resource-scheduler', () => {
         )
         expect(wrapper.find('div')).to.have.length(1)
       })
-
-      // Can not test asynchonously added icons (wrapper.find(Icon) reveals <i/> instead of <Icon />).
-      //
-      // it('should show colored icon if color is defined', () => {
-      //   const color = '#f44242'
-      //   const calendarTypes = [
-      //     {
-      //       name: 'lecturer',
-      //       targetEntity: 'User',
-      //       formBase: 'UserCalendar',
-      //       label: 'Dozent',
-      //       color
-      //     },
-      //     {
-      //       name: 'noColor',
-      //       targetEntity: 'User',
-      //       formBase: 'UserCalendar',
-      //       label: 'No Color'
-      //     }
-      //   ]
-
-      //   const wrapper = render(
-      //     <SearchPanel
-      //       addCalendarsOfType={EMPTY_FUNC}
-      //       updateRequestedCalendars={EMPTY_FUNC}
-      //       calendarTypes={calendarTypes}
-      //     />
-      //   )
-
-      //   expect(wrapper.find('.color-icon')).to.have.length(2)
-      //   expect(wrapper.find('.color-icon')).to.have.style('color', color)
-      //   expect(wrapper.find('.color-icon').last()).to.not.have.property('style')
-      // })
     })
   })
 })
