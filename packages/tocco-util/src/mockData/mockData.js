d126ce11b8bd252111ce14d17013086d8bf3a2e7
Daniel Keller <dkeller@tocco.ch>
2018-09-19 08:54:28 +0200

    feat(tocco-util): add report mock data

--- packages/tocco-util/src/mockData/mockData.js
+++ packages/tocco-util/src/mockData/mockData.js
@@ -1,12 +1,14 @@
 import {setupForms} from './forms'
 import {setupEntities} from './entities'
 import {setupActions} from './actions'
+import {setupReports} from './reports'
 import {setupUpload} from './upload'
 
 export const setupFetchMock = (fetchMock, entityStore, timeout = 1000) => {
   setupForms(fetchMock, entityStore, timeout)
   setupEntities(fetchMock, entityStore, timeout)
   setupActions(fetchMock, entityStore, timeout)
+  setupReports(fetchMock, entityStore, timeout)
   setupUpload(fetchMock, entityStore, timeout)
 }
 
