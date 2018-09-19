e22d1d5cb1a722fb56d5c1b8f541409705d950c2
Daniel Keller <dkeller@tocco.ch>
2018-09-19 08:55:48 +0200

    refactor(tocco-util): adjust report util and reformat component

--- packages/tocco-util/src/actions/actions.js
+++ packages/tocco-util/src/actions/actions.js
@@ -1,7 +1,5 @@
-export const COMPONENT_TYPE_ACTION = 'action'
-export const COMPONENT_TYPE_ACTION_GROUP = 'action-group'
+import componentTypes from './componentTypes'
 
-export const isAction = componentType =>
-  !!componentType && (componentType === COMPONENT_TYPE_ACTION_GROUP || componentType === COMPONENT_TYPE_ACTION)
+export const isAction = componentType => !!componentType && Object.values(componentTypes).indexOf(componentType) >= 0
 
 export const modeFitsScopes = (mode, scopes) => (!mode || !scopes || scopes.length < 0 || scopes.includes(mode))
