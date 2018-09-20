b01af085c8a0f163510cfc3d41a647a8c9e2473b
Niklaus Hug <nhug@tocco.ch>
2018-09-20 11:21:21 +0200

    refactor(tocco-ui): describe Panel tests precisely

--- packages/tocco-ui/src/Panel/PanelBody.spec.js
+++ packages/tocco-ui/src/Panel/PanelBody.spec.js
@@ -7,13 +7,15 @@ import StyledPanelBody from './StyledPanelBody'
 // import 'styled-components-test-utils/lib/chai';
 
 describe('tocco-ui', function() {
-  describe('PanelBody', function() {
-    it('should render parent and children', () => {
-      const wrapper = mount(<PanelBody><span>text-1</span><span>text-2</span></PanelBody>)
-      expect(wrapper.find(StyledPanelBody)).to.have.length(1)
-      expect(wrapper.find('span')).to.have.length(2)
-      expect(wrapper.find('span').first().text()).to.equal('text-1')
-      expect(wrapper.find('span').last().text()).to.equal('text-2')
+  describe('Panel', function() {
+    describe('PanelBody', function() {
+      it('should render parent and children', () => {
+        const wrapper = mount(<PanelBody><span>text-1</span><span>text-2</span></PanelBody>)
+        expect(wrapper.find(StyledPanelBody)).to.have.length(1)
+        expect(wrapper.find('span')).to.have.length(2)
+        expect(wrapper.find('span').first().text()).to.equal('text-1')
+        expect(wrapper.find('span').last().text()).to.equal('text-2')
+      })
     })
   })
 })
