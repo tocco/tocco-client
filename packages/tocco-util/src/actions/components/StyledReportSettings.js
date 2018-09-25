b21e27b9aac0d4f6cd0107f634d6eca27ddd04be
Niklaus Hug <nhug@tocco.ch>
2018-09-25 15:19:50 +0200

    style(tocco-util): harmonize ReportSettings style
    - remove button on top to motivate users to check settings before downloading a report
    - use look instead of ink to emphasize button
    - move style to styled-components and harmonize spacing

---
+++ packages/tocco-util/src/actions/components/StyledReportSettings.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const StyledReportSettings = styled.div`
+  && {
+    > div:first-of-type {
+      margin-bottom: 20px;
+    }
+  }
+`
+
+export default StyledReportSettings
