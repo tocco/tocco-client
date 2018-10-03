e163de8f1553777320a209b92e4ef38d677b56fd
Daniel Keller <dkeller@tocco.ch>
2018-10-03 09:01:40 +0200

    refactor(tocco-util): update report generation

--- packages/tocco-util/src/actions/modules/actionHandlers/report.js
+++ packages/tocco-util/src/actions/modules/actionHandlers/report.js
@@ -15,7 +15,7 @@ export default function* (actionDefinition, entity, ids) {
 }
 
 export function* displayReportSettings(actionDefinition, answerChannel) {
-  const {body: settingsDefinition} = yield call(requestSaga, `reports/${actionDefinition.reportId}/settings`)
+  const {body: settingsDefinition} = yield call(requestSaga, `report/${actionDefinition.reportId}/settings`)
   const onSubmit = values => answerChannel.put(values)
   const settingsModalId = yield call(uuid)
 
@@ -45,7 +45,7 @@ export function* awaitSettingsSubmit(definition, answerChannel, settingsModalId,
       acceptedStatusCodes: [400]
     }
 
-    const generationsResponse = yield call(requestSaga, `reports/${definition.reportId}/generations`, requestOptions)
+    const generationsResponse = yield call(requestSaga, `report/${definition.reportId}/generations`, requestOptions)
 
     if (generationsResponse.status === 400) {
       yield call(handleFailedGenerationsRequest, settingsModalId, generationsResponse)
@@ -59,11 +59,11 @@ export function* handleReportGenerations(settingsModalId, generationsResponse) {
   yield put(notifier.removeModalComponent(settingsModalId))
   const pollingUrl = generationsResponse.headers.get('Location')
   const blockingInfoId = yield call(uuid)
-  yield put(notifier.blockingInfo(blockingInfoId, 'title', 'message', 'file-pdf'))
+  yield put(notifier.blockingInfo(blockingInfoId, 'client.common.report.inProgress', null, 'file-pdf'))
 
   const completed = yield call(checkReportStatusLoop, pollingUrl)
   yield put(notifier.removeBlockingInfo(blockingInfoId))
-  if (completed.body.reportStatus === 'completed') {
+  if (completed.body.status === 'completed') {
     yield call(handleSuccessfulReport, completed)
   } else {
     yield call(handleFailedReport)
@@ -72,19 +72,19 @@ export function* handleReportGenerations(settingsModalId, generationsResponse) {
 
 export function* handleSuccessfulReport(completed) {
   const result = completed.body._links.result
-  const outputJob = yield call(requestSaga, result.href)
+  const outputJob = yield call(requestSaga, result.href, {queryParams: {_paths: 'document'}})
   const {fileName, binaryLink} = outputJob.body.paths.document.value.value
   yield call(downloadUrl, binaryLink, fileName)
-  yield put(notifier.info('success', 'title', null))
+  yield put(notifier.info('success', 'client.common.report.successful', null))
 }
 
 export function* handleFailedReport() {
-  yield put(notifier.info('error', 'title', 'message'))
+  yield put(notifier.info('error', 'client.common.report.failed', null))
 }
 
 export function* handleFailedGenerationsRequest(modalId, generationsResponse) {
-  const title = 'client.common.report.defaultErrorMessage'
-  const message = generationsResponse.body.message || 'client.common.report.defaultErrorMessage'
+  const title = 'client.common.report.failedGenerationTitle'
+  const message = generationsResponse.body.message || 'client.common.report.failedGenerationMessage'
   yield put(notifier.info('warning', title, message))
 }
 
@@ -92,7 +92,7 @@ export function* checkReportStatusLoop(location) {
   let delayer = 1
   while (true) {
     const response = yield call(requestSaga, location)
-    if (response.body.reportStatus === 'in_progress') {
+    if (response.body.status === 'in_progress') {
       yield delay(500 * delayer)
       if (delayer < 10) delayer++
     } else {
@@ -105,7 +105,7 @@ export function downloadUrl(url, fileName) {
   const a = document.createElement('a')
   a.href = url
   a.download = fileName || url.split('/').pop()
-
+  a.target = '_blank'
   document.body.appendChild(a)
   a.click()
   if (window.URL.revokeObjectURL) { window.URL.revokeObjectURL(url) }
