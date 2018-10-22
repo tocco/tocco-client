f8ade656e82e3330ce58fcf1af4320e3ccc848bb
Cyril Khan <ckhan@tocco.ch>
2018-10-22 10:12:02 +0200

    feat(toco-ui): add MultiDate Component
    An upgrade to date-range component to set one or a range of dates.

--- packages/tocco-ui/src/EditableValue/typeEditors/DateRangeEdit.js
+++ packages/tocco-ui/src/EditableValue/typeEditors/DateRangeEdit.js
@@ -4,47 +4,151 @@ import moment from 'moment'
 import {injectIntl, intlShape} from 'react-intl'
 
 import DateAbstract from './DateAbstract'
-import {momentJStoToFlatpickrFormat} from '../utils'
+import {atMostOne, momentJStoToFlatpickrFormat, toLocalDateString} from '../utils'
 
-const DateRangeEdit = props => {
-  const flatpickrOptions = {
-    mode: 'range',
-    altFormat: momentJStoToFlatpickrFormat(moment().locale(props.intl.locale)._locale.longDateFormat('L')),
-    dateFormat: 'Y-m-d',
-    allowInput: false,
-    ...props.options
+class DateRangeEdit extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      showCalendar: true,
+      showDoubleCalendar: false,
+      label: this.RANGE_CALENDAR_LABEL
+    }
+    this.onChangeObject = {
+      exactValue: null,
+      fromValue: null,
+      toValue: null
+    }
+    this.counter = 1
+  }
+  DATE_FORMAT = 'YYYY-MM-DD'
+  RANGE_CALENDAR_LABEL = 'Range Calendar'
+  SINGLE_CALENDAR_LABEL = 'Exact Calendar'
+
+  getLocalizedAltFormat = () => moment().locale(this.props.intl.locale)._locale.longDateFormat('L')
+
+  parseDate = s => {
+    const momentDate = moment(s, [this.getLocalizedAltFormat(), this.DATE_FORMAT])
+    return momentDate.isValid() ? momentDate.toDate() : null
   }
 
-  const value = props.value && props.value.from && props.value.to
-    ? [props.value.from, props.value.to]
-    : null
+  compareTimeIntervals = () => {
+    const fromValue = this.onChangeObject.fromValue
+    const toValue = this.onChangeObject.toValue
+    return moment(fromValue).isBefore(toValue)
+  }
 
-  const handleChange = dates => {
-    if (dates && dates.length === 2) {
-      props.onChange({
-        from: dates[0],
-        to: dates[1]
-      })
+  handleChange = dates => {
+    const dateTime = atMostOne(dates)
+    this.onChangeObject.exactValue = dateTime ? toLocalDateString(dateTime) : null
+    this.props.onChange(this.onChangeObject)
+  }
+
+  handleFromDateChange = dates => {
+    const dateTime = atMostOne(dates)
+    this.onChangeObject.fromValue = dateTime ? toLocalDateString(dateTime) : null
+    if (this.onChangeObject.fromValue && this.onChangeObject.toValue) {
+      if (this.compareTimeIntervals() === false) {
+        this.onChangeObject.fromValue = null
+      }
+    }
+    this.props.onChange(this.onChangeObject)
+  }
+
+  handleToDateChange = dates => {
+    const dateTime = atMostOne(dates)
+    this.onChangeObject.toValue = dateTime ? toLocalDateString(dateTime) : null
+    if (this.onChangeObject.fromValue && this.onChangeObject.toValue) {
+      if (this.compareTimeIntervals() === false) {
+        this.onChangeObject.toValue = null
+      }
+    }
+    this.props.onChange(this.onChangeObject)
+  }
+
+  toggleRangeView = () => {
+    this.setState(prevState => ({
+      showCalendar: !prevState.showCalendar,
+      showDoubleCalendar: !prevState.showDoubleCalendar
+    }))
+    this.counter += 1
+    this.switchValues()
+  }
+
+  switchValues = () => {
+    if (this.counter % 2 === 0) {
+      this.setState({label: this.SINGLE_CALENDAR_LABEL})
+      if (this.onChangeObject.exactValue !== null) {
+        if (this.onChangeObject.fromValue === null && this.onChangeObject.toValue === null) {
+          for (const key in this.onChangeObject) {
+            if (this.onChangeObject.hasOwnProperty(key)) {
+              this.onChangeObject[key] = this.onChangeObject.exactValue
+            }
+          }
+        }
+      } else if (this.counter % 2 === 1) {
+        this.setState({label: this.RANGE_CALENDAR_LABEL})
+        if (this.onChangeObject.exactValue === null) {
+          if (this.onChangeObject.fromValue !== null) {
+            this.onChangeObject.exactValue = this.onChangeObject.fromValue
+          }
+          if (this.onChangeObject.toValue !== null && this.onChangeObject.fromValue === null) {
+            this.onChangeObject.exactValue = this.onChangeObject.toValue
+          }
+        }
+      }
     }
   }
 
-  return (
-    <DateAbstract
-      value={value}
-      onChange={handleChange}
-      readOnly={props.readOnly}
-      options={{...props.options, flatpickrOptions}}
-      events={props.events}
-    />
-  )
+  render() {
+    const flatpickrOptions = {
+      altFormat: momentJStoToFlatpickrFormat(this.getLocalizedAltFormat()),
+      dateFormat: momentJStoToFlatpickrFormat(this.DATE_FORMAT),
+      allowInput: true,
+      parseDate: this.parseDate,
+      ...(this.props.options ? this.props.options.flatpickrOptions : {})
+    }
+
+    return (
+      <div>
+        {this.state.showCalendar && <DateAbstract
+          value={[this.props.value.exactValue]}
+          onChange={this.handleChange}
+          options={{...this.props.options, flatpickrOptions}}
+          readOnly={this.props.readOnly}
+          events={this.props.events}
+        />}
+        <button onClick={this.toggleRangeView}>{this.state.label}</button>
+        {this.state.showDoubleCalendar
+        && <div>
+          <DateAbstract
+            value={[this.props.value.fromValue]}
+            onChange={this.handleFromDateChange}
+            options={{...this.props.options, flatpickrOptions}}
+            readOnly={this.props.readOnly}
+            events={this.props.events}
+          />
+          <DateAbstract
+            value={[this.props.value.toValue]}
+            onChange={this.handleToDateChange}
+            options={{...this.props.options, flatpickrOptions}}
+            readOnly={this.props.readOnly}
+            events={this.props.events}
+          />
+        </div>
+        }
+      </div>
+    )
+  }
 }
 
 DateRangeEdit.propTypes = {
   intl: intlShape.isRequired,
   onChange: PropTypes.func.isRequired,
   value: PropTypes.shape({
-    from: PropTypes.string,
-    to: PropTypes.string
+    exactValue: PropTypes.string,
+    fromValue: PropTypes.string,
+    toValue: PropTypes.string
   }),
   readOnly: PropTypes.bool,
   options: PropTypes.shape({
