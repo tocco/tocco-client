b00c11af6d89328890476e83334562daa44f1c51
Daniel Keller <dkeller@tocco.ch>
2018-11-06 13:03:46 +0100

    refactor(tocco-util): pass config to autoRestartSaga

--- packages/tocco-util/src/appFactory/store/sagaHelpers.js
+++ packages/tocco-util/src/appFactory/store/sagaHelpers.js
@@ -2,11 +2,11 @@ import errorLogging from '../../errorLogging'
 
 import {call, put, all} from 'redux-saga/effects'
 
-export const autoRestartSaga = generator => {
+export const autoRestartSaga = (generator, config) => {
   return function* autoRestarting(...args) {
     while (true) {
       try {
-        yield call(generator, ...args)
+        yield call(generator, config, ...args)
       } catch (error) {
         yield put(errorLogging.logError('client.common.unexpectedError', 'client.common.unexpectedError', error))
       }
