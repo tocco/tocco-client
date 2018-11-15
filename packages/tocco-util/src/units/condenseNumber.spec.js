d41c0176dbe10e448c2a70478f6a58cb4c2b05a6
Niklaus Hug <nhug@tocco.ch>
2018-11-15 17:01:57 +0100

    refactor: rename addSystemUnit to condenseNumber, handle edge cases

--- packages/tocco-util/src/units/condenseNumber.spec.js
+++ packages/tocco-util/src/units/condenseNumber.spec.js
@@ -1,32 +1,40 @@
-import {addSystemUnit} from './addSystemUnit'
+import {condenseNumber} from './condenseNumber'
 
 describe('tocco-util', () => {
   describe('units', () => {
-    describe('addSystemUnit', () => {
+    describe('condenseNumber', () => {
       test('shorten numbers correctly', () => {
         const scenarios = [
+          [0, '0'],
           [1, '1'],
-          [12, '12'],
-          [123, '123'],
-          [1234, '1k'],
+          [999, '999'],
+          [1000, '1k'],
           [12345, '12k'],
           [123456, '123k'],
-          [1234567, '1M'],
+          [999499, '999k'],
+          [999500, '1M'],
           [12345678, '12M'],
           [123456789, '123M'],
+          [999499000, '999M'],
+          [999500000, '1G'],
           [1234567890, '1G'],
           [12345678901, '12G'],
           [123456789012, '123G'],
+          [999499000000, '999G'],
+          [999500000000, '1T'],
           [1234567890123, '1T'],
           [12345678901234, '12T'],
           [123456789012345, '123T'],
+          [999499000000000, '999T'],
+          [999500000000000, '1P'],
           [1234567890123456, '1P'],
           [12345678901234567, '12P'],
           [123456789012345678, '123P'],
           [1234567890123456789, '1235P'],
           [12345678901234567890, '12346P']
         ]
-        scenarios.map(scenario => expect(addSystemUnit(scenario[0])).to.equal(scenario[1]))
+        scenarios.map(scenario => condenseNumber(scenario[0]))
+        scenarios.map(scenario => expect(condenseNumber(scenario[0])).to.equal(scenario[1]))
       })
     })
   })
