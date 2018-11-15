958ecd10d227b2a548cd6896c3fb76617378c28f
Niklaus Hug <nhug@tocco.ch>
2018-11-15 17:01:57 +0100

    refactor: rename addSystemUnit to condenseNumber, handle edge cases

---
+++ packages/tocco-util/src/units/condenseNumber.js
@@ -0,0 +1,21 @@
+import _round from 'lodash/round'
+
+export const condenseNumber = value => {
+  const ranges = [
+    {base: 0, symbol: ''},
+    {base: 3, symbol: 'k'},
+    {base: 6, symbol: 'M'},
+    {base: 9, symbol: 'G'},
+    {base: 12, symbol: 'T'},
+    {base: 15, symbol: 'P'}
+  ]
+
+  const index = ranges.findIndex(range =>
+    _round(value / 10 ** range['base']) < 1000)
+
+  const range = index >= 0
+    ? ranges[index]
+    : ranges[ranges.length - 1]
+
+  return `${_round(value / 10 ** range['base'])}${range['symbol']}`
+}
