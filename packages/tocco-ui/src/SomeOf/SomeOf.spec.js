606d555ae241e830bf765a45975b7ae16651be67
Niklaus Hug <nhug@tocco.ch>
2018-11-15 17:04:43 +0100

    refactor(tocco-ui): test SomeOf not as explicit

--- packages/tocco-ui/src/SomeOf/SomeOf.spec.js
+++ packages/tocco-ui/src/SomeOf/SomeOf.spec.js
@@ -7,7 +7,10 @@ describe('tocco-ui', () => {
   describe('SomeOf', () => {
     test('should display values', () => {
       const wrapper = mount(<SomeOf some={12} of={345}/>)
-      expect(wrapper.find('span').text()).to.equal('12 / 345')
+      const text = wrapper.find('span').text()
+      expect(text).to.include('12')
+      expect(text).to.include('345')
+      expect(text.length).to.be.at.least(6)
     })
 
     test('should have one defaultProps', () => {
