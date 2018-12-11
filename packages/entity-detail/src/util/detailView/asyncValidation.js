6fe1bc72eab1f7fc5f81d58360ac1da2954cfab9
Cyril Khan <ckhan@tocco.ch>
2018-12-11 15:58:37 +0100

    refactor(entity-detail): add async type validator to asyncValidation

--- packages/entity-detail/src/util/detailView/asyncValidation.js
+++ packages/entity-detail/src/util/detailView/asyncValidation.js
@@ -1,5 +1,9 @@
 import {SubmissionError} from 'redux-form'
 import {form, rest} from 'tocco-app-extensions'
+import {addErrors} from 'tocco-app-extensions/src/form/syncValidation'
+import _isEmpty from 'lodash/isEmpty'
+import _get from 'lodash/get'
+import validators from 'tocco-app-extensions/src/form/validators'
 
 import modes from '../modes'
 
@@ -15,6 +19,47 @@ const hasError = errors => (
   errors && Object.keys(errors).length > 0
 )
 
+export const asyncVerify = async(values, entityModel) => {
+  let errors = {}
+
+  errors = await asyncValidateTypes(values, entityModel, errors)
+
+  return errors
+}
+
+export const shouldUseAsyncValidator = type => {
+  const asyncValidators = ['phone']
+  return asyncValidators.includes(type)
+}
+
+export const asyncValidateTypes = async(values, entityModel, errors) => {
+  const errorArray = []
+  for (const key in values) {
+    if (values.hasOwnProperty(key)) {
+      if (values[key]) {
+        const type = _get(entityModel, key + '.type')
+
+        if (shouldUseAsyncValidator(type)) {
+          const typeValidator = validators.typeValidators[type]
+
+          if (typeValidator) {
+            const fieldModel = entityModel[key]
+            const validationError = await typeValidator(values[key], fieldModel)
+
+            if (validationError) {
+              const error = addErrors(errors, key, validationError)
+              errorArray.push(new Promise(resolve => resolve(error)))
+            }
+          }
+        }
+      }
+    }
+  }
+  return Promise.all(errorArray).then(value => {
+    return value
+  })
+}
+
 const validateRequest = (values, initialValues, entityName, entityId, entityModel, mode) => {
   const dirtyFields = form.getDirtyFields(initialValues, values)
 
@@ -25,13 +70,20 @@ const validateRequest = (values, initialValues, entityName, entityId, entityMode
     body: entity
   }
 
-  return rest.simpleRequest(`entities/${entity.model}${entity.key ? `/${entity.key}` : ''}`, options)
-    .then(resp => resp.body)
-    .then(body => {
-      if (body.valid) {
+  return asyncVerify(values, entityModel)
+    .then(errorObject => {
+      if (_isEmpty(errorObject)) {
+        return rest.simpleRequest(`entities/${entity.model}${entity.key ? `/${entity.key}` : ''}`, options)
+      }
+    })
+    .then(resp => {
+      if (!resp) {
         return {}
       }
-      return form.validationErrorToFormError(entity, body.errors)
+      if (resp.body.valid) {
+        return {}
+      }
+      return form.validationErrorToFormError(entity, resp.body.errors)
     })
 }
 
