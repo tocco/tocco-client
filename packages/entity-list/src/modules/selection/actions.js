6f211eb9da94224a081c2e43735e0788c20e2247
Daniel Keller <dkeller@tocco.ch>
2019-01-04 10:32:20 +0100

    refactor(entity-list): pass new selection object to actions

--- packages/entity-list/src/modules/selection/actions.js
+++ packages/entity-list/src/modules/selection/actions.js
@@ -4,6 +4,8 @@ export const SET_SELECTION_MODE = 'list/setSelectionMode'
 export const CLEAR_SELECTION = 'selection/CLEAR_SELECTION'
 export const SET_SHOW_SELECTION_CONTROLLER = 'selection/SET_SHOW_SELECTION_CONTROLLER'
 export const SET_TABLE_SELECTION_STYLE = 'selection/SET_TABLE_SELECTION_STYLE'
+export const SET_QUERY = 'selection/SET_QUERY'
+export const SET_QUERY_COUNT = 'selection/SET_QUERY_COUNT'
 export const TOGGLE_SHOW_SELECTED_RECORDS = 'selection/TOOGLE_SHOW_SELECTED_RECORDS'
 export const RELOAD_DATA = 'selection/RELOAD_DATA'
 
@@ -44,6 +46,20 @@ export const setTableSelectionStyle = tableSelectionStyle => ({
   type: SET_TABLE_SELECTION_STYLE,
   payload: {
     tableSelectionStyle
+  }
+})
+
+export const setQuery = query => ({
+  type: SET_QUERY,
+  payload: {
+    query
+  }
+})
+
+export const setQueryCount = queryCount => ({
+  type: SET_QUERY_COUNT,
+  payload: {
+    queryCount
   }
 })
 
