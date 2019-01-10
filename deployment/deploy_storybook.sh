5314fdec718c2e268c84aa5b62c4d1f44561e3df
Daniel Keller <dkeller@tocco.ch>
2019-01-10 15:24:29 +0100

    chore: deploy storybook

--- deployment/deploy_storybook.sh
+++ deployment/deploy_storybook.sh
@@ -9,15 +9,31 @@ then
   exit 0
 fi
 
+
 rev=$(git rev-parse --short HEAD)
 BRANCH_FOLDER_NAME="${TRAVIS_BRANCH//\//_}"
-echo ">>> ${BRANCH_FOLDER_NAME}"
-mkdir dist/deployment
-mkdir dist/deployment/$BRANCH_FOLDER_NAME
-mv dist/storybook dist/deployment/$BRANCH_FOLDER_NAME
-cd dist/deployment
 
-git init
+
+# Get the deploy key by using Travis's stored variables to  decrypt deploy_key.enc
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ./deployment/deploy_key.enc -out deploy_key -d
+chmod 600 deploy_key
+eval `ssh-agent -s`
+ssh-add deploy_key
+
+# Clone repo and move storybook into it
+cd dist
+git clone https://github.com/tocco/tocco-client.git TARGET_BRANCH --single-branch --branch gh-pages --depth 1
+mkdir $TARGET_BRANCH/$BRANCH_FOLDER_NAME
+mv storybook/* $TARGET_BRANCH/$BRANCH_FOLDER_NAME
+cd $TARGET_BRANCH
+
+
+# Commit and push
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
@@ -26,18 +42,7 @@ git fetch upstream
 git reset upstream/$TARGET_BRANCH
 
 git add -A .
-git commit -m "rebuild pages at ${rev}"
-
-# Get the deploy key by using Travis's stored variables to  decrypt deploy_key.enc
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../../../deployment/deploy_key.enc -out deploy_key -d
-chmod 600 deploy_key
-eval `ssh-agent -s`
-ssh-add deploy_key
+git commit -m "rebuild storybook at ${rev}"
 
 # Now that we're all set up, we can push.
 git push -q git@github.com:tocco/tocco-client.git HEAD:$TARGET_BRANCH
