695e825ea2cdeb7a054f79ae757fb17773a20df4
Daniel Keller <dkeller@tocco.ch>
2019-01-14 09:59:27 +0100

    refactor(storybook): add readme addon

--- storybook/addons.js
+++ storybook/addons.js
@@ -1,4 +1,5 @@
 import '@storybook/addon-knobs/register'
+import 'storybook-readme/register'
 import '@storybook/addon-actions/register'
 import '@storybook/addon-options/register'
 import 'storybook-addon-intl/register'
