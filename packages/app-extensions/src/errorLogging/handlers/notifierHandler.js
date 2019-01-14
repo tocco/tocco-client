69d8924889dfb20f1f999fddd86f01de7067ebd2
Daniel Keller <dkeller@tocco.ch>
2019-01-14 09:58:28 +0100

    refactor(app-extensions): rename error logging handler to notifier

--- packages/app-extensions/src/errorLogging/handlers/notifierHandler.js
+++ packages/app-extensions/src/errorLogging/handlers/notifierHandler.js
@@ -2,7 +2,7 @@ import notifier from '../../notifier'
 
 import {put, call} from 'redux-saga/effects'
 
-export default function* toastr(title, description, error) {
+export default function* notifierHandler(title, description, error) {
   const action = yield call(notifier.info, 'error', title, description, 'exclamation-triangle', 0)
   yield put(action)
 }
