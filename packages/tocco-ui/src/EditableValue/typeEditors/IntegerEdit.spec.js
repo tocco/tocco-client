3fd65797a23744f236b1e2834b6836f059ab274b
Cyril Khan <ckhan@tocco.ch>
2019-01-21 09:01:07 +0100

    refactor(tocco-ui): add local intl context

--- packages/tocco-ui/src/EditableValue/typeEditors/IntegerEdit.spec.js
+++ packages/tocco-ui/src/EditableValue/typeEditors/IntegerEdit.spec.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import {mount} from 'enzyme'
-import {IntlStub} from 'tocco-test-util'
+import {mountWithIntl} from 'tocco-test-util/src/intlEnzyme/intlEnzyme'
 
 import IntegerEdit from './IntegerEdit'
 import NumberEdit from './NumberEdit'
@@ -12,17 +11,15 @@ describe('tocco-ui', () => {
     describe('typeEditors', () => {
       describe('IntegerEdit ', () => {
         test('should render NumberEdit', () => {
-          const optionsObject = {intl: {...IntlStub, locale: 'en'}}
-          const wrapper = mount(
-            <IntegerEdit value={1234567} options={optionsObject} onChange={EMPTY_FUNC} />
+          const wrapper = mountWithIntl(
+            <IntegerEdit value={1234567} options={{}} onChange={EMPTY_FUNC} />
           )
           expect(wrapper.find(NumberEdit)).to.have.length(1)
         })
 
         test('should render NumberEdit with correct value', () => {
-          const optionsObject = {intl: {...IntlStub, locale: 'en'}}
-          const wrapper = mount(
-            <IntegerEdit value={1234567} options={optionsObject} onChange={EMPTY_FUNC} />
+          const wrapper = mountWithIntl(
+            <IntegerEdit value={1234567} options={{}} onChange={EMPTY_FUNC} />
           )
           expect(wrapper.find(NumberEdit).props().value).to.be.eql(1234567)
         })
