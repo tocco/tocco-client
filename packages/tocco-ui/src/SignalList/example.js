71a5aac3219b996300fcbcb9e8bc1bf9705e6298
Cyril Khan <ckhan@tocco.ch>
2019-01-22 10:52:37 +0100

    refactor(tocco-ui): revert example files
    - adjust look selection of ButtonGroup/ButtonLink

--- packages/tocco-ui/src/SignalList/example.js
+++ packages/tocco-ui/src/SignalList/example.js
@@ -4,73 +4,73 @@ import React from 'react'
 import SignalList from './'
 // real-import:import {SignalList} from 'tocco-ui'
 
-export const insertSignalListContent = () => (
-  <div>
-    {/* start example */}
-    <SignalList.List>
-      <SignalList.Item
-        label="base"
-      />
-      <SignalList.Item
-        condition="primary"
-        label="primary"
-      />
-      <SignalList.Item
-        condition="danger"
-        label="danger"
-      />
-      <SignalList.Item
-        condition="success"
-        label="success"
-      />
-      <SignalList.Item
-        condition="warning"
-        label="warning"
-      />
-    </SignalList.List>
+export default () => {
+  return (
+    <div>
+      {/* start example */}
+      <SignalList.List>
+        <SignalList.Item
+          label="base"
+        />
+        <SignalList.Item
+          condition="primary"
+          label="primary"
+        />
+        <SignalList.Item
+          condition="danger"
+          label="danger"
+        />
+        <SignalList.Item
+          condition="success"
+          label="success"
+        />
+        <SignalList.Item
+          condition="warning"
+          label="warning"
+        />
+      </SignalList.List>
 
-    <SignalList.List>
-      <SignalList.Item
-        condition="danger"
-        label="danger supercalifragilisticexpialidocioussupercalifragilisticexpialidocious">
-        <SignalList.List>
-          <SignalList.Item
-            condition="danger"
-            label="A. signalize error supercalifragilisticexpialidocioussupercalifragilisticexpialidocious">
-            <SignalList.List>
-              <SignalList.Item
-                condition="success"
-                label="A.1. signalize success supercalifragilisticexpialidocioussupercalifragilisticexpialidocious"
-              />
-              <SignalList.Item
-                condition="danger"
-                label="B.2 signalize danger"
-              />
-              <SignalList.Item
-                condition="danger"
-                label="B.3. signalize danger"
-              />
-            </SignalList.List>
-          </SignalList.Item>
-          <SignalList.Item
-            condition="warning"
-            label="B. signalize warning">
-            <SignalList.List>
-              <SignalList.Item
-                condition="warning"
-                label="B.1. signalize warning"
-              />
-              <SignalList.Item
-                condition="success"
-                label="B.2. signalize success"
-              />
-            </SignalList.List>
-          </SignalList.Item>
-        </SignalList.List>
-      </SignalList.Item>
-    </SignalList.List>
-    {/* end example */}
-  </div>
-)
-
-export default () => (insertSignalListContent())
+      <SignalList.List>
+        <SignalList.Item
+          condition="danger"
+          label="danger supercalifragilisticexpialidocioussupercalifragilisticexpialidocious">
+          <SignalList.List>
+            <SignalList.Item
+              condition="danger"
+              label="A. signalize error supercalifragilisticexpialidocioussupercalifragilisticexpialidocious">
+              <SignalList.List>
+                <SignalList.Item
+                  condition="success"
+                  label="A.1. signalize success supercalifragilisticexpialidocioussupercalifragilisticexpialidocious"
+                />
+                <SignalList.Item
+                  condition="danger"
+                  label="B.2 signalize danger"
+                />
+                <SignalList.Item
+                  condition="danger"
+                  label="B.3. signalize danger"
+                />
+              </SignalList.List>
+            </SignalList.Item>
+            <SignalList.Item
+              condition="warning"
+              label="B. signalize warning">
+              <SignalList.List>
+                <SignalList.Item
+                  condition="warning"
+                  label="B.1. signalize warning"
+                />
+                <SignalList.Item
+                  condition="success"
+                  label="B.2. signalize success"
+                />
+              </SignalList.List>
+            </SignalList.Item>
+          </SignalList.List>
+        </SignalList.Item>
+      </SignalList.List>
+      {/* end example */}
+    </div>
+  )
+}
