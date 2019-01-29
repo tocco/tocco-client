59e58741adde973a0156aa3f15db420cb696beb5
Niklaus Hug <nhug@tocco.ch>
2019-01-29 14:07:23 +0100

    fix(sso-login): fix tests

--- packages/sso-login/src/utils/providers.spec.js
+++ packages/sso-login/src/utils/providers.spec.js
@@ -13,18 +13,24 @@ describe('sso-login', () => {
               unique_id: 'google',
               label: 'Google',
               active: true,
-              button_primary_color: '#4285f4',
-              button_secondary_color: '#fff',
+              button_primary_color: '#bad6ff',
               button_icon: 'fab, google',
               button_label: 'Mit Google einloggen'
+            },
+            {
+              unique_id: 'facebook',
+              label: 'Facebook',
+              active: true,
+              button_primary_color: '#3A5696',
+              button_label: 'Mit Facebook einloggen'
             },
             {
               unique_id: 'office365',
               label: 'Office 365',
               active: true,
-              button_primary_color: '#ea3e23',
-              button_secondary_color: '#f3f4f7',
-              button_icon: 'fab,microsoft',
+              button_primary_color: '#d33b17',
+              button_secondary_color: '#dddddd',
+              button_icon: 'fab, microsoft',
               button_label: 'Mit Office 356 einloggen'
             }
           ]
