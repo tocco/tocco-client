37f16cfd065eed56b5c53413830433131ebf5705
Daniel Keller <dkeller@tocco.ch>
2019-01-29 10:59:06 +0100

    refactor(entity-list): adjust selection object to backend

--- packages/entity-list/src/util/actions.js
+++ packages/entity-list/src/util/actions.js
@@ -5,15 +5,17 @@ export const deriveSelectionFromState = state => {
 
   if (selection.selectionMode === selectionModes.ALL) {
     return {
-      mode: 'QUERY',
-      payload: selection.query,
+      entityName: state.entityList.entityName,
+      type: 'QUERY',
+      query: selection.query,
       count: selection.queryCount || 0
     }
   }
 
   return {
-    mode: 'ID',
-    payload: selection.selection,
+    entityName: state.entityList.entityName,
+    type: 'ID',
+    ids: selection.selection,
     count: selection.selection.length || 0
   }
 }
