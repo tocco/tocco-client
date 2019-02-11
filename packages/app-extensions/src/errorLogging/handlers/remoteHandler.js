58b1457267eb2d7949fb42dade83e9c635f6b7e8
Daniel Keller <dkeller@tocco.ch>
2019-02-11 13:05:06 +0100

    refactor(app-extension): improve logging
    - set errorLogging in createStore and remove as (unused) parameter
    - improve remote logging
    - replace obsolete log handler in entity-browser

--- packages/app-extensions/src/errorLogging/handlers/remoteHandler.js
+++ packages/app-extensions/src/errorLogging/handlers/remoteHandler.js
@@ -1,10 +1,10 @@
 import {call} from 'redux-saga/effects'
 
 export default function* remoteLogger(title, description, error) {
-  const message = `${title} \n${description} \n${error}`
+  const message = `ERROR MESSAGE: ${error.message} \nSTACK: ${error.stack ? error.stack.substring(0, 1000) : '-'}`
   const options = {
     method: 'POST',
     credentials: 'include'
   }
-  yield call(fetch, `${__BACKEND_URL__}/nice2/log?level=error&message=${message}`, options)
+  yield call(fetch, `${__BACKEND_URL__}/nice2/log?level=error&message=${encodeURI(message)}`, options)
 }
