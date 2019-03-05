c28bbe6a89359a93f59e857c00fa0fc04e4cab82
Niklaus Hug <nhug@tocco.ch>
2019-03-05 13:43:59 +0100

    refactor(tocco-ui): use SignalList in ErrorList
    - use styled components instead of icon list
    - drop icon list based on bootstrap

--- packages/tocco-ui/src/FormField/ErrorList.spec.js
+++ packages/tocco-ui/src/FormField/ErrorList.spec.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {shallow} from 'enzyme'
 
 import ErrorList from './ErrorList'
+import SignalList from '../SignalList'
 
 describe('tocco-ui', () => {
   describe('FormField', () => {
@@ -12,10 +13,10 @@ describe('tocco-ui', () => {
           error2: ['error2-1', 'error2-2']
         }
         const wrapper = shallow(<ErrorList error={error}/>)
-
-        expect(wrapper).to.contain('error1-1')
-        expect(wrapper).to.contain('error2-1')
-        expect(wrapper).to.contain('error2-2')
+        expect(wrapper.find(SignalList.Item)).to.have.length(3)
+        expect(wrapper.find(SignalList.Item).at(0)).to.have.prop('label', 'error1-1')
+        expect(wrapper.find(SignalList.Item).at(1)).to.have.prop('label', 'error2-1')
+        expect(wrapper.find(SignalList.Item).at(2)).to.have.prop('label', 'error2-2')
       })
     })
   })
