276b9681aeb45af47cd7f7477fd1762e7470e4ab
Cyril Khan <ckhan@tocco.ch>
2019-03-08 13:37:22 +0100

    fix(config): fix BACKEND remote env variable

--- config/_production.js
+++ config/_production.js
@@ -1,4 +1,11 @@
-export default () => ({
+import {getUrl, getNoMock} from './_development'
+
+export default config => ({
   compiler_hash_type: 'chunkhash',
-  compiler_devtool: false
+  compiler_devtool: false,
+  globals: {
+    ...config.globals,
+    __BACKEND_URL__: getUrl() || JSON.stringify(''),
+    __NO_MOCK__: getNoMock() || false
+  }
 })
