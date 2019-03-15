23cf891c74499184566a284c96d32b2bebbf4627
Daniel Keller <dkeller@tocco.ch>
2019-03-15 11:36:51 +0100

    refactor(entity-list): change selection view switch to buttons
    - rearrange components
    - add titles
    - add text resources

--- packages/entity-list/src/components/SelectionController/SelectionController.spec.js
+++ packages/entity-list/src/components/SelectionController/SelectionController.spec.js
@@ -21,7 +21,6 @@ describe('entity-list', () => {
     describe('Selection', () => {
       it('should display message operateOnQueriedItems and queryCount', () => {
         const wrapper = intlEnzyme.mountWithIntl(<SelectionController {...baseProps}/>)
-        expect(wrapper.find(FormattedMessage).prop('id')).to.be.equal('client.entity-list.operateOnQueriedItems')
         expect(wrapper.find(FormattedMessage).prop('values').count).to.be.equal(123)
         expect(wrapper.find(Button)).to.have.length(0)
       })
@@ -31,8 +30,7 @@ describe('entity-list', () => {
         const wrapper = intlEnzyme.mountWithIntl(<SelectionController {...baseProps} selection={selection}/>)
         expect(wrapper.find(FormattedMessage).prop('id')).to.be.equal('client.entity-list.operateOnSelectedItems')
         expect(wrapper.find(FormattedMessage).prop('values').count).to.be.equal(99)
-        expect(wrapper.find(Button)).to.have.length(2)
-        expect(wrapper.find(Button).first().prop('label')).to.be.equal('client.entity-list.showSelectedItemsOnly')
+        expect(wrapper.find(Button)).to.have.length(3)
       })
     })
   })
