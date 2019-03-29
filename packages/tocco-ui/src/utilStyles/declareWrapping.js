65f370a3786b71d172f557ed8756b83a917bd15f
Niklaus Hug <nhug@tocco.ch>
2019-03-29 17:17:13 +0100

    refactor(tocco-ui): shorten declareWrapping code

--- packages/tocco-ui/src/utilStyles/declareWrapping.js
+++ packages/tocco-ui/src/utilStyles/declareWrapping.js
@@ -1,23 +1,14 @@
-const declareNoneWrappingText = props => {
-  return `
-    display: inline-block;
-    max-width: 100%;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-  `
-}
+export const declareNoneWrappingText = () => `
+  display: inline-block;
+  max-width: 100%;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`
 
-const declareWrappingText = props => {
-  return `
-    hyphens: auto;
-    overflow-wrap: break-word;
-    white-space: normal;
-    word-wrap: break-word;
-  `
-}
-
-export {
-  declareNoneWrappingText,
-  declareWrappingText
-}
+export const declareWrappingText = () => `
+  hyphens: auto;
+  overflow-wrap: break-word;
+  white-space: normal;
+  word-wrap: break-word;
+`
