c757380a6ed9abac2d095cbf8b661a45d7006deb
Daniel Keller <dkeller@tocco.ch>
2019-05-06 14:04:10 +0200

    chore: changelog script sub-packages support
    - improve changelog script to lookup dev dependencies of a package and
      collect changelog messages of them as well.

--- scripts/helpers.sh
+++ scripts/helpers.sh
@@ -1,3 +1,25 @@
+function transformPackageName() {
+  name=$1
+  if [ $name == "tocco-ui" ] || [ $name == "tocco-util" ] || [ $name == "tocco-theme" ];
+  then
+      echo "${1}"
+  else
+      echo "${name//"tocco-"/}"
+  fi
+}
+
+function getDevDependenciesGreps() {
+  dev_dependencies=$(json -f "packages/${package}/package.json" devDependencies | json -ka)
+  package_greps=""
+  for item in ${dev_dependencies//\\n/}
+  do
+    transformed_name=$(transformPackageName $item)
+    package_greps+=" --grep=${transformed_name}"
+  done
+
+  echo "${package_greps}"
+}
+
 function setColors(){
   color_blue=`tput setaf 4`
   color_green=`tput setaf 2`
@@ -9,7 +31,9 @@ function setGitVars() {
   current_branch=$(git rev-parse --abbrev-ref HEAD)
   last_release_tag=$(git describe --tags --match 'tocco-'${package}'@*' --abbrev=0 ${current_branch})
   last_version=$(echo ${last_release_tag} | awk -F '@' '{print $2}')
-  changelog=$(git log --pretty='%b' "${last_release_tag}"..HEAD --grep="${package}" --reverse | grep -E '^Changelog:' | awk '{gsub("Changelog:", "- ", $0); print}')
+
+  greps=$(getDevDependenciesGreps)
+  changelog=$(git log --pretty='%b' "${last_release_tag}"..HEAD --grep="${package}" ${greps} --reverse | grep -E '^Changelog:' | awk '{gsub("Changelog:", "- ", $0); print}')
 }
 
 function checkPackage() {
