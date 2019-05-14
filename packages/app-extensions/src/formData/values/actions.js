b9b27558c3768bbc6219be306dcff2937e99349f
Daniel Keller <dkeller@tocco.ch>
2019-05-14 09:44:47 +0200

    refactor(app-extensions): enhance formData
    - add touch action
    - bind isDirty and errors options

--- packages/app-extensions/src/formData/values/actions.js
+++ packages/app-extensions/src/formData/values/actions.js
@@ -1,4 +1,5 @@
 export const CHANGE_FIELD_VALUE = 'formData/CHANGE_FIELD_VALUE'
+export const TOUCH_FIELD = 'formData/TOUCH_FIELD'
 
 export const changeFieldValue = (formName, fieldName, value) => ({
   type: CHANGE_FIELD_VALUE,
@@ -6,5 +7,13 @@ export const changeFieldValue = (formName, fieldName, value) => ({
     formName,
     fieldName,
     value
+  }
+})
+
+export const touchField = (formName, fieldName) => ({
+  type: TOUCH_FIELD,
+  payload: {
+    formName,
+    fieldName
   }
 })
