8324dfc2cd3c4c6d881c1240672c4279c769b0a6
Cyril Khan <ckhan@tocco.ch>
2019-05-17 07:52:03 +0200

    refactor(tocco-ui): reduce related number stories to number
    - set propTypes allowNegative into options in NumberEdit

--- packages/tocco-ui/src/EditableValue/EditableValue.stories.js
+++ packages/tocco-ui/src/EditableValue/EditableValue.stories.js
@@ -144,59 +144,6 @@ storiesOf('Tocco-UI | EditableValue', module)
       />
   )
   .add(
-    'Decimal',
-    () =>
-      <EditableValueStory
-        type="decimal"
-        knobType={number}
-        options={object('options', {
-          minValue: -30000,
-          maxValue: 30000,
-          allowNegative: true,
-          decimalScale: 5
-        })}
-        defaultValue={1234.34}
-      />
-  ).add(
-    'Latitude',
-    () =>
-      <EditableValueStory
-        type="coordinate"
-        knobType={number}
-        options={object('options', {
-          minValue: -90,
-          maxValue: 90,
-          allowNegative: true
-        })}
-        defaultValue={45}
-      />
-  ).add(
-    'Longitude',
-    () =>
-      <EditableValueStory
-        type="coordinate"
-        knobType={number}
-        options={object('options', {
-          minValue: -180,
-          maxValue: 180,
-          allowNegative: true
-        })}
-        defaultValue={123}
-      />
-  ).add(
-    'Long',
-    () =>
-      <EditableValueStory
-        type="long"
-        knobType={number}
-        options={object('options', {
-          minValue: -300000,
-          maxValue: 300000,
-          allowNegative: true
-        })}
-        defaultValue={12345}
-      />
-  ).add(
     'Location',
     () =>
       <EditableValueStory
@@ -273,19 +220,6 @@ storiesOf('Tocco-UI | EditableValue', module)
         defaultValue={[{key: 2, display: 'Two v'}]}
       />
   ).add(
-    'MoneyAmount',
-    () =>
-      <EditableValueStory
-        type="moneyamount"
-        knobType={number}
-        options={object('options', {
-          prePointDigits: 8,
-          allowNegative: true,
-          decimalScale: 2
-        })}
-        defaultValue={1234.56}
-      />
-  ).add(
     'Number',
     () =>
       <EditableValueStory
@@ -300,36 +234,6 @@ storiesOf('Tocco-UI | EditableValue', module)
           allowNegative: true
         })}
         defaultValue={123.45}
-      />
-  ).add(
-    'Decimal',
-    () =>
-      <EditableValueStory
-        type="decimal"
-        knobType={number}
-        options={object('options', {
-          prePointDigits: 8,
-          postPointDigits: 2,
-          minValue: -30000,
-          maxValue: 30000,
-          allowNegative: true,
-          decimalScale: 5
-        })}
-        defaultValue={12345}
-      />
-  ).add(
-    'Long',
-    () =>
-      <EditableValueStory
-        type="long"
-        knobType={number}
-        options={object('options', {
-          minValue: -300000,
-          maxValue: 300000,
-          allowNegative: true,
-          decimalScale: 0
-        })}
-        defaultValue={12345}
       />
   ).add(
     'Phone',
