7b879bed5a40815de7acf47b723228e192dcc454
Niklaus Hug <nhug@tocco.ch>
2019-05-24 09:08:32 +0200

    fix(tocco-ui): harmonize fields height and spacing
    - Values for min-height and padding are magic numbers based on StyledEditableValue.

--- packages/tocco-ui/src/EditableValue/typeEditors/StyledTextEdit.js
+++ packages/tocco-ui/src/EditableValue/typeEditors/StyledTextEdit.js
@@ -6,6 +6,8 @@ import {StyledInputCss} from '../StyledEditableValue'
 const StyledTextareaAutosize = styled(TextareaAutosize)`
   && {
     ${StyledInputCss}
+    padding-top: 2px;
+    padding-bottom: 2px;
   }
 `
 
