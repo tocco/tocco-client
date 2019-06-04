edd47959b723dbd658edac15f469ab41db36aaca
Niklaus Hug <nhug@tocco.ch>
2019-06-04 13:31:27 +0200

    refactor(tocco-theme): move tocco-theme description
    - remove comments from defaultTheme
    - move description to repository tocco-docs

--- packages/tocco-theme/src/ToccoTheme/defaultTheme.js
+++ packages/tocco-theme/src/ToccoTheme/defaultTheme.js
@@ -1,7 +1,4 @@
 export default {
-  /* Paper, text and primary are the main colors. Whereas signal colors are only used to
-   * emphasize states. Paper must be a light color. Colors text and primary must have
-   * a high contrast to paper colors. */
   colors: {
     paper: '#fff',
     primary: '#9E2124',
@@ -25,42 +22,26 @@ export default {
       }
     }
   },
-  /* Use web safe fonts or load fonts from a web service.
-   * Declare font stack by variables monospace and regular (e.g. "font-family: [VARIABLE];").
-   * Load fonts from web font services (e.g. "@import url([VARIABLE]);". */
   fontFamily: {
     monospace: 'Menlo, Monaco, Consolas, "Courier New", monospace',
     regular: '"Helvetica Neue", Helvetica, Arial, sans-serif',
     url: ''
   },
-  /* All font sizes are calculated automatically as an exponential scale.
-   * Both values must be provided as unitless factors. Base is interpreted as
-   * rem, hence it inherits from websites root. */
   fontSize: {
     base: 1.4,
     factor: 1.3
   },
-  /* Change weights for extra light or extra bold typography but ensure that
-   * font files does provide such weights. */
   fontWeights: {
     regular: 400,
     bold: 700
   },
-  /* Line height is a factor applied on font size. */
   lineHeights: {
     dense: 1,
     regular: 1.4
   },
-  /* Radii is used to round corners of elements like panels, field and buttons. */
   radii: {
     regular: '4px'
   },
-  /* Base and factor are used to control vertical and horizontal white
-   * space rhythm. All spaces are calculated automatically as an exponential scale.
-   * Both values must be provided as unitless factors. Base is interpreted as
-   * rem, hence it inherits from websites root. It is recommended to use
-   * fontSice.base multiplied by lineHeights.regular for space.base and powers
-   * of for space.factor two to ensure alignment */
   space: {
     base: 2,
     factor: 2
