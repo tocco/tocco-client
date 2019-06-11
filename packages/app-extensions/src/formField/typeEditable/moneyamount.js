bf0ecf9ae5f3bbeaa372f4aa72c0fd4ee9041f94
Cyril Khan <ckhan@tocco.ch>
2019-06-11 14:19:06 +0200

    refactor(tocco-ui): set 2 decimalpoints to money component
    - add typeEditable moneyamount
    - map moneyamount to type moneyamount in editableValueFactory
    
    Refs: TOCDEV-470

---
+++ packages/app-extensions/src/formField/typeEditable/moneyamount.js
@@ -0,0 +1,14 @@
+import _get from 'lodash/get'
+
+export default {
+  getOptions: ({modelField}) => {
+    return {
+      postPointDigits: _get(modelField, 'validation.decimalDigits.postPointDigits', null),
+      prePointDigits: _get(modelField, 'validation.decimalDigits.prePointDigits', null),
+      minValue: _get(modelField, 'validation.numberRange.fromIncluding', null),
+      maxValue: _get(modelField, 'validation.numberRange.toIncluding', null),
+      allowNegative: true,
+      fixedDecimalScale: 2
+    }
+  }
+}
