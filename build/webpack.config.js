d1df65200bb0acce013619eaa3ee7a77826d8ba3
Daniel Keller <dkeller@tocco.ch>
2019-06-12 08:45:20 +0200

    chore: file loader in all modes (not only dev)

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -240,41 +240,41 @@ if (!__PROD__) {
     test: /\.scss$/,
     use: ['style-loader', 'css-loader', `sass-loader?data=$node-env:${config.env};&includePaths[]=${paths.client()}/packages/tocco-theme/node_modules/`]  // eslint-disable-line
   })
-
-  // File loaders
-  /* eslint-disable */
-  webpackConfig.module.rules.push(
-    {
-      test: /\.woff(\?.*)?$/,
-      use: 'file-loader?name=fonts/[name].[ext]&mimetype=application/font-woff'
-    },
-    {
-      test: /\.woff2(\?.*)?$/,
-      use: 'file-loader?name=fonts/[name].[ext]&mimetype=application/font-woff2'
-    },
-    {
-      test: /\.otf(\?.*)?$/,
-      use: 'file-loader?name=fonts/[name].[ext]&mimetype=font/opentype'
-    },
-    {
-      test: /\.ttf(\?.*)?$/,
-      use: 'file-loader?name=fonts/[name].[ext]&mimetype=application/octet-stream'
-    },
-    {
-      test: /\.eot(\?.*)?$/,
-      use: 'file-loader?name=fonts/[name].[ext]'
-    },
-    {
-      test: /\.svg(\?.*)?$/,
-      use: 'file-loader?name=fonts/[name].[ext]&mimetype=image/svg+xml'
-    },
-    {
-      test: /\.(png|jpg)$/,
-      use: 'file-loader?limit=8192'
-    }
-  )
-  /* eslint-enable */
 }
+
+// File loaders
+/* eslint-disable */
+webpackConfig.module.rules.push(
+  {
+    test: /\.woff(\?.*)?$/,
+    use: 'file-loader?name=fonts/[name].[ext]&mimetype=application/font-woff'
+  },
+  {
+    test: /\.woff2(\?.*)?$/,
+    use: 'file-loader?name=fonts/[name].[ext]&mimetype=application/font-woff2'
+  },
+  {
+    test: /\.otf(\?.*)?$/,
+    use: 'file-loader?name=fonts/[name].[ext]&mimetype=font/opentype'
+  },
+  {
+    test: /\.ttf(\?.*)?$/,
+    use: 'file-loader?name=fonts/[name].[ext]&mimetype=application/octet-stream'
+  },
+  {
+    test: /\.eot(\?.*)?$/,
+    use: 'file-loader?name=fonts/[name].[ext]'
+  },
+  {
+    test: /\.svg(\?.*)?$/,
+    use: 'file-loader?name=fonts/[name].[ext]&mimetype=image/svg+xml'
+  },
+  {
+    test: /\.(png|jpg)$/,
+    use: 'file-loader?limit=8192'
+  }
+)
+/* eslint-enable */
 
 const packageWebpackFile = packageDir + '/build/webpack.js'
 if (fs.existsSync(packageWebpackFile)) {
