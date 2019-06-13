834df7a0ca47d2a5b7ad05023f1eb9981c1caeba
Niklaus Hug <nhug@tocco.ch>
2019-06-13 19:10:18 +0200

    refactor(entity-list): remove bootstrap, use tocco-ui

---
+++ packages/entity-list/src/components/SearchForm/StyledSearchForm.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components'
+import {scale} from 'tocco-ui'
+import {StyledButton} from 'tocco-ui/src/Button'
+import {StyledButtonGroup} from 'tocco-ui/src/ButtonGroup'
+import {StyledLayoutBox} from 'tocco-ui/src/Layout'
+
+const StyledSearchForm = styled.div`
+  && {
+    ${StyledLayoutBox} {
+      padding-bottom: 0;
+    }
+  }
+`
+
+const StyledSearchFormButtonGroup = styled(StyledButtonGroup)`
+  && {
+    width: 100%;
+
+    ${StyledButton}:not(:last-child) {
+      margin-right: ${scale.space(-1)};
+    }
+  }
+`
+
+const StyledSearchFormButtonGroupGap = styled.div`
+  && {
+    flex-grow: 1;
+  }
+`
+
+export {
+  StyledSearchForm,
+  StyledSearchFormButtonGroup,
+  StyledSearchFormButtonGroupGap
+}
