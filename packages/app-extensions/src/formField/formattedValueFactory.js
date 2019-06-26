3f9a9dfe2ad88495d31bcdee9ef732035856ba73
Niklaus Hug <nhug@tocco.ch>
2019-06-26 16:56:53 +0200

    refactor(app-extensions): remove obsolete html tags
    Refs: WEB-85

--- packages/app-extensions/src/formField/formattedValueFactory.js
+++ packages/app-extensions/src/formField/formattedValueFactory.js
@@ -2,4 +2,4 @@ import React from 'react'
 import {FormattedValue} from 'tocco-ui'
 
 export default type => (formField, modelField, formName, value, info, events, formData) =>
-  <output><FormattedValue type={type} value={value}/></output>
+  <FormattedValue type={type} value={value}/>
