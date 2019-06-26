9717fad96778036a2901c565f2f564214eb50fd8
Niklaus Hug <nhug@tocco.ch>
2019-06-26 09:53:59 +0200

    refactor(tocco-ui): harmonize UploadProgress styles
    - replace bootstrap by styled-components
    - remove obsolete html tags
    - replace Font Awesome spinner with IconTocco
    - align correctly

---
+++ packages/tocco-ui/src/Upload/StyledUploadProgress.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components'
+
+import {declareFont, scale} from '../utilStyles'
+import {StyledSpan} from '../Typography'
+import {StyledIconToccoWrapper} from '../IconTocco'
+
+export const StyledUploadProgress = styled.div`
+  && {
+    padding-top: ${scale.space(-2)};
+    text-align: center;
+    display: inline-block;
+  }
+`
+
+export const StyledUploadProgressText = styled(StyledSpan)`
+  && {
+    ${declareFont({fontSize: scale.font(-1)})}
+  }
+`
+
+export const StyledUploadProgressIconAndText = styled.div`
+  && {
+    text-align: center;
+    display: block;
+
+    ${StyledIconToccoWrapper} {
+      display: inline-block;
+      height: ${scale.font(-1)};
+      width: ${scale.font(-1)};
+    }
+  }
+`
