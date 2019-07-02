338f5c11fbc8179221e1c25c907de3a81c8f86c7
Niklaus Hug <nhug@tocco.ch>
2019-07-02 14:40:46 +0200

    feat(scheduler): style matrix with styled-components
    - copy currently required css from bootstrap to StyledFullCalendar (mainly for typography and popover)
    - couple StyledFullCalendar with tocco-theme

--- packages/scheduler/src/components/FullCalendar/FullCalendar.spec.js
+++ packages/scheduler/src/components/FullCalendar/FullCalendar.spec.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {intlEnzyme} from 'tocco-test-util'
+import {intlEnzyme, TestThemeProvider} from 'tocco-test-util'
 import {Button, Menu, Typography} from 'tocco-ui'
 
 import FullCalendar from './FullCalendar'
@@ -10,7 +10,11 @@ describe('scheduler', () => {
       const baseProps = {onRefresh: () => {}}
 
       test('should render wrapping div with id and calendar div', () => {
-        const wrapper = intlEnzyme.mountWithIntl(<FullCalendar {...baseProps}/>)
+        const wrapper = intlEnzyme.mountWithIntl(
+          <TestThemeProvider>
+            <FullCalendar {...baseProps}/>
+          </TestThemeProvider>
+        )
         const menu = wrapper.find(Menu.Button)
         expect(menu).to.have.length(1)
         expect(menu.find(Button)).to.have.length(7)
@@ -32,30 +36,58 @@ describe('scheduler', () => {
       ]
 
       test('should show resources', () => {
-        const wrapper = intlEnzyme.mountWithIntl(<FullCalendar {...baseProps}/>)
+        const wrapper = intlEnzyme.mountWithIntl(
+          <TestThemeProvider>
+            <FullCalendar {...baseProps}/>
+          </TestThemeProvider>
+        )
         expect(wrapper.html()).to.not.have.string(mockResources[0].title)
-        wrapper.setProps({resources: mockResources})
+      })
+
+      test('should show mocked resources', () => {
+        const wrapper = intlEnzyme.mountWithIntl(
+          <TestThemeProvider>
+            <FullCalendar {...baseProps} resources={mockResources}/>
+          </TestThemeProvider>
+        )
         expect(wrapper.html()).to.have.string(mockResources[0].title)
       })
 
       test('should show events', () => {
-        const wrapper = intlEnzyme.mountWithIntl(<FullCalendar locale="de" {...baseProps} resources={mockResources} />)
+        const wrapper = intlEnzyme.mountWithIntl(
+          <TestThemeProvider>
+            <FullCalendar locale="de" {...baseProps} resources={mockResources} />
+          </TestThemeProvider>
+        )
         wrapper.setProps({events: mockEvents})
         // its not possible to check for updated events
       })
 
       const getMonth = locale => new Date().toLocaleString(locale, {month: 'long'})
 
-      test('should set locale on fullcalendar', () => {
-        const wrapper = intlEnzyme.mountWithIntl(<FullCalendar {...baseProps} locale="de"/>)
+      test('should set locale de on fullcalendar', () => {
+        const wrapper = intlEnzyme.mountWithIntl(
+          <TestThemeProvider>
+            <FullCalendar {...baseProps} locale="de"/>
+          </TestThemeProvider>
+        )
         expect(wrapper.html()).to.have.string(getMonth('de'))
-        wrapper.setProps({locale: 'fr'})
+      })
+
+      test('should set locale fr on fullcalendar', () => {
+        const wrapper = intlEnzyme.mountWithIntl(
+          <TestThemeProvider>
+            <FullCalendar {...baseProps} locale="fr"/>
+          </TestThemeProvider>
+        )
         expect(wrapper.html()).to.have.string(getMonth('fr'))
       })
 
       test('should render resources', () => {
         const wrapper = intlEnzyme.mountWithIntl(
-          <FullCalendar onCalendarRemove={() => {}} {...baseProps} resources={mockResources} />
+          <TestThemeProvider>
+            <FullCalendar onCalendarRemove={() => {}} {...baseProps} resources={mockResources} />
+          </TestThemeProvider>
         )
         expect(wrapper.first().html()).to.have.string('remove-resource-btn')
       })
