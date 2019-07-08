566e6fae5d4dcacfadbcb7858514914e858141cf
Cyril Khan <ckhan@tocco.ch>
2019-07-08 07:57:36 +0200

    refactor(tocco-client): remove lazyComponent
    - add React.lazy to load react-quill in HtmlEdit
    - add mounted flag to async loading Icon to prevent noop memory leak
    
    Refs: TOCDEV-406

--- packages/tocco-ui/src/EditableValue/typeEditors/HtmlEdit.js
+++ packages/tocco-ui/src/EditableValue/typeEditors/HtmlEdit.js
@@ -1,8 +1,7 @@
 import PropTypes from 'prop-types'
-import React from 'react'
+import React, {lazy, Suspense} from 'react'
 import styled from 'styled-components'
 
-import lazyComponent from '../../util/lazyComponent'
 import {declareTypograhpy} from '../../Typography'
 
 const StyledHtmlEdit = styled.div`
@@ -30,7 +29,7 @@ class HtmlEdit extends React.Component {
     import(/* webpackChunkName: "quill" */ '!style-loader!css-loader!react-quill/dist/quill.snow.css')
     import(/* webpackChunkName: "quill" */ '!style-loader!css-loader!react-quill/dist/quill.core.css')
 
-    this.lazyQuill = lazyComponent(() => import(/* webpackChunkName: "quill" */ 'react-quill'), 'default')
+    this.lazyQuill = lazy(() => import(/* webpackChunkName: "quill" */ 'react-quill'))
   }
 
   handleChange = value => {
@@ -42,15 +41,17 @@ class HtmlEdit extends React.Component {
   render() {
     return (
       <StyledHtmlEdit>
-        <this.lazyQuill
-          name={this.props.name}
-          onChange={this.handleChange}
-          id={this.props.id}
-          theme="snow"
-          value={this.props.value}
-          readOnly={this.props.immutable}
-          modules={{toolbar: !this.props.immutable}}
-        />
+        <Suspense fallback={<i/>}>
+          <this.lazyQuill
+            name={this.props.name}
+            onChange={this.handleChange}
+            id={this.props.id}
+            theme="snow"
+            value={this.props.value}
+            readOnly={this.props.immutable}
+            modules={{toolbar: !this.props.immutable}}
+          />
+        </Suspense>
       </StyledHtmlEdit>
     )
   }
