5b50e846005bc5d6bec8a9c3bbc2d5f649e16a92
Cyril Khan <ckhan@tocco.ch>
2019-07-16 11:00:58 +0200

    feat(sso-login): add encodedWindowUrl to sso url in ProviderButton
    - The encodedWindowUrl as parameter is needed for the registration
      part in the sso-login process. From now on login and registrations
      are possible through sso.
    
    Refs: TOCDEV-611

--- packages/sso-login/src/components/ProviderButton/ProviderButton.js
+++ packages/sso-login/src/components/ProviderButton/ProviderButton.js
@@ -8,7 +8,8 @@ import StyledProviderButton from './StyledProviderButton'
 const ProviderButton = ({provider, loginCompleted, loginEndpoint}) => {
   const clickHandler = () => {
     const baseUrl = __DEV__ ? '' : __BACKEND_URL__
-    const url = `${baseUrl}${loginEndpoint}?provider=${provider.unique_id}`
+    const encodedWindowUrl = encodeURIComponent(window.location.href)
+    const url = `${baseUrl}${loginEndpoint}?provider=${provider.unique_id}&sourceUri=${encodedWindowUrl}`
 
     const popUp = window.open(url, provider.label, getPopUpFeatures(650, 500))
 
