33afea5746b1be956e985a26536827b90babfc72
Daniel Keller <dkeller@tocco.ch>
2019-07-18 17:08:49 +0200

    feat(entity-detail): add defaultValue App param
    - this for example allows to pre set a relation if the create view is
      opened from a relation list.

--- packages/entity-detail/src/modules/entityDetail/sagas.js
+++ packages/entity-detail/src/modules/entityDetail/sagas.js
@@ -73,19 +73,22 @@ export function* unloadDetailView() {
 export function* loadEntityModel(entityName) {
   const entityModel = yield call(fetchModel, entityName)
   yield put(actions.setEntityModel(entityModel))
+  return entityModel
 }
 
 export function* loadDetailView() {
-  const {entityName, formName, mode} = yield select(entityDetailSelector)
+  const {entityName, formName, mode, defaultValues} = yield select(entityDetailSelector)
 
-  yield call(loadEntityModel, entityName)
+  const model = yield call(loadEntityModel, entityName)
   const formDefinition = yield call(loadDetailFormDefinition, formName)
 
   if (mode === modes.CREATE) {
     yield put(actions.setEntity({paths: {}, model: entityName}))
     const fieldDefinitions = yield call(form.getFieldDefinitions, formDefinition)
-    const defaultValues = yield call(form.getDefaultValues, fieldDefinitions)
-    yield put(formActions.initialize(FORM_ID, defaultValues))
+    const formDefaultValues = yield call(form.getDefaultValues, fieldDefinitions)
+    const inputDefaultValues = yield call(form.loadDisplaysOfRelationFields, defaultValues, model)
+
+    yield put(formActions.initialize(FORM_ID, {...formDefaultValues, ...inputDefaultValues}))
   } else {
     yield call(loadData)
   }
