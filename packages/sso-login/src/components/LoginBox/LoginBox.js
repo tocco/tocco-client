38e8279d546b884b4ea38ff4e996a7bfdaa71b46
Adrian Schmid <aschmid@tocco.ch>
2019-08-09 10:26:35 +0200

    feat(sso-login): add automatic initialisation of login windows
    add possibility to pass an optional "autoLogin" parameter. if this parameter
    is set, the login process is automatically started as soon as the providers
    are loaded.

--- packages/sso-login/src/components/LoginBox/LoginBox.js
+++ packages/sso-login/src/components/LoginBox/LoginBox.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import {openLoginWindow} from '../../utils/loginWindow'
 import ProviderButton from '../ProviderButton/ProviderButton'
 
 class LoginBox extends React.Component {
   constructor(props) {
     super(props)
     props.loadProviders()
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (this.props.autoLogin && prevProps.providers.length === 0 && this.props.providers.length > 0) {
+      const autoLoginProvider = this.props.providers.find(entity => entity.unique_id === this.props.autoLogin)
+      if (autoLoginProvider) {
+        openLoginWindow(this.props.loginEndpoint, this.props.loginCompleted, autoLoginProvider)
+      }
+    }
   }
 
   render() {
@@ -26,7 +36,8 @@ LoginBox.propTypes = {
   loginEndpoint: PropTypes.string,
   loginCompleted: PropTypes.func.isRequired,
   loadProviders: PropTypes.func.isRequired,
-  providers: PropTypes.array
+  providers: PropTypes.array,
+  autoLogin: PropTypes.string
 }
 
 export default LoginBox
