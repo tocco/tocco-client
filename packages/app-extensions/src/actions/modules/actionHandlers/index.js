146a3ec6ee68e2c5b5422bd96fff28bab1ad4ee3
Daniel Keller <dkeller@tocco.ch>
2019-08-16 10:15:53 +0200

    feat(app-extension-detail): add custom action handler

--- packages/app-extensions/src/actions/modules/actionHandlers/index.js
+++ packages/app-extensions/src/actions/modules/actionHandlers/index.js
@@ -1,8 +1,10 @@
 import actionTypes from '../../actionTypes'
 import handleSimpleAction from './simpleAction'
 import handleReportAction from './report'
+import handleCustomAction from './customAction'
 
 export default {
   [actionTypes.SIMPLE]: handleSimpleAction,
-  [actionTypes.REPORT]: handleReportAction
+  [actionTypes.REPORT]: handleReportAction,
+  [actionTypes.CUSTOM]: handleCustomAction
 }
