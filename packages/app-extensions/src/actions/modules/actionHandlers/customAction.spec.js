eab2b909d66386564fc90da9c72ed16d6a53bb2d
Daniel Keller <dkeller@tocco.ch>
2019-08-16 15:57:07 +0200

    test(app-extensions): adjust custom action test

--- packages/app-extensions/src/actions/modules/actionHandlers/customAction.spec.js
+++ packages/app-extensions/src/actions/modules/actionHandlers/customAction.spec.js
@@ -10,26 +10,24 @@ describe('app-extensions', () => {
           test('should call custom action from config', () => {
             const definition = {id: 'new'}
             const newActionSpy = sinon.spy()
+            const entity = {}
+            const selection = {}
+            const parent = {}
+            const params = {}
             const config = {
               customActions: {
                 'new': newActionSpy
               }
             }
-            return expectSaga(customActionHandler, definition, {}, {}, {}, {}, config)
-              .call(newActionSpy)
+            return expectSaga(customActionHandler, definition, entity, selection, parent, params, config)
+              .call(newActionSpy, definition, entity, selection, parent, params, config)
               .run()
           })
 
-          test('should call custom action from config', () => {
-            const definition = {id: 'new'}
-            const newActionSpy = sinon.spy()
-            const config = {
-              customActions: {
-                'new': newActionSpy
-              }
-            }
+          test('should run without exception if custom action type is not configured', () => {
+            const definition = {id: 'something'}
+            const config = {}
             return expectSaga(customActionHandler, definition, {}, {}, {}, {}, config)
-              .call(newActionSpy)
               .run()
           })
         })
