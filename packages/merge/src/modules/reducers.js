7f91bd6ede3f60a972663fee2cb42ebb8782dd5b
Daniel Keller <dkeller@tocco.ch>
2019-08-27 11:14:11 +0200

    chore: curly spacing lint rule
    - will be investigated later. but is needed as a fast fix. otherwise
      with the updated libraries  there are 6000 linting errors. This is to much for
      the heap.

--- packages/merge/src/modules/reducers.js
+++ packages/merge/src/modules/reducers.js
@@ -1,6 +1,6 @@
 import mergeStrategy from './mergeStrategy/'
-import wizard, { sagas as wizardSagas } from './wizard/'
-import mergeMatrix, { sagas as mergeMatrixSagas } from './mergeMatrix/'
+import wizard, {sagas as wizardSagas} from './wizard/'
+import mergeMatrix, {sagas as mergeMatrixSagas} from './mergeMatrix/'
 
 export default {
   mergeMatrix,
