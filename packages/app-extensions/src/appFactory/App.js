71c563ecb37b9edf24410efb78c1068dd08d819e
Daniel Keller <dkeller@tocco.ch>
2019-09-04 11:44:41 +0200

    refactor(app-extensions): fix wrong propType

--- packages/app-extensions/src/appFactory/App.js
+++ packages/app-extensions/src/appFactory/App.js
@@ -24,7 +24,7 @@ const App = ({store, initIntlPromise, name, content, theme}) =>
 
 App.propTypes = {
   store: PropTypes.object.isRequired,
-  initIntlPromise: PropTypes.func.isRequired,
+  initIntlPromise: PropTypes.object.isRequired,
   name: PropTypes.string.isRequired,
   content: PropTypes.node.isRequired,
   theme: PropTypes.object
