1793c2a18e6286259a4e9ce2a61e36ec4105dd34
Daniel Keller <dkeller@tocco.ch>
2019-09-06 09:22:27 +0200

    feat(admin): add detail relation view query param support

--- packages/admin/src/routes/entities/index.js
+++ packages/admin/src/routes/entities/index.js
@@ -1,10 +1,15 @@
 import {route, js} from 'tocco-util'
 import React, {memo} from 'react'
 import {ReactReduxContext} from 'react-redux'
+import _omit from 'lodash/omit'
+import _omitBy from 'lodash/omitBy'
+
+const ignoredAttributes = ['location.key', 'location.search']
 
 function areEqual(prevProps, nextProps) {
   const diff = js.difference(prevProps, nextProps)
-  return Object.entries(diff).length === 0
+  const clean = _omitBy(_omit(diff, ignoredAttributes), v => Object.keys(v).length === 0)
+  return Object.entries(clean).length === 0
 }
 
 export default memo(
