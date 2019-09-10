a4c44e0247384ffbbce6fd1ef8e382ec069dc0c5
Daniel Keller <dkeller@tocco.ch>
2019-09-10 16:39:12 +0200

    refactor(tocco-ui): improve formattedValue
    - Single select accpets array as well to make it less error-prone
    - add new mappings
     - Map Integer with String
    - unite mappings
    - remove formatted Values with encapsulated value objects (coordinate and login)
    
    Refs: TOCDEV-885

--- packages/tocco-ui/src/FormattedValue/typeFormatters/SingleSelectFormatter.js
+++ packages/tocco-ui/src/FormattedValue/typeFormatters/SingleSelectFormatter.js
@@ -1,19 +1,27 @@
 import PropTypes from 'prop-types'
 import React from 'react'
+import {js} from 'tocco-util'
 
 import Typography from '../../Typography'
 
-const SingleSelectFormatter = ({value, options = {}}) => {
+const SingleSelectFormatter = ({value, options}) => {
+  value = js.getOrFirst(value)
+
   const display = <Typography.Span>{value.display}</Typography.Span>
 
-  return options.linkFactory ? options.linkFactory(value.key, display) : display
+  return options && options.linkFactory ? options.linkFactory(value.key, display) : display
 }
 
+const valuePropType = PropTypes.shape({
+  display: PropTypes.string,
+  key: PropTypes.string
+})
+
 SingleSelectFormatter.propTypes = {
-  value: PropTypes.shape({
-    display: PropTypes.string,
-    key: PropTypes.string
-  }),
+  value: PropTypes.oneOfType([
+    valuePropType,
+    PropTypes.arrayOf(valuePropType)
+  ]),
   options: PropTypes.shape({
     linkFactory: PropTypes.func
   })
