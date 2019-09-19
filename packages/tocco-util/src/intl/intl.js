21c073e3f02a5d936cfe174bb09f1f1954954338
Daniel Keller <dkeller@tocco.ch>
2019-09-19 10:52:22 +0200

    refactor(tocco-util): refactor intl
    - use async/await
    - cache values
    - store text resources individual so they have to be fetched only once

--- packages/tocco-util/src/intl/intl.js
+++ packages/tocco-util/src/intl/intl.js
@@ -1,20 +1,24 @@
 import {updateIntl} from 'react-intl-redux'
 
-export const initIntl = (store, modules, locale) => {
-  const loadLocale = locale
-    ? Promise.resolve(locale)
-    : getUserInfo().then(userInfo => Promise.resolve(userInfo.locale))
+import cache from '../cache'
 
-  return loadLocale.then(locale => setLocale(store, modules, locale))
+export const initIntl = async(store, modules, forcedLocale) => {
+  let locale = forcedLocale
+  if (!locale) {
+    locale = await getUserLocale()
+  }
+  await setLocale(store, modules, locale)
+  return true
 }
 
-export const setLocale = (store, modules, locale) => (
-  loadTextResources(locale, modules)
-    .then(textResources => store.dispatch(updateIntl({
-      locale: locale.replace('_', '-'),
-      messages: textResources
-    })))
-)
+export const setLocale = async(store, modules, locale) => {
+  const textResources = await loadTextResources(locale, modules)
+
+  store.dispatch(updateIntl({
+    locale: locale.replace('_', '-'),
+    messages: textResources
+  }))
+}
 
 const fetchOptions = {
   method: 'GET',
@@ -24,16 +28,57 @@ const fetchOptions = {
   credentials: 'include'
 }
 
-const getUserInfo = () => (
-  fetch(`${__BACKEND_URL__}/nice2/username`, fetchOptions)
-    .then(response => response.json())
-)
+export const getUserLocale = async() => {
+  const cachedUserLocale = cache.get('user', 'locale')
 
-const loadTextResources = (locale, modules) => {
+  if (cachedUserLocale) {
+    return cachedUserLocale
+  }
+
+  const response = await fetch(`${__BACKEND_URL__}/nice2/username`, fetchOptions)
+  const userInfo = await response.json()
+  const {locale} = userInfo
+  cache.add('user', 'locale', locale)
+  return locale
+}
+
+export const loadTextResources = async(locale, modules) => {
+  let result = {}
+  const notLoadedModules = []
+
+  modules.forEach(module => {
+    const cachedModuleResource = cache.get('textResource', module)
+    if (cachedModuleResource) {
+      result = {...result, ...cachedModuleResource}
+    } else {
+      notLoadedModules.push(module)
+    }
+  })
+
+  if (notLoadedModules.length > 0) {
+    const resources = await fetchTextResources(locale, notLoadedModules)
+
+    notLoadedModules.forEach(module => {
+      const filtered = Object.keys(resources)
+        .filter(key => key.includes(module))
+        .reduce((obj, key) => {
+          obj[key] = resources[key]
+          return obj
+        }, {})
+
+      cache.add('textResource', module, filtered)
+      result = {...result, ...filtered}
+    })
+  }
+
+  return result
+}
+
+const fetchTextResources = async(locale, modules) => {
   const moduleRegex = `(${modules.join('|')})`
   const moduleParam = moduleRegex ? `&module=${moduleRegex}` : ''
   const url = `${__BACKEND_URL__}/nice2/textresource?locale=${locale}${moduleParam}`
 
-  return fetch(url, fetchOptions)
-    .then(response => response.json())
+  const response = await fetch(url, fetchOptions)
+  return response.json()
 }
