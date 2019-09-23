f519324e025511a22715afdbc6a18f9666d489cb
Daniel Keller <dkeller@tocco.ch>
2019-09-23 14:03:38 +0200

    refactor(tocco-ui): add icon showcase story

--- packages/tocco-ui/src/Icon/Icon.stories.js
+++ packages/tocco-ui/src/Icon/Icon.stories.js
@@ -1,50 +1,24 @@
 import React from 'react'
 import {storiesOf} from '@storybook/react'
-import {select, withKnobs} from '@storybook/addon-knobs'
+import {withKnobs} from '@storybook/addon-knobs'
 
 import {Icon} from './Icon'
+import IconsShowcase from './IconShowcase'
 
 storiesOf('Tocco-UI | Icon', module)
   .addDecorator(withKnobs)
   .add(
-    'Icon Cards',
+    'Icon Examples',
     () => (
       <div>
-        <Icon icon="address-card" />
-        <Icon icon="far, address-card"/>
-        <Icon icon="fab, microsoft"/>
+        <Icon icon="lemon" style={{color: '#CCCC00', fontSize: '50px'}}/>
+        <Icon icon="balance-scale-left" style={{fontSize: '5rem'}}/>
+        <Icon icon="microsoft" style={{color: 'blue', fontSize: '50px'}}/>
+        <Icon icon="tocco" style={{color: '#C01E30', fontSize: '80px'}}/>
       </div>
     )
   )
   .add(
-    'Icon Append',
-    () => (
-      <div>
-        Appended Icon <Icon icon={select('Icon', ['user', 'envelope']) || 'user'} position="append" />
-      </div>
-    )
-  )
-  .add(
-    'Icon Prepend',
-    () => (
-      <div>
-        <Icon icon={select('Icon', ['user', 'envelope']) || 'user'} position="prepend"/> Prepended Icon
-      </div>
-    )
-  )
-  .add(
-    'Icon Between',
-    () => (
-      <div>
-        Icon between <Icon icon={select('Icon', ['user', 'envelope']) || 'user'} position="between"/> Text
-      </div>
-    )
-  )
-  .add(
-    'Icon Sole',
-    () => (
-      <div>
-        Icon Dense <Icon icon={select('Icon', ['user', 'envelope']) || 'user'} position="sole"/>
-      </div>
-    )
+    'Icons',
+    () => <IconsShowcase/>
   )
