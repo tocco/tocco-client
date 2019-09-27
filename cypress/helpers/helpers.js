1590ca79b31cf29b6e29cd3a3a0fcb346ba50f65
Daniel Keller <dkeller@tocco.ch>
2019-09-27 17:21:59 +0200

    chore: support of local cypress execution

--- cypress/helpers/helpers.js
+++ cypress/helpers/helpers.js
@@ -1,8 +1,14 @@
 const convert = s => s.replace(/ /g, '-').toLowerCase()
 
+const githubPages = 'https://tocco.github.io/tocco-client'
+
 const getStoryUrl = (chapter, story) => {
   const prBranch = Cypress.env('PR_BRANCH')
-  const deployUrl = `https://tocco.github.io/tocco-client/${prBranch ? prBranch.replace('/', '_') : 'master'}`
+
+  const deployUrl = Cypress.config().baseUrl
+    ? Cypress.config().baseUrl
+    : githubPages + `/${prBranch ? prBranch.replace('/', '_') : 'master'}`
+
   return `${deployUrl}/iframe.html?id=${chapter.map(c => convert(c)).join('-')}--${convert(story)}#/`
 }
 
