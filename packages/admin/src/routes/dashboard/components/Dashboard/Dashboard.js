a891da69a64f46d86be9f379b29f6203aaae0c7d
Daniel Keller <dkeller@tocco.ch>
2019-10-01 16:55:31 +0200

    refactor(admin): show version in dashboard

--- packages/admin/src/routes/dashboard/components/Dashboard/Dashboard.js
+++ packages/admin/src/routes/dashboard/components/Dashboard/Dashboard.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {Typography} from 'tocco-ui'
+import {Typography, Icon} from 'tocco-ui'
+import styled from 'styled-components'
+
+const StyledDashBoard = styled.div`
+  display: flex;
+  justify-content: center;
+  
+  div { 
+      padding:  15px;
+  }
+`
 
 const Dashboard = props => {
-  return (
-    <div>
-      <Typography.H1>Home / Dashboard</Typography.H1>
-      <Typography.Span>{JSON.stringify(props.match)}</Typography.Span>
-    </div>
-  )
+  const packageJson = require('../../../../../package')
+  return <StyledDashBoard>
+    <div><Icon style={{fontSize: '80px'}} icon="tocco"/></div>
+    <Typography.H1>Welcome to beta v{packageJson.version}</Typography.H1>
+  </StyledDashBoard>
 }
 
 Dashboard.propTypes = {
