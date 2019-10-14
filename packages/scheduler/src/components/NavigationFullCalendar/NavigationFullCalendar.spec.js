b86039b3ae05a73676323bd367548852ccb4672b
Daniel Keller <dkeller@tocco.ch>
2019-10-14 14:53:44 +0200

    refactor(scheduler): add date picker, rearrange buttons
    Changelog: Add date picker

--- packages/scheduler/src/components/NavigationFullCalendar/NavigationFullCalendar.spec.js
+++ packages/scheduler/src/components/NavigationFullCalendar/NavigationFullCalendar.spec.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {intlEnzyme} from 'tocco-test-util'
-import {Button, Typography} from 'tocco-ui'
+import {Button} from 'tocco-ui'
 
 import NavigationFullCalendar, {getButtonInkProps} from './NavigationFullCalendar'
 
@@ -19,21 +19,22 @@ describe('scheduler', () => {
           const chooseNext = sinon.spy()
           const choosePrev = sinon.spy()
           const chooseToday = sinon.spy()
+          const goToDate = sinon.spy()
           const refresh = sinon.spy()
           const mockProps = {
-            changeRange: changeRange,
-            changeView: changeView,
-            chooseNext: chooseNext,
-            choosePrev: choosePrev,
-            chooseToday: chooseToday,
+            changeRange,
+            changeView,
+            chooseNext,
+            choosePrev,
+            chooseToday,
+            goToDate,
             isLoading: false,
-            refresh: refresh,
+            refresh,
             title: 'title text',
             type: 'timelineWeek'
           }
 
           const wrapper = intlEnzyme.mountWithIntl(<NavigationFullCalendar {...mockProps} />)
-          expect(wrapper.find(Typography.H3).text()).to.equal('title text')
           expect(wrapper.find(Button)).to.have.length(7)
           wrapper.find('button').map(button => button.simulate('click'))
           expect(changeRange).to.have.property('callCount', 3)
@@ -42,34 +43,6 @@ describe('scheduler', () => {
           expect(choosePrev).to.have.property('callCount', 1)
           expect(chooseToday).to.have.property('callCount', 1)
           expect(refresh).to.have.property('callCount', 1)
-        })
-
-        test('should signal states', () => {
-          const mockProps = {
-            changeRange: () => {},
-            changeView: () => {},
-            chooseNext: () => {},
-            choosePrev: () => {},
-            chooseToday: () => {},
-            refresh: () => {},
-            title: ''
-          }
-
-          let wrapper = intlEnzyme.mountWithIntl(
-            <NavigationFullCalendar {...mockProps} isLoading={false} type="timelineWeek" />)
-          let buttons = wrapper.find(Button)
-          expect(buttons.at(3).prop('icon')).to.equal('sync')
-          expect(buttons.at(3).prop('pending')).to.be.false
-          expect(buttons.at(5).prop('ink')).to.equal('primary')
-          expect(buttons.at(6).prop('ink')).to.be.undefined
-
-          wrapper = intlEnzyme.mountWithIntl(
-            <NavigationFullCalendar {...mockProps} isLoading={true} type="timelineMonth" />)
-          buttons = wrapper.find(Button)
-          expect(buttons.at(3).prop('icon')).to.equal('')
-          expect(buttons.at(3).prop('pending')).to.be.true
-          expect(buttons.at(5).prop('ink')).to.be.undefined
-          expect(buttons.at(6).prop('ink')).to.equal('primary')
         })
       })
     })
