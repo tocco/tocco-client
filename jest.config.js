bfe4b9d803965c8e88c8f34ffb8b224554064eb7
Daniel Keller <dkeller@tocco.ch>
2019-12-03 11:56:18 +0100

    chore: jest remove project config
    - this will prevent the test from running twice

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,6 @@
 const base = require('./build/jest.config.base.js')
 
 module.exports = {
-  projects: [
-    '<rootDir>/packages/*'
-  ],
   rootDir: './',
   ...base,
   coverageReporters: ['json', 'lcov']
