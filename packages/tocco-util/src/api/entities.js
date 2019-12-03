1baeb98308b64d77c85622dc8cbe91db29683258
Daniel Keller <dkeller@tocco.ch>
2019-12-03 11:40:12 +0100

    refactor(tocco-util): add api helpers

--- packages/tocco-util/src/api/entities.js
+++ packages/tocco-util/src/api/entities.js
@@ -1,27 +1,97 @@
 import _pick from 'lodash/pick'
 import _get from 'lodash/get'
+import _set from 'lodash/set'
+import _reduce from 'lodash/reduce'
+import _isObject from 'lodash/isObject'
 
-const relevantRelationAttributes = ['key', 'display', 'model']
-export const typeValueExtractor = {
-  'display-expression': value => value,
-  'entity': value => value ? _pick(value, relevantRelationAttributes) : null,
-  'entity-list': value => value.length > 0
-    ? value.map(childValue => _pick(childValue, relevantRelationAttributes)) : null,
-  'multi': value => value.length > 0
-    ? {multi: true, values: value.map(childValue => typeValueExtractor[childValue.type](childValue.value))} : null,
-  'field': value => {
-    if (value.value === null) {
-      return null
+const metaFields = {
+  KEY: '__key',
+  MODEL: '__model',
+  VERSION: '__version'
+}
+
+export const relationFieldTypes = ['single-select-box', 'multi-select-box', 'single-remote-field', 'multi-remote-field']
+export const relevantRelationAttributes = ['key', 'display', 'model', 'version']
+
+const typeValueExtractor = (type, value) => {
+  switch (type) {
+    case 'entity':
+      return value ? _pick(value, relevantRelationAttributes) : null
+    case 'entity-list':
+      return value.length > 0 ? value.map(childValue => _pick(childValue, relevantRelationAttributes)) : null
+    case 'login':
+      return _get(value, 'username')
+    case 'longitude':
+    case 'latitude':
+      return _get(value, 'value')
+    default:
+      return value
+  }
+}
+
+/**
+ * Transforms a entity object into a flatten structure wil only one hierarchy of properties.
+ * @param {object} entity - Entity as retrieved by the rest endpoint.
+ */
+export const getFlattenEntity = entity => ({
+  [metaFields.KEY]: entity.key,
+  [metaFields.MODEL]: entity.model,
+  [metaFields.VERSION]: entity.version,
+  ...flattenPath(entity.paths)
+})
+
+const flattenPath = (paths, currentPath = []) =>
+  _reduce(
+    paths,
+    (acc, path, pathId) => {
+      let subPaths = {}
+      if (path.value) {
+        if (Array.isArray(path.value)) {
+          subPaths = path.value.reduce((acc, v) => {
+            const flatten = flattenPath(v.paths, [...currentPath, pathId])
+            const combinedArrayVales = _reduce(flatten, (acc2, val, key) =>
+              ({...acc2, [key]: [...(acc[key] || []), val]}), {})
+            return {...acc, ...combinedArrayVales}
+          }, {})
+        } else {
+          subPaths = flattenPath(path.value.paths, [...currentPath, pathId])
+        }
+      }
+
+      return {...acc, [[...currentPath, pathId].join('.')]: typeValueExtractor(path.type, path.value), ...subPaths}
+    },
+    {}
+  )
+
+/**
+ * Transforms a flatten entity back to a api compatible entity object. Only regards fields that are marked as dirty.
+ * @param {object} flattenEntity - Entity that was prior transformed with 'getFlattenEntity'
+ * @param {array} dirtyFields - List of dirty field names (paths)
+ */
+export const toEntity = (flattenEntity, dirtyFields) => {
+  const ignoredPaths = fieldName => (Object.values(metaFields).includes(fieldName))
+      || !(dirtyFields && dirtyFields.some(dirtyField => dirtyField.startsWith(fieldName)))
+
+  const valueSimplifier = value => {
+    if (Array.isArray(value)) {
+      return value.map(v => valueSimplifier(v))
     }
 
-    switch (value.type) {
-      case 'login':
-        return _get(value, 'value.username')
-      case 'longitude':
-      case 'latitude':
-        return _get(value, 'value.value')
-      default:
-        return value.value
+    return _isObject(value) ? _pick(value, ['key', 'version']) : value
+  }
+
+  const paths = Object.keys(flattenEntity).reduce((acc, path) => {
+    if (!ignoredPaths(path)) {
+      const t = path.split('.').join('.paths.')
+      return _set(acc, t, valueSimplifier(flattenEntity[path]))
     }
+    return acc
+  }, {})
+
+  return {
+    model: flattenEntity[metaFields.MODEL],
+    key: flattenEntity[metaFields.KEY],
+    version: flattenEntity[metaFields.VERSION],
+    paths
   }
 }
--- packages/tocco-util/src/api/entities.spec.js
+++ packages/tocco-util/src/api/entities.spec.js
@@ -1,44 +1,282 @@
-import {typeValueExtractor} from './entities'
+import {getFlattenEntity, toEntity} from './entities'
 
 describe('tocco-util', () => {
   describe('api', () => {
-    describe('typeValueExtractor', () => {
-      test('should handle display epressions', () => {
-        const value = '<h1>Test</h1>'
-        const result = typeValueExtractor['display-expression'](value)
-        expect(result).to.eql(value)
+    describe('getEntityObject', () => {
+      test('should set key, model and version', () => {
+        const entity = {
+          version: 1,
+          key: '23',
+          model: 'User'
+        }
+
+        const result = getFlattenEntity(entity)
+
+        expect(result.__key).to.eql('23')
+        expect(result.__version).to.eql(1)
+        expect(result.__model).to.eql('User')
       })
 
-      test('should handle entity', () => {
-        const value = {key: '1', display: 'Test', link: 'test', model: 'User'}
-        const result = typeValueExtractor.entity(value)
-        const expectedResult = {key: '1', display: 'Test', model: 'User'}
-        expect(result).to.eql(expectedResult)
+      test('should set simple paths', () => {
+        const entity = {
+          paths: {
+            firstname: {
+              type: 'string',
+              writable: null,
+              value: 'Peter'
+            },
+            birthdate: {
+              type: 'birthdate',
+              writable: null,
+              value: '2019-11-27'
+            }
+          }
+        }
+
+        const result = getFlattenEntity(entity)
+
+        expect(result).to.have.property('firstname', 'Peter')
+        expect(result).to.have.property('birthdate', '2019-11-27')
       })
 
-      test('should handle entity-list', () => {
-        const value = [{key: '1', display: 'Test', link: 'test', model: 'User'},
-          {key: '71', display: 'Test 4', link: 'test', model: 'User'}]
-        const result = typeValueExtractor['entity-list'](value)
-        const expectedResult = [{key: '1', display: 'Test', model: 'User'},
-          {key: '71', display: 'Test 4', model: 'User'}]
-        expect(result).to.eql(expectedResult)
+      test('should set to 1 relations', () => {
+        const entity = {
+          paths: {
+            relGender: {
+              type: 'entity',
+              writable: null,
+              value: {
+                _links: null,
+                key: '1',
+                model: 'Gender',
+                version: 3,
+                paths: {}
+              }
+            }
+          }
+        }
+
+        const result = getFlattenEntity(entity)
+
+        expect(result.relGender).to.eql({key: '1', model: 'Gender', version: 3})
       })
 
-      test('should extract values of special field types', () => {
-        const value = {type: 'login', value: {username: 'dake'}}
-        const result = typeValueExtractor.field(value)
-        expect(result).to.eql('dake')
+      test('should set to N relations', () => {
+        const entity = {
+          paths: {
+            relUser_code1: {
+              type: 'entity-list',
+              writable: null,
+              value: [
+                {
+                  _links: null,
+                  key: '1',
+                  model: 'User_code1',
+                  version: 1,
+                  paths: {}
+                },
+                {
+                  _links: null,
+                  key: '301',
+                  model: 'User_code1',
+                  version: 0,
+                  paths: {}
+                }
+              ]
+            }
+          }
+        }
+
+        const result = getFlattenEntity(entity)
+
+        const expectedValue = [
+          {key: '1', model: 'User_code1', version: 1},
+          {key: '301', model: 'User_code1', version: 0}
+        ]
+
+        expect(result.relUser_code1).to.deep.eql(expectedValue)
       })
 
-      test('should return null if value is null', () => {
-        let value = {type: 'login', value: null}
-        let result = typeValueExtractor.field(value)
-        expect(result).to.eql(null)
+      test('should handle multi paths', () => {
+        const entity = {
+          paths: {
+            relUser: {
+              type: 'entity',
+              writable: null,
+              value: {
+                _links: null,
+                key: '1',
+                model: 'User',
+                version: 340,
+                paths: {
+                  relNationality: {
+                    type: 'entity',
+                    writable: null,
+                    value: {
+                      _links: null,
+                      key: '107',
+                      model: 'Country',
+                      version: 1,
+                      paths: {
+                        ioc: {
+                          type: 'string',
+                          writable: null,
+                          value: 'SUI'
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
 
-        value = {type: 'string', value: null}
-        result = typeValueExtractor.field(value)
-        expect(result).to.eql(null)
+        const result = getFlattenEntity(entity)
+
+        expect(result.relUser).to.eql({key: '1', model: 'User', version: 340})
+        expect(result['relUser.relNationality']).to.eql({key: '107', model: 'Country', version: 1})
+        expect(result['relUser.relNationality.ioc']).to.eql('SUI')
+      })
+
+      test('should handle to N multi paths', () => {
+        const entity = {
+          paths: {
+            relUser: {
+              type: 'entity',
+              writable: null,
+              value: {
+                _links: null,
+                key: '1',
+                model: 'User',
+                version: 340,
+                paths: {
+                  relUser_code1: {
+                    type: 'entity-list',
+                    writable: null,
+                    value: [
+                      {
+                        _links: null,
+                        key: '1',
+                        model: 'User_code1',
+                        version: 1,
+                        paths: {
+                          active: {
+                            type: 'boolean',
+                            writable: null,
+                            value: true
+                          }
+                        }
+                      },
+                      {
+                        _links: null,
+                        key: '301',
+                        model: 'User_code1',
+                        version: 0,
+                        paths: {
+                          active: {
+                            type: 'boolean',
+                            writable: null,
+                            value: false
+                          }
+                        }
+                      }
+                    ]
+                  }
+                }
+              }
+            }
+          }
+        }
+
+        const result = getFlattenEntity(entity)
+
+        expect(result.relUser).to.eql({key: '1', model: 'User', version: 340})
+        expect(result['relUser.relUser_code1']).to.eql([
+          {key: '1', model: 'User_code1', version: 1},
+          {key: '301', model: 'User_code1', version: 0}]
+        )
+
+        expect(result['relUser.relUser_code1.active']).to.eql([true, false])
+      })
+    })
+
+    describe('toEntity', () => {
+      test('should map dirty values to entity paths', () => {
+        const values = {
+          firstname: 'peter',
+          lastname: 'griffin'
+        }
+
+        const dirtyFields = ['firstname', 'lastname']
+        const result = toEntity(values, dirtyFields)
+
+        expect(result.paths.firstname).to.eql('peter')
+        expect(result.paths.lastname).to.eql('griffin')
+      })
+
+      test('should set multi paths correctly', () => {
+        const values = {
+          'relGender': {key: '2', version: 3, model: 'Gender'},
+          'relGender.relXy': {key: '33', version: 4},
+          'relGender.relXy.Z': 'TEST'
+        }
+
+        const dirtyFields = ['relGender.relXy.Z']
+        const result = toEntity(values, dirtyFields)
+
+        const expected = {
+          key: '2',
+          version: 3,
+          paths: {
+            relXy: {
+              key: '33',
+              version: 4,
+              paths: {
+                Z: 'TEST'
+              }
+            }
+          }
+        }
+
+        expect(result.paths.relGender).to.deep.eql(expected)
+      })
+
+      test('should ignore pristine fields', () => {
+        const values = {
+          firstname: 'peter',
+          lastname: 'griffin'
+        }
+
+        const dirtyFields = ['firstname']
+        const result = toEntity(values, dirtyFields)
+        expect(result.paths).to.have.property('firstname')
+        expect(result.paths).to.not.have.property('lastname')
+      })
+
+      test('should simplify object values', () => {
+        const values = {
+          relGender: {key: '2', version: 3, model: 'Gender', display: 'W'}
+        }
+
+        const dirtyFields = ['relGender']
+        const result = toEntity(values, dirtyFields)
+
+        expect(result.paths.relGender).to.eql({key: '2', version: 3})
+      })
+
+      test('should set model, key and version', () => {
+        const values = {
+          __version: 3,
+          __key: '2',
+          __model: 'User'
+        }
+
+        const result = toEntity(values, [])
+
+        expect(result.key).to.eql('2')
+        expect(result.model).to.eql('User')
+        expect(result.version).to.eql(3)
       })
     })
   })
