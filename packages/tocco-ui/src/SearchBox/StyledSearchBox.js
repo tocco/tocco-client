0080c2057c49da533263268d7178e447ffc81acc
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2019-12-04 11:25:30 +0100

    refactor(tocco-ui): style search field
    Refs: TOCDEV-1166
    Chanelog: style search field

--- packages/tocco-ui/src/SearchBox/StyledSearchBox.js
+++ packages/tocco-ui/src/SearchBox/StyledSearchBox.js
@@ -1,16 +1,29 @@
 import styled from 'styled-components'
 
-import {scale} from '../utilStyles'
+import {scale, theme} from '../utilStyles'
 import {StyledInputCss} from '../EditableValue/StyledEditableValue'
+import {StyledStatedValueBox} from '../StatedValue'
 
 const StyledSearchBoxForm = styled.form`
   && {
     margin-bottom: ${scale.space(0)};
+    
+    ${StyledStatedValueBox} {
+      border: 0;
+      width: 82%;
+      border-bottom: 1px solid ${theme.color('text')};
+      margin: 1rem 0 1rem 1.8rem;
+      padding-right: .3rem;
+
+      &:focus {
+        outline: none;
+      }
+    }
   }
 `
 
 const StyledSearchBoxInput = styled.input.attrs({
-  type: 'text'
+  type: 'search'
 })`
   && {
     ${StyledInputCss}
