156c2dbe6a6266123fef429286555c73f94414c9
Daniel Keller <dkeller@tocco.ch>
2019-12-05 11:13:34 +0100

    refactor(tocco-util): improve debouncer
    - add custom change function naming
    - support of ref (forwardRef)

--- packages/tocco-util/src/react/Debouncer.spec.js
+++ packages/tocco-util/src/react/Debouncer.spec.js
@@ -41,6 +41,28 @@ describe('tocco-util', () => {
 
         expect(wrapper.html()).to.have.string('123')
       })
+
+      test('should support custom change function name', async() => {
+        const initialValue = 'Test'
+        const onChangeSpy = sinon.spy()
+
+        const TestComponent2 = Debouncer(({value, onSearch, xy}) => {
+          return <div> <input value={value} onChange={e => onSearch(e.target.value)}/> {xy} </div>
+        }, 100, 'onSearch')
+
+        const wrapper = mount(<TestComponent2 value={initialValue} onSearch={onChangeSpy}/>)
+
+        const input = wrapper.find('input')
+
+        input.simulate('change', {target: {value: 'Test1'}})
+        input.simulate('change', {target: {value: 'Test2'}})
+
+        await new Promise(resolve => setTimeout(() => {
+          input.simulate('change', {target: {value: 'Test3'}})
+          input.simulate('change', {target: {value: 'Test4'}})
+          resolve()
+        }, 200))
+      })
     })
   })
 })
