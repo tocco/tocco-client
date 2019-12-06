e1f8eec20dae8e0760cf47c02514be2ebfd66a11
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2019-12-06 13:59:25 +0100

    refactor(tocco-ui): adpapt styles
    Refs: TOCDEV-1166
    Changelog: extend index exports, change searchbox styles

--- packages/tocco-ui/src/Layout/index.js
+++ packages/tocco-ui/src/Layout/index.js
@@ -2,10 +2,12 @@ import Box from './LayoutBox'
 import Container from './LayoutContainer'
 import StyledLayoutBox from './StyledLayoutBox'
 import StyledLayoutContainer from './StyledLayoutContainer'
+import {StyledScrollbar} from './StyledScrollbar'
 
 export {
   StyledLayoutBox,
-  StyledLayoutContainer
+  StyledLayoutContainer,
+  StyledScrollbar
 }
 
 export default {
