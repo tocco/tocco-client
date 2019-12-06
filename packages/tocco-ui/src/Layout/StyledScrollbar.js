2537ce665e5a07cdcd1fbce30cc14ddaeb090143
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2019-12-06 13:50:41 +0100

    refactor(tocco-ui): make scrollbar style globally available
    Refs: TOCDEV-1166
    Changelog: make scrollbar styles globally available

---
+++ packages/tocco-ui/src/Layout/StyledScrollbar.js
@@ -0,0 +1,20 @@
+import {css} from 'styled-components'
+
+import {theme} from '../utilStyles'
+
+export const StyledScrollbar = css`
+  ::-webkit-scrollbar {
+    width: 10px;
+  }
+   
+  ::-webkit-scrollbar-thumb {
+    background-color: ${theme.color('text')};
+    
+    &:hover {
+      background-color: ${theme.color('secondary')};
+    }
+  }
+  
+  scrollbar-color: ${theme.color('text')} transparent; // Firefox workaround
+  scrollbar-width: thin;
+`
