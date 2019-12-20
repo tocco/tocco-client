84ac0ad4200ca4b8983e1bccdba6bef2a099399f
Daniel Keller <dkeller@tocco.ch>
2019-12-20 08:01:06 +0100

    refactor(tocco-util): remove duplicate fetchMock setups

--- packages/tocco-util/src/mockData/entities.js
+++ packages/tocco-util/src/mockData/entities.js
@@ -8,53 +8,6 @@ const evenFilter = (value, idx) => idx % 2 === 0
 
 export const setupEntities = (fetchMock, entityStore, timeout) => {
   fetchMock.get(
-    new RegExp('^.*?/nice2/rest/forms/User_list$'),
-    require('./data/user_list_form.json')
-  )
-  fetchMock.get(
-    new RegExp('^.*?/nice2/rest/forms/User_search$'),
-    require('./data/user_search_form.json')
-  )
-  fetchMock.get(
-    new RegExp('^.*?/nice2/rest/forms/User_detail$'),
-    require('./data/user_detail_form.json')
-  )
-  fetchMock.get(
-    new RegExp('^.*?/nice2/rest/forms/User_create$'),
-    require('./data/user_detail_form.json')
-  )
-
-  fetchMock.get(
-    new RegExp('^.*?/nice2/rest/forms/User_detail_relDummySubGrid_list$'),
-    require('./data/dummy_entity_list_form.json')
-  )
-
-  fetchMock.get(
-    new RegExp('^.*?/nice2/rest/forms/User_detail_relDummySubGrid_search$'),
-    require('./data/dummy_entity_search_form.json')
-  )
-
-  fetchMock.get(
-    new RegExp('^.*?/nice2/rest/forms/Dummy_entity_list$'),
-    require('./data/dummy_entity_list_form.json')
-  )
-
-  fetchMock.get(
-    new RegExp('^.*?/nice2/rest/forms/Dummy_entity_search$'),
-    require('./data/dummy_entity_search_form.json')
-  )
-
-  fetchMock.get(
-    new RegExp('^.*?/nice2/rest/forms/User_Dummy_entity_detail$'),
-    require('./data/dummy_entity_detail_form.json')
-  )
-
-  fetchMock.get(
-    new RegExp('^.*?/nice2/rest/forms/User_Dummy_entity_create$'),
-    require('./data/dummy_entity_create_form.json')
-  )
-
-  fetchMock.get(
     new RegExp('^.*?/nice2/rest/entities/User/model$'),
     require('./data/user_model.json')
   )
