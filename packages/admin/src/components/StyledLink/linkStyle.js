250ec59440aadaeba03f90dd27de00e6e7c78109
Daniel Keller <dkeller@tocco.ch>
2020-01-08 14:53:10 +0100

    refactor(admin): adjust link styling
    - make sure children of a link are styled properly
    - remove list link styling

--- packages/admin/src/components/StyledLink/linkStyle.js
+++ packages/admin/src/components/StyledLink/linkStyle.js
@@ -5,29 +5,21 @@ import _get from 'lodash/get'
 export const linkStyle = css`
   ${declareFont()}
 
-  color: ${props => props.neutral
-    ? theme.color('text')
-    : theme.color('primary')};
-  text-decoration: ${props => props.neutral ? 'underline' : 'none'};
-
-  &:hover,
-  &:focus {
-    color: ${props => props.neutral
-    ? shadeColor(_get(props.theme, 'colors.text'), 1)
-    : shadeColor(_get(props.theme, 'colors.primary'), 1)};
-    text-decoration: ${props => props.neutral ? 'none' : 'underline'};
-  }
-
-  &:active {
-   text-decoration: ;
-    color: ${props => props.neutral
-    ? shadeColor(_get(props.theme, 'colors.text'), 2)
-    : shadeColor(_get(props.theme, 'colors.primary'), 2)};
-    text-decoration: ${props => props.neutral ? 'none' : 'underline'};
+  text-decoration: none;
+  color: ${theme.color('text')}
+   
+  & * {
+    color: ${theme.color('text')}
+    text-decoration: none;
   }
   
-   &.active {
-   text-decoration: ;
+  &:hover, &:hover *,
+  &:focus, &:focus * {
+    color: ${theme.color('secondaryLight')};
+    text-decoration: ${props => props.neutral ? 'none' : 'underline'};
+  }
+
+  &:active, &:active * {
     color: ${props => props.neutral
     ? shadeColor(_get(props.theme, 'colors.text'), 2)
     : shadeColor(_get(props.theme, 'colors.primary'), 2)};
