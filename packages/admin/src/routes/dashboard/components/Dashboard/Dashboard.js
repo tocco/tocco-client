d5eeb7680456c252adafbbf5a76130ad8b6ccc8a
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2020-01-16 13:46:51 +0100

    refactor(admin): change welcome screen
    Refs: TOCDEV-1347
    Changelog: change welcome screen

--- packages/admin/src/routes/dashboard/components/Dashboard/Dashboard.js
+++ packages/admin/src/routes/dashboard/components/Dashboard/Dashboard.js
@@ -1,23 +1,55 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {Typography, Icon} from 'tocco-ui'
+import {Typography, theme} from 'tocco-ui'
 import styled from 'styled-components'
+
+import ToccoLogo from '../../../../assets/tocco_red.svg'
+
+const StyledDashBoardWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: ${theme.color('paper')}
+`
+
+const InnerWrapper = styled.div`
+  width: 600px;
+  height: 50px;
+  position: relative;
+  left: -320px
+`
 
 const StyledDashBoard = styled.div`
   display: flex;
-  justify-content: center;
+  height: 75%;
+  justify-content: flex-end;
+  align-items: flex-end;
+  margin-right: -180px;
   
-  div { 
-      padding:  15px;
+  && {
+    * {
+      color: ${theme.color('primary')};
+      font-size: 10rem;
+      letter-spacing: .5rem;
+    }
+    
+    ${InnerWrapper} {
+      * {font-size: 3rem;}
+      text-align: right;
+    }
   }
 `
 
 const Dashboard = () => {
   const packageJson = require('../../../../../package')
-  return <StyledDashBoard>
-    <div><Icon style={{fontSize: '80px'}} icon="tocco"/></div>
-    <Typography.H1>Welcome to beta v{packageJson.version}</Typography.H1>
-  </StyledDashBoard>
+  return <StyledDashBoardWrapper>
+    <StyledDashBoard>
+      <Typography.H1>Welcome</Typography.H1>
+      <InnerWrapper>
+        <img src={ToccoLogo} alt="tocco-logo"/>
+        <Typography.H3>beta v{packageJson.version}</Typography.H3>
+      </InnerWrapper>
+    </StyledDashBoard>
+  </StyledDashBoardWrapper>
 }
 
 Dashboard.propTypes = {
