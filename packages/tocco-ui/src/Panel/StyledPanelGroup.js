0e7edd09520451c43823f4596359f998a111dd58
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2020-01-21 16:28:50 +0100

    refactor(tocco-ui): adapt styling
    Refs: TOCDEV-1167
    Changelog: Change List View Styling

--- packages/tocco-ui/src/Panel/StyledPanelGroup.js
+++ packages/tocco-ui/src/Panel/StyledPanelGroup.js
@@ -4,6 +4,7 @@ import {scale} from '../utilStyles'
 import StyledPanel from './StyledPanel'
 
 const StyledPanelGroup = styled.div`
+  margin-bottom: ${scale.space(0)};
   && {
     ${StyledPanel}:not(:last-child) {
       margin-bottom: ${scale.space(-1)};
