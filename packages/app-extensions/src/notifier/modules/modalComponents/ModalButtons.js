b8c7023a180d2dbb0385f02c2abf438a11d254b5
Daniel Keller <dkeller@tocco.ch>
2020-01-21 09:43:51 +0100

    refactor(app-extensions): adjust notifier to new buttons

--- packages/app-extensions/src/notifier/modules/modalComponents/ModalButtons.js
+++ packages/app-extensions/src/notifier/modules/modalComponents/ModalButtons.js
@@ -1,18 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {Button, ButtonGroup, design} from 'tocco-ui'
+import {Button, design} from 'tocco-ui'
 
 const ModalButtons = ({buttons}) =>
-  <ButtonGroup look="raised">
-    {buttons.map((button, i) =>
-      <Button
-        {...(button.primary ? {ink: design.ink.PRIMARY} : {})}
-        key={i}
-        label={button.label}
-        onClick={button.callback}
-      />
-    )}
-  </ButtonGroup>
+  buttons.map((button, i) =>
+    <Button
+      {...(button.primary ? {ink: design.ink.PRIMARY} : {})}
+      key={i}
+      label={button.label}
+      onClick={button.callback}
+    />
+  )
 
 ModalButtons.propTypes = {
   buttons: PropTypes.arrayOf(
