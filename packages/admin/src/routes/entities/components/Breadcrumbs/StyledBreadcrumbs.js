c3551dcac4e7e8f9cd74bcfbf8b7713428855bc6
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2020-01-23 09:10:32 +0100

    refactor(admin): remove excess padding
    Refs: TOCDEV-1167

--- packages/admin/src/routes/entities/components/Breadcrumbs/StyledBreadcrumbs.js
+++ packages/admin/src/routes/entities/components/Breadcrumbs/StyledBreadcrumbs.js
@@ -6,7 +6,7 @@ import {StyledLink} from '../../../../components/StyledLink'
 export const StyledBreadcumbs = styled.div`
   background-color: ${theme.color('backgroundBreadcrumbs')};
   width: 100%;
-  padding: .95rem 1.7rem;
+  padding: .8rem 1.7rem;
   
   span:nth-child(even) {
     margin-left: 1.5rem;
