4b43a732c6ab0467cbdd89f2cea923c17cd5b5b4
Daniel Keller <dkeller@tocco.ch>
2020-02-04 11:36:45 +0100

    refactor(tocco-ui): add basic bootstrap support for HTML field
    - Regard breakword property
    
    Refs: TOCDEV-1439
    Changelog: Basic display-expression bootstrap class support

--- packages/tocco-ui/src/FormattedValue/typeFormatters/HtmlFormatter.js
+++ packages/tocco-ui/src/FormattedValue/typeFormatters/HtmlFormatter.js
@@ -2,21 +2,48 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import styled from 'styled-components'
 
-import {scale} from '../../utilStyles'
-import {declareTypograhpy} from '../../Typography'
+import {scale, theme} from '../../utilStyles'
+import {declareTypograhpy, StyledSpan} from '../../Typography'
 
-const StyledHtmlFormatter = styled.div`
-  && {
+const StyledHtmlFormatter = styled(StyledSpan)`
+  &&& {
     ${props => declareTypograhpy(props, 'html')}
     margin: 0 0 ${scale.space(-1)};
 
     &:last-child {
       margin-bottom: 0;
     }
+
+    .label {
+      border-radius: 6px;
+      padding: .3em;
+    }
+
+    .label-danger {
+      background-color: ${theme.color('signal.danger.paper')};
+      color: ${theme.color('signal.danger.text')};
+    }
+
+    .label-info {
+      background-color: ${theme.color('signal.info.paper')};
+      color: ${theme.color('signal.info.text')};
+    }
+
+    .label-warning {
+      background-color: ${theme.color('signal.warning.paper')};
+      color: ${theme.color('signal.warning.text')};
+    }
+
+    .label-success {
+      background-color: ${theme.color('signal.success.paper')};
+      Color: ${theme.color('signal.success.text')};
+    }
   }
 `
 
-const HtmlFormatter = props => <StyledHtmlFormatter dangerouslySetInnerHTML={{__html: props.value}}/>
+const HtmlFormatter = props => {
+  return <StyledHtmlFormatter dangerouslySetInnerHTML={{__html: props.value}}/>
+}
 
 HtmlFormatter.propTypes = {
   value: PropTypes.string.isRequired
