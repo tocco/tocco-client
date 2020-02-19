4d246dc4fb5ac1dceca4c3fc87a1cc9e7429554d
Daniel Keller <dkeller@tocco.ch>
2020-02-19 16:26:00 +0100

    refactor(tocco-ui): add margin to checkbox

--- packages/tocco-ui/src/EditableValue/typeEditors/BoolEdit.js
+++ packages/tocco-ui/src/EditableValue/typeEditors/BoolEdit.js
@@ -1,5 +1,10 @@
 import PropTypes from 'prop-types'
 import React from 'react'
+import styled from 'styled-components'
+
+const StyledBoolEdit = styled.div`
+  margin: 5px 0px;
+`
 
 const BoolEdit = props => {
   const handleChange = e => {
@@ -9,14 +14,16 @@ const BoolEdit = props => {
   }
 
   return (
-    <input
-      type="checkbox"
-      checked={props.value || false}
-      name={props.name}
-      onChange={handleChange}
-      id={props.id}
-      disabled={props.immutable}
-    />
+    <StyledBoolEdit>
+      <input
+        type="checkbox"
+        checked={props.value || false}
+        name={props.name}
+        onChange={handleChange}
+        id={props.id}
+        disabled={props.immutable}
+      />
+    </StyledBoolEdit>
   )
 }
 
