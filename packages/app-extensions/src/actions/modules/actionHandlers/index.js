b4ea2f8117fdc5b46d5d8e177b64e303a9e8341f
Roland Züger <rzueger@tocco.ch>
2020-02-26 14:23:54 -0500

    feat(app-extension): add handler for legacy actions (ExtJs)
    - The first time a legacy action handler is executed, the required
      ExtJS and legacy-client sources are loaded
    - There's are still some things to do. For instance, there are several
      types of record selections that must be supported eventually (e.g.
      actual selection of some particular records by key or a set of entities
      defined by query conditions only). At the moment, only actual selection
      by entity key is implemented.
    
    Refs: TOCDEV-1507

--- packages/app-extensions/src/actions/modules/actionHandlers/index.js
+++ packages/app-extensions/src/actions/modules/actionHandlers/index.js
@@ -2,9 +2,11 @@ import actionTypes from '../../actionTypes'
 import handleSimpleAction from './simpleAction'
 import handleReportAction from './report'
 import handleCustomAction from './customAction'
+import handleLegacyAction from './legacyAction'
 
 export default {
   [actionTypes.SIMPLE]: handleSimpleAction,
   [actionTypes.REPORT]: handleReportAction,
-  [actionTypes.CUSTOM]: handleCustomAction
+  [actionTypes.CUSTOM]: handleCustomAction,
+  [actionTypes.LEGACY]: handleLegacyAction
 }
