9254357108846d18c7ecc786ba5e5e104709d514
Daniel Keller <dkeller@tocco.ch>
2020-03-11 20:55:52 +0100

    refactor(tocco-ui): change panel animation
    - remove dom observer and resize handler
    - introduce animation lib
    - fix nice 2 compatibility bug (TOCDEV-1323)
    
    Refs: TOCDEV-1323
    Changelog: Panel animation refactoring

--- packages/tocco-ui/src/Panel/PanelBody.js
+++ packages/tocco-ui/src/Panel/PanelBody.js
@@ -6,74 +6,26 @@ import StyledPanelBody from './StyledPanelBody'
 /**
  * Only <Panel.Body/> is affected by the visibility state.
  */
-class PanelBody extends React.PureComponent {
-  state = {}
 
-  setHeight = () => {
-    this.outerEl.setAttribute('style', 'height: auto; animation: none;')
-    this.setState({
-      heightIfOpen: `${this.outerEl.offsetHeight}px`
-    })
-    this.outerEl.removeAttribute('style')
-  }
-
-  relevantAttributes = ['data-image-in-cache', 'style']
-  observerCallback = mutationsList => {
-    const isNotIgnorableAttributeChange = mutation =>
-      mutation.type !== 'attributes' || this.relevantAttributes.includes(mutation.attributeName)
-
-    if (mutationsList.some(isNotIgnorableAttributeChange)) {
-      this.setHeight()
-    }
-  }
-
-  connectObserver() {
-    this.observer = new MutationObserver(this.observerCallback)
-    this.observer.observe(this.innerEl, {
-      attributes: true,
-      childList: true,
-      subtree: true,
-      characterData: true
-    })
-  }
-
-  disconnectObserver() {
-    if (this.observer) {
-      this.observer.disconnect()
-    }
-  }
-
-  componentDidMount() {
-    this.setHeight()
-    window.addEventListener('resize', this.setHeight)
-    this.connectObserver()
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.setHeight)
-    this.disconnectObserver()
-  }
-
-  render() {
-    const {
-      children,
-      isFramed,
-      isOpen
-    } = this.props
-
-    return (
-      <StyledPanelBody
-        isFramed={isFramed}
-        isOpen={isOpen}
-        ref={outerEl => { this.outerEl = outerEl }}
-        heightIfOpen={this.state.heightIfOpen}
-      >
-        <div ref={innerEl => { this.innerEl = innerEl }}>
-          {children}
-        </div>
-      </StyledPanelBody>
-    )
-  }
+const PanelBody = ({children, isFramed, isOpen}) => {
+  const currentVariant = isOpen ? 'open' : 'closed'
+  return (
+    <StyledPanelBody
+      initial={currentVariant}
+      animate={currentVariant}
+      variants={{
+        closed: {height: 0},
+        open: {height: 'auto'}
+      }}
+      transition={{ease: 'easeInOut', duration: 0.3}}
+      isFramed={isFramed}
+      isOpen={isOpen}
+    >
+      <div>
+        {children}
+      </div>
+    </StyledPanelBody>
+  )
 }
 
 PanelBody.propTypes = {
