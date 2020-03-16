fae4e25c228994aa478434fbcdfaf41a3a0a8f3f
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2020-03-16 09:32:29 +0100

    refactor(merge): adapt styles for old client display
    Refs: TOCDEV-1624

--- packages/merge/src/components/MergeWizard/MergeWizard.js
+++ packages/merge/src/components/MergeWizard/MergeWizard.js
@@ -1,24 +1,32 @@
 import PropTypes from 'prop-types'
 import React from 'react'
+import styled from 'styled-components'
+import {theme} from 'tocco-ui'
 
 import MergeMatrixContainer from './../../containers/MergeMatrixContainer'
 import MergeStrategyContainer from './../../containers/MergeStrategyContainer'
 import {Wizard} from './../Wizard'
 import MergeResponse from './MergeResponse'
 
+const StyledWizarWrapper = styled.div`
+  background-color: ${theme.color('backgroundBody')};
+  padding: 5px;
+`
 class MergeStrategy extends React.Component {
   render() {
     const saveButtonLabel = this.props.intl.formatMessage({id: 'client.merge.saveButton'})
 
     if (!this.props.mergeResponse.merged) {
       return (
-        <Wizard
-          save={{fn: this.props.saveMerge, label: saveButtonLabel}}
-          intl={this.props.intl}
-        >
-          <MergeStrategyContainer/>
-          <MergeMatrixContainer/>
-        </Wizard>
+        <StyledWizarWrapper>
+          <Wizard
+            save={{fn: this.props.saveMerge, label: saveButtonLabel}}
+            intl={this.props.intl}
+          >
+            <MergeStrategyContainer/>
+            <MergeMatrixContainer/>
+          </Wizard>
+        </StyledWizarWrapper>
       )
     } else {
       return (
