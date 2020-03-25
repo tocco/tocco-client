cfe999e50dca13e71535527ce5c01563ef7367f6
Daniel Keller <dkeller@tocco.ch>
2020-03-25 14:26:00 +0100

    fix(tocco-util): allow null param

--- packages/tocco-util/src/js/html.js
+++ packages/tocco-util/src/js/html.js
@@ -1,6 +1,6 @@
 /**
  * Removes HTML Tags and returns a formatting neutral string
  */
-export const adjustedHTMLString = (html = '') => (
-  html.replace(/&shy;/g, '')
+export const adjustedHTMLString = html => (
+  html ? html.replace(/&shy;/g, '') : html
 )
