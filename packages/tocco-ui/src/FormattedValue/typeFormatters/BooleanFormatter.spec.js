931fe18f77143e773db4a04093ce20bb0c8ea4cb
Daniel Keller <dkeller@tocco.ch>
2020-03-30 10:54:43 +0200

    refactor(tocco-ui): fit icon to font-size

--- packages/tocco-ui/src/FormattedValue/typeFormatters/BooleanFormatter.spec.js
+++ packages/tocco-ui/src/FormattedValue/typeFormatters/BooleanFormatter.spec.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import {shallow} from 'enzyme'
+import {mount} from 'enzyme'
+import {TestThemeProvider} from 'tocco-test-util'
 
 import Icon from '../../Icon'
 import BooleanFormatter from './BooleanFormatter'
@@ -9,15 +10,19 @@ describe('tocco-ui', () => {
     describe('typeFormatters', () => {
       describe('BooleanFormatter ', () => {
         test('should format true value', () => {
-          const wrapper = shallow(
-            <BooleanFormatter value/>
+          const wrapper = mount(<TestThemeProvider>
+            <BooleanFormatter value={true}/>
+          </TestThemeProvider>
           )
+
           expect(wrapper.find(Icon).prop('icon')).to.equal('check')
         })
 
         test('should format falsy value', () => {
-          const wrapper = shallow(
-            <BooleanFormatter value={false}/>
+          const wrapper = mount(
+            <TestThemeProvider>
+              <BooleanFormatter value={false}/>
+            </TestThemeProvider>
           )
           expect(wrapper.find(Icon).prop('icon')).to.equal('times')
         })
