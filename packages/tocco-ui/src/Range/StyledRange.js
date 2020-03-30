dfb11056a9bf95a1b55a19e71c97a507fe5d4990
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2020-03-30 16:03:46 +0200

    refactor(tocco-ui): adapt range styling
    Refs: TOCDEV-1682

--- packages/tocco-ui/src/Range/StyledRange.js
+++ packages/tocco-ui/src/Range/StyledRange.js
@@ -5,6 +5,14 @@ import {scale, shadeColor} from '../utilStyles'
 import {StyledButton} from '../Ball/StyledBall'
 import {StyledSpan} from '../Typography'
 
+export const StyledInputWrapper = styled.div`
+  width: 100%;
+
+  ${StyledButton} {
+    margin-right: ${scale.space(-2)};
+  }
+`
+
 const StyledRange = styled.div`
   display: flex;
 
@@ -14,14 +22,8 @@ const StyledRange = styled.div`
     ${StyledSpan} {
       position: absolute;
       display: inline-block;
-      font-weight: bold;
       min-width: 3rem;
-      top: ${scale.space(-0.5)};
-    }
-
-    ${StyledButton} {
-      position: relative;
-      left: 2.7rem;
+      top: ${scale.space(-0.2)};
     }
   }
 
@@ -32,19 +34,15 @@ const StyledRange = styled.div`
   }
 `
 
-export const StyledInputWrapper = styled.div`
-  width: 100%;
-`
-
 export const StyledInputItemWrapper = styled.div`
   position: relative;
 `
 
 export const StyledEditableValue = styled.div`
-  background-color: ${props => shadeColor(_get(props.theme, 'colors.paper'), 1)};
+  border: 1px solid ${props => shadeColor(_get(props.theme, 'colors.paper'), 4)};
   width: 15rem;
   display: inline-block;
-  padding-left: ${scale.space(-0.5)};
+  padding: ${scale.space(-2.5)} 0 ${scale.space(-2.5)} ${scale.space(-0.5)};
   margin-top: ${scale.space(-1)};
   margin-left: 3.2rem;
 `
