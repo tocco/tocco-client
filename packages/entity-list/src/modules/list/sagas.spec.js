63eedec2dc159f25a98cf76f0d6917de6a5061ff
Daniel Keller <dkeller@tocco.ch>
2020-04-07 13:24:57 +0200

    refactor(entity-list): only set fallback sorting if field exists
    Refs: TOCDEV-1650
    Changelog: Fix fallback sorting bug

--- packages/entity-list/src/modules/list/sagas.spec.js
+++ packages/entity-list/src/modules/list/sagas.spec.js
@@ -64,6 +64,7 @@ describe('entity-list', () => {
               call(sagas.loadFormDefinition, formDefinition, formName)
             ]))
 
+            expect(gen.next().value).to.eql(call(sagas.setSorting))
             expect(gen.next().value).to.eql(call(sagas.loadData, 1))
             expect(gen.next().value).to.eql(call(sagas.queryChanged))
             expect(gen.next().value).to.eql(put(actions.setInProgress(false)))
@@ -195,11 +196,19 @@ describe('entity-list', () => {
         })
 
         describe('setSorting saga', () => {
+          const entityModel = {
+            paths: {
+              update_timestamp: {}
+            }
+          }
+
+          const formDefinition = {}
           test('should extract sorting of form', () => {
             const sorting = [{field: 'firstname', order: 'asc'}]
             return expectSaga(sagas.setSorting)
               .provide([
-                [matchers.call.fn(getSorting), sorting]
+                [matchers.call.fn(getSorting), sorting],
+                [select(sagas.listSelector), {entityModel, formDefinition}]
               ])
               .put(actions.setSorting(sorting))
               .run()
@@ -208,9 +217,20 @@ describe('entity-list', () => {
           test('should set fallback if not defined in form', () => {
             return expectSaga(sagas.setSorting)
               .provide([
-                [matchers.call.fn(getSorting), []]
+                [matchers.call.fn(getSorting), []],
+                [select(sagas.listSelector), {entityModel, formDefinition}]
               ])
-              .put(actions.setSorting(sagas.FALLBACK_SORTING))
+              .put(actions.setSorting([{field: sagas.FALLBACK_SORTING_FIELD, order: 'desc'}]))
+              .run()
+          })
+
+          test('should not set sorting if fallback does not exist in model', () => {
+            return expectSaga(sagas.setSorting)
+              .provide([
+                [matchers.call.fn(getSorting), []],
+                [select(sagas.listSelector), {entityModel: {paths: {}}, formDefinition}]
+              ])
+              .not.put.like({action: {type: 'list/SET_SORTING'}})
               .run()
           })
         })
@@ -342,14 +362,12 @@ describe('entity-list', () => {
             return expectSaga(sagas.loadFormDefinition, formDefinition, formName)
               .provide([
                 [matchers.call.fn(rest.fetchForm), loadedFormDefinition],
-                [matchers.call.fn(sagas.setSorting)],
                 [matchers.call.fn(getSelectable), selectable],
                 [matchers.call.fn(getEndpoint), endpoint],
                 [matchers.call.fn(getConstriction), constriction]
               ])
               .call(rest.fetchForm, formName, 'list')
               .put(actions.setFormDefinition(loadedFormDefinition))
-              .call(sagas.setSorting, loadedFormDefinition)
               .put(actions.setFormSelectable(selectable))
               .put(actions.setEndpoint(endpoint))
               .put(actions.setConstriction(constriction))
