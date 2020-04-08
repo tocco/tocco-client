634326657eaf766a3d71301c9f401578b5c86915
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2020-04-08 11:35:09 +0200

    refactor(tocco-ui): align items in range
    Refs: TOCDEV-1722

--- packages/tocco-ui/src/Range/StyledRange.js
+++ packages/tocco-ui/src/Range/StyledRange.js
@@ -1,7 +1,17 @@
 import styled from 'styled-components'
 
+import {scale} from '../utilStyles'
+
 export const StyledInputWrapper = styled.div`
   width: 100%;
+`
+export const StyledInputItemWrapper = styled.div`
+  display: inline-block;
+  width: 100%;
+
+  * {
+    line-height: unset !important;
+  }
 `
 
 const StyledRange = styled.div`
@@ -12,16 +22,19 @@ const StyledRange = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: baseline;
+
+    ${/* sc-selector */StyledInputItemWrapper}:nth-child(3) {
+      margin-right: ${scale.space(0)};
+
+      * {
+        text-align: right;
+      }
+    }
   }
-`
 
-export const StyledInputItemWrapper = styled.div`
-  display: inline-block;
-  width: 100%;
-
-  * {
-    line-height: unset !important;
-    text-align: center;
+  .extender {
+    display: flex;
+    align-items: center;
   }
 `
 
