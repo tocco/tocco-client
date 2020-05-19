f09a020f69a1318b2bce2898fd2685f5cbe3793a
Daniel Keller <dkeller@tocco.ch>
2020-05-19 09:50:00 +0200

    refactor(tocco-util): disable caching for DEV
    Refs: TOCDEV-1022
    Changelog: Disable caching for DEV

--- packages/tocco-util/src/cache/cache.spec.js
+++ packages/tocco-util/src/cache/cache.spec.js
@@ -44,6 +44,51 @@ describe('tocco-util', () => {
         const cachedValue = get(type, id)
         expect(cachedValue.a).to.eql(21)
       })
+
+      test('should not cache if __DEV__', () => {
+        const value = 'Test'
+        const type = 'label'
+        const id = 1
+
+        const resetDEVValue = __DEV__
+        // eslint-disable-next-line no-global-assign
+        __DEV__ = true
+        add(type, id, value)
+
+        const cachedValue = get(type, id)
+        expect(cachedValue).to.eql(undefined)
+
+        // eslint-disable-next-line no-global-assign
+        __DEV__ = resetDEVValue
+      })
+
+      test('should not cache if nice env is not production', () => {
+        const value = 'Test'
+        const type = 'label'
+        const id = 1
+
+        global.app = {
+          getRunEnv: () => 'DEVELOPMENT'
+        }
+
+        add(type, id, value)
+        const cachedValue = get(type, id)
+        expect(cachedValue).to.eql(undefined)
+      })
+
+      test('should cache if nice env is production', () => {
+        const value = 'Test'
+        const type = 'label'
+        const id = 1
+
+        global.app = {
+          getRunEnv: () => 'PRODUCTION'
+        }
+
+        add(type, id, value)
+        const cachedValue = get(type, id)
+        expect(cachedValue).to.eql(value)
+      })
     })
 
     describe('clear', () => {
--- packages/tocco-util/src/nice/utils.js
+++ packages/tocco-util/src/nice/utils.js
@@ -1,9 +1,8 @@
-/* eslint-disable no-undef */
 import _isFunction from 'lodash/isFunction'
 
 export const getRunEnv = () => {
   if (window.app && _isFunction(window.app.getRunEnv)) {
-    return app.getRunEnv()
+    return window.app.getRunEnv()
   }
 
   return 'PRODUCTION'
