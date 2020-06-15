7f240f785ae6db7b93316d0827661017870a838c
Daniel Keller <dkeller@tocco.ch>
2020-06-15 09:42:36 +0200

    refactor(entity-list): enhance api
    - add onNavigateToAction event
    - add actionAppComponent

--- packages/app-extensions/src/actions/modules/actionHandlers/customAction.js
+++ packages/app-extensions/src/actions/modules/actionHandlers/customAction.js
@@ -1,8 +1,35 @@
-import {call} from 'redux-saga/effects'
+import {call, put} from 'redux-saga/effects'
+import React from 'react'
+import {consoleLogger} from 'tocco-util'
+
+import notifier from '../../../notifier'
 
 export default function* (definition, selection, parent, params, config) {
-  const handler = config && config.customActions && config.customActions[definition.id]
-  if (handler) {
-    yield call(handler, definition, selection, parent, params, config)
+  if (definition.appId) {
+    if (definition.fullscreen) {
+      if (config.customActions && config.customActions.fullscreen) {
+        yield call(config.customActions.fullscreen, definition, selection)
+      } else {
+        consoleLogger.logError('Unable to render custom action: No fullscreen handler provided')
+      }
+    } else {
+      if (config.appComponent) {
+        const ActionComponent = config.appComponent
+        yield put(notifier.modalComponent(
+          `action-${definition.appId}`,
+          null,
+          null,
+          () => <ActionComponent appId={definition.appId} selection={selection}/>,
+          true
+        ))
+      } else {
+        consoleLogger.logError('Unable to render custom action: No appComponent provided')
+      }
+    }
+  } else {
+    const handler = config && config.customActions && config.customActions[definition.id]
+    if (handler) {
+      yield call(handler, definition, selection, parent, params, config)
+    }
   }
 }
