c484958b5dc2f23f1092d26238985f5032e642ce
Daniel Keller <dkeller@tocco.ch>
2020-07-06 09:44:12 +0200

    refactor(tocco-util): add selection helper
    - add propType for selection
    - getEntities helper

--- packages/tocco-util/src/selection/selection.js
+++ packages/tocco-util/src/selection/selection.js
@@ -1,4 +1,8 @@
 import queryStringUtil from 'query-string'
+import {call} from 'redux-saga/effects'
+
+import consoleLogger from '../consoleLogger'
+import selectionTypes from './selectionTypes'
 
 export const selectionToQueryString = selection =>
   `selection=${encodeURIComponent(JSON.stringify(selection))}`
@@ -6,4 +10,32 @@ export const selectionToQueryString = selection =>
 export const queryStringToSelection = queryString => {
   const obj = queryStringUtil.parse(queryString)
   return obj.selection ? JSON.parse(obj.selection) : null
+}
+
+const limit = 100000
+/**
+ * Helper to get all entitiy keys of a selection.
+ * @param {object}  selection - selection object
+ * @param {function} fetchEntities - rest helper function
+ * @returns {object} Object containing entityName and an array of keys (limited if selection is query)
+ */
+export function* getEntities(selection, fetchEntities) {
+  const {entityName} = selection
+  if (selection.type === selectionTypes.QUERY) {
+    const entities = yield call(fetchEntities, selection.entityName, {...selection.query, limit})
+
+    if (entities.length > limit) {
+      yield call(consoleLogger.logError, `Selected records exceed limit of ${limit}`)
+    }
+
+    return {
+      entityName,
+      keys: entities.map(e => e.key)
+    }
+  }
+
+  return {
+    entityName,
+    keys: selection.ids
+  }
 }
---
+++ packages/tocco-util/src/selection/selectionTypes.js
@@ -0,0 +1,4 @@
+export default {
+  ID: 'ID',
+  QUERY: 'QUERY'
+}
