399f1da32e6ab1509e1932a4d7f130a120c69670
Daniel Keller <dkeller@tocco.ch>
2020-07-13 10:53:06 +0200

    refactor(delete): use RouterLink and format output

--- packages/delete/src/components/InfoPart/InfoPart.js
+++ packages/delete/src/components/InfoPart/InfoPart.js
@@ -1,17 +1,24 @@
 import React from 'react'
-import {Typography, Link} from 'tocco-ui'
+import {Typography, RouterLink} from 'tocco-ui'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
 
 import LinkPopOver from './LinkPopOver'
 import {relatedPropType} from '../../utils/deleteRequestParser'
+
+const NonBreakingText = styled.span`
+  white-space: nowrap;
+`
 
 const InfoPart = ({entityName, entityLabel, keys, relatedEntities, maxCountLink}) => {
   const primaryTql = 'IN(pk,' + keys.join(',') + ')'
   return <Typography.Span>
     <Typography.B>
-      {entityLabel} (<Link href={`/e/${entityName}/list?tql=${primaryTql}`} rel="noopener noreferrer" target="_blank">
-        {keys.length}
-      </Link>)
+      {entityLabel} ({
+        keys.length > 0
+          ? <RouterLink to={`/e/${entityName}/list?tql=${primaryTql}`} target="_blank">{keys.length}</RouterLink>
+          : <Typography.Span>{keys.length}</Typography.Span>
+      })
     </Typography.B>
     {
       Object.keys(relatedEntities).length > 0
@@ -23,16 +30,16 @@ const InfoPart = ({entityName, entityLabel, keys, relatedEntities, maxCountLink}
           const linkText = [...relatedEntity.keys, ...relatedEntity.keysOtherBu].length
 
           const Count = relatedEntity.keys.length > 0
-            ? <Link href={`/e/${entityName}/list?tql=${tql}`} rel="noopener noreferrer" target="_blank">
+            ? <RouterLink to={`/e/${entityName}/list?tql=${tql}`} target="_blank">
               {linkText}
-            </Link>
+            </RouterLink>
             : <Typography.Span>{linkText}</Typography.Span>
 
           const Content = <LinkPopOver relatedEntity={relatedEntity} maxCountLink={maxCountLink}>{Count}</LinkPopOver>
 
           return <React.Fragment
             key={'entity-info-' + entityName}>
-            {relatedEntity.entityLabel} ({Content})
+            <NonBreakingText>{relatedEntity.entityLabel} ({Content})</NonBreakingText>
           </React.Fragment>
         }).reduce((prev, curr) => [prev, ', ', curr])}
       </>
