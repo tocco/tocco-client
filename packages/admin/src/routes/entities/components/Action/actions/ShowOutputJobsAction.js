3aebe98d0756bdb55c2e68e82bb7f6cfb7ed6f6c
Roland Züger <rzueger@tocco.ch>
2020-07-15 09:16:45 -0500

    fix(admin): remove unnecessary timeout in ShowOutputJobsAction
    The problem described in TOCDEV-2080 doesn't occur anymore.
    It has been fixed with TOCDEV-1939.
    
    Refs: TOCDEV-2080, TOCDEV-2075

--- packages/admin/src/routes/entities/components/Action/actions/ShowOutputJobsAction.js
+++ packages/admin/src/routes/entities/components/Action/actions/ShowOutputJobsAction.js
@@ -1,24 +1,11 @@
-import React, {useEffect, useState} from 'react'
+import React from 'react'
 import {Redirect} from 'react-router-dom'
 import PropTypes from 'prop-types'
 
-const ShowOutputJobsAction = props => {
-  const [doRedirect, setDoRedirect] = useState(false)
-
-  useEffect(() => {
-    // If we redirect to the Output_job list right away, the UI gets stuck between
-    // two views because of some race conditions. This `setTimeout` makes sure all
-    // running sagas have completed when we redirect to avoid messing up the state.
-    setTimeout(() => setDoRedirect(true), 0)
-  }, [])
-
-  return doRedirect
-    ? <Redirect to={{
-      pathname: '/e/Output_job/list',
-      search: `tql=${encodeURIComponent(`entity=="${props.selection.entityName}"`)}`
-    }}/>
-    : null
-}
+const ShowOutputJobsAction = props => <Redirect to={{
+  pathname: '/e/Output_job/list',
+  search: `tql=${encodeURIComponent(`entity=="${props.selection.entityName}"`)}`
+}}/>
 
 ShowOutputJobsAction.propTypes = {
   selection: PropTypes.shape({
