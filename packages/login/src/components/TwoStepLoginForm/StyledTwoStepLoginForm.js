41d628f00bd6902ff5b106c38271f898d1883ec8
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2020-07-24 13:52:31 +0200

    refactor(login): style editable value
    Refs: TOCDEV-1930
    Changelog: Style Inputfield in two step login form

--- packages/login/src/components/TwoStepLoginForm/StyledTwoStepLoginForm.js
+++ packages/login/src/components/TwoStepLoginForm/StyledTwoStepLoginForm.js
@@ -1,8 +1,15 @@
 import styled from 'styled-components'
 import {StyledButton} from 'tocco-ui/src/Button'
+import {StyledEditableValue, StyledInputCss} from 'tocco-ui/src/EditableValue/StyledEditableValue'
 
 export const StyledTwoStepLogin = styled.div`
   ${StyledButton} {
     width: 100%;
+  }
+
+  ${StyledEditableValue} {
+    ${StyledInputCss}
+    padding-top: .7rem;
+    padding-bottom: .7rem;
   }
 `
