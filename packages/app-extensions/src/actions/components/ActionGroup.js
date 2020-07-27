0db59c4b06d3b8616bb8407a6ff7a69a06b98353
Daniel Keller <dkeller@tocco.ch>
2020-07-27 15:45:23 +0200

    refactor(app-extensions): use new Menu in Action

--- packages/app-extensions/src/actions/components/ActionGroup.js
+++ packages/app-extensions/src/actions/components/ActionGroup.js
@@ -1,101 +1,42 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 import {injectIntl, intlShape} from 'react-intl'
-import {Button, Menu} from 'tocco-ui'
+import {ButtonMenu, MenuItem} from 'tocco-ui'
 
-import GroupElement from './GroupElement'
 import {isValidSelection, selectionText} from './selectionHelper'
 
-const MainAction = ({definition, selectedCount, onClick, disabled, intl}) => {
+const getChildMenuItems = input => {
+  const {onClick, definition, selectedCount, intl} = input
   const validSelection = isValidSelection(selectedCount, definition)
+  const disabled = definition.readonly === true || !validSelection
   const title = selectionText(selectedCount, definition, intl)
-  const buttonDisabled = definition.readonly === true || !validSelection || disabled
-
-  return (
-    <Menu.Item look="raised">
-      <Button
-        data-cy={`action-${definition.id}`}
-        disabled={buttonDisabled}
-        icon={definition.icon}
-        label={definition.label}
-        onClick={() => { onClick(definition) }}
-        title={title}
-      />
-    </Menu.Item>
-  )
+  return <MenuItem
+    onClick={() => !disabled && onClick(definition)}
+    disabled={disabled}
+    title={title}
+    key={`MenuItem-${definition.id}`}
+  >
+    {definition.label}
+    {definition.componentType === 'action-group' && definition.children.map(child => {
+      return getChildMenuItems({...input, definition: child})
+    })
+    }
+  </MenuItem>
 }
 
-MainAction.propTypes = {
-  intl: intlShape.isRequired,
-  definition: PropTypes.object.isRequired,
-  onClick: PropTypes.func.isRequired,
-  selectedCount: PropTypes.number,
-  disabled: PropTypes.bool
-}
+const ActionGroup = props => {
+  const {definition, onClick} = props
+  const hasDefaultAction = (definition.defaultAction && Object.keys(definition.defaultAction).length > 0)
 
-const ActionGroup = ({definition, onClick, selectedCount, disabled, intl}) => {
-  const hasMainAction = (definition.defaultAction && Object.keys(definition.defaultAction).length > 0)
-
-  if (hasMainAction) {
-    return (
-      <Menu.Button
-        look="raised"
-      >
-        <Menu.Item>
-          <Menu.ButtonGroup>
-
-            <MainAction
-              definition={definition.defaultAction}
-              onClick={onClick}
-              selectedCount={selectedCount}
-              disabled={disabled}
-              intl={intl}
-            />
-            {definition.children.length > 0 ? <Menu.ItemFlyout
-              isToggleable={definition.readonly !== true}
-            >
-              <Menu.Stack>
-                {definition.children.map((actionDefinition, idx) =>
-                  <GroupElement
-                    disabled={disabled}
-                    definition={actionDefinition}
-                    key={idx}
-                    onClick={onClick}
-                    selectedCount={selectedCount}
-                  />
-                )}
-              </Menu.Stack>
-            </Menu.ItemFlyout>
-              : <React.Fragment/>
-            }
-          </Menu.ButtonGroup>
-        </Menu.Item>
-      </Menu.Button>
-    )
-  } else {
-    return (
-      <Menu.Button
-        look="raised"
-      >
-        <Menu.ItemFlyout
-          isToggleable={definition.readonly !== true}
-          label={definition.label}
-        >
-          <Menu.Stack>
-            {definition.children.map((actionDefinition, idx) =>
-              <GroupElement
-                disabled={disabled}
-                definition={actionDefinition}
-                key={idx}
-                onClick={onClick}
-                selectedCount={selectedCount}
-              />
-            )}
-          </Menu.Stack>
-        </Menu.ItemFlyout>
-      </Menu.Button>
-    )
-  }
+  const label = hasDefaultAction ? definition.defaultAction.label : definition.label
+  const onClickHandler = hasDefaultAction ? () => { onClick(definition.defaultAction) } : null
+  return <ButtonMenu buttonProps={{look: 'raised'}} label={label} onClick={onClickHandler}>
+    {
+      definition.children.map((childDefinition, idx) => {
+        return getChildMenuItems({...props, definition: childDefinition})
+      })
+    }
+  </ButtonMenu>
 }
 
 ActionGroup.propTypes = {
