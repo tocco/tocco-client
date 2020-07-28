744cb6aebb39cf6d5c220dc37a6649d079cffb05
PadPalon <luca.neukom@gmail.com>
2020-07-28 15:37:38 +0200

    feat(entity-list): add button to reset sorting
    Refs: TOCDEV-2054
    Changelog: Add button to reset sorting to entity-list

--- packages/entity-list/src/components/Table/NavigationCellHeader.js
+++ packages/entity-list/src/components/Table/NavigationCellHeader.js
@@ -1,20 +1,22 @@
 import React from 'react'
 import {BallMenu, MenuItem} from 'tocco-ui'
 import {connect} from 'react-redux'
+import PropTypes from 'prop-types'
+import {FormattedMessage} from 'react-intl'
 
-import {loadPreferences} from '../../modules/preferences/actions'
+import {resetSorting} from '../../modules/preferences/actions'
 
-const NavigationCellHeader = () =>
+const NavigationCellHeader = props =>
   <BallMenu buttonProps={{icon: 'ellipsis-v'}}>
-    <MenuItem onClick={() => {}}>Menu Entry</MenuItem>
+    <MenuItem onClick={() => props.resetSorting()}><FormattedMessage id="client.entity-list.sorting.reset"/></MenuItem>
   </BallMenu>
 
 NavigationCellHeader.propTypes = {
-
+  resetSorting: PropTypes.func.isRequired
 }
 
 const mapActionCreators = {
-  loadPreferences
+  resetSorting
 }
 
 export default connect(null, mapActionCreators)(NavigationCellHeader)
