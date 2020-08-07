585d5a22f5c8cd0db61d531c56323fe8115c0ed9
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2020-08-07 08:46:54 +0200

    refactor(entity-detail): change height rendering of form
    Refs: TOCDEV-2166

--- packages/entity-detail/src/components/DetailForm/DetailForm.js
+++ packages/entity-detail/src/components/DetailForm/DetailForm.js
@@ -10,7 +10,9 @@ import ErrorBox from '../ErrorBox'
 import SaveButton from './SaveButton'
 
 const StyledForm = styled.form`
-  display: contents;
+  display: flex;
+  flex-direction: column;
+  flex: 1;
 `
 
 const DetailForm = props => {
@@ -62,25 +64,23 @@ const DetailForm = props => {
     focusErrorFields()
   }
 
-  return (
-    <StyledForm
-      onSubmit={handleSubmit}
-      onKeyDown={handleKeyPress}
-    >
-      <form.FormBuilder
-        entity={props.entity}
-        model={props.entityModel}
-        formName={props.form}
-        formDefinition={props.formDefinition}
-        formValues={props.formValues}
-        fieldMappingType={props.formDefinition.readOnly ? 'readonly' : 'editable'}
-        mode={props.mode}
-        componentMapping={{[form.componentTypes.SUB_TABLE]: SubGrid}}
-        customActions={customActions}
-      />
-      {!props.valid && props.anyTouched && <ErrorBox formErrors={props.formErrors} showErrors={showErrors}/>}
-    </StyledForm>
-  )
+  return <StyledForm
+    onSubmit={handleSubmit}
+    onKeyDown={handleKeyPress}
+  >
+    <form.FormBuilder
+      entity={props.entity}
+      model={props.entityModel}
+      formName={props.form}
+      formDefinition={props.formDefinition}
+      formValues={props.formValues}
+      fieldMappingType={props.formDefinition.readOnly ? 'readonly' : 'editable'}
+      mode={props.mode}
+      componentMapping={{[form.componentTypes.SUB_TABLE]: SubGrid}}
+      customActions={customActions}
+    />
+    {!props.valid && props.anyTouched && <ErrorBox formErrors={props.formErrors} showErrors={showErrors}/>}
+  </StyledForm>
 }
 
 DetailForm.propTypes = {
