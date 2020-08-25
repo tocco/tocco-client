46657902b6ec68a17c5f229b936521ecfb30d59d
PadPalon <luca.neukom@gmail.com>
2020-08-25 15:16:48 +0200

    feat(entity-list): add button to reset all preferences
    Refs: TOCDEV-2058
    Changelog: Added button to reset all preferences

--- packages/entity-list/src/modules/preferences/actions.js
+++ packages/entity-list/src/modules/preferences/actions.js
@@ -3,6 +3,7 @@ export const SET_POSITIONS = 'preferences/SET_POSITIONS'
 export const CHANGE_POSITION = 'preferences/CHANGE_POSITION'
 export const SET_SORTING = 'preferences/SET_SORTING'
 export const RESET_SORTING = 'preferences/RESET_SORTING'
+export const RESET_PREFERENCES = 'preferences/RESET_PREFERENCES'
 
 export const loadPreferences = () => ({
   type: LOAD_PREFERENCES
@@ -33,5 +34,10 @@ export const setSorting = sorting => ({
 
 export const resetSorting = () => ({
   type: RESET_SORTING,
+  payload: {}
+})
+
+export const resetPreferences = () => ({
+  type: RESET_PREFERENCES,
   payload: {}
 })
