723a456c231f576f8ffb81421cac6893b66275db
Daniel Keller <dkeller@tocco.ch>
2020-08-26 09:50:17 +0200

    refactor(app-extensions): remove catch in sendRequest
    - Details of failed request can be seen in Network tab.
    - Enhance error message with body message

--- packages/app-extensions/src/rest/request.js
+++ packages/app-extensions/src/rest/request.js
@@ -4,7 +4,8 @@ const handleError = (response, acceptedErrorCodes = [], acceptedStatusCodes = []
   if (!response.ok
     && !acceptedStatusCodes.includes(response.status)
     && !(response.body && acceptedErrorCodes.includes(response.body.errorCode))) {
-    throw new Error(response.statusText)
+    const msg = `${response.statusText} ${response.body ? response.body.message : ''}`
+    throw new Error(msg)
   }
 
   return response
@@ -28,8 +29,4 @@ export function sendRequest(url, options, acceptedErrorCodes, acceptedStatusCode
   return fetch(url, options)
     .then(response => (extractBody(response)))
     .then(response => (handleError(response, acceptedErrorCodes, acceptedStatusCodes)))
-    .catch(error => {
-      error.message = `REST request error: ${error.message} \n url: ${url}, options: ${JSON.stringify(options)}`
-      throw error
-    })
 }
