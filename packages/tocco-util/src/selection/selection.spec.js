72deac895d42fe958d4bbb7e03d86e588714253f
Roland Züger <rzueger@tocco.ch>
2020-09-03 11:52:06 -0500

    refactor(tocco-util): Remove unused selection query string utils
    The methods `selectionToQueryString` and `queryStringToSelection` have
    been replaced with the more generic new `queryString` util.
    
    Refs: TOCDEV-2224

--- packages/tocco-util/src/selection/selection.spec.js
+++ packages/tocco-util/src/selection/selection.spec.js
@@ -1,34 +1,10 @@
 import {expectSaga} from 'redux-saga-test-plan'
 import {consoleLogger} from 'tocco-util'
 
-import {selectionToQueryString, queryStringToSelection, getEntities} from './selection'
+import {getEntities} from './selection'
 
 describe('tocco-util', () => {
   describe('selection', () => {
-    const selection = {
-      count: 25,
-      entityName: 'Input',
-      query: {
-        tql: 'IN(relInput_status.pk,2)'
-      },
-      type: 'QUERY'
-    }
-
-    const queryString = 'selection=%7B%22count%22%3A25%2C%22entityName%22%3A%22Input%22%2C%22'
-      + 'query%22%3A%7B%22tql%22%3A%22IN(relInput_status.pk%2C2)%22%7D%2C%22type%22%3A%22QUERY%22%7D'
-
-    describe('selectionToQueryString', () => {
-      test('should build query param string from selection object', () => {
-        expect(selectionToQueryString(selection)).to.eql(queryString)
-      })
-    })
-
-    describe('queryStringToSelection', () => {
-      test('should build selection object from query string', () => {
-        expect(queryStringToSelection(queryString)).to.eql(selection)
-      })
-    })
-
     describe('getEntities', () => {
       test('should return object with name and array', () => {
         const entityName = 'User'
