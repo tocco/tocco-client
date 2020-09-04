39ff4c77fb2245906c559f90e19e3abbf5df1f5a
Daniel Keller <dkeller@tocco.ch>
2020-09-04 09:05:18 +0200

    refactor(tocco-util): improve Table.js
    - no error if data prop is undefined
    - add static cell conditional rendering if data changed
    - adjust cursor if sortable
    - pass idx to row

--- packages/tocco-ui/src/Table/StaticCell.js
+++ packages/tocco-ui/src/Table/StaticCell.js
@@ -1,24 +1,35 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import {js} from 'tocco-util'
 
 import {StyledTableCell} from './StyledTable'
 import {Typography} from '../index'
 import {columnPropType, rowDataPropType} from './propTypes'
 
-const StaticCell = React.memo(({column, rowData}) =>
-  <StyledTableCell
+const StaticCell = React.memo(({column, rowData, rowIdx}) => {
+  return <StyledTableCell
     {...column.rightAligned === true && {style: {textAlign: 'right'}}}
     data-cy="list-cell"
   >
     {column.CellRenderer
-      ? <column.CellRenderer rowData={rowData} column={column} />
+      ? <column.CellRenderer rowData={rowData} column={column} rowIdx={rowIdx}/>
       : <Typography.Span>{rowData[column.id]}</Typography.Span>
     }
   </StyledTableCell>
-, props => !props.column.dynamic)
+})
 
 StaticCell.propTypes = {
   rowData: rowDataPropType.isRequired,
-  column: columnPropType.isRequired
+  column: columnPropType.isRequired,
+  rowIdx: PropTypes.number
 }
 
-export default StaticCell
+const areEqual = (prevProps, nextProps) => {
+  if (!prevProps.column.dynamic) {
+    return true
+  }
+  const diff = Object.keys(js.difference(prevProps, nextProps))
+  return diff.length === 0
+}
+
+export default React.memo(StaticCell, areEqual)
