76983dc16675b882265e9b56ee193dab25e2b8e0
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2020-09-17 16:46:48 +0200

    refactor(admin): style errow view
    Refs: TOCDEV-2325
    Changelog: Style error view

--- packages/admin/src/components/ErrorView/ErrorView.js
+++ packages/admin/src/components/ErrorView/ErrorView.js
@@ -1,23 +1,35 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {FormattedMessage} from 'react-intl'
-import {Typography, Icon} from 'tocco-ui'
+import {Typography, Icon, theme, scale} from 'tocco-ui'
 import styled from 'styled-components'
 
 const StyledErrorView = styled.div`
-  width: 100%;
+  width: 50vw;
+  max-width: 650px;
+  background-color: ${theme.color('paper')};
+  padding: ${scale.space(1)};
   text-align: center;
+  margin: 20vh auto auto;
 
   h1 {
-    max-width: 650px;
     margin: 0 auto;
   }
 `
 
+const StyledIconWrapper = styled.span`
+  color: ${theme.color('signal.danger.text')};
+  font-size: ${scale.font(23)};
+`
+
 const ErrorView = ({message, technicalReason}) => {
   return <StyledErrorView>
-    <Icon icon="times" style={{color: '#C01E30', fontSize: '80px'}}/>
-    <Typography.H1><FormattedMessage id={message || 'client.admin.errorView.defaultTitle'}/></Typography.H1>
+    <StyledIconWrapper>
+      <Icon icon="times"/>
+    </StyledIconWrapper>
+    <Typography.H1>
+      <FormattedMessage id={message || 'client.admin.errorView.defaultTitle'}/>
+    </Typography.H1>
     <Typography.Span>{technicalReason}</Typography.Span>
   </StyledErrorView>
 }
