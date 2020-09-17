c04114c6dd33e5fd347ad4eee6c5c72da219d6fa
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2020-09-17 09:03:38 +0200

    refactor(input-edit): remove obsolete dependency/component
    Refs: TOCDEV-2363

--- packages/input-edit/src/components/InputEdit/StyledInputEdit.js
+++ packages/input-edit/src/components/InputEdit/StyledInputEdit.js
@@ -1,14 +1,6 @@
 import styled, {css} from 'styled-components'
 import {scale, StyledScrollbar, theme} from 'tocco-ui'
 
-const drawerSymbol = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAeCAYAAADkftS9AAAAIklEQVQoU2M4c+bMfxAGAgYYmwGrIIiDjrELjpo5aiZeMwF+yNnOs5KSvgAAAABJRU5ErkJggg==' // eslint-disable-line max-len
-
-export const resizerStyle = {
-  background: `transparent url(${drawerSymbol}) 50% no-repeat`,
-  width: '11px',
-  cursor: 'col-resize'
-}
-
 export const StyledPaneWrapper = styled.div`
   display: flex;
   height: 100%;
