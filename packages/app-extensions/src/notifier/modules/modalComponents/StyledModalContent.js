756b0e40eb67c3fc11908f00d50093c20a73df5c
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2020-10-05 14:41:47 +0200

    refactor(app-extensions): refactor modal
    Refs: TOCDEV-2008

--- packages/app-extensions/src/notifier/modules/modalComponents/StyledModalContent.js
+++ packages/app-extensions/src/notifier/modules/modalComponents/StyledModalContent.js
@@ -9,36 +9,25 @@ import {StyledLayoutBox} from 'tocco-ui/src/Layout'
 import {StyledTitleWrapper} from '../../components/TitleMessage'
 import {StyledCloseButton} from './ModalContent'
 
-const modalWidth = 700
-const gutterWidth = 30
-const totalWidth = modalWidth + gutterWidth
-// eslint-disable-next-line max-len
-export const basePadding = 1.8 * parseFloat(getComputedStyle(document.documentElement).fontSize) // calc px value from 1.8rem
+const maxModalWidth = 700
+export const basePadding = scale.space(0.5)
 
 const StyledModalContent = styled.div.attrs(({isClosing}) => ({
   className: `rrt-confirm animated ${isClosing ? 'fadeOut' : 'fadeIn'}`
 }))`
   &&& {
+    display: flex;
+    flex-direction: column;
     background-color: ${theme.color('paper')};          // reset: react-redux-toastr (confirm.scss)
     border-radius: 0;           // reset: react-redux-toastr (confirm.scss)
     box-shadow: 2px 2px 10px rgba(0, 0, 0, .4);         // reset: react-redux-toastr (confirm.scss)
-    left: 0;                                            // reset: react-redux-toastr (confirm.scss)
-    margin-left: ${Math.ceil((gutterWidth - basePadding) / 2)}px;       // reset: react-redux-toastr (confirm.scss)
-    padding: 0 ${basePadding}px ${basePadding}px; // reset: react-redux-toastr (confirm.scss)
-    width: calc(100% - ${gutterWidth}px - ${basePadding}px);
+    padding: 0 ${basePadding} ${basePadding}; // reset: react-redux-toastr (confirm.scss)
+    width: auto;
+    max-width: ${maxModalWidth}px;
     ${StyledScrollbar}
-
-    @media (min-width: ${totalWidth}px) {
-      left: 50%;                                        // reset: react-redux-toastr (confirm.scss)
-      margin-left: -${Math.ceil((totalWidth + basePadding) / 2)}px;     // reset: react-redux-toastr (confirm.scss)
-      width: ${totalWidth}px;                           // reset: react-redux-toastr (confirm.scss)
-
-      && {
-        ${StyledTitleWrapper} {
-          max-width: ${totalWidth}px;
-        }
-      }
-    }
+    margin: auto;
+    left: 0;
+    right: 0;
 
     ${StyledCloseButton} {
       top: ${basePadding}px;  // reset
@@ -48,14 +37,15 @@ const StyledModalContent = styled.div.attrs(({isClosing}) => ({
     .advanced-search-button-wrapper {
       box-sizing: border-box;
       background: ${theme.color('paper')};
-      padding-top: ${basePadding}px;
+      padding-top: ${basePadding};
       padding-bottom: ${scale.space(0.2)};
       width: 100%;
       z-index: 1;
     }
 
     ${StyledTitleWrapper} {
-      max-width: calc(100% - ${gutterWidth}px - ${basePadding}px);
+      width: 100%;
+      max-width: ${maxModalWidth}px;
     }
 
     .advanced-search-button-wrapper {
@@ -68,7 +58,7 @@ const StyledModalContent = styled.div.attrs(({isClosing}) => ({
       position: sticky;
       bottom: 0;
       padding-top: ${scale.space(0)};
-      padding-bottom: ${basePadding}px;
+      padding-bottom: ${basePadding};
       background-color: ${theme.color('paper')};
       transform: translateY(1.8rem);
     }
