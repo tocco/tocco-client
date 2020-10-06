5333b5457ce193e637ad2c39ecba95ee73fa6531
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2020-10-06 10:14:36 +0200

    refactor(entity-list): style column picker
    Refs: TOCDEV-2437
    Changelog: Style column picker dialog inside modal

---
+++ packages/entity-list/src/components/ColumnPicker/StyledColumnPicker.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components'
+import {scale, theme} from 'tocco-ui'
+
+export const StyledUl = styled.ul`
+  list-style-type: none;
+  padding-left: ${scale.space(-0.5)};
+  margin-bottom: -${scale.space(0)};
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+`
+
+export const StyledCheckbox = styled.input`
+  vertical-align: top;
+  margin-right: ${scale.space(-1)};
+
+  &:hover {
+    cursor: pointer;
+  }
+`
+
+export const StyledButtonWrapper = styled.div`
+  position: sticky;
+  bottom: 0;
+  padding-top: ${scale.space(0)};
+  padding-bottom: ${scale.space(0)};
+  background-color: ${theme.color('paper')};
+  transform: translateY(1.8rem);
+`
