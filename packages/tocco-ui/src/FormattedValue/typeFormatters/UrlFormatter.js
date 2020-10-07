63e75aa578b57d7d2edb71fd54eceebd7504ff97
Daniel Keller <dkeller@tocco.ch>
2020-10-07 11:28:00 +0200

    refactor(tocco-ui): add breakwords property to url
    Refs: TOCDEV-2323
    Changelog: Restrict urls in table to one line

--- packages/tocco-ui/src/FormattedValue/typeFormatters/UrlFormatter.js
+++ packages/tocco-ui/src/FormattedValue/typeFormatters/UrlFormatter.js
@@ -3,12 +3,12 @@ import React from 'react'
 
 import Link from '../../Link'
 
-const UrlFormatter = props => (
-  <Link href={props.value} label={props.value}/>
-)
+const UrlFormatter = props =>
+  <Link href={props.value} label={props.value} breakWords={props.breakWords}/>
 
 UrlFormatter.propTypes = {
-  value: PropTypes.string
+  value: PropTypes.string,
+  breakWords: PropTypes.bool
 }
 
 export default UrlFormatter
