470538bff54149dc71dd7ff6a7c3732c7035c8a5
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2020-10-09 10:07:03 +0200

    refactor(entity-list): relocate styled components
    Refs: TOCDEV-2550

--- packages/entity-list/src/components/SearchFilterList/SearchFilterList.js
+++ packages/entity-list/src/components/SearchFilterList/SearchFilterList.js
@@ -1,63 +1,15 @@
-/* stylelint-disable no-descending-specificity */
 import React from 'react'
 import PropTypes from 'prop-types'
-import {Typography, Button, theme, StyledSpan, scale} from 'tocco-ui'
-import styled from 'styled-components'
+import {Typography} from 'tocco-ui'
 import {FormattedMessage} from 'react-intl'
 
-const SearchFilterListWrapper = styled.div`
-  margin-top: .4rem;
-  margin-bottom: .4rem;
-`
-
-const StyledSpanSearchFilter = styled(StyledSpan)`
-  width: 90%;
-`
-
-const StyledButton = styled(Button)`
-  && {
-    display: none;
-    background-color: transparent;
-    border: 0;
-    flex: 1;
-    height: auto;
-    border-radius: 0;
-    padding: 0 ${scale.space(-1)} 0 0;
-
-    &:hover * {
-      color: ${theme.color('secondary')};
-    }
-  }
-`
-
-export const StyledSearchFilterButton = styled.div`
-  border-radius: ${theme.radii('medium')};
-  display: flex;
-  padding: .3rem 0 .3rem 1rem;
-  margin-bottom: .2rem;
-  background-color: ${({active}) => active && theme.color('secondary')};
-  align-items: stretch;
-  justify-content: space-between;
-
-  && {
-    * {
-      color: ${({active}) => active && theme.color('paper')};
-    }
-  }
-
-  :hover {
-    ${StyledButton} {
-      display: flex;
-      justify-content: flex-end;
-    }
-    background-color: ${theme.color('secondaryLight')};
-
-    * {
-      color: ${theme.color('paper')};
-    }
-    cursor: pointer;
-  }
-`
+import {
+  SearchFilterListWrapper,
+  StyledSpanSearchFilter,
+  StyledButton,
+  StyledSearchFilterButton,
+  StyledMessageWrapper
+} from './StyledComponents'
 
 const SearchFilterButton = ({setActive, active, label}) =>
   <StyledSearchFilterButton active={active} onClick={() => setActive(!active)}>
@@ -81,14 +33,18 @@ const AdminSearchForm = ({searchFilters, setSearchFilterActive, executeSearch}) 
   if (!searchFilters) return null
 
   if (searchFilters.length === 0) {
-    return <div style={{paddingLeft: '8px'}}><Typography.I>
-      <FormattedMessage id="client.entity-list.noSearchFilters"/>
-    </Typography.I></div>
+    return <StyledMessageWrapper>
+      <Typography.I>
+        <FormattedMessage id="client.entity-list.noSearchFilters"/>
+      </Typography.I>
+    </StyledMessageWrapper>
   }
 
+  const sortedFilterList = [...searchFilters]
+    .sort((a, b) => a.defaultFilter ? -1 : a.sorting - b.sorting)
+
   return <SearchFilterListWrapper>
-    {searchFilters
-      .sort((a, b) => a.defaultFilter ? -1 : a.sorting - b.sorting)
+    {sortedFilterList
       .map(searchFilter =>
         <SearchFilterButton
           key={searchFilter.uniqueId}
