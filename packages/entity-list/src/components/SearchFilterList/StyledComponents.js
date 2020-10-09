e2df55e583f6e695cabb638c240ddcde387a570c
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2020-10-09 10:07:03 +0200

    refactor(entity-list): relocate styled components
    Refs: TOCDEV-2550

---
+++ packages/entity-list/src/components/SearchFilterList/StyledComponents.js
@@ -0,0 +1,61 @@
+/* stylelint-disable no-descending-specificity */
+import styled from 'styled-components'
+import {Button, scale, StyledSpan, theme} from 'tocco-ui'
+
+export const SearchFilterListWrapper = styled.div`
+  margin-top: .4rem;
+  margin-bottom: .4rem;
+`
+
+export const StyledSpanSearchFilter = styled(StyledSpan)`
+  width: 90%;
+`
+
+export const StyledButton = styled(Button)`
+  && {
+    display: none;
+    background-color: transparent;
+    border: 0;
+    flex: 1;
+    height: auto;
+    border-radius: 0;
+    padding: 0 ${scale.space(-1)} 0 0;
+
+    &:hover * {
+      color: ${theme.color('secondary')};
+    }
+  }
+`
+
+export const StyledSearchFilterButton = styled.div`
+  border-radius: ${theme.radii('medium')};
+  display: flex;
+  padding: .3rem 0 .3rem 1rem;
+  margin-bottom: .2rem;
+  background-color: ${({active}) => active && theme.color('secondary')};
+  align-items: stretch;
+  justify-content: space-between;
+
+  && {
+    * {
+      color: ${({active}) => active && theme.color('paper')};
+    }
+  }
+
+  :hover {
+    ${StyledButton} {
+      display: flex;
+      justify-content: flex-end;
+    }
+    background-color: ${theme.color('secondaryLight')};
+    cursor: pointer;
+
+    * {
+      color: ${theme.color('paper')};
+    }
+  }
+`
+
+export const StyledMessageWrapper = styled.div`
+  padding-left: 8px;
+`
