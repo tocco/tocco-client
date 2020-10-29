04342406ef56cb8b575d1defbed29a4b3e984adc
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2020-10-29 10:14:14 +0100

    refactor(admin): add favicons for all browsers
    Refs: TOCDEV-2630

--- packages/admin/src/components/LoginGuard/LoginGuard.js
+++ packages/admin/src/components/LoginGuard/LoginGuard.js
@@ -4,17 +4,24 @@ import {LoadMask} from 'tocco-ui'
 import {Helmet} from 'react-helmet'
 
 import Login from '../../components/Login'
-import fav from './favicon.ico'
 import Admin from '../Admin'
 
 const LoginGuard = ({doSessionCheck, loggedIn}) => {
-  useEffect(() => { doSessionCheck() }, [])
+  useEffect(() => {
+    doSessionCheck()
+  }, [])
 
   return (
     <div>
       <Helmet defer={false}>
         <title>Tocco</title>
-        <link rel="icon" type="image/png" href={fav} sizes="16x16"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png"/>
+        <link rel="manifest" href="./site.webmanifest"/>
+        <link rel="mask-icon" href="./safari-pinned-tab.svg" color="#5bbad5"/>
+        <meta name="msapplication-TileColor" content="#da532c"/>
+        <meta name="theme-color" content="#ffffff"/>
       </Helmet>
       <LoadMask required={[loggedIn !== undefined]} loadingText="Logging in...">
         <div>
