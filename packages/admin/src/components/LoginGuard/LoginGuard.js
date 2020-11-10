24840e2674a1e95dcec83cb775470aa76a6d5d17
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2020-11-10 14:58:27 +0100

    chore: add app-manifest-loader
    Refs: TOCDEV-2630

--- packages/admin/src/components/LoginGuard/LoginGuard.js
+++ packages/admin/src/components/LoginGuard/LoginGuard.js
@@ -6,6 +6,7 @@ import {Helmet} from 'react-helmet'
 import favApple from './apple-touch-icon.png'
 import fav32 from './favicon-32x32.png'
 import fav16 from './favicon-16x16.png'
+import manifest from './site.webmanifest'
 import safariPinned from './safari-pinned-tab.svg'
 import Login from '../../components/Login'
 import Admin from '../Admin'
@@ -22,7 +23,7 @@ const LoginGuard = ({doSessionCheck, loggedIn}) => {
         <link rel="apple-touch-icon" sizes="180x180" href={favApple}/>
         <link rel="icon" type="image/png" sizes="32x32" href={fav32}/>
         <link rel="icon" type="image/png" sizes="16x16" href={fav16}/>
-        <link rel="manifest" href="manifest.json"/>
+        <link rel="manifest" href={manifest}/>
         <link rel="mask-icon" href={safariPinned} color="#5bbad5"/>
         <meta name="msapplication-TileColor" content="#da532c"/>
         <meta name="theme-color" content="#ffffff"/>
