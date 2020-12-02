eff7757c8597ffda763457060c637ffc385f0b8f
Daniel Keller <dkeller@tocco.ch>
2020-12-02 17:04:44 +0100

    refactor(tocco-theme): adjust dark theme

--- packages/tocco-theme/src/ToccoTheme/darkTheme.js
+++ packages/tocco-theme/src/ToccoTheme/darkTheme.js
@@ -1,49 +1,56 @@
 export default {
   colors: {
-    paper: '#585858',
+    paper: '#131313',
     primary: '#A9A9A9',
-    text: '#080808',
+    secondary: '#11587f',
+    secondaryLight: '#022e40',
+    text: '#d5d5d5',
+    backgroundBody: '#131313',
+    backgroundBreadcrumbs: '##131313',
     signal: {
       danger: {
-        paper: '#7f0000',
-        text: '#f2e5e5'
+        paper: '#EF9A9A',
+        text: '#B71C1C'
       },
       info: {
-        paper: '#0a2663',
+        paper: '#81D4FA',
         text: '#0288D1'
       },
       success: {
-        paper: '#05400C',
-        text: '#A5D6A7'
+        paper: '#A5D6A7',
+        text: '#388E3C'
       },
       warning: {
-        paper: '#B48411',
-        text: '#b43211'
+        paper: '#FFF59D',
+        text: '#F57F17'
       }
     }
   },
   fontFamily: {
-    monospace: 'Menlo, Monaco, Consolas, "Courier New", monospace',
-    regular: '"Helvetica Neue", Helvetica, Arial, sans-serif',
-    url: ''
+    monospace: 'Ubuntu Mono, monospace',
+    regular: 'Ubuntu, sans-serif',
+    url: 'https://fonts.googleapis.com/css?family=Ubuntu+Mono:300,500|Ubuntu:300,500&display=swap'
   },
   fontSize: {
-    base: 1.4,
-    factor: 1.3
+    base: 1.3,
+    factor: 1.1
   },
   fontWeights: {
-    regular: 400,
-    bold: 700
+    regular: 300,
+    bold: 500
   },
   lineHeights: {
     dense: 1,
+    light: 1.2,
     regular: 1.4
   },
   radii: {
-    regular: '4px'
+    regular: '4px',
+    medium: '13px',
+    large: '25px'
   },
   space: {
-    base: 2,
+    base: 1.3,
     factor: 2
   }
 }
