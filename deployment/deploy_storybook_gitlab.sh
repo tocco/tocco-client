baf5590cf5a699e31977d04f0986c2b22efe2bf8
Daniel Keller <dkeller@tocco.ch>
2020-12-03 12:55:42 +0100

    chore: add gitlab ci config
    Refs: TOCDEV-2846

--- deployment/deploy_storybook_gitlab.sh
+++ deployment/deploy_storybook_gitlab.sh
@@ -2,23 +2,9 @@
 set -e # Exit with nonzero exit code if anything fails
 PAGES_BRANCH="gh-pages"
 
-if [ "$TRAVIS_PULL_REQUEST" != "false"  ]
-then
-  echo "This is a pull request. No deploy of showcase app!"
-  exit 0
-fi
-
-echo "Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc"
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-
 echo "open ssl connection"
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ./deployment/deploy_key.enc -out deploy_key -d
-chmod 600 deploy_key
 eval `ssh-agent -s`
-ssh-add deploy_key
+ssh-add <(echo "$GITHUB_DEPLOY_KEY" | base64 -d)
 
 echo "Clone ${PAGES_BRANCH} branch"
 cd dist
@@ -26,7 +12,7 @@ git clone https://github.com/tocco/tocco-client.git $PAGES_BRANCH --single-branc
 
 
 echo "Copy storybook to folder in repo"
-BRANCH_FOLDER_NAME="${TRAVIS_BRANCH//\//_}"
+BRANCH_FOLDER_NAME="${CI_COMMIT_BRANCH//\//_}"
 echo "folder name: ${BRANCH_FOLDER_NAME}"
 rm -rf $PAGES_BRANCH/$BRANCH_FOLDER_NAME
 mkdir $PAGES_BRANCH/$BRANCH_FOLDER_NAME
@@ -35,7 +21,7 @@ cd $PAGES_BRANCH
 
 echo "Commit and push"
 git config user.name "Travis CI"
-git config user.email "$GITLAB_USER_EMAIL"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
 
 git add -A
 rev=$(git rev-parse --short HEAD)
