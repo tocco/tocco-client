1b9c44ea4d95ea8fc7c4dcefd8b7809c346df953
Stefan Würsten <swuersten@tocco.ch>
2020-12-08 14:11:51 +0100

    refactor(merge): merge v2 selection
    - add logic for radio button and checkbox
    - add set target entity functionality
    - call merge action endpoint
    
    Refs: TOCDEV-2743

--- packages/merge/src/modules/merge/actions.js
+++ packages/merge/src/modules/merge/actions.js
@@ -1,7 +1,12 @@
 export const INITIALIZE = 'merge/INITIALIZE'
 export const SET_SELECTION = 'merge/SET_SELECTION'
 export const SET_SOURCE_DATA = 'merge/SET_SOURCE_DATA'
-export const EXECUTE_MERGE = 'merge/EXECUTE_MERGE'export const SET_MERGE_RESPONSE = 'merge/SET_MERGE_RESPONSE'
+export const SET_SELECTED_SINGLE = 'merge/SET_SELECTED_SINGLE'
+export const SET_SELECTED_MULTIPLE = 'merge/SET_SELECTED_MULTIPLE'
+export const SET_SELECTED_MULTIPLE_ALL = 'merge/SET_SELECTED_MULTIPLE_ALL'
+export const SET_TARGET_ENTITY = 'merge/SET_TARGET_ENTITY'
+export const EXECUTE_MERGE = 'merge/EXECUTE_MERGE'
+export const SET_MERGE_RESPONSE = 'merge/SET_MERGE_RESPONSE'
 
 export const initialize = () => ({
   type: INITIALIZE
@@ -18,6 +23,40 @@ export const setSourceData = sourceData => ({
   type: SET_SOURCE_DATA,
   payload: {
     sourceData
+  }
+})
+
+export const setSelectedSingle = (name, entityKey) => ({
+  type: SET_SELECTED_SINGLE,
+  payload: {
+    name,
+    entityKey
+  }
+})
+
+export const setSelectedMultiple = (name, entityKey, relatedEntityKey, isSelected) => ({
+  type: SET_SELECTED_MULTIPLE,
+  payload: {
+    name,
+    entityKey,
+    relatedEntityKey,
+    isSelected
+  }
+})
+
+export const setSelectedMultipleAll = (name, entityKey, isSelected) => ({
+  type: SET_SELECTED_MULTIPLE_ALL,
+  payload: {
+    name,
+    entityKey,
+    isSelected
+  }
+})
+
+export const setTargetEntity = entityKey => ({
+  type: SET_TARGET_ENTITY,
+  payload: {
+    entityKey
   }
 })
 
