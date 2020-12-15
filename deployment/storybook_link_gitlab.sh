a10f960500a07642cfa190e052ae88e951be8d26
Daniel Keller <dkeller@tocco.ch>
2020-12-15 13:23:19 +0100

    chore: fix deploy script for ci

--- deployment/storybook_link_gitlab.sh
+++ deployment/storybook_link_gitlab.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -eu # Exit with nonzero exit code if anything fails
-echo "$CI_EXTERNAL_PULL_REQUEST_IID"
-echo "$CI_EXTERNAL_PULL_REQUEST_SOURCE_REPOSITORY"
 
-BRANCH_NAME="${CI_EXTERNAL_PULL_REQUEST_IID//\//_}"
+echo "Commenting storybook url to pull request"
+BRANCH_NAME="${CI_EXTERNAL_PULL_REQUEST_SOURCE_BRANCH_NAME//\//_}"
 curl -H "Authorization: token ${GITHUB_BOT_TOKEN}" -X POST \
 -d "{\"body\": \"https://tocco.github.io/tocco-client/${BRANCH_NAME}\"}" \
 "https://api.github.com/repos/${CI_EXTERNAL_PULL_REQUEST_SOURCE_REPOSITORY}/issues/${CI_EXTERNAL_PULL_REQUEST_IID}/comments"
