557c52f91fe2f69f74d2aaf511a03307871fb07e
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2020-12-17 11:04:29 +0100

    refactor(tocco-ui): refactor components
    Refs: TOCDEV-2924
    Changelog: Refactor Layout components to use CSS Grid instead of JS for nested elements

--- packages/tocco-ui/src/Layout/LayoutContainer.js
+++ packages/tocco-ui/src/Layout/LayoutContainer.js
@@ -1,81 +1,30 @@
 import PropTypes from 'prop-types'
-import React from 'react'
+import React, {memo, useRef, useState, useEffect} from 'react'
 
 import StyledLayoutContainer from './StyledLayoutContainer'
-import {design} from '../utilStyles'
 
-/**
- * Use <Layout.Container/> to apply grid and gutter on <Layout.Box>.
- * Define how many cells are displayed per row and breakpoint.
- */
-class LayoutContainer extends React.PureComponent {
-  constructor(props) {
-    super(props)
-    this.ref = React.createRef()
-    this.state = {}
+const LayoutContainer = ({children}) => {
+  const ref = useRef(null)
+  const [containerWidth, setContainerWidth] = useState(0)
+
+  const setDeviceIndependentPixelWidth = () => {
+    setContainerWidth(Math.floor(ref.current.offsetWidth / window.devicePixelRatio))
   }
 
-  setDeviceIndependentPixelWidth = () => {
-    this.setState({
-      containerWidth: Math.floor(this.ref.current.offsetWidth / window.devicePixelRatio)
-    })
-  }
+  useEffect(() => {
+    setDeviceIndependentPixelWidth()
+    window.addEventListener('resize', setDeviceIndependentPixelWidth)
+  }, [])
 
-  componentDidMount() {
-    this.setDeviceIndependentPixelWidth()
-    window.addEventListener('resize', this.setDeviceIndependentPixelWidth)
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.setDeviceIndependentPixelWidth)
-  }
-
-  render() {
-    return (
-      <StyledLayoutContainer
-        ref={this.ref}
-        isNestedCorrectly={this.props.parent !== design.layout.CONTAINER}
-      >
-        {
-          React.Children.map(this.props.children, child =>
-            React.cloneElement(child, {
-              containerWidth: this.state.containerWidth,
-              maxCellsPerRow: this.props.maxCellsPerRow,
-              parent: design.layout.CONTAINER
-            })
-          )
-        }
-      </StyledLayoutContainer>
-    )
-  }
-}
-
-LayoutContainer.defaultProps = {
-  maxCellsPerRow: {
-    sm: 1,
-    md: 2,
-    lg: 3,
-    xl: 4
-  },
-  parent: design.layout.NONE
+  return (
+    <StyledLayoutContainer ref={ref} containerWidth={containerWidth}>
+      {children}
+    </StyledLayoutContainer>
+  )
 }
 
 LayoutContainer.propTypes = {
-  children: PropTypes.node,
-  /**
-   * Define how many cells are displayed per row at most.
-   * The space is distributed equally on cells.
-   * Remaining cells fill up the last row completely.
-   * Current breakpoints are 500, 1000 and 1500 density independent pixels.
-   */
-  maxCellsPerRow: PropTypes.shape({
-    sm: PropTypes.number,
-    md: PropTypes.number,
-    lg: PropTypes.number,
-    xl: PropTypes.number
-  }),
-  /* Internal mechanism to determine if nesting is correct. Do never set manually. */
-  parent: design.layoutPropTypes
+  children: PropTypes.node
 }
 
-export default LayoutContainer
+export default memo(LayoutContainer)
