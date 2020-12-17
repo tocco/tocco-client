2bb5c391b022bfb08974f8d401f577b4ed0b3c34
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2020-12-17 11:09:11 +0100

    refactor(app-extensions): fix positioning and z-index
    Refs: TOCDEV-2924
    Changelog: Fix positioning and z-index of ActionsWrapper

--- packages/app-extensions/src/form/StyledFormBuilder.js
+++ packages/app-extensions/src/form/StyledFormBuilder.js
@@ -6,6 +6,9 @@ export const StyledActionsWrapper = styled.div`
   padding-bottom: ${scale.space(-0.5)};
   display: flex;
   flex-wrap: wrap;
+  z-index: 2; // Higher than StyledIndicatorsContainerWrapper
+  position: sticky;
+  top: 0;
 
   & > * {
     margin-top: ${scale.space(-0.5)};
