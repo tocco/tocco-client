2bf602feb3c50ce9d7743369bb76a2c3514ec4d2
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2021-01-07 13:25:16 +0100

    refactor(admin): remove favicons
    Refs: TOCDEV-2630
    Changelog: Remove favicons inside react project, as they will loaded via the static index.html https://git.tocco.ch/c/nice2/+/39288

--- packages/admin/src/components/LoginGuard/LoginGuard.js
+++ packages/admin/src/components/LoginGuard/LoginGuard.js
@@ -3,12 +3,6 @@ import PropTypes from 'prop-types'
 import {LoadMask} from 'tocco-ui'
 import {Helmet} from 'react-helmet'
 
-import favApple from './apple-touch-icon.png'
-import fav32 from './favicon-32x32.png'
-import fav16 from './favicon-16x16.png'
-import fav from './favicon.ico'
-import manifest from './site.webmanifest'
-import safariPinned from './safari-pinned-tab.svg'
 import Login from '../../components/Login'
 import Admin from '../Admin'
 
@@ -21,14 +15,6 @@ const LoginGuard = ({doSessionCheck, loggedIn}) => {
     <div>
       <Helmet defer={false}>
         <title>Tocco</title>
-        <link rel="apple-touch-icon" sizes="180x180" href={favApple}/>
-        <link rel="icon" type="image/png" sizes="32x32" href={fav32}/>
-        <link rel="icon" type="image/png" sizes="16x16" href={fav16}/>
-        <link rel="icon" type="image/x-icon" href={fav}/>
-        <link rel="manifest" href={manifest}/>
-        <link rel="mask-icon" href={safariPinned} color="#5bbad5"/>
-        <meta name="msapplication-TileColor" content="#da532c"/>
-        <meta name="theme-color" content="#ffffff"/>
       </Helmet>
       <LoadMask required={[loggedIn !== undefined]} loadingText="Logging in...">
         <div>
