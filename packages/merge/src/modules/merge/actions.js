20ec90efd32f2ce7b91ed9dd360d49675821bea4
Stefan Würsten <swuersten@tocco.ch>
2021-01-18 14:37:56 +0100

    feat(merge): add navigation to entity list for many relations
    - different handling between new and old client
    - pass total number of all-or-nothing relation as separate property as the user may not has read access to all entities

--- packages/merge/src/modules/merge/actions.js
+++ packages/merge/src/modules/merge/actions.js
@@ -9,6 +9,7 @@ export const EXECUTE_MERGE = 'merge/EXECUTE_MERGE'
 export const SET_MERGE_RESPONSE = 'merge/SET_MERGE_RESPONSE'
 export const SET_MERGE_ERROR = 'merge/SET_MERGE_ERROR'
 export const CLOSE = 'merge/CLOSE'
+export const OPEN_ENTITY_LIST = 'merge/OPEN_ENTITY_LIST'
 
 export const initialize = () => ({
   type: INITIALIZE
@@ -83,4 +84,12 @@ export const setMergeError = (errorMsg, validationErrors) => ({
 
 export const close = () => ({
   type: CLOSE
+})
+
+export const openEntityList = (model, keys) => ({
+  type: OPEN_ENTITY_LIST,
+  payload: {
+    model,
+    keys
+  }
 })
