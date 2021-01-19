50daf062e23e687ce653fa4e99212c92536c7c44
Stefan Würsten <swuersten@tocco.ch>
2021-01-19 15:58:56 +0100

    fix(entity-list): fix sorting of search filters
    - a default filter is always listed before all other search filters
    
    Refs: TOCDEV-2804

--- packages/entity-list/src/components/SearchFilterList/utils.js
+++ packages/entity-list/src/components/SearchFilterList/utils.js
@@ -1,5 +1,5 @@
 export const searchFilterCompare = (a, b) => {
-  return a.defaultFilter
-    ? -1
+  return a.defaultFilter || b.defaultFilter
+    ? a.defaultFilter ? -1 : 1
     : (a.sorting === null) - (b.sorting === null) || a.sorting - b.sorting
 }
