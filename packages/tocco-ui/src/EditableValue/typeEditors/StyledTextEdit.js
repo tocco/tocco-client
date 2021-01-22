e50ac5505011ca624a29897c6c6f62d363e07901
Daniel Keller <dkeller@tocco.ch>
2021-01-22 16:30:13 +0100

    refactor(tocco-ui): change text component
    - replace auto size text component with a similar one. The previous components
      showed some performance problems in safari
    
    Refs: TOCDEV-3032
    Changelog: Text autosize new component (Fixes Safari performance problems)

--- packages/tocco-ui/src/EditableValue/typeEditors/StyledTextEdit.js
+++ packages/tocco-ui/src/EditableValue/typeEditors/StyledTextEdit.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import TextareaAutosize from 'react-autosize-textarea'
+import TextareaAutosize from 'react-textarea-autosize'
 import styled from 'styled-components'
 
 import {StyledInputCss} from '../StyledEditableValue'
