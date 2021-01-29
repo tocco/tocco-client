89d705e660fa2701f553a738d2274763a97fff67
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2021-01-29 09:21:12 +0100

    refactor(two-factor-connector): harmonize spacing
    Refs: TOCDEV-3048
    Changelog: Harmonize icon size and text spacing

--- packages/two-factor-connector/src/components/Secret/StyledComponents.js
+++ packages/two-factor-connector/src/components/Secret/StyledComponents.js
@@ -11,11 +11,4 @@ export const StyledParagraph = styled.div`
   display: flex;
   justify-content: center;
   margin-top: ${scale.space(1)};
-  margin-bottom: ${scale.space(-0.5)};
-`
-
-export const StyledBoldText = styled.div`
-  display: flex;
-  justify-content: center;
-  text-align: center;
 `
