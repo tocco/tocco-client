f34e24a169479d2b7c12f15c1c5882c6d86c1ec7
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2021-02-04 10:00:10 +0100

    refactor(two-factor-connector): harmonize spacing
    Refs: TOCDEV-3048
    Changelog: Harmonize text spacing in two factor connector

--- packages/two-factor-connector/src/components/Secret/StyledComponents.js
+++ packages/two-factor-connector/src/components/Secret/StyledComponents.js
@@ -4,11 +4,12 @@ import {scale} from 'tocco-ui'
 export const StyledQRCodeWrapper = styled.div`
   display: flex;
   justify-content: center;
-  margin: ${scale.space(1)};
+  margin: ${scale.space(1)} ${scale.space(1)} ${scale.space(1.5)} ${scale.space(1)};
 `
 
 export const StyledParagraph = styled.div`
   display: flex;
   justify-content: center;
-  margin-top: ${scale.space(1)};
+  margin-top: ${scale.space(-1)};
+  margin-bottom: ${scale.space(-0.9)};
 `
