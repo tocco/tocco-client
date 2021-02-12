d3aece64a3ca28be3a77cd8822f4e79475571251
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2021-02-12 14:19:23 +0100

    refactor(merge): style table header
    Refs: TOCDEV-3082
    Changelog: Style merge table header contents for better UX

--- packages/merge/src/components/MergeTable/StyledComponents.js
+++ packages/merge/src/components/MergeTable/StyledComponents.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import {theme} from 'tocco-ui'
 
 export const StyledLabelWrapper = styled.div`
   display: flex;
@@ -11,4 +12,10 @@ export const StyledLabelWrapper = styled.div`
 
 export const StyledTableWrapper = styled.div`
   flex: 1;
+`
+
+export const StyledStatusWrapper = styled.span`
+  color: ${({isChecked}) => isChecked
+    ? theme.color('signal.success.text')
+    : theme.color('signal.danger.text')};
 `
