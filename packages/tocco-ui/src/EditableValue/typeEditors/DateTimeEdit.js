584b511854230321044d0b46e72f979f1a793d4b
PadPalon <luca.neukom@gmail.com>
2021-02-15 15:56:16 +0100

    feat(tocco-ui): expand DateTimeEdit
    - parse manually entered dates
    - add button to clear field
    - handle Enter key the same as Tab
    
    Refs: TOCDEV-3091

--- packages/tocco-ui/src/EditableValue/typeEditors/DateTimeEdit.js
+++ packages/tocco-ui/src/EditableValue/typeEditors/DateTimeEdit.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types'
-import React from 'react'
+import React, {useMemo} from 'react'
 import {injectIntl, intlShape} from 'react-intl'
 import moment from 'moment'
 
@@ -7,13 +7,28 @@ import DateAbstract from './DateAbstract'
 import {atMostOne, momentJStoToFlatpickrFormat} from '../utils'
 
 export const DateTimeEdit = props => {
-  const altDateFormat = momentJStoToFlatpickrFormat(moment().locale(props.intl.locale)._locale.longDateFormat('L'))
-  const altTimeFormat = momentJStoToFlatpickrFormat(moment().locale(props.intl.locale)._locale.longDateFormat('LT'))
+  const momentDateFormat = moment().locale(props.intl.locale)._locale.longDateFormat('L')
+  const momentTimeFormat = moment().locale(props.intl.locale)._locale.longDateFormat('LT')
+  const altDateFormat = momentJStoToFlatpickrFormat(momentDateFormat)
+  const altTimeFormat = momentJStoToFlatpickrFormat(momentTimeFormat)
+
+  const parseDate = useMemo(() => s => {
+    const momentDate = moment(s, ['D.M.YYYY H:m', momentDateFormat, momentTimeFormat])
+    return momentDate.isValid() ? momentDate.toDate() : null
+  }, [])
+
+  const onBlur = (dateString, values, setValue) => {
+    const parsed = parseDate(dateString)
+    if (values[0] - parsed !== 0) {
+      setValue(parsed)
+    }
+  }
 
   const flatpickrOptions = {
     enableTime: true,
     time_24hr: true,
-    allowInput: false,
+    allowInput: true,
+    parseDate: parseDate,
     altFormat: `${altDateFormat} ${altTimeFormat}`,
     dateFormat: 'Y-m-d\\TH:i:S.000\\Z',
     ...(props.options ? props.options.flatpickrOptions : {})
@@ -25,6 +40,7 @@ export const DateTimeEdit = props => {
     <DateAbstract
       value={[props.value]}
       onChange={handleChange}
+      onBlur={onBlur}
       immutable={props.immutable}
       options={{...props.options, flatpickrOptions}}
       events={props.events}
