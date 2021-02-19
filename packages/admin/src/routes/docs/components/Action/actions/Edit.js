6d6a404ab63d196c13ad971fc5ca8f8e33943e17
Daniel Keller <dkeller@tocco.ch>
2021-02-19 17:01:36 +0100

    feat(admin): add dms edit action
    Refs: TOCDEV-3114
    Changelog: Add dms edit action

---
+++ packages/admin/src/routes/docs/components/Action/actions/Edit.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {injectIntl, intlShape} from 'react-intl'
+import EntityDetailApp from 'tocco-entity-detail/src/main'
+import {selection} from 'tocco-util'
+
+const EditAction = ({selection, onSuccess, onCancel, intl, context}) => {
+  const [entityName, entityKey] = selection.ids[0].split('/')
+
+  if (entityName === 'Resource') {
+    context.history.push(`/docs/doc/${entityKey}/detail`)
+    onCancel()
+  }
+
+  const handleEntityUpdated = () => {
+    const remoteEvents = [{
+      type: 'entity-update-event',
+      payload: {
+        entities: [{
+          entityName: 'Docs_list_item',
+          key: selection.ids[0]
+        }]
+      }
+    }]
+
+    onSuccess({
+      message: intl.formatMessage({id: 'client.admin.docs.editSuccessful'}),
+      remoteEvents
+    })
+  }
+
+  return <EntityDetailApp
+      entityName={entityName}
+      formName={`Dms${entityName}`}
+      entityId={entityKey}
+      mode="update"
+      onEntityUpdated={handleEntityUpdated}
+    />
+}
+
+EditAction.propTypes = {
+  selection: selection.propType,
+  context: PropTypes.shape({
+    history: PropTypes.shape({
+      push: PropTypes.func.isRequired
+    }).isRequired
+  }).isRequired,
+  onSuccess: PropTypes.func.isRequired,
+  onCancel: PropTypes.func.isRequired,
+  intl: intlShape.isRequired
+}
+
+export default injectIntl(EditAction)
