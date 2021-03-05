bfe026db810b10a4821c28bfe1d2bd576061321d
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2021-03-05 11:03:43 +0100

    refactor(tocco-ui): style and refactor load mask
    Refs: TOCDEV-3298
    Changelog: Prevent action buttons disappearing on scroll

--- packages/tocco-ui/src/LoadMask/StyledLoadMask.js
+++ packages/tocco-ui/src/LoadMask/StyledLoadMask.js
@@ -19,11 +19,24 @@ const fadeInAnimation = css`
   animation: ${fadeIn} 400ms ease-in-out both;
 `
 
+export const StyledLoadingIconAndTest = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: ${scale.space(-1)};
+  ${fadeInAnimation};
+
+  > ${StyledSpan} {
+    margin-top: ${scale.space(-1)};
+    z-index: 1;
+  }
+`
+
 const StyledLoadMask = styled.div`
   display: flex;
   flex-direction: column;
   flex: 1;
-  height: 100%;
 
   // Safari 10.1+ https://stackoverflow.com/questions/16348489/is-there-a-css-hack-for-safari-only-not-chrome
   @media not all and (min-resolution: .01dpcm) {
@@ -31,21 +44,6 @@ const StyledLoadMask = styled.div`
       transform: translateZ(0);
     }
   }
-  ${({isInitialized}) => !isInitialized && css`
-    && {
-      flex-flow: column nowrap;
-      justify-content: center;
-      align-items: center;
-      padding: ${scale.space(-1)};
-      width: 100%;
-      ${fadeInAnimation};
-
-      > ${StyledSpan} {
-        margin-top: ${scale.space(-1)};
-        z-index: 1;
-      }
-    }
-  `}
 `
 
 export default StyledLoadMask
