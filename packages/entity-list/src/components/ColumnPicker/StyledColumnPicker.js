f8f4df6280c2369be34786c9df38b03d9d3ed651
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2021-03-12 09:59:28 +0100

    refactor(entity-list): reset column picker label
    Refs: TOCDEV-3294
    Changelog: Reset label spacing inside column picker for old client

--- packages/entity-list/src/components/ColumnPicker/StyledColumnPicker.js
+++ packages/entity-list/src/components/ColumnPicker/StyledColumnPicker.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
-import {scale, theme} from 'tocco-ui'
+import {scale, theme, StyledButton} from 'tocco-ui'
+import {StyledLabel} from 'tocco-ui/src/Typography'
 
 export const StyledColumnPickerWrapper = styled.div`
   display: grid;
@@ -8,9 +9,14 @@ export const StyledColumnPickerWrapper = styled.div`
 
 export const StyledUl = styled.ul`
   list-style-type: none;
-  padding-left: ${scale.space(-0.5)};
+  margin-top: ${scale.space(-0.5)} !important; // Nice2 Reset
+  padding-left: ${scale.space(-0.5)} !important; // Nice2 Reset
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+
+  ${StyledLabel} {
+    padding: 0 !important; // Nice2 Reset
+  }
 `
 
 export const StyledCheckbox = styled.input`
@@ -27,6 +33,12 @@ export const StyledButtonWrapper = styled.div`
   bottom: 0;
   padding-top: ${scale.space(0)};
   background-color: ${theme.color('paper')};
+  display: flex;
+  justify-content: flex-end;
+
+  ${StyledButton} {
+    margin-right: 0;
+  }
 `
 
 export const StyledId = styled.span`
