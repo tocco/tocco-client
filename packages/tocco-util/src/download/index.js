fdb2dff93a95dd0325721949549831c327e94b97
PadPalon <luca.neukom@gmail.com>
2021-03-24 16:48:17 +0100

    feat(tocco-util): add method to download a stream as a file
    Refs: TOCDEV-3305

--- packages/tocco-util/src/download/index.js
+++ packages/tocco-util/src/download/index.js
@@ -1,7 +1,8 @@
-import {downloadUrl, openUrl, downloadSupportedByBrowser, addParameterToURL} from './download'
+import {downloadUrl, downloadReadableStream, openUrl, downloadSupportedByBrowser, addParameterToURL} from './download'
 
 export default {
   downloadUrl,
+  downloadReadableStream,
   openUrl,
   downloadSupportedByBrowser,
   addParameterToURL
