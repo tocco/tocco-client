c9f6b42c6325e4bb337ce5abe5cdbcb4ac39d84c
Gitlab CI <tocco.github.bot@gmail.com>
2021-03-25 07:44:29 +0000

    rebuild storybook at e406fac

---
+++ pr_bundle-optimization/22.623f5416174e99a74e1d.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[22],{3725:function(module,__webpack_exports__,__webpack_require__){"use strict";__webpack_require__.r(__webpack_exports__);var framer_motion__WEBPACK_IMPORTED_MODULE_0__=__webpack_require__(3744);__webpack_exports__.default=framer_motion__WEBPACK_IMPORTED_MODULE_0__.a}}]);
+//# sourceMappingURL=22.623f5416174e99a74e1d.bundle.js.map
