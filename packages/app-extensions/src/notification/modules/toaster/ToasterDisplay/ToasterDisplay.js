928f4ffe45db87cd150b7bac50b5a8fc3a8773da
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2021-04-08 10:46:24 +0200

    refactor(app-extensions): style notifciations
    Refs: TOCDEV-3445
    Changelog: Style new notification boxes

--- packages/app-extensions/src/notification/modules/toaster/ToasterDisplay/ToasterDisplay.js
+++ packages/app-extensions/src/notification/modules/toaster/ToasterDisplay/ToasterDisplay.js
@@ -1,37 +1,27 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import styled from 'styled-components'
-import {declareFont} from 'tocco-ui'
 
 import Toaster from './Toaster'
 import {ToasterPropType} from '../toaster'
-
-const StyledToasterBox = styled.div`
-  ${declareFont()};
-  position: absolute;
-  height: 100vh;
-  width: 200px;
-  right: 10px;
-  z-index: 99999999999;
-`
+import {StyledToasterBox} from './StyledComponents'
 
 const ToasterDisplay = ({toasters, removeToaster}) => {
   const toastersSorted = Object.values(toasters)
     .sort((n1, n2) => new Date(n1.timestamp) - new Date(n2.timestamp))
 
-  return (
-        <StyledToasterBox>
-            {toastersSorted.map(toaster => {
-              return <Toaster
-              key={`toaster-${toaster.key}`}
-              toaster={toaster}
-              closeToaster={(key, manually) => {
-                removeToaster(key, manually)
-              }}
-              />
-            })}
-        </StyledToasterBox>
-  )
+  return <>
+    {toasters && <StyledToasterBox>
+      {toastersSorted.map(toaster => {
+        return <Toaster
+          key={`toaster-${toaster.key}`}
+          toaster={toaster}
+          closeToaster={(key, manually) => {
+            removeToaster(key, manually)
+          }}
+        />
+      })}
+    </StyledToasterBox>}
+  </>
 }
 
 ToasterDisplay.propTypes = {
