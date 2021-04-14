eba6415c4bce959c47f462cc1ebe2ae8b698074f
Gitlab CI <tocco.github.bot@gmail.com>
2021-04-14 14:22:17 +0000

    rebuild storybook at 4a9a27b

---
+++ pr_admin-dms-fix/21.06660f8900e0e3a3f2ae.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[21],{3743:function(module,__webpack_exports__,__webpack_require__){"use strict";__webpack_require__.r(__webpack_exports__);var tocco_entity_detail_src_main__WEBPACK_IMPORTED_MODULE_0__=__webpack_require__(3663);__webpack_require__.d(__webpack_exports__,"default",(function(){return tocco_entity_detail_src_main__WEBPACK_IMPORTED_MODULE_0__.a}))}}]);
+//# sourceMappingURL=21.06660f8900e0e3a3f2ae.bundle.js.map
