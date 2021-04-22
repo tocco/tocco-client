8a584d87c7e33af636f0ad3e5aa4db80f5f81ecd
Gitlab CI <tocco.github.bot@gmail.com>
2021-04-22 08:59:38 +0000

    rebuild storybook at 6d1a436

---
+++ pr_table-padding/21.1d9b2dfa257367cbec56.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[21],{3743:function(module,__webpack_exports__,__webpack_require__){"use strict";__webpack_require__.r(__webpack_exports__);var tocco_entity_detail_src_main__WEBPACK_IMPORTED_MODULE_0__=__webpack_require__(3663);__webpack_require__.d(__webpack_exports__,"default",(function(){return tocco_entity_detail_src_main__WEBPACK_IMPORTED_MODULE_0__.a}))}}]);
+//# sourceMappingURL=21.1d9b2dfa257367cbec56.bundle.js.map
