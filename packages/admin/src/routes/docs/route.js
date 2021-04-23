6b25768c2963623ea5653053a4e38ddfad5c0df0
Daniel Keller <dkeller@tocco.ch>
2021-04-23 09:36:20 +0200

    refactor(admin): adjust docs navigation Strategy
    - Edit ListLink to open docs-browser
    - add query params support for rootnodes
    
    Refs: TOCDEV-3350

--- packages/admin/src/routes/docs/route.js
+++ packages/admin/src/routes/docs/route.js
@@ -1,14 +1,38 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import DocsBrowser from 'tocco-docs-browser/src/main'
+import {AdminLink as StyledLink} from 'tocco-ui'
+import queryString from 'query-string'
 
-import {ListLink, DetailLink} from '../entities/utils/navigationStrategy'
+import {DetailLink} from '../entities/utils/navigationStrategy'
 
-const DocsBrowserApp = props => (
-  <DocsBrowser {...props} navigationStrategy={{
-    ListLink,
-    DetailLink
-  }}/>
-)
+export const ListLink = ({entityName, entityKeys, children}) => {
+  const rootNodes = entityKeys.map(key => ({entityName, key}))
+  return <StyledLink to={`/docs?rootNodes=${JSON.stringify(rootNodes)}`} target="_blank">{children}</StyledLink>
+}
+
+ListLink.propTypes = {
+  entityName: PropTypes.string.isRequired,
+  children: PropTypes.any.isRequired,
+  entityKeys: PropTypes.arrayOf(PropTypes.string)
+}
+
+const DocsBrowserApp = props => {
+  const queryParams = queryString.parse(props.history.location.search)
+
+  return <DocsBrowser
+    {...props}
+    navigationStrategy={{
+      ListLink,
+      DetailLink
+    }}
+    {...queryParams.rootNodes && {rootNodes: JSON.parse(queryParams.rootNodes)}}
+/>
+}
+
+DocsBrowserApp.propTypes = {
+  history: PropTypes.object.isRequired
+}
 
 export default {
   container: DocsBrowserApp
