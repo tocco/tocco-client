0466e6a0c1060b120e59c08a6e7ec608170fa26b
Gitlab CI <tocco.github.bot@gmail.com>
2021-04-27 05:47:12 +0000

    rebuild storybook at f0372fc

---
+++ pr_table-padding/23.105671b5b040be3ba693.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[23],{3730:function(module,__webpack_exports__,__webpack_require__){"use strict";__webpack_require__.r(__webpack_exports__);var framer_motion__WEBPACK_IMPORTED_MODULE_0__=__webpack_require__(3750);__webpack_exports__.default=framer_motion__WEBPACK_IMPORTED_MODULE_0__.a}}]);
+//# sourceMappingURL=23.105671b5b040be3ba693.bundle.js.map
