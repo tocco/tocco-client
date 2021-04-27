bbfc1018d248559990f7c9e500be0fb4d379f6b9
PadPalon <luca.neukom@gmail.com>
2021-04-27 14:05:27 +0200

    fix(input-edit): only load data when selection update is done
    Refs: TOCDEV-3491

--- packages/input-edit/src/modules/inputEdit/actions.js
+++ packages/input-edit/src/modules/inputEdit/actions.js
@@ -1,6 +1,7 @@
 export const SET_SELECTION = 'inputEdit/SET_SELECTION'
 export const UPDATE_SELECTION = 'inputEdit/UPDATE_SELECTION'
 export const SET_HANDLE_NOTIFICATIONS = 'inputEdit/SET_HANDLE_NOTIFICATIONS'
+export const SET_UPDATE_IN_PROGRESS = 'inputEdit/SET_UPDATE_IN_PROGRESS'
 
 export const setSelection = selection => ({
   type: SET_SELECTION,
@@ -16,5 +17,12 @@ export const setHandleNotifications = handleNotifications => ({
   type: SET_HANDLE_NOTIFICATIONS,
   payload: {
     handleNotifications
+  }
+})
+
+export const setSelectionUpdateInProgress = updateInProgress => ({
+  type: SET_UPDATE_IN_PROGRESS,
+  payload: {
+    updateInProgress
   }
 })
--- packages/input-edit/src/modules/inputEdit/reducer.js
+++ packages/input-edit/src/modules/inputEdit/reducer.js
@@ -1,15 +1,17 @@
 import {reducer as reducerUtil} from 'tocco-util'
 
-import {SET_SELECTION, SET_HANDLE_NOTIFICATIONS} from './actions'
+import {SET_SELECTION, SET_HANDLE_NOTIFICATIONS, SET_UPDATE_IN_PROGRESS} from './actions'
 
 const initialState = {
   selection: null,
-  handleNotifications: false
+  handleNotifications: false,
+  updateInProgress: false
 }
 
 const ACTION_HANDLERS = {
   [SET_SELECTION]: reducerUtil.singleTransferReducer('selection'),
-  [SET_HANDLE_NOTIFICATIONS]: reducerUtil.singleTransferReducer('handleNotifications')
+  [SET_HANDLE_NOTIFICATIONS]: reducerUtil.singleTransferReducer('handleNotifications'),
+  [SET_UPDATE_IN_PROGRESS]: reducerUtil.singleTransferReducer('updateInProgress')
 }
 
 export default function reducer(state = initialState, action) {
