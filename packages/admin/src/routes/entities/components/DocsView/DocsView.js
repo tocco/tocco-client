e0bc85f0d36c0204fda421a15096083f12a3d4dd
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2021-05-04 11:29:52 +0200

    refactor(docs-browser): adjust breadcrumbs padding
    Refs: TOCDEV-3571
    Changelog: Adjust left padding of Breadcrumbs in list view

--- packages/admin/src/routes/entities/components/DocsView/DocsView.js
+++ packages/admin/src/routes/entities/components/DocsView/DocsView.js
@@ -3,7 +3,7 @@ import DocsBrowserApp from 'tocco-docs-browser/src/main'
 import {rest} from 'tocco-app-extensions'
 import PropTypes from 'prop-types'
 
-const DocsView = ({entityName, entityKey, showActions}) => {
+const DocsView = ({entityName, entityKey, showActions, hasLeftPadding}) => {
   const [folderKey, setFolderKey] = useState(null)
 
   const fetchFolder = async() => {
@@ -16,24 +16,30 @@ const DocsView = ({entityName, entityKey, showActions}) => {
   useEffect(() => {
     fetchFolder()
   }, [])
-    
+
   return folderKey
     && <DocsBrowserApp
-        searchFormType="none"
-        rootNodes={[
-          {key: folderKey, entityName: 'Folder'}
-        ]}
-        embedded={true}
-        showActions={showActions}
-        {...showActions === false ? {selectionStyle: 'none'} : {}}
-        disableViewPersistor={true}
-      />
+      hasLeftPadding={hasLeftPadding}
+      sortable={false}
+      searchFormType="none"
+      rootNodes={[
+        {
+          key: folderKey,
+          entityName: 'Folder'
+        }
+      ]}
+      embedded={true}
+      showActions={showActions}
+      {...showActions === false ? {selectionStyle: 'none'} : {}}
+      disableViewPersistor={true}
+    />
 }
 
 DocsView.propTypes = {
   entityName: PropTypes.string.isRequired,
   entityKey: PropTypes.string.isRequired,
-  showActions: PropTypes.bool.isRequired
+  showActions: PropTypes.bool.isRequired,
+  hasLeftPadding: PropTypes.bool
 }
 
 export default DocsView
