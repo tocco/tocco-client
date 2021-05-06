8060380fdac52086841349c5b99fe70d70a46f46
Gitlab CI <tocco.github.bot@gmail.com>
2021-05-06 07:58:30 +0000

    rebuild storybook at cf2f131

---
+++ master/20.3329eb631fb1e3f8bb1c.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[20],{3739:function(module,__webpack_exports__,__webpack_require__){"use strict";__webpack_require__.r(__webpack_exports__);var tocco_entity_list_src_main__WEBPACK_IMPORTED_MODULE_0__=__webpack_require__(635);__webpack_require__.d(__webpack_exports__,"default",(function(){return tocco_entity_list_src_main__WEBPACK_IMPORTED_MODULE_0__.a}))}}]);
+//# sourceMappingURL=20.3329eb631fb1e3f8bb1c.bundle.js.map
