69d65cd21189fa9c90b39754862be20f2269e5df
Gitlab CI <tocco.github.bot@gmail.com>
2021-05-07 16:56:29 +0000

    rebuild storybook at df9102a

---
+++ dependabot_npm_and_yarn_ua-parser-js-0.7.28/25.5a2d77b4381e2c5ab617.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[25],{3662:function(module,__webpack_exports__,__webpack_require__){"use strict";__webpack_require__.r(__webpack_exports__);var framer_motion__WEBPACK_IMPORTED_MODULE_0__=__webpack_require__(3786);__webpack_exports__.default=framer_motion__WEBPACK_IMPORTED_MODULE_0__.a}}]);
+//# sourceMappingURL=25.5a2d77b4381e2c5ab617.bundle.js.map
