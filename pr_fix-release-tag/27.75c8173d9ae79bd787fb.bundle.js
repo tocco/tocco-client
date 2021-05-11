dc8c354cf6324240451a76a8a3e12a41af6c0175
Gitlab CI <tocco.github.bot@gmail.com>
2021-05-11 21:35:13 +0000

    rebuild storybook at f09dc5b

---
+++ pr_fix-release-tag/27.75c8173d9ae79bd787fb.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[27],{3775:function(module,__webpack_exports__,__webpack_require__){"use strict";__webpack_require__.r(__webpack_exports__);var framer_motion__WEBPACK_IMPORTED_MODULE_0__=__webpack_require__(3790);__webpack_exports__.default=framer_motion__WEBPACK_IMPORTED_MODULE_0__.a}}]);
+//# sourceMappingURL=27.75c8173d9ae79bd787fb.bundle.js.map
