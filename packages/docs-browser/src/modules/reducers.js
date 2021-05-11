086fed692a618626a69167a58ad66e9377047b04
Stefan Würsten <swuersten@tocco.ch>
2021-05-11 07:36:00 +0200

    fix(docs-browser): fix navigation problem in move action
    parent was changed before form name was updated but docsView was only rerendered if parent was changed. instead of passing the form name directly pass a function which determine the form name which results in a synchronous update of the parent and form name
    
    Refs: TOCDEV-3623

--- packages/docs-browser/src/modules/reducers.js
+++ packages/docs-browser/src/modules/reducers.js
@@ -2,11 +2,11 @@ import {combineReducers} from 'redux'
 
 import path, {sagas as pathSagas} from './path'
 import create, {sagas as createSagas} from './create'
-import list, {sagas as listSagas} from './list'
+import {sagas as listSagas} from './list'
 import move, {sagas as moveSagas} from './move'
 
 export default {
-  docs: combineReducers({path, create, list, move})
+  docs: combineReducers({path, create, move})
 }
 
 export const sagas = [pathSagas, createSagas, listSagas, moveSagas]
