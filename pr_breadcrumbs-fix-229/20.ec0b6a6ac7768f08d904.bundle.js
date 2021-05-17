a64061bbe0cdd7cba85a59576565b0bea8a66b44
Gitlab CI <tocco.github.bot@gmail.com>
2021-05-17 10:49:59 +0000

    rebuild storybook at c23dfbe

---
+++ pr_breadcrumbs-fix-229/20.ec0b6a6ac7768f08d904.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[20],{3740:function(module,__webpack_exports__,__webpack_require__){"use strict";__webpack_require__.r(__webpack_exports__);var tocco_entity_list_src_main__WEBPACK_IMPORTED_MODULE_0__=__webpack_require__(635);__webpack_require__.d(__webpack_exports__,"default",(function(){return tocco_entity_list_src_main__WEBPACK_IMPORTED_MODULE_0__.a}))}}]);
+//# sourceMappingURL=20.ec0b6a6ac7768f08d904.bundle.js.map
