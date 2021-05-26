19abce6da6cc59b73a941e6f4d53d4a1ee0277c0
Gitlab CI <tocco.github.bot@gmail.com>
2021-05-26 12:40:45 +0000

    rebuild storybook at 7f6fb5a

---
+++ pr_info-fix/32.bacc2337.iframe.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[32],{4938:function(module,__webpack_exports__,__webpack_require__){"use strict";__webpack_require__.r(__webpack_exports__);var framer_motion__WEBPACK_IMPORTED_MODULE_0__=__webpack_require__(5155);__webpack_exports__.default=framer_motion__WEBPACK_IMPORTED_MODULE_0__.a}}]);
