d888d37807ef9183cbe83d455a0e009b22e585c5
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2021-05-28 10:40:58 +0200

    refactor(entity-list): modal button alignment
    Refs: TOCDEV-3688
    Changelog: Adjust spacing and alignment of button inside modal

---
+++ packages/entity-list/src/modules/searchForm/StyledComponents.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+import {Button, scale} from 'tocco-ui'
+
+export const StyledButton = styled(Button)`
+  float: right;
+  margin-top: ${scale.space(0)};
+  margin-right: 0;
+`
