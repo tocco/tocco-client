d21f2d8264c785392f7d1ccfa92a86353cf4b5ec
Daniel Keller <dkeller@tocco.ch>
2021-06-07 11:46:41 +0200

    chore: copy scripts from master
    inlucing tagging and cherry-pick

---
+++ scripts/tagging.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# This script will look for release commits since the last release tag on the current branch and 
+# then create the missing tags. Thus all releases have tags that dont have to be merged within the according commit.
+
+echo "---Release Tagging Script---"
+source ./scripts/github.sh
+setupGithub
+
+echo "Cloning branch $CI_COMMIT_BRANCH"
+git clone git@github.com:tocco/tocco-client.git ../github --single-branch --branch $CI_COMMIT_BRANCH --depth 50
+cd ../github
+
+latest_tag=$(git describe --tags --abbrev=0 --match "*@[0-9]*.[0-9]*.[0-9]*")
+echo "Latest tag found on branch: $latest_tag"
+matching_commit=$(git log -1 --format=%H $latest_tag)
+echo "Matching commit: $matching_commit"
+
+for commit in $(git rev-list $matching_commit..HEAD)
+do
+    msg="$(git log --format=%s -n 1 $commit)"
+    full_msg="$(git log --format=%B -n 2 $commit)"
+    isPublish="$(echo $msg | grep -c 'chore: publish')"
+    echo "found commit $isPublish: $msg"
+    if [ $isPublish == 1 ]; then
+        echo "Commit '$msg' ($commit) is a release commit"
+        tag_name="$(echo "$full_msg" | grep -e '- ' | sed 's/- //g')"
+
+        echo "Delete existing tag _$tag_name"
+        git push --delete origin _$tag_name
+
+        echo "Create tag  '$tag_name'"
+        git tag -f $tag_name $commit 
+    fi
+done
+echo "pushing tags..."
+git push --tags
