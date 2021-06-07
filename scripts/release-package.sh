d4274f28bb11470b7701f6ec38d39ca354c882a4
Daniel Keller <dkeller@tocco.ch>
2021-06-07 11:46:41 +0200

    chore: copy scripts from master
    inlucing tagging and cherry-pick

--- scripts/release-package.sh
+++ scripts/release-package.sh
@@ -1,18 +1,50 @@
 #!/bin/bash
-
 package=$1
 
 source ./scripts/helpers.sh
 setColors
 checkPackage
 setGitVars
+setNiceVersion
+setCurrentReleaseTag
+setNextVersion
+
+git fetch --tags -f
+
+if [[ $* == *--auto* ]]; then
+  auto=true
+  echo "Questions are disabled and automatically with default values answered"
+else
+  auto=false
+fi
+
+if [[ -z $(git status -s) ]]
+then
+  echo "${color_green}Git tree is clean!${color_reset}"
+else
+  echo "${color_red}Git tree is dirty, please commit changes before running the release script.${color_reset}"
+  exit 1
+fi
+
+if [[ -z "${changelog}" && $auto = true ]]; then
+	echo "${color_red}Skip package ${package} because changelog is empty. ${color_reset}"
+	exit 0
+fi
 
 echo "---------------------"
 echo "info latest release tag: ${color_blue}${last_release_tag}${color_reset}"
 echo "info latest version: ${color_blue}${last_version}${color_reset}"
+echo "release tag: ${color_blue}${release_tag}${color_reset}"
 echo -e  "Generated changelog:\n${color_blue}${changelog}${color_reset}"
 
-read -p "question New version: : " new_version
+if [[ $auto = false ]]; then
+  read -p "New version [${next_version}] : " new_version
+fi
+
+if [[ -z "${new_version}" ]]; then
+	new_version="${next_version}"
+	echo "New version is ${new_version}"
+fi
 
 cd packages/${package}
 changelog_file="./changelog.md"
@@ -23,28 +55,55 @@ if [[ ! -e "${changelog_file}" ]]; then
 fi
 
 (echo  -e "${new_version}\n${changelog}\n" && cat "${changelog_file}") > "${tmp_file}" && mv "${tmp_file}" "${changelog_file}"
-read -p "${color_green}Edit the changelog and press ENTER to continue${color_reset}"
+
+if [[ $auto = true ]]; then
+  echo "Skip editing changelog"
+else
+  read -p "${color_green}Edit the changelog and press ENTER to continue${color_reset}"
+fi
+
+if [[ $auto = false ]]; then
+  targetBranch=releasing/${package}@${new_version}
+  echo "Checkin out new branch ${targetBranch}"
+  git checkout -b ${targetBranch}
+fi
 
 git commit -m "docs(${package}): changelog ${new_version}" ${changelog_file}
 echo "releasing and publishing ${package} with version ${new_version}"
 yarn publish --new-version ${new_version}
 
-read -p "Push commits and tags (y/n)?" CONT
-if [ "$CONT" = "y" ]; then
-  git push
+if [[ $auto = true ]]; then
+  PUSH="n"
+  echo "Pushing commits and tags is skipped"
+else
+  read -p "Push commits and tags (y/n)?" PUSH
+fi
+
+if [ "$PUSH" = "y" ]; then
   git push --tags
-  echo "${color_green}Commits and tags pushed!${color_reset}"
+  git push
+  echo "${color_green}Commits and tags pushed to ${targetBranch}!${color_reset}"
 else
   echo "${color_red}Nothing pushed!${color_reset}"
 fi
 
-read -p "Create a npm dist tag? (type n for 'no' or the tag name for yes (e.g. nice227) " TAG
-if [ "$TAG" = "n" ]; then
-  echo "${color_red}No npm tag created!${color_reset}"
+if [[ $auto = false ]]; then
+  echo "Checkin out ${current_branch}"
+  git checkout ${current_branch}
+fi
+
+if [[ $auto = true ]]; then
+  CREATE_TAG="y"
 else
-  echo "Trying to execute: npm dist-tag add tocco-${package}@${new_version} ${TAG}"
-  npm dist-tag add tocco-${package}@${new_version} ${TAG}
+  read -p "Create a npm dist tag ${release_tag} for current version (y/n)?" CREATE_TAG
+fi
+
+if [ "$CREATE_TAG" = "y" ]; then
+  echo "Trying to execute: npm dist-tag add tocco-${package}@${new_version} ${release_tag}"
+  npm dist-tag add tocco-${package}@${new_version} ${release_tag} --registry=https://registry.npmjs.org/
   echo "${color_green}Npm tag created!${color_reset}"
+else
+  echo "${color_red}No npm tag created!${color_reset}"
 fi
 
 echo "${color_green}Done!${color_reset}"
