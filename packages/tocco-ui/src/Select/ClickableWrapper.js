69d71d6b81cbfdc473841b6a979dc3d30fef444d
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2021-06-09 11:26:51 +0200

    refactor(tocco-ui): improve input field UX
    Refs: TOCDEV-3762
    Changelog: Improve input field UX by better highlighting fields and labels

--- packages/tocco-ui/src/Select/ClickableWrapper.js
+++ packages/tocco-ui/src/Select/ClickableWrapper.js
@@ -1,6 +1,11 @@
 import styled from 'styled-components'
 
+import {scale} from '../utilStyles'
+
 export default styled.span`
+  * {
+    font-size: ${scale.font(1.5)};
+  }
   pointer-events: auto;
   cursor: pointer;
 `
