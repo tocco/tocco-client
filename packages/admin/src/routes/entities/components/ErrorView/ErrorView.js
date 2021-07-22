6538b9e72816f1cafdc4b62b659f5c91869b87e5
Daniel Keller <dkeller@tocco.ch>
2021-07-22 13:59:01 +0200

    refactor(admin): improve error handling
    - show breadcrumbs if entity or a record could not be loaded
    - better messages if an error occured
    - move error view to entity route
    
    Refs: TOCDEV-3949
    Changelog: Better entity error handling

--- packages/admin/src/routes/entities/components/ErrorView/ErrorView.js
+++ packages/admin/src/routes/entities/components/ErrorView/ErrorView.js
@@ -4,6 +4,8 @@ import {FormattedMessage} from 'react-intl'
 import {Typography, Icon, theme, scale} from 'tocco-ui'
 import styled from 'styled-components'
 
+import {currentViewPropType} from '../../utils/propTypes'
+
 const StyledErrorView = styled.div`
   width: 50vw;
   max-width: 650px;
@@ -21,21 +23,33 @@ const StyledIconWrapper = styled.span`
   font-size: ${scale.font(13.4)};
 `
 
-const ErrorView = ({message, technicalReason}) => {
+const ErrorView = ({currentViewInfo}) => {
+  const prefix = 'client.admin.entity.errorView.'
+  const title = currentViewInfo.type === 'detail'
+    ? 'detailTitle'
+    : 'defaultTitle'
+
+  const description = currentViewInfo.type === 'detail'
+    ? 'detailDescription'
+    : currentViewInfo.type === 'list' && currentViewInfo.error.relationName
+      ? 'relationDescription'
+      : 'listDescription'
+
   return <StyledErrorView>
     <StyledIconWrapper>
       <Icon icon="times"/>
     </StyledIconWrapper>
     <Typography.H1>
-      <FormattedMessage id={message || 'client.admin.errorView.defaultTitle'}/>
+      <FormattedMessage id={prefix + title}/>
     </Typography.H1>
-    <Typography.Span>{technicalReason}</Typography.Span>
+    <Typography.Span><FormattedMessage id={prefix + description} values={currentViewInfo.error}/></Typography.Span>
   </StyledErrorView>
 }
 
 ErrorView.propTypes = {
   technicalReason: PropTypes.string,
-  message: PropTypes.string
+  message: PropTypes.string,
+  currentViewInfo: currentViewPropType
 }
 
 export default ErrorView
