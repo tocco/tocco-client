b1e3f78209925eb003a84ce69d94a007ed70908f
Gitlab CI <tocco.github.bot@gmail.com>
2021-07-29 14:16:51 +0000

    rebuild storybook at 557940e

---
+++ releasing__nice-releases_229_docs-browser@0.2.4-hotfix229.7/20.0dd858fca0d26602f2b4.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[20],{3740:function(module,__webpack_exports__,__webpack_require__){"use strict";__webpack_require__.r(__webpack_exports__);var tocco_entity_list_src_main__WEBPACK_IMPORTED_MODULE_0__=__webpack_require__(635);__webpack_require__.d(__webpack_exports__,"default",(function(){return tocco_entity_list_src_main__WEBPACK_IMPORTED_MODULE_0__.a}))}}]);
+//# sourceMappingURL=20.0dd858fca0d26602f2b4.bundle.js.map
