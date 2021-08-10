bfc9da24210fc633e2e9380ab242e14886cb3ac4
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2021-08-10 09:23:22 +0200

    refactor(tocco-ui): change link style
    Refs: TOCDEV-4081
    Changelog: Add underline to links

--- packages/tocco-ui/src/AdminLink/linkStyle.js
+++ packages/tocco-ui/src/AdminLink/linkStyle.js
@@ -5,27 +5,25 @@ import {declareFont, shadeColor, theme} from '../utilStyles'
 
 export const linkStyle = css`
   ${declareFont()}
-  text-decoration: none;
   color: ${theme.color('text')};
 
   * {
     color: ${theme.color('text')};
-    text-decoration: none;
+    text-decoration: ${({neutral}) => neutral ? 'none' : 'underline'};
   }
 
   &:hover,
   &:hover *,
   &:focus,
   &:focus * {
-    color: ${theme.color('secondaryLight')};
-    text-decoration: ${props => props.neutral ? 'none' : 'underline'};
+    text-decoration: ${({neutral}) => neutral ? 'none' : 'underline'};
   }
 
   &:active,
   &:active * {
-    color: ${props => props.neutral
-              ? shadeColor(_get(props.theme, 'colors.text'), 2)
-              : shadeColor(_get(props.theme, 'colors.primary'), 2)};
-    text-decoration: ${props => props.neutral ? 'none' : 'underline'};
+    color: ${({neutral, theme}) => neutral
+              ? shadeColor(_get(theme, 'colors.text'), 2)
+              : shadeColor(_get(theme, 'colors.primary'), 2)};
+    text-decoration: ${({neutral}) => neutral ? 'none' : 'underline'};
   }
 `
