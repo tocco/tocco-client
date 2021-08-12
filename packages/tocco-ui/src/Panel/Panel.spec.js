93de0388c99902f39442b4a848a99361dada114d
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2021-08-12 07:46:47 +0200

    chore: fix test
    Refs: TOCDEV-4083

--- packages/tocco-ui/src/Panel/Panel.spec.js
+++ packages/tocco-ui/src/Panel/Panel.spec.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import {mount} from 'enzyme'
+import {TestThemeProvider} from 'tocco-test-util'
 
 import Panel from './'
 import Typography from '../Typography'
 import Icon from '../Icon'
-import Ball from '../Ball'
 
 const panelIsOpen = wrapper => wrapper.find(Icon).props().icon === 'chevron-up'
 
@@ -13,57 +13,64 @@ describe('tocco-ui', () => {
     describe('Panel', () => {
       test('should render a open panel by default', () => {
         const wrapper = mount(
-          <Panel.Wrapper>
-            <Panel.Header>
-              <Typography.H4>Title</Typography.H4>
-            </Panel.Header>
-            <div>body</div>
-          </Panel.Wrapper>)
+          <TestThemeProvider>
+            <Panel.Wrapper>
+              <Panel.Header>
+                <Typography.H4>Title</Typography.H4>
+              </Panel.Header>
+              <div>body</div>
+            </Panel.Wrapper>
+          </TestThemeProvider>)
 
         expect(panelIsOpen(wrapper)).to.be.true
       })
 
       test('should regard isOpenInitial prop for state', () => {
         const wrapper = mount(
-          <Panel.Wrapper isOpenInitial={false}>
-            <Panel.Header>
-              <Typography.H4>Title</Typography.H4>
-            </Panel.Header>
-            <div>body</div>
-          </Panel.Wrapper>)
+          <TestThemeProvider>
+            <Panel.Wrapper isOpenInitial={false}>
+              <Panel.Header>
+                <Typography.H4>Title</Typography.H4>
+              </Panel.Header>
+              <div>body</div>
+            </Panel.Wrapper>
+          </TestThemeProvider>)
 
         expect(panelIsOpen(wrapper)).to.be.false
       })
 
       test('should open and close on click', () => {
         const wrapper = mount(
-          <Panel.Wrapper>
-            <Panel.Header>
-              <Typography.H4>Title</Typography.H4>
-            </Panel.Header>
-            <div>body</div>
-          </Panel.Wrapper>)
+          <TestThemeProvider>
+            <Panel.Wrapper>
+              <Panel.Header>
+                <Typography.H4>Title</Typography.H4>
+              </Panel.Header>
+              <div>body</div>
+            </Panel.Wrapper>
+          </TestThemeProvider>)
 
         expect(panelIsOpen(wrapper)).to.be.true
-        wrapper.find(Ball).simulate('click')
+        wrapper.find(Icon).simulate('click')
         expect(panelIsOpen(wrapper)).to.be.false
-        wrapper.find(Ball).simulate('click')
+        wrapper.find(Icon).simulate('click')
         expect(panelIsOpen(wrapper)).to.be.true
       })
 
       test('should open and close on click', () => {
         const onToggleSpy = sinon.spy()
         const wrapper = mount(
-
-          <Panel.Wrapper controlledIsOpen={false} onToggle={onToggleSpy}>
-            <Panel.Header>
-              <Typography.H4>Title</Typography.H4>
-            </Panel.Header>
-            <div>body</div>
-          </Panel.Wrapper>)
+          <TestThemeProvider>
+            <Panel.Wrapper controlledIsOpen={false} onToggle={onToggleSpy}>
+              <Panel.Header>
+                <Typography.H4>Title</Typography.H4>
+              </Panel.Header>
+              <div>body</div>
+            </Panel.Wrapper>
+          </TestThemeProvider>)
 
         expect(panelIsOpen(wrapper)).to.be.false
-        wrapper.find(Ball).simulate('click')
+        wrapper.find(Icon).simulate('click')
         expect(onToggleSpy).to.have.been.calledWith(true)
       })
     })
