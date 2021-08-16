9129439c189fa9b6a31c0368f9520ed0c672f078
Daniel Keller <dkeller@tocco.ch>
2021-08-16 17:52:29 +0200

    refactor(app-extensions): set money default value
    Refs: TOCDEV-4094
    Changelog: Money post point digits fallback

--- packages/app-extensions/src/field/editableTypeConfigs/moneyamount.js
+++ packages/app-extensions/src/field/editableTypeConfigs/moneyamount.js
@@ -1,14 +1,12 @@
 import _get from 'lodash/get'
 
 export default {
-  getOptions: ({formField}) => {
-    return {
-      postPointDigits: _get(formField, 'validation.decimalDigits.postPointDigits', null),
-      prePointDigits: _get(formField, 'validation.decimalDigits.prePointDigits', null),
-      minValue: _get(formField, 'validation.numberRange.fromIncluding', null),
-      maxValue: _get(formField, 'validation.numberRange.toIncluding', null),
-      allowNegative: true,
-      fixedDecimalScale: true
-    }
-  }
+  getOptions: ({formField}) => ({
+    postPointDigits: _get(formField, 'validation.decimalDigits.postPointDigits', 2),
+    prePointDigits: _get(formField, 'validation.decimalDigits.prePointDigits', null),
+    minValue: _get(formField, 'validation.numberRange.fromIncluding', null),
+    maxValue: _get(formField, 'validation.numberRange.toIncluding', null),
+    allowNegative: true,
+    fixedDecimalScale: true
+  })
 }
