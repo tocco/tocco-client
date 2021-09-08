2dfa0a0fa7f568b2f1a56181ac170a66e1a4c4f4
PadPalon <luca.neukom@gmail.com>
2021-09-08 12:46:58 +0200

    test(app-extensions): add some rendering tests

--- packages/tocco-ui/src/Select/Select.spec.js
+++ packages/tocco-ui/src/Select/Select.spec.js
@@ -3,6 +3,7 @@ import ReactSelect from 'react-select'
 import {mount} from 'enzyme'
 import {TestThemeProvider} from 'tocco-test-util'
 
+import Ball from '../Ball'
 import Select from './Select'
 
 describe('tocco-ui', () => {
@@ -15,6 +16,48 @@ describe('tocco-ui', () => {
           </TestThemeProvider>
         )
         expect(wrapper.find(ReactSelect)).to.have.length(1)
+      })
+
+      test('should render advanced search button', () => {
+        const wrapper = mount(
+          <TestThemeProvider>
+            <Select openAdvancedSearch={() => {}} immutable={false}/>
+          </TestThemeProvider>
+        )
+        expect(wrapper.find(Ball).filterWhere(b => b.props().icon === 'search')).to.have.length(1)
+      })
+      test('should not render advanced search button when disabled', () => {
+        const wrapper = mount(
+          <TestThemeProvider>
+            <Select openAdvancedSearch={() => {}} immutable={true}/>
+          </TestThemeProvider>
+        )
+        expect(wrapper.find(Ball).filterWhere(b => b.props().icon === 'search')).to.have.length(0)
+      })
+      test('should not render advanced search button without function', () => {
+        const wrapper = mount(
+          <TestThemeProvider>
+            <Select immutable={false}/>
+          </TestThemeProvider>
+        )
+        expect(wrapper.find(Ball).filterWhere(b => b.props().icon === 'search')).to.have.length(0)
+      })
+
+      test('should render remote create button', () => {
+        const wrapper = mount(
+          <TestThemeProvider>
+            <Select createPermission={true}/>
+          </TestThemeProvider>
+        )
+        expect(wrapper.find(Ball).filterWhere(b => b.props().icon === 'plus')).to.have.length(1)
+      })
+      test('should not render remote create button when not allowed', () => {
+        const wrapper = mount(
+          <TestThemeProvider>
+            <Select createPermission={false}/>
+          </TestThemeProvider>
+        )
+        expect(wrapper.find(Ball).filterWhere(b => b.props().icon === 'plus')).to.have.length(0)
       })
     })
   })
