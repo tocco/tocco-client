d694f407b9c506b525352ca24663056bcfc4187c
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2021-09-24 15:34:11 +0200

    refactor(tocco-ui): adapt underline style
    Refs: TOCDEV-4100
    Changelog: Harmonize link styling to be consistent

--- packages/tocco-ui/src/Link/StyledLink.js
+++ packages/tocco-ui/src/Link/StyledLink.js
@@ -1,12 +1,10 @@
 import styled from 'styled-components'
-import _get from 'lodash/get'
 
 import {
   declareFont,
   declareNoneWrappingText,
   declareWrappingText,
-  theme,
-  shadeColor
+  theme
 } from '../utilStyles'
 
 const StyledLink = styled.a`
@@ -14,23 +12,15 @@ const StyledLink = styled.a`
   ${({breakWords}) => breakWords ? declareWrappingText() : declareNoneWrappingText()}
 
   && {
-    color: ${({neutral}) => neutral
-    ? theme.color('text')
-    : theme.color('secondary')};
-    text-decoration: ${({neutral}) => neutral ? 'underline' : 'none'};
+    color: ${theme.color('text')};
+    text-decoration: ${({neutral}) => neutral ? 'none' : 'underline'};
 
     &:hover,
     &:focus {
-      color: ${({neutral, theme}) => neutral
-    ? shadeColor(_get(theme, 'colors.text'), 1)
-    : shadeColor(_get(theme, 'colors.secondary'), 1)};
       text-decoration: ${({neutral}) => neutral ? 'none' : 'underline'};
     }
 
     &:active {
-      color: ${({neutral, theme}) => neutral
-    ? shadeColor(_get(theme, 'colors.text'), 2)
-    : shadeColor(_get(theme, 'colors.secondary'), 2)};
       text-decoration: ${({neutral}) => neutral ? 'none' : 'underline'};
     }
   }
