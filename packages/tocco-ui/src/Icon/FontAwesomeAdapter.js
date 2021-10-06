3a94da6a7269965ddb67cfbedff9841a344527e5
PadPalon <luca.neukom@gmail.com>
2021-10-06 10:54:54 +0200

    feat(tocco-ui): add new font awesome icon mappings
    Refs: TOCDEV-3942

--- packages/tocco-ui/src/Icon/FontAwesomeAdapter.js
+++ packages/tocco-ui/src/Icon/FontAwesomeAdapter.js
@@ -3,31 +3,89 @@ import PropTypes from 'prop-types'
 import _get from 'lodash/get'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {library} from '@fortawesome/fontawesome-svg-core'
+import {faSquare as faSquareSolid, faSquareFull, faStar as faStarSolid} from '@fortawesome/pro-solid-svg-icons'
 import {
-  faSquareFull, faStar as faStarSolid, faSquare as faSquareSolid
-} from '@fortawesome/pro-solid-svg-icons'
-import {
-  faTimes, faCircle, faHorizontalRule, faSync, faMinus, faTimesCircle, faInfoCircle,
-  faCheckCircle, faExclamationCircle, faTrash, faList, faPhone, faSearch, faFolder,
-  faExternalLink, faChevronDoubleRight, faChevronDoubleLeft, faChevronRight, faChevronLeft, faChevronUp, faChevronDown,
-  faPlus, faGlobe, faFileExcel, faFilePdf, faFile, faSortDown, faSortUp, faArrowRight, faArrowLeft, faQuestionCircle,
-  faCog, faFolders, faStar, faFileAlt, faFileCode, faFileCsv, faFileExport, faFileImport, faFileSpreadsheet, faIdBadge,
-  faCheck, faMapMarked, faBars, faInfo, faCompress, faBook, faEllipsisV, faSquare, faExclamation, faBell,
-  faArrowToBottom, faArrowToTop, faBox, faBoxOpen, faNewspaper
+  faArrowLeft,
+  faArrowRight,
+  faArrowToBottom,
+  faArrowToTop,
+  faBars,
+  faBell,
+  faBook,
+  faBox,
+  faBoxOpen,
+  faCheck,
+  faCheckCircle,
+  faChevronDoubleLeft,
+  faChevronDoubleRight,
+  faChevronDown,
+  faChevronLeft,
+  faChevronRight,
+  faChevronUp,
+  faCircle,
+  faCog,
+  faComment,
+  faCompress,
+  faEllipsisV,
+  faExclamation,
+  faExclamationCircle,
+  faExternalLink,
+  faFile,
+  faFileAlt,
+  faFileCode,
+  faFileCsv,
+  faFileExcel,
+  faFileExport,
+  faFileImport,
+  faFilePdf,
+  faFileSpreadsheet,
+  faFolder,
+  faFolders,
+  faGlobe,
+  faHorizontalRule,
+  faIdBadge,
+  faInfo,
+  faInfoCircle,
+  faList,
+  faMapMarked,
+  faMinus,
+  faNewspaper,
+  faPen,
+  faPhone,
+  faPlus,
+  faQuestionCircle,
+  faSave,
+  faSearch,
+  faSortDown,
+  faSortUp,
+  faSquare,
+  faStar,
+  faSync,
+  faTimes,
+  faTimesCircle,
+  faTrash
 } from '@fortawesome/pro-light-svg-icons'
-import {faGoogle, faFacebook, faFacebookF, faDropbox, faMicrosoft, faJira, faApple}
-  from '@fortawesome/free-brands-svg-icons'
+import {
+  faApple,
+  faDropbox,
+  faFacebook,
+  faFacebookF,
+  faGoogle,
+  faJira,
+  faMicrosoft
+} from '@fortawesome/free-brands-svg-icons'
+
 (() => {
   library.add(
     faChevronDoubleLeft, faChevronDoubleRight, faChevronRight,
-    faChevronLeft, faChevronUp, faChevronDown, faGlobe, faPlus, faFolder, faFolders, faStar, faStarSolid, faTimes,
-    faArrowRight, faArrowLeft, faFile, faFileAlt, faFileCode, faFileCsv, faFileExcel, faFileExport,
+    faChevronLeft, faChevronUp, faChevronDown, faGlobe, faPlus, faFolder, faFolders, faStar, faStarSolid,
+    faTimes, faArrowRight, faArrowLeft, faFile, faFileAlt, faFileCode, faFileCsv, faFileExcel, faFileExport,
     faFileImport, faFilePdf, faFileSpreadsheet, faIdBadge, faCheck, faExclamation, faCircle,
     faCheckCircle, faGoogle, faFacebook, faFacebookF, faDropbox, faMicrosoft, faJira, faApple, faBars, faExternalLink,
     faPhone, faMapMarked, faSearch, faCog, faInfo, faHorizontalRule, faSync, faSquareFull,
     faCompress, faBook, faSortUp, faSortDown, faQuestionCircle, faEllipsisV, faMinus, faTimesCircle, faInfoCircle,
     faExclamationCircle, faTrash, faList, faSquare, faBell, faArrowToBottom, faArrowToTop, faSquareSolid, faBox,
-    faBoxOpen, faNewspaper)
+    faBoxOpen, faNewspaper, faComment, faSave, faPen)
 })()
 
 /**
