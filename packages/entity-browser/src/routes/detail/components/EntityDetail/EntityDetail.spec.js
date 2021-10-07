33ac884fda9a991ac86c907cb93ab5aa9761dac9
Isabel Boos <iboos@tocco.ch>
2021-10-07 10:19:50 +0200

    test(entity-browser): fix silent failing test
    Fix silent failing test which will not be silent failing on node version >= 16 anymore

--- packages/entity-browser/src/routes/detail/components/EntityDetail/EntityDetail.spec.js
+++ packages/entity-browser/src/routes/detail/components/EntityDetail/EntityDetail.spec.js
@@ -3,23 +3,34 @@ import EntityDetailApp from 'tocco-entity-detail/src/main'
 import {mount} from 'enzyme'
 import {IntlStub} from 'tocco-test-util'
 import {MemoryRouter} from 'react-router-dom'
+import fetchMock from 'fetch-mock'
 
 import EntityDetail from './'
+import {setupFetchMock} from './mockData/setupFetchMock'
 
 const EMPTY_FUNC = () => {}
 
 describe('entity-browser', () => {
   describe('components', () => {
     describe('EntityDetail', () => {
+      beforeEach(() => {
+        fetchMock.reset()
+        fetchMock.restore()
+      })
+
       test('should render App ', () => {
         const detailParams = {
           entityName: 'User',
-          formName: 'UserSearch'
+          entityId: '1',
+          formName: 'User',
+          mode: 'update'
         }
 
         const routerMock = {
           match: {url: '/detail', history: {}}
         }
+      
+        setupFetchMock(fetchMock)
 
         const wrapper = mount(
           <MemoryRouter>
