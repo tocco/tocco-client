023203d0b76236a15c75dc5af0b46e20de1742b3
Isabel Boos <iboos@tocco.ch>
2021-10-12 15:09:03 +0200

    feat(dashboard): added drag and drop POC
    Refs: TOCDEV-4262

--- packages/admin/src/routes/dashboard/components/Dashboard/Dashboard.js
+++ packages/admin/src/routes/dashboard/components/Dashboard/Dashboard.js
@@ -1,17 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import DashboardApp from 'tocco-dashboard/src/main'
 
-import ToccoLogo from '../../../../assets/tocco_red.svg'
-import {StyledDashBoardWrapper, StyledSlogan, StyledLogo, StyledTitle} from './StyledComponents'
+import {StyledDashBoardWrapper} from './StyledComponents'
 
 const Dashboard = () => {
-  const packageJson = require('../../../../../package')
   return (
     <StyledDashBoardWrapper>
-      <StyledSlogan>
-        <StyledLogo src={ToccoLogo} alt="tocco-logo" height="53.34" width="580"/>
-        <StyledTitle>beta v{packageJson.version}</StyledTitle>
-      </StyledSlogan>
+      <DashboardApp/>
     </StyledDashBoardWrapper>
   )
 }
