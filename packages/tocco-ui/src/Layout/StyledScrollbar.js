ea032b9f3f2a7ed085aa49fc6aee551a733d98a5
Yilmaz Kurnazcan <yilmaz.kurnazcan@live.de>
2021-10-14 15:07:12 +0200

    refactor(tocco-ui): change scrollbar color
    Refs: TOCDEV-4326
    Changelog: Lighten scrollbar color

--- packages/tocco-ui/src/Layout/StyledScrollbar.js
+++ packages/tocco-ui/src/Layout/StyledScrollbar.js
@@ -1,21 +1,21 @@
 import {css} from 'styled-components'
 import _get from 'lodash/get'
 
-import {shadeColor, theme} from '../utilStyles'
+import {shadeColor} from '../utilStyles'
 
 export const StyledScrollbar = css`
   ::-webkit-scrollbar {
-    width: 7px;
-    height: 7px;
+    width: 6px;
+    height: 6px;
   }
 
   ::-webkit-scrollbar-thumb {
-    background-color: ${({theme}) => shadeColor(_get(theme, 'colors.text'), 2)};
+    background-color: ${({theme}) => shadeColor(_get(theme, 'colors.paper'), 4)};
 
     &:hover {
-      background-color: ${theme.color('secondary')};
+      background-color: ${({theme}) => shadeColor(_get(theme, 'colors.text'), 5)};
     }
   }
-  scrollbar-color: ${theme.color('text')} transparent; // Firefox workaround
+  scrollbar-color: ${({theme}) => shadeColor(_get(theme, 'colors.paper'), 4)} transparent; // Firefox workaround
   scrollbar-width: thin;
 `
