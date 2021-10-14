ab1cb9c0c06ac8a2054516e2e9916499edef5859
Yilmaz Kurnazcan <yilmaz.kurnazcan@live.de>
2021-10-14 10:38:53 +0200

    refactor(entity-list): harmonize spacing
    Refs: TOCDEV-4375
    Changelog: Harmonize paragraph spacing whithin fields

--- packages/entity-list/src/util/fieldFactory.js
+++ packages/entity-list/src/util/fieldFactory.js
@@ -4,6 +4,7 @@ import {Typography} from 'tocco-ui'
 import {formData, field} from 'tocco-app-extensions'
 import {js} from 'tocco-util'
 
+import {StyledSpan} from './StyledComponents'
 import LazyDataEnhancer from '../components/LazyDataEnhancer'
 
 export const MultiSeparator = () => (<Typography.Span>, </Typography.Span>)
@@ -11,37 +12,52 @@ export const MultiSeparator = () => (<Typography.Span>, </Typography.Span>)
 const multiTypes = ['multi-select-box', 'multi-remote-field']
 
 export default (fieldDefinition, entity, intl) => {
-  const {id, path, dataType} = fieldDefinition
+  const {
+    id,
+    path,
+    dataType
+  } = fieldDefinition
   const isMultiType = multiTypes.includes(dataType)
   const pathValue = entity[path]
   const values = !isMultiType && Array.isArray(pathValue) ? pathValue : [pathValue]
-  return <span key={id} style={{marginRight: '2px'}}>
-    {values.map((v, idx) =>
-      <formData.FormDataContainer key={`formDataContainer-${entity.__key}-${path}-${idx}`} navigationStrategy={true}>
-        <FormattedValueWrapper type={dataType} value={v} intl={intl} formField={fieldDefinition}/>
-      </formData.FormDataContainer>
-    ).reduce((prev, curr, idx) => [prev, <MultiSeparator key={'sep' + idx}/>, curr])
-    }
-  </span>
+  const formDataContainer = values.map((v, idx) =>
+    <formData.FormDataContainer key={`formDataContainer-${entity.__key}-${path}-${idx}`} navigationStrategy={true}>
+      <FormattedValueWrapper type={dataType} value={v} intl={intl} formField={fieldDefinition}/>
+    </formData.FormDataContainer>
+  ).reduce((prev, curr, idx) => [prev, <MultiSeparator key={'sep' + idx}/>, curr])
+
+  return (
+    <StyledSpan key={id}>
+      {formDataContainer}
+    </StyledSpan>
+  )
 }
 
-const FormattedValueWrapper = ({value, type, formData, intl, formField}) => {
+const FormattedValueWrapper = ({
+  value,
+  type,
+  formData,
+  intl,
+  formField
+}) => {
   const modelField = {
     targetEntity: value && js.getOrFirst(value).model
   }
 
   const Field = useMemo(() => field.factory('list', type), [])
 
-  return <LazyDataEnhancer value={value} type={type}>
-    <Field
-      type={type}
-      formField={formField}
-      modelField={modelField}
-      value={value}
-      formData={formData}
-      breakWords={false}
-    />
-  </LazyDataEnhancer>
+  return (
+    <LazyDataEnhancer value={value} type={type}>
+      <Field
+        type={type}
+        formField={formField}
+        modelField={modelField}
+        value={value}
+        formData={formData}
+        breakWords={false}
+      />
+    </LazyDataEnhancer>
+  )
 }
 
 FormattedValueWrapper.propTypes = {
