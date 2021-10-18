e02a5a465a79060c7c13e61c2d7e4a6b7752b167
Isabel Boos <iboos@tocco.ch>
2021-10-18 09:52:56 +0200

    feat(dashboard): change infobox height
    Refs: TOCDEV-4333

--- packages/dashboard/src/components/InfoBox/typeContent/InfoBoxHtmlFieldContent.js
+++ packages/dashboard/src/components/InfoBox/typeContent/InfoBoxHtmlFieldContent.js
@@ -2,9 +2,13 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import {FormattedValue} from 'tocco-ui'
 
+import {StyledInfoBoxContentWrapper} from './StyledComponents'
+
 const InfoBoxHtmlFieldContent = ({content}) => {
   const {text} = content
-  return <FormattedValue type="html" value={text}/>
+  return <StyledInfoBoxContentWrapper scrollable>
+    <FormattedValue type="html" value={text}/>
+  </StyledInfoBoxContentWrapper>
 }
 
 InfoBoxHtmlFieldContent.propTypes = {
