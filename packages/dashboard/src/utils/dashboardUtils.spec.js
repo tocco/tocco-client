6991f8036ab42788fc764dcf04cbeb84d185d0db
Isabel Boos <iboos@tocco.ch>
2021-10-20 13:21:48 +0200

    feat(dashboard): distribute unpositioned infoboxes
    Refs: TOCDEV-4333

--- packages/dashboard/src/utils/dashboardUtils.spec.js
+++ packages/dashboard/src/utils/dashboardUtils.spec.js
@@ -4,7 +4,6 @@ import {
   appendDraggedAsLastItemToDropped,
   getRenderInfoBoxesForColumn,
   moveDraggedToDropped,
-  prepareInfoBoxes,
   sortInfoBoxes
 } from './dashboardUtils'
 import DropTypes from './dropTypes'
@@ -356,45 +355,6 @@ describe('dashboard', () => {
           const renderBoxes = getRenderInfoBoxesForColumn(draggingId, currentlyDragOver, position, column, boxes)
 
           expect(renderBoxes).to.deep.equal(expectedBoxes)
-        })
-      })
-      describe('prepareInfoBoxes', () => {
-        test('should sort infoBoxes', () => {
-          const boxes = [
-            {id: 1, position: '0:0'},
-            {id: 2, position: '1:0'},
-            {id: 5, position: '1:2'},
-            {id: 4, position: '1:1'},
-            {id: 7, position: '2:1'},
-            {id: 3, position: '0:0'},
-            {id: 6, position: '2:0'}
-          ]
-
-          const expectedOrder = [1, 2, 3, 4, 5, 6, 7]
-
-          const sortedBoxes = prepareInfoBoxes(boxes)
-
-          expect(sortedBoxes.map(b => b.id)).to.deep.equal(expectedOrder)
-        })
-
-        test('should map `position` to `col` and `row`', () => {
-          const boxes = [
-            {id: 1, position: '0:0'},
-            {id: 2, position: '1:0'},
-            {id: 3, position: '1:1'},
-            {id: 4, position: '1:2'}
-          ]
-
-          const expectedBoxes = [
-            {id: 1, position: '0:0', col: 0, row: 0},
-            {id: 2, position: '1:0', col: 0, row: 0},
-            {id: 3, position: '1:1', col: 0, row: 1},
-            {id: 4, position: '1:2', col: 0, row: 2}
-          ]
-
-          const infoBoxes = prepareInfoBoxes(boxes)
-
-          expect(infoBoxes).to.deep.equal(expectedBoxes)
         })
       })
     })
