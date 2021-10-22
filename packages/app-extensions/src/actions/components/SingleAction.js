d2642c806548036f53fbbe0f4c44048a25993ea3
PadPalon <luca.neukom@gmail.com>
2021-10-22 16:27:21 +0200

    feat(app-extensions): handle buttonType and useLabel in SingleAction
    Refs: TOCDEV-4340

--- packages/app-extensions/src/actions/components/SingleAction.js
+++ packages/app-extensions/src/actions/components/SingleAction.js
@@ -1,29 +1,37 @@
-
 import PropTypes from 'prop-types'
 import React from 'react'
 import {Button} from 'tocco-ui'
 import {injectIntl} from 'react-intl'
 
-import {selectionText, isValidSelection} from './selectionHelper'
+import {isValidSelection, selectionText} from './selectionHelper'
 
-export const SingleAction = ({definition, onClick, selectedCount, disabled, intl}) => {
-  const validSelection = isValidSelection(selectedCount, definition)
-  const title = selectionText(selectedCount, definition, intl)
-  return (
-    <Button
-      data-cy={`action-${definition.id}`}
-      look="raised"
-      onClick={e => {
-        onClick(definition)
-        e.stopPropagation()
-      }}
-      icon={definition.icon}
-      {...definition.label && definition.useLabel === 'NO' ? {title: definition.label} : {label: definition.label}}
-      {...(definition.readonly === true || !validSelection || disabled) && {disabled: true}}
-      title={title}
-    />
-  )
-}
+export const SingleAction = ({
+  definition,
+  onClick,
+  selectedCount,
+  disabled,
+  intl
+}) => <Button
+    iconOnly={definition.buttonType === 'ICON'}
+    data-cy={`action-${definition.id}`}
+    look={definition.buttonType === 'ICON' ? 'flat' : 'raised'}
+    withoutBackground={definition.buttonType === 'ICON'}
+    onClick={e => {
+      onClick(definition)
+      e.stopPropagation()
+    }}
+    icon={definition.icon}
+    label={getLabel(definition)}
+    title={getTitle(selectedCount, definition, intl)}
+    disabled={definition.readonly === true || !isValidSelection(selectedCount, definition) || disabled}
+  />
+
+const getLabel = definition => definition.label && shouldUseLabel(definition) ? definition.label : ''
+
+const getTitle = (selectedCount, definition, intl) => selectionText(selectedCount, definition, intl)
+  || (!shouldUseLabel(definition) ? definition.label : '')
+
+const shouldUseLabel = definition => !(definition.useLabel === 'NO' || definition.useLabel === 'HIDE')
 
 SingleAction.propTypes = {
   intl: PropTypes.object.isRequired,
