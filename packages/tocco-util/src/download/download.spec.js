b34713db7eb3601fc67429e49d729d7f74789600
PadPalon <luca.neukom@gmail.com>
2021-10-22 17:21:32 +0200

    fix(tocco-ui): move tests from View.spec.js to download

--- packages/tocco-util/src/download/download.spec.js
+++ packages/tocco-util/src/download/download.spec.js
@@ -1,4 +1,4 @@
-import {addParameterToURL} from './download'
+import {addParameterToURL, getDownloadUrl} from './download'
 
 describe('tocco-util', () => {
   describe('download', () => {
@@ -11,6 +11,17 @@ describe('tocco-util', () => {
       test('should append query parameter', () => {
         const result = addParameterToURL('http://www.tocco.ch?test=true', 'test2', 'asd')
         expect(result).to.eql('http://www.tocco.ch?test=true&test2=asd')
+      })
+    })
+    describe('getDownloadUrl', () => {
+      test('should add download param to url without query params', () => {
+        const result = getDownloadUrl('https://tocco.ch/files/test.pdf')
+        expect(result).to.eql('https://tocco.ch/files/test.pdf?download=true')
+      })
+
+      test('should add download param to url with query params', () => {
+        const result = getDownloadUrl('www.tocco.ch/245df43d/test.pdf?yc=a234ms')
+        expect(result).to.eql('www.tocco.ch/245df43d/test.pdf?yc=a234ms&download=true')
       })
     })
   })
