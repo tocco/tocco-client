d57863d36c6e2af11bd6bf2cb708033d69387550
Isabel Boos <iboos@tocco.ch>
2021-10-27 16:48:42 +0200

    feat(admin): add breadcrumbs to dashboard
    Changelog: add breadcrumbs to dashboard
    Refs: TOCDEV-4423

--- packages/admin/src/routes/dashboard/components/Dashboard/Dashboard.js
+++ packages/admin/src/routes/dashboard/components/Dashboard/Dashboard.js
@@ -1,22 +1,34 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import DashboardApp from 'tocco-dashboard/src/main'
+import {Breadcrumbs} from 'tocco-ui'
 
-import {StyledDashBoardWrapper} from './StyledComponents'
+import {StyledDashboardWrapper, StyledWrapper, StyledBreadcrumbs} from './StyledComponents'
 import navigationStrategy from '../../../entities/utils/navigationStrategy'
 
-const Dashboard = ({match, history, emitAction}) => {
+const Dashboard = ({match, history, emitAction, intl}) => {
+  const msg = id => intl.formatMessage({id})
+
   return (
-    <StyledDashBoardWrapper>
-      <DashboardApp
-        emitAction={emitAction}
-        navigationStrategy={navigationStrategy(history, match)}
-      />
-    </StyledDashBoardWrapper>
+    <StyledWrapper>
+      <StyledBreadcrumbs>
+        <Breadcrumbs
+          currentView={{display: msg('client.admin.dashboard'), title: 'Tocco'}}
+          pathPrefix="/dashboard"
+        />
+      </StyledBreadcrumbs>
+      <StyledDashboardWrapper>
+        <DashboardApp
+          emitAction={emitAction}
+          navigationStrategy={navigationStrategy(history, match)}
+        />
+      </StyledDashboardWrapper>
+    </StyledWrapper>
   )
 }
 
 Dashboard.propTypes = {
+  intl: PropTypes.object.isRequired,
   match: PropTypes.object.isRequired,
   history: PropTypes.object.isRequired,
   emitAction: PropTypes.func.isRequired
