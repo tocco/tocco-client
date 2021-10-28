be60735f838e1b160fad9991a992c9cedc41ff23
Isabel Boos <iboos@tocco.ch>
2021-10-28 09:36:52 +0200

    feat(tocco-ui): create link button - review outcome
    Changelog: create link button
    Refs: TOCDEV-4423

--- packages/tocco-ui/src/Button/StyledRouterLinkButton.js
+++ packages/tocco-ui/src/Button/StyledRouterLinkButton.js
@@ -3,9 +3,9 @@ import styled from 'styled-components'
 
 import {buttonStyle} from './buttonStyle'
 
-const RouterStyledLinkButton = styled(Link)`
+const StyledRouterLinkButton = styled(Link)`
   text-decoration: none;
   ${buttonStyle}
 `
 
-export default RouterStyledLinkButton
+export default StyledRouterLinkButton
