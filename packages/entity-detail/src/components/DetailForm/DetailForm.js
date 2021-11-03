49126af000c373c32d5fb6aed372a5e1dc40eb51
PadPalon <luca.neukom@gmail.com>
2021-11-03 10:44:21 +0100

    fix(entity-detail): don't fire touched when nothing is touched
    Refs: BS-5936
    Cherry-pick: Up

--- packages/entity-detail/src/components/DetailForm/DetailForm.js
+++ packages/entity-detail/src/components/DetailForm/DetailForm.js
@@ -29,8 +29,8 @@ const DetailForm = props => {
   const formEl = useRef(null)
 
   useEffect(() => {
-    fireTouched(dirty)
-  }, [dirty])
+    fireTouched(dirty && anyTouched)
+  }, [dirty, anyTouched])
 
   useLayoutEffect(() => {
     const firstInput = formEl.current.querySelector('input:not([disabled]),textarea:not([disabled])')
