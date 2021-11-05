2de3325e19a2c7550570178ad007398faec65513
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2021-11-05 11:17:09 +0100

    refactor(tocco-ui): fix missing popover and overflow
    Refs: TOCDEV-4462
    Changelog: fix missing popover in single values and overflowing in firefox

--- packages/tocco-ui/src/Select/SingleValue.js
+++ packages/tocco-ui/src/Select/SingleValue.js
@@ -2,21 +2,24 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import {components} from 'react-select'
 import _get from 'lodash/get'
-import styled from 'styled-components'
 import {html} from 'tocco-util'
 
 import Popover from '../Popover'
 import ClickableWrapper from './ClickableWrapper'
-import {theme} from '../utilStyles'
-
-const StyledSingleValueWrapper = styled.div`
-  overflow-x: hidden;
-  color: ${({isDisabled}) => isDisabled ? theme.color('text') : 'inherit'};
-`
+import {StyledSingleValueWrapper} from './StyledComponents'
 
 export const SingleValue = props => {
-  const {data, selectProps, isDisabled, children} = props
-  const {tooltips, loadTooltip, DetailLink} = selectProps
+  const {
+    data,
+    selectProps,
+    isDisabled,
+    children
+  } = props
+  const {
+    tooltips,
+    loadTooltip,
+    DetailLink
+  } = selectProps
   const tooltip = _get(tooltips, data.key, null)
   const content = DetailLink
     ? <ClickableWrapper onMouseDown={e => {
@@ -27,18 +30,20 @@ export const SingleValue = props => {
     </ClickableWrapper>
     : children
 
-  return <components.SingleValue {...props}>
-    <StyledSingleValueWrapper
-      onMouseOver={() => loadTooltip && !tooltip && loadTooltip(data.key)}
-      isDisabled={isDisabled}
-    >
-      <Popover content={tooltip
-        ? <div dangerouslySetInnerHTML={{__html: html.sanitizeHtml(tooltip)}}/>
-        : null}>
-        {content}
-      </Popover>
-    </StyledSingleValueWrapper>
-  </components.SingleValue>
+  return (
+    <components.SingleValue {...props}>
+      <StyledSingleValueWrapper
+        onMouseOver={() => loadTooltip && !tooltip && loadTooltip(data.key)}
+        isDisabled={isDisabled}
+      >
+        <Popover content={tooltip
+          ? <div dangerouslySetInnerHTML={{__html: html.sanitizeHtml(tooltip)}}/>
+          : null}>
+          {content}
+        </Popover>
+      </StyledSingleValueWrapper>
+    </components.SingleValue>
+  )
 }
 
 SingleValue.propTypes = {
