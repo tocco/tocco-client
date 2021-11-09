fdb6de2a9c7b4b2ffbd207d998305ac20354eab5
PadPalon <luca.neukom@gmail.com>
2021-11-09 13:37:36 +0100

    feat(tocco-ui): implement TQL highlighting
    Refs: TOCDEV-4258

--- packages/tocco-ui/src/CodeEditor/TqlMode.js
+++ packages/tocco-ui/src/CodeEditor/TqlMode.js
@@ -1,14 +1,198 @@
 import ace from 'ace-builds/src-min-noconflict/ace'
-import 'ace-builds/src-min-noconflict/mode-sql'
+import 'ace-builds/src-min-noconflict/mode-text'
 import _mergeWith from 'lodash/mergeWith'
 import _isArray from 'lodash/isArray'
 
-// TODO implement
-export class TqlHighlightRules extends ace.require('ace/mode/sql_highlight_rules').SqlHighlightRules {
+export class TqlHighlightRules extends ace.require('ace/mode/text_highlight_rules').TextHighlightRules {
   constructor() {
     super()
+
+    const operators = ['==', '!=', '<=', '>=', '<', '>', '~=']
+    const atoms = ['true', 'false', 'null']
+    const types = [
+      'string', 'long', 'serial', 'bool', 'boolean', 'double', 'float', 'time', 'timestamp', 'char', 'varchar', 'date',
+      'datetime'
+    ]
+    const placeholders = [
+      'now_date', 'yesterday_date', 'tomorrow_date', 'today_date', 'now_start', 'yesterday_start', 'tomorrow_start',
+      'today_start', 'now', 'yesterday', 'tomorrow', 'today', 'currentUsername', 'currentUser', 'currentBu'
+    ]
+    const functions = [
+      'in', 'dateadd', 'datetimeadd', 'duedate', 'birthdayin', 'fulltext', 'likeall', 'distance', 'inboundingbox'
+    ]
+
+    const commonWhere = [
+      {
+        token: 'keyword.operator',
+        regex: /\s*(and|or|not)($|\s+)/,
+        caseInsensitive: true
+      },
+      {
+        token: 'support.class.relation',
+        regex: /rel/,
+        push: 'model'
+      },
+      {
+        token: 'keyword.exists',
+        regex: /exists\s*/,
+        push: 'exists',
+        caseInsensitive: true
+      },
+      {
+        token: 'keyword.count',
+        regex: /count\s*/,
+        push: 'count',
+        caseInsensitive: true
+      },
+      {
+        token: ['keyword.function', 'paren.lparen', 'text', 'paren.rparen', 'text'],
+        regex: `((?:${functions.join('|')})\\s*)(\\()(.*?)(\\))($|\\s+)`,
+        caseInsensitive: true
+      },
+      {
+        token: ['keyword.other', 'support.function.field', 'text'],
+        regex: /(\.)?([a-z_]+)($|\s+)/,
+        push: 'comparison'
+      }
+    ]
+
     const customRules = {
       start: [
+        {
+          token: 'keyword.find',
+          regex: /find($|\s+)/,
+          next: 'model',
+          caseInsensitive: true
+        },
+        {
+          token: 'keyword.where',
+          regex: /where($|\s+)/,
+          next: 'rootwhere',
+          caseInsensitive: true
+        }
+      ],
+      model: [
+        {
+          token: 'support.class.model',
+          regex: /[A-Z][a-z_]*\s*/,
+          next: 'pop'
+        }
+      ],
+      rootwhere: [
+        {
+          token: 'keyword.order_by',
+          regex: /order by($|\s+)/,
+          next: 'order',
+          caseInsensitive: true
+        },
+        ...commonWhere
+      ],
+      comparison: [
+        {
+          token: 'keyword.operator',
+          regex: `(${operators.join('|')})\\s*`,
+          next: 'value'
+        }
+      ],
+      value: [
+        {
+          token: 'string',
+          regex: /".*?"\s*/,
+          next: 'pop'
+        },
+        {
+          token: 'constant.numeric',
+          regex: /[0-9]+(.[0-9]+)?\s*/,
+          next: 'pop'
+        },
+        {
+          token: 'constant.language',
+          regex: `:(${placeholders.join('|')})\\s*`,
+          next: 'pop',
+          caseInsensitive: true
+        },
+        {
+          token: 'constant.language',
+          regex: `(${atoms.join('|')})\\s*`,
+          next: 'pop',
+          caseInsensitive: true
+        },
+        {
+          token: 'support.type',
+          regex: `(${types.join('|')}):\\s*`,
+          caseInsensitive: true
+        }
+      ],
+      exists: [
+        {
+          token: 'paren.lparen',
+          regex: /\(\s*/
+        },
+        {
+          token: 'support.class.relation',
+          regex: /rel/,
+          push: 'model'
+        },
+        {
+          token: 'keyword.where',
+          regex: /where($|\s+)/,
+          next: 'existswhere',
+          caseInsensitive: true
+        },
+        {
+          token: 'paren.rparen',
+          regex: /\)\s*/,
+          next: 'pop'
+        }
+      ],
+      existswhere: [
+        ...commonWhere,
+        {
+          token: 'paren.rparen',
+          regex: /\)\s*/,
+          next: 'pop'
+        }
+      ],
+      count: [
+        {
+          token: 'paren.lparen',
+          regex: /\(\s*/
+        },
+        {
+          token: 'support.class.relation',
+          regex: /rel/,
+          push: 'model'
+        },
+        {
+          token: 'keyword.where',
+          regex: /where($|\s+)/,
+          next: 'countwhere',
+          caseInsensitive: true
+        },
+        {
+          token: 'paren.rparen',
+          regex: /\)\s*/,
+          next: 'comparison'
+        }
+      ],
+      countwhere: [
+        ...commonWhere,
+        {
+          token: 'paren.rparen',
+          regex: /\)\s*/,
+          next: 'comparison'
+        }
+      ],
+      order: [
+        {
+          token: 'support.class.relation',
+          regex: /rel/,
+          push: 'model'
+        },
+        {
+          token: ['keyword.other', 'support.function.field', 'keyword', 'text'],
+          regex: /(\.)?([a-z_]+\s*)(asc|desc)?($|,|\s+)/
+        }
       ]
     }
     this.$rules = _mergeWith(
@@ -16,6 +200,8 @@ export class TqlHighlightRules extends ace.require('ace/mode/sql_highlight_rules
       customRules,
       (objValue, srcValue) => _isArray(objValue) ? objValue.concat(srcValue) : undefined
     )
+
+    this.normalizeRules()
   }
 }
 
@@ -27,7 +213,7 @@ const TqlCompleter = () => ({
   }
 })
 
-export default class TqlMode extends ace.require('ace/mode/sql').Mode {
+export default class TqlMode extends ace.require('ace/mode/text').Mode {
   constructor() {
     super()
     this.HighlightRules = TqlHighlightRules
