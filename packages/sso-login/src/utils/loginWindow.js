ccb27bf09bca2a79e59cee32fd9af3c6ce3227da
Isabel Boos <iboos@tocco.ch>
2021-11-25 16:06:04 +0100

    feat(sso-login): use correct backend-url
    - also consider dynamic set backend-url
      - get backend-url from env
    
    Refs: TOCDEV-387

--- packages/sso-login/src/utils/loginWindow.js
+++ packages/sso-login/src/utils/loginWindow.js
@@ -1,7 +1,9 @@
+import {env} from 'tocco-util'
+
 import {getPopUpFeatures} from './popUp'
 
 export const openLoginWindow = (loginEndpoint, loginCompleted, provider) => {
-  const baseUrl = __DEV__ ? '' : __BACKEND_URL__
+  const baseUrl = __DEV__ ? '' : env.getBackendUrl()
   const encodedWindowUrl = encodeURIComponent(window.location.href)
   const url = `${baseUrl}${loginEndpoint}?provider=${provider.unique_id}&sourceUri=${encodedWindowUrl}`
 
