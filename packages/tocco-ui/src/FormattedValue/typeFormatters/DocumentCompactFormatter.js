d985e23851d3342514508af1621bba74ae4f74a3
Stefan Würsten <swuersten@tocco.ch>
2021-11-26 11:29:52 +0100

    feat(tocco-ui): remove DocumentCompactFormatter Popover
    Refs: TOCDEV-4474
    Cherrypick: up

--- packages/tocco-ui/src/FormattedValue/typeFormatters/DocumentCompactFormatter.js
+++ packages/tocco-ui/src/FormattedValue/typeFormatters/DocumentCompactFormatter.js
@@ -1,29 +1,22 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 import {download} from 'tocco-util'
-import styled from 'styled-components'
 
 import Link from '../../Link'
-import Popover from '../../Popover'
 
-const StyledPreviewImage = styled.img`
-  width: 200px;
-`
-
-const DocumentCompactFormatter = ({value}) => <>
-    <Popover content={<StyledPreviewImage alt={value.fileName} src={value.thumbnailLink}/>}>
-      <Link
-        alt={value.alt || value.fileName}
-        download={value.fileName}
-        icon="external-link"
-        look="raised"
-        target="_blank"
-        href={value.binaryLink}
-        onClick={e => {
-          e.stopPropagation()
-        }}
-      />
-    </Popover>
+const DocumentCompactFormatter = ({value}) => (
+  <>
+    <Link
+      alt={value.alt || value.fileName}
+      download={value.fileName}
+      icon="external-link"
+      look="raised"
+      target="_blank"
+      href={value.binaryLink}
+      onClick={e => {
+        e.stopPropagation()
+      }}
+    />
     <Link
       alt={value.alt || value.fileName}
       download={value.fileName}
@@ -35,6 +28,7 @@ const DocumentCompactFormatter = ({value}) => <>
       }}
     />
   </>
+)
 
 DocumentCompactFormatter.propTypes = {
   value: PropTypes.shape({
