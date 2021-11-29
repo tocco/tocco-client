5e7cc9385edc2d0c7e6df3d4c8549658a3df3a04
Stefan Würsten <swuersten@tocco.ch>
2021-11-29 12:57:12 +0100

    feat(app-extensions): add tooltip for document compact
    Refs: TOCDEV-4474
    Cherry-pick: up

--- packages/app-extensions/src/field/formattedTypeConfigs/document.js
+++ packages/app-extensions/src/field/formattedTypeConfigs/document.js
@@ -1,5 +1,13 @@
+import _get from 'lodash/get'
+
 export default {
+  dataContainerProps: () => ({
+    tooltips: ['Resource'],
+    navigationStrategy: true
+  }),
   getOptions: ({formData}) => ({
-    downloadTitle: formData.intl.formatMessage({id: 'client.component.upload.downloadTitle'})
+    downloadTitle: formData.intl.formatMessage({id: 'client.component.upload.downloadTitle'}),
+    tooltips: _get(formData.tooltips, 'Resource', null),
+    loadTooltip: id => formData.loadTooltip('Resource', id)
   })
 }
