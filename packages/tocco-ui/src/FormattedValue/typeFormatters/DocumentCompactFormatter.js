6fc06a1b6895694d7108f5a09647b04d040fecb9
Stefan Würsten <swuersten@tocco.ch>
2021-11-29 12:57:12 +0100

    feat(app-extensions): add tooltip for document compact
    Refs: TOCDEV-4474
    Cherry-pick: up

--- packages/tocco-ui/src/FormattedValue/typeFormatters/DocumentCompactFormatter.js
+++ packages/tocco-ui/src/FormattedValue/typeFormatters/DocumentCompactFormatter.js
@@ -1,42 +1,58 @@
 import PropTypes from 'prop-types'
 import React from 'react'
-import {download} from 'tocco-util'
+import {download, html} from 'tocco-util'
+import _get from 'lodash/get'
 
 import Link from '../../Link'
+import Popover from '../../Popover'
 
-const DocumentCompactFormatter = ({value}) => (
-  <>
-    <Link
-      alt={value.alt || value.fileName}
-      download={value.fileName}
-      icon="external-link"
-      look="raised"
-      target="_blank"
-      href={value.binaryLink}
-      onClick={e => {
-        e.stopPropagation()
-      }}
-    />
-    <Link
-      alt={value.alt || value.fileName}
-      download={value.fileName}
-      icon="arrow-to-bottom"
-      look="raised"
-      href={download.getDownloadUrl(value.binaryLink)}
-      onClick={e => {
-        e.stopPropagation()
-      }}
-    />
-  </>
-)
+const DocumentCompactFormatter = ({value, options}) => {
+  const {tooltips, loadTooltip} = options || {}
+  const tooltip = _get(tooltips, value.resourceKey, null)
+
+  return (
+    <>
+      <Popover content={tooltip ? <div dangerouslySetInnerHTML={{__html: html.sanitizeHtml(tooltip)}}/> : null}>
+        <span onMouseOver={() => loadTooltip && !tooltip && loadTooltip(value.resourceKey)}>
+          <Link
+            alt={value.alt || value.fileName}
+            download={value.fileName}
+            icon="external-link"
+            look="raised"
+            target="_blank"
+            href={value.binaryLink}
+            onClick={e => {
+              e.stopPropagation()
+            }}
+          />
+        </span>
+      </Popover>
+      <Link
+        alt={value.alt || value.fileName}
+        download={value.fileName}
+        icon="arrow-to-bottom"
+        look="raised"
+        href={download.getDownloadUrl(value.binaryLink)}
+        onClick={e => {
+          e.stopPropagation()
+        }}
+      />
+    </>
+  )
+}
 
 DocumentCompactFormatter.propTypes = {
   value: PropTypes.shape({
     alt: PropTypes.string,
     binaryLink: PropTypes.string.isRequired,
     thumbnailLink: PropTypes.string.isRequired,
-    fileName: PropTypes.string.isRequired
-  }).isRequired
+    fileName: PropTypes.string.isRequired,
+    resourceKey: PropTypes.string
+  }).isRequired,
+  options: PropTypes.shape({
+    tooltips: PropTypes.objectOf(PropTypes.string),
+    loadTooltip: PropTypes.func
+  })
 }
 
 export default DocumentCompactFormatter
