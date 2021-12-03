0c086801539802934a218ca8190d7e9070246a3d
Isabel Boos <iboos@tocco.ch>
2021-12-03 17:21:50 +0100

    refactor(docs-browser): export chooseDocument in main
    Refs: TOCDEV-4477

--- packages/docs-browser/src/modules/chooseDocument/index.js
+++ packages/docs-browser/src/modules/chooseDocument/index.js
@@ -1,7 +1,9 @@
 import moduleSagas from './sagas'
+import * as actions from './actions'
 
 export default {
   addToStore: store => {
     store.sagaMiddleware.run(moduleSagas)
-  }
+  },
+  actions
 }
