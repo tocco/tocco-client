26407a4acbc1d519fe045d42a0ad2812b9f662f1
PadPalon <luca.neukom@gmail.com>
2021-12-06 09:57:09 +0100

    fix(resource-scheduler): fix resourcescheduler.stories.js

--- packages/resource-scheduler/src/resourcescheduler.stories.js
+++ packages/resource-scheduler/src/resourcescheduler.stories.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {v4 as uuid} from 'uuid'
 
-import {ResourceSchedulerApp} from './main'
+import ResourceSchedulerApp from './main'
 
 export default {
   title: 'Apps/Resource Scheduler',
