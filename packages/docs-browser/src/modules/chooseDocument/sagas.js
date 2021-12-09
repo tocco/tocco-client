a78e8df7937ca7bb9f01cec470302edb78a479fa
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2021-12-09 09:07:54 +0100

    refactor(docs-browser): fix docs-browser modal
    Refs: TOCDEV-4547
    Changelog: fix docs-browser height inside modal

--- packages/docs-browser/src/modules/chooseDocument/sagas.js
+++ packages/docs-browser/src/modules/chooseDocument/sagas.js
@@ -33,39 +33,42 @@ export function* chooseDocument({payload: {setDocument, formName, formFieldId}})
           }
         }
 
-        return (
-          <>
-            <LazyDocsBrowser
-              memoryHistory={true}
-              listLimit={10}
-              selectionStyle="none"
-              searchFormType="none"
-              disableViewPersistor={true}
-              getListFormName={parent =>
-                parent === null ? 'ChooseDocument_root_docs_list_item' : 'ChooseDocument_docs_list_item'
-              }
-              getCustomLocation={getCustomLocation}
-              navigationStrategy={{}}
-              embedded={true}
-              noLeftPadding={true}
-              sortable={false}
-              onSelectChange={setSelection}
-            />
-            <Button
-              onClick={() => {
-                close()
-                setDocument(formName, formFieldId, selection[0].replace('Resource/', ''))
-              }}
-              look="raised"
-              ink="primary"
-              disabled={selection.length !== 1 || !selection[0].startsWith('Resource/')}
-            >
-              <FormattedMessage id="client.docs-browser.chooseDocument.select" />
-            </Button>
-          </>
-        )
-      },
-      true
-    )
-  )
+      const handleClick = () => {
+        close()
+        setDocument(formName, formFieldId, selection[0].replace('Resource/', ''))
+      }
+      const isDisabled = selection.length !== 1 || !selection[0].startsWith('Resource/')
+      const listFormName = parent => parent === null
+        ? 'ChooseDocument_root_docs_list_item'
+        : 'ChooseDocument_docs_list_item'
+
+      return (
+        <div>
+          <LazyDocsBrowser
+            memoryHistory={true}
+            listLimit={10}
+            selectionStyle="none"
+            searchFormType="none"
+            disableViewPersistor={true}
+            getListFormName={listFormName}
+            getCustomLocation={getCustomLocation}
+            navigationStrategy={{}}
+            embedded={true}
+            noLeftPadding={true}
+            sortable={false}
+            onSelectChange={setSelection}
+          />
+          <Button
+            onClick={handleClick}
+            look="raised"
+            ink="primary"
+            disabled={isDisabled}
+          >
+            <FormattedMessage id="client.docs-browser.chooseDocument.select"/>
+          </Button>
+        </div>
+      )
+    },
+    true
+  ))
 }
