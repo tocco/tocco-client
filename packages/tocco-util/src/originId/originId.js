327567f43d063e2f267753a4763ba77d18786d9e
Roland Züger <rzueger@tocco.ch>
2021-12-10 10:32:21 +0100

    fix(tocco-util): store originId on window
    Refs: TOCDEV-4160

--- packages/tocco-util/src/originId/originId.js
+++ packages/tocco-util/src/originId/originId.js
@@ -1,14 +1,14 @@
 import {v4 as uuid} from 'uuid'
 
-const originIdName = 'originId'
+const originIdName = '_tocco_originId'
 const originIdPrefix = 'client_'
 
 const getOriginId = () => {
-  if (sessionStorage.getItem(originIdName)) {
-    return sessionStorage.getItem(originIdName)
+  if (window[originIdName]) {
+    return window[originIdName]
   }
   const id = `${originIdPrefix}_${uuid()}`
-  sessionStorage.setItem(originIdName, id)
+  window[originIdName] = id
   return id
 }
 
