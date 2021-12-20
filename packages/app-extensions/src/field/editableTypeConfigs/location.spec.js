d819325560f0a3b2a256c3831bc6343d43410f51
Roland Züger <rzueger@tocco.ch>
2021-12-20 12:09:55 +0100

    fix(app-extensions): fix (strange) validation error in location fields
    The string fields in the location mapping are non nullable. Therefore,
    we need to set an empty string instead of `null` if the location object
    contains `null` as value for them.
    
    Refs: TOCDEV-4633
    Cherry-pick: Up
    Changelog: validation errors fixed for location fields

---
+++ packages/app-extensions/src/field/editableTypeConfigs/location.spec.js
@@ -0,0 +1,56 @@
+import location from './location'
+
+describe('app-extensions', () => {
+  describe('field', () => {
+    describe('editableTypeConfigs', () => {
+      describe('location', () => {
+        describe('getEvents', () => {
+          describe('onChange', () => {
+            const testOnChange = (
+              locationObject,
+              expectedChangeFieldValueCalls
+            ) => {
+              const formField = {
+                locationMapping: {
+                  district: 'admincode2'
+                }
+              }
+              const formName = 'test-form-name'
+              const changeFieldValue = jest.fn()
+              const formData = {
+                changeFieldValue
+              }
+
+              const {onChange} = location.getEvents({
+                formField,
+                formName,
+                formData
+              })
+              onChange(locationObject)
+
+              expect(changeFieldValue.mock.calls).to.eql(
+                expectedChangeFieldValueCalls
+              )
+            }
+
+            test('should set location property according to location mapping', () =>
+              testOnChange(
+                {
+                  district: 'Zürich'
+                },
+                [['test-form-name', 'admincode2', 'Zürich']]
+              ))
+
+            test('should set empty string if location property is null', () =>
+              testOnChange(
+                {
+                  district: null
+                },
+                [['test-form-name', 'admincode2', '']]
+              ))
+          })
+        })
+      })
+    })
+  })
+})
