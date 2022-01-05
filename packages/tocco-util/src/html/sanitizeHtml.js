6799cb0539f4bd57cad292c9938cb79f47599714
Isabel Boos <iboos@tocco.ch>
2022-01-05 14:14:09 +0100

    fix(tocco-util): allow whitelisted inline css for nice tooltips
    - css can be dangerous and were not allowed
    - however some inline css is used for nice tooltips
    - to still support those a defined set of inline css is valid
    
    Changelog: allow whitelisted inline css for nice tooltips
    Refs: TOCDEV-4767
    Cherry-pick: Up

--- packages/tocco-util/src/html/sanitizeHtml.js
+++ packages/tocco-util/src/html/sanitizeHtml.js
@@ -1,10 +1,43 @@
 import DOMPurify from 'dompurify'
 
+const allowedCssProperties = ['border', 'border-spacing', 'padding', 'margin', 'text-align', 'width']
+
+const isCssFunction = value => /\w+\(/.test(value)
+
+const sanitizeInlineStyle = styles =>
+  allowedCssProperties
+    .map(prop => {
+      const value = styles[prop]
+      return value && typeof value === 'string' && !isCssFunction(value) ? `${prop}:${value};` : null
+    })
+    .filter(Boolean)
+    .join('')
+
+const handleAfterSanitizeAttributes = node => {
+  if (node && typeof node.hasAttribute === 'function' && node.hasAttribute('style')) {
+    const sanitizedStyle = sanitizeInlineStyle(node.style)
+    if (sanitizedStyle) {
+      node.setAttribute('style', sanitizedStyle)
+    } else {
+      node.removeAttribute('style')
+    }
+  }
+
+  return node
+}
+
+DOMPurify.addHook('afterSanitizeAttributes', handleAfterSanitizeAttributes)
+
+/**
+ * Does not allow javascript and styles, except some whitelisted inline styles.
+ * @param {*} html
+ * @returns sanitized html
+ */
 const sanitizeHtml = html => {
   if (!html) {
     return html
   }
-  return DOMPurify.sanitize(html, {FORBID_TAGS: ['style'], FORBID_ATTR: ['style']})
+  return DOMPurify.sanitize(html, {FORBID_TAGS: ['style']})
 }
 
 export default sanitizeHtml
