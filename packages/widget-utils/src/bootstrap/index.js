a7d4af7c0c6b5fa1e20b04d25cd420d50296d216
Isabel Boos <iboos@tocco.ch>
2022-01-13 10:21:38 +0100

    feat(widget-utils): run bootstrap script after dom loaded
    To guarantee that the bootstrap script is considering
    all widgets on the page, it only should get executed
    once after the dom is loaded.
    
    Changelog: run bootstrap script after dom loaded
    Refs: TOCDEV-4804

--- packages/widget-utils/src/bootstrap/index.js
+++ packages/widget-utils/src/bootstrap/index.js
@@ -1,13 +1,16 @@
 import bootstrapWidgets from './bootstrapWidgets'
 import {loadScriptAsync, getBackendUrl} from './utils'
 
-;(async () => {
+;(() => {
   const backendUrl = getBackendUrl(document)
 
   const params = {
     backendUrl
   }
 
-  await loadScriptAsync(`${backendUrl}/nice2/javascript/nice2-react.release.js`)
-  await bootstrapWidgets(params)
+  // guarantee to initalize all widgets on the page
+  document.addEventListener('DOMContentLoaded', async () => {
+    await loadScriptAsync(`${backendUrl}/nice2/javascript/nice2-react.release.js`)
+    await bootstrapWidgets(params)
+  })
 })()
