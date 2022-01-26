aa14e581d293e5c613d087db78c36ff8b343c874
Stefan Würsten <swuersten@tocco.ch>
2022-01-26 14:51:35 +0100

    refactor(connect-principal, copy): use load mask
    Refs: TOCDEV-4871
    Cherry-pick: up

--- packages/connect-principal/src/components/Progress/Progress.js
+++ packages/connect-principal/src/components/Progress/Progress.js
@@ -1,40 +1,24 @@
 import React, {useEffect} from 'react'
 import PropTypes from 'prop-types'
-import {LoadingSpinner} from 'tocco-ui'
-import styled from 'styled-components'
+import {LoadMask} from 'tocco-ui'
 import SsoLoginApp from 'tocco-sso-login/src/main'
-
-const StyledDiv = styled.div`
-  text-align: center;
-  display: block;
-  margin-left: auto;
-  margin-right: auto;
-  width: 100%;
-  height: 40px;
-`
 
 const Progress = ({checkAccessRights, connectPrincipal, setShowSsoLoginApp, showSsoLoginApp}) => {
   useEffect(() => {
     checkAccessRights()
   }, [checkAccessRights])
 
-  if (showSsoLoginApp) {
-    const loginCompleted = ({provider, sub: ssoSubject}) => {
-      setShowSsoLoginApp(false)
-      connectPrincipal(provider, ssoSubject)
-    }
-    return (
+  const loginCompleted = ({provider, sub: ssoSubject}) => {
+    setShowSsoLoginApp(false)
+    connectPrincipal(provider, ssoSubject)
+  }
+  return (
+    <LoadMask required={[showSsoLoginApp]}>
       <SsoLoginApp
         ssoLoginEndpoint="/sso-lookup"
         loginCompleted={loginCompleted}/>
-    )
-  } else {
-    return (
-      <StyledDiv>
-        <LoadingSpinner size="30px"/>
-      </StyledDiv>
-    )
-  }
+    </LoadMask>
+  )
 }
 
 Progress.propTypes = {
