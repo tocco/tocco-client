eab28007c123ba942d0cfc988f40ce54c78c1c33
Stefan Würsten <swuersten@tocco.ch>
2022-01-26 14:51:35 +0100

    refactor(copy): use load mask
    Refs: TOCDEV-4871
    Cherry-pick: up

--- packages/copy/src/components/CopyProgress/CopyProgress.js
+++ packages/copy/src/components/CopyProgress/CopyProgress.js
@@ -1,27 +1,13 @@
 import PropTypes from 'prop-types'
 import React, {useEffect} from 'react'
-import styled from 'styled-components'
-import {LoadingSpinner} from 'tocco-ui'
-
-const StyledDiv = styled.div`
-  text-align: center;
-  display: block;
-  margin-left: auto;
-  margin-right: auto;
-  width: 100%;
-  height: 40px;
-`
+import {LoadMask} from 'tocco-ui'
 
 const CopyProgress = ({startCopy}) => {
   useEffect(() => {
     startCopy()
   }, [startCopy])
 
-  return (
-    <StyledDiv>
-      <LoadingSpinner size="30px" />
-    </StyledDiv>
-  )
+  return <LoadMask/>
 }
 
 CopyProgress.propTypes = {
