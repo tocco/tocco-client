4c946fd0683a081d63f8d95fd77daae880c42156
Isabel Boos <iboos@tocco.ch>
2022-02-01 15:06:18 +0100

    fix(tocco-ui): fix react attribute error
    - `netural` prop is passed further down to react-router link
    - the link component passes all props to the html tag underneath
    - React expect string value instead of bool and prints error
    - `neutral` should not be treated as html attribute in this case
    - never forward `neutral` prop on AdminLink to html tag

--- packages/tocco-ui/src/AdminLink/StyledLink.js
+++ packages/tocco-ui/src/AdminLink/StyledLink.js
@@ -3,6 +3,9 @@ import styled from 'styled-components'
 
 import {linkStyle} from './linkStyle'
 
-export default styled(Link)`
+export default styled(Link).withConfig({
+  // do not forward `neutral` prop to HTML
+  shouldForwardProp: (prop, defaultValidatorFn) => !['neutral'].includes(prop) && defaultValidatorFn(prop)
+})`
   ${linkStyle}
 `
