ab7a6b6207fbb708c4f4c63f673197f751e5783c
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2022-02-11 14:04:36 +0100

    refactor(tocco-theme): add widget theme
    Refs: TOCDEV-4926
    Changelog: add widget theme

---
+++ packages/tocco-theme/src/ToccoTheme/widgetTheme.js
@@ -0,0 +1,57 @@
+export default {
+  name: 'widget',
+  colors: {
+    paper: '#FFFFFF',
+    primary: '#000000',
+    secondary: '#8C8C8C',
+    secondaryLight: '#4D4D4D',
+    text: '#000',
+    backgroundBody: '#EDF1F5',
+    backgroundBreadcrumbs: '#DEE6ED',
+    backgroundPopover: '#424242',
+    signal: {
+      danger: {
+        paper: '#EF9A9A',
+        text: '#961b1b'
+      },
+      info: {
+        paper: '#81D4FA',
+        text: '#027cc1'
+      },
+      success: {
+        paper: '#A5D6A7',
+        text: '#337f37'
+      },
+      warning: {
+        paper: '#FFF59D',
+        text: '#d54300'
+      }
+    }
+  },
+  fontFamily: {
+    monospace: 'inherit',
+    regular: 'inherit',
+  },
+  fontSize: {
+    base: 1.0,
+    factor: 1.0
+  },
+  fontWeights: {
+    regular: 300,
+    bold: 500
+  },
+  lineHeights: {
+    dense: 1,
+    light: 1.2,
+    regular: 1.4
+  },
+  radii: {
+    regular: '0px',
+    medium: '2px',
+    large: '4px'
+  },
+  space: {
+    base: 1.0,
+    factor: 2
+  }
+}
