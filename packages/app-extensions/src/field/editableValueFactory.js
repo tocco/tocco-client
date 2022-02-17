97ac2371223ac5915505b120a1ee51b6aad71ad0
PadPalon <luca.neukom@gmail.com>
2022-02-17 16:20:53 +0100

    fix(app-extensions): try to find editableTypeConfig for mapped type
    Refs: TOCDEV-2442

--- packages/app-extensions/src/field/editableValueFactory.js
+++ packages/app-extensions/src/field/editableValueFactory.js
@@ -4,27 +4,22 @@ import {EditableValue, Range} from 'tocco-ui'
 
 import editableTypeConfigs from './editableTypeConfigs'
 
-const getEvents = (type, formField, formName, formData, events) =>
-  editableTypeConfigs[type] && editableTypeConfigs[type].getEvents
-    ? editableTypeConfigs[type].getEvents({formField, formName, formData, events})
-    : events
+const getEvents = (typeConfig, formField, formName, formData, events) =>
+  typeConfig && typeConfig.getEvents ? typeConfig.getEvents({formField, formName, formData, events}) : events
 
-const getValue = (type, formField, formName, formData, currentValue) =>
-  editableTypeConfigs[type] && editableTypeConfigs[type].getValue
-    ? editableTypeConfigs[type].getValue({formField, formName, formData, currentValue})
-    : currentValue
+const getValue = (typeConfig, formField, formName, formData, currentValue) =>
+  typeConfig && typeConfig.getValue ? typeConfig.getValue({formField, formName, formData, currentValue}) : currentValue
 
-const getOptions = (type, formField, formName, formData) =>
-  editableTypeConfigs[type] && editableTypeConfigs[type].getOptions
-    ? editableTypeConfigs[type].getOptions({formField, formName, formData})
-    : {}
+const getOptions = (typeConfig, formField, formName, formData) =>
+  typeConfig && typeConfig.getOptions ? typeConfig.getOptions({formField, formName, formData}) : {}
 
 const EditableValueFactory = ({type, range, formField, formName, value, info, events, formData}) => {
   const formType = formField.dataType || formField.componentType
-  const options = getOptions(formType, formField, formName, formData)
+  const typeConfig = editableTypeConfigs[type] || editableTypeConfigs[formType]
+  const options = getOptions(typeConfig, formField, formName, formData)
 
-  events = getEvents(formType, formField, formName, formData, events)
-  value = getValue(formType, formField, formName, formData, value)
+  events = getEvents(typeConfig, formField, formName, formData, events)
+  value = getValue(typeConfig, formField, formName, formData, value)
 
   const Component = range ? Range : EditableValue
 
