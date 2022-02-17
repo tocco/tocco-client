22b1cd1d016c75f76f367b53fd7d075800a43477
PadPalon <luca.neukom@gmail.com>
2022-02-17 17:16:23 +0100

    fix(app-extensions): use existing textresources for booleanSelect
    Refs: TOCDEV-2442

--- packages/app-extensions/src/field/editableTypeConfigs/booleanSelect.js
+++ packages/app-extensions/src/field/editableTypeConfigs/booleanSelect.js
@@ -1,6 +1,6 @@
 export default {
   getOptions: ({formData}) => ({
-    trueLabel: formData.intl.formatMessage({id: 'client.component.boolean-select.trueLabel'}),
-    falseLabel: formData.intl.formatMessage({id: 'client.component.boolean-select.falseLabel'})
+    trueLabel: formData.intl.formatMessage({id: 'client.common.yes'}),
+    falseLabel: formData.intl.formatMessage({id: 'client.common.no'})
   })
 }
