d5b29106c9e0079a3f75f5d371e2b8496e307ec2
PadPalon <luca.neukom@gmail.com>
2022-02-17 11:50:16 +0100

    feat(admin): close menu when navigating to dashboard
    Refs: TOCDEV-4936
    Changelog: menu now closes when navigating to the dashboard
    Cherry-pick: Up

--- packages/admin/src/components/Header/HeaderContainer.js
+++ packages/admin/src/components/Header/HeaderContainer.js
@@ -2,6 +2,7 @@ import {injectIntl} from 'react-intl'
 import {connect} from 'react-redux'
 import {notification} from 'tocco-app-extensions'
 
+import {setMenuOpen} from '../../modules/navigation/actions'
 import {doLogout, loadBusinessUnits, changeBusinessUnit} from '../../modules/session/actions'
 import Header from './Header'
 
@@ -11,7 +12,8 @@ const mapActionCreators = {
   changeBusinessUnit,
   openModalComponent: notification.modal,
   removeModalComponent: notification.removeModal,
-  info: notification.toaster
+  info: notification.toaster,
+  setMenuOpen
 }
 
 const mapStateToProps = (state, props) => ({
