8d14767ce147cdc8728993f0e4e7a6b15bfb7341
Isabel Boos <iboos@tocco.ch>
2022-02-22 08:26:37 +0100

    refactor(tocco-util): move path/regex handling to util
    Refs: TOCDEV-5060

--- packages/docs-browser/src/modules/routing/sagas.js
+++ packages/docs-browser/src/modules/routing/sagas.js
@@ -1,24 +1,12 @@
-import pathToRegexp from 'path-to-regexp'
 import {takeLatest, all, put} from 'redux-saga/effects'
+import {route} from 'tocco-util'
 
 import * as actions from './actions'
 
 const pathRegex = '/docs/:model/:key/:view(list|detail)'
 
 export function* setParams({payload: {path}}) {
-  const pathParts = []
-  const re = pathToRegexp(pathRegex, pathParts)
-  const res = re.exec(path)
-
-  let params = {}
-  if (res !== null) {
-    params = pathParts.reduce((acc, pathPart, idx) => {
-      return {
-        ...acc,
-        [pathPart.name]: res[idx + 1]
-      }
-    }, {})
-  }
+  const params = route.extractParamsFromPath(pathRegex, path) || {}
 
   yield put(actions.setParams(params))
 }
---
+++ packages/tocco-util/src/route/path.js
@@ -0,0 +1,19 @@
+import pathToRegexp from 'path-to-regexp'
+
+export const extractParamsFromPath = (pathRegex, path) => {
+  const pathParts = []
+  const re = pathToRegexp(pathRegex, pathParts)
+  const res = re.exec(path)
+
+  if (res !== null) {
+    return pathParts.reduce(
+      (acc, pathPart, idx) => ({
+        ...acc,
+        [pathPart.name]: res[idx + 1]
+      }),
+      {}
+    )
+  }
+
+  return null
+}
