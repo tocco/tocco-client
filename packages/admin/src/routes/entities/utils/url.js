926666e2ba93555ffa33e05e195758cb02d1015f
Isabel Boos <iboos@tocco.ch>
2022-02-22 08:26:37 +0100

    refactor(tocco-util): move path/regex handling to util
    Refs: TOCDEV-5060

--- packages/admin/src/routes/entities/utils/url.js
+++ packages/admin/src/routes/entities/utils/url.js
@@ -1,4 +1,4 @@
-import pathToRegexp from 'path-to-regexp'
+import {route} from 'tocco-util'
 
 const entityUrlRegex = '/e/:entity/:key?/:relation*/:view(list|detail|edit|create|relations|action)/:actionId?'
 const actionUrlRegex = '/e/action/:actionId'
@@ -9,27 +9,8 @@ const actionUrlRegex = '/e/action/:actionId'
  * @param pathname {String} url / path starting with /e/
  */
 export const getPathInfo = pathname => {
-  const pathParts = []
-  const re = pathToRegexp(entityUrlRegex, pathParts)
-  const res = re.exec(pathname)
+  const entityParams = route.extractParamsFromPath(entityUrlRegex, pathname)
+  const actionParams = route.extractParamsFromPath(actionUrlRegex, pathname)
 
-  if (res !== null) {
-    return pathParts.reduce((acc, pathPart, idx) => {
-      return {
-        ...acc,
-        [pathPart.name]: res[idx + 1]
-      }
-    }, {})
-  }
-
-  const actionPathRegex = pathToRegexp(actionUrlRegex, pathParts)
-  const parseResult = actionPathRegex.exec(pathname)
-
-  if (parseResult !== null) {
-    return {
-      actionId: parseResult[1]
-    }
-  }
-
-  return null
+  return entityParams || actionParams
 }
--- packages/tocco-util/src/route/index.js
+++ packages/tocco-util/src/route/index.js
@@ -1,3 +1,4 @@
+import {extractParamsFromPath} from './path'
 import {loadRoute} from './route'
 import Router from './Router'
 import RouteWithSubRoutes from './RouteWithSubRoutes'
@@ -5,5 +6,6 @@ import RouteWithSubRoutes from './RouteWithSubRoutes'
 export default {
   loadRoute,
   Router,
-  RouteWithSubRoutes
+  RouteWithSubRoutes,
+  extractParamsFromPath
 }
