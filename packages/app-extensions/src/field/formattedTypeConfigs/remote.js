caf96079cd20ae8cd5dbbbc1cb60e6cc9fbc57b6
Isabel Boos <iboos@tocco.ch>
2022-02-24 09:01:17 +0100

    fix(tocco-ui): fix table flicker on firefox (part 2)
    - on ffox the table was flickering while moused moved over links
    - having a react component inside the options is not working smoothly
    
    Refs: TOCDEV-5006
    Cherry-pick: Up

--- packages/app-extensions/src/field/formattedTypeConfigs/remote.js
+++ packages/app-extensions/src/field/formattedTypeConfigs/remote.js
@@ -1,5 +1,4 @@
 import _get from 'lodash/get'
-import React from 'react'
 
 export default {
   dataContainerProps: ({formField}) => ({
@@ -9,13 +8,7 @@ export default {
   getOptions: ({formField, formData}) => ({
     tooltips: _get(formData.tooltips, formField.targetEntity, null),
     loadTooltip: id => formData.loadTooltip(formField.targetEntity, id),
-    DetailLink:
-      formData.navigationStrategy && formData.navigationStrategy.DetailLink
-        ? ({entityKey, children}) => (
-            <formData.navigationStrategy.DetailLink entityName={formField.targetEntity} entityKey={entityKey}>
-              {children}
-            </formData.navigationStrategy.DetailLink>
-          )
-        : null
+    navigationStrategy: formData.navigationStrategy,
+    linkProps: {entityName: formField.targetEntity}
   })
 }
