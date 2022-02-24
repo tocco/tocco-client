2521dc9d8219b81e1afc0de3cc6015b3ac819409
Isabel Boos <iboos@tocco.ch>
2022-02-24 09:01:17 +0100

    fix(tocco-ui): fix table flicker on firefox (part 2)
    - on ffox the table was flickering while moused moved over links
    - having a react component inside the options is not working smoothly
    
    Refs: TOCDEV-5006

--- packages/tocco-ui/src/FormattedValue/typeFormatters/MultiSelectFormatter.spec.js
+++ packages/tocco-ui/src/FormattedValue/typeFormatters/MultiSelectFormatter.spec.js
@@ -21,7 +21,7 @@ describe('tocco-ui', () => {
           const wrapper = mount(<MultiSelectFormatter
             value={value}
             breakWords={true}
-            options={{DetailLink: ({key, children}) => <a>{children}</a>}}
+            options={{navigationStrategy: {DetailLink: ({key, children}) => <a>{children}</a>}}}
           />)
           expect(wrapper.find('a')).to.have.length(2)
         })
