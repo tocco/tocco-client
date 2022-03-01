47c3fe05e7e402f8e4ad07cad2d61272b5ba6abf
Isabel Boos <iboos@tocco.ch>
2022-03-01 09:27:57 +0100

    refactor(devcon): use route component instead of config
    - preparation for react router upgrade
    - react router v6 does only support `element` on route component
    - much easier to migrate with route components
    
    Refs: TOCDEV-4660

---
+++ packages/devcon/src/components/devcon/DevCon.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import {Redirect, Route, Switch} from 'react-router-dom'
+import {notification} from 'tocco-app-extensions'
+import {Typography} from 'tocco-ui'
+
+import DbRefactoring from '../dbrefactoring/DbRefactoring'
+import LogView from '../log/LogView'
+import ModelValidationView from '../modelvalidation/ModelValidationView'
+import SqlLogView from '../sqllog/LogView'
+import {StyledDevCon, StyledNavigation, StyledRouterLink} from './StyledDevCon'
+
+const DevCon = () => (
+  <StyledDevCon>
+    <notification.Notifications />
+    <Typography.Span>Tocco Developer Console</Typography.Span>
+    <StyledNavigation>
+      <StyledRouterLink to="/log">Log</StyledRouterLink>
+      <StyledRouterLink to="/modelvalidation">Model Validation</StyledRouterLink>
+      <StyledRouterLink to="/dbrefactoring">DB Refactoring</StyledRouterLink>
+      <StyledRouterLink to="/sqllog">SQL Log</StyledRouterLink>
+    </StyledNavigation>
+    <Switch>
+      <Route path="/" exact render={() => <Redirect to={{pathname: '/log'}} />} />
+      <Route path="/log" exact render={() => <LogView />} />
+      <Route path="/dbrefactoring" exact render={() => <DbRefactoring />} />
+      <Route path="/modelvalidation" exact render={() => <ModelValidationView />} />
+      <Route path="/sqllog" exact render={() => <SqlLogView />} />
+    </Switch>
+  </StyledDevCon>
+)
+
+DevCon.propTypes = {}
+
+export default DevCon
