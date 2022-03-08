d0bfdc9817043f53eb3226f195779d7cc3c009a4
Stefan Würsten <swuersten@tocco.ch>
2022-03-08 08:34:22 +0100

    refactor(entity-detail): refactor dispatch actions
    Refs: TOCDEV-4665
    Changelog: refactor dispatch actions

--- packages/entity-detail/src/input.js
+++ packages/entity-detail/src/input.js
@@ -1,19 +1,54 @@
+import {action} from 'tocco-util'
+
 import {
   loadDetailView,
-  setEntityName,
-  setEntityId,
-  setFormName,
-  setMode,
   setAppId,
-  setDefaultValues
+  setDefaultValues,
+  setEntityId,
+  setEntityName,
+  setFormName,
+  setMode
 } from './modules/entityDetail/actions'
 
-export const getDispatchActions = input => [
-  setEntityName(input.entityName),
-  setEntityId(input.entityId ? input.entityId : null),
-  setMode(input.mode),
-  setFormName(input.formName),
-  setAppId(input.id),
-  setDefaultValues(input.defaultValues),
-  loadDetailView()
+export const getDispatchActions = input => {
+  const actions = action.getDispatchActions(input, actionSettings)
+
+  if (actions.length > 0) {
+    actions.push(loadDetailView())
+  }
+
+  return actions
+}
+
+const actionSettings = [
+  {
+    name: 'entityName',
+    action: setEntityName,
+    argsFactory: input => [input.entityName]
+  },
+  {
+    name: 'entityId',
+    action: setEntityId,
+    argsFactory: input => [input.entityId || null]
+  },
+  {
+    name: 'mode',
+    action: setMode,
+    argsFactory: input => [input.mode]
+  },
+  {
+    name: 'formName',
+    action: setFormName,
+    argsFactory: input => [input.formName]
+  },
+  {
+    name: 'id',
+    action: setAppId,
+    argsFactory: input => [input.id]
+  },
+  {
+    name: 'defaultValues',
+    action: setDefaultValues,
+    argsFactory: input => [input.defaultValues]
+  }
 ]
