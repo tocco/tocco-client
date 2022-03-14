0fe44781c2a904adac6062c69b6fdcabc4150f29
Stefan Würsten <swuersten@tocco.ch>
2022-03-14 13:11:18 +0100

    feat(widget-utils): add assetUrl

--- packages/widgets/widget-utils/src/bootstrap/index.js
+++ packages/widgets/widget-utils/src/bootstrap/index.js
@@ -1,15 +1,23 @@
 import bootstrapWidgets from './bootstrapWidgets'
 import {getBackendUrl, loadScriptAsync} from './utils'
 ;(() => {
-  const backendUrl = getBackendUrl(document)
+  let backendUrl, assetUrl
+  if (window.backendUrl) {
+    backendUrl = window.backendUrl
+    assetUrl = getBackendUrl(document)
+  } else {
+    backendUrl = getBackendUrl(document)
+    assetUrl = getBackendUrl(document)
+  }
 
   const params = {
-    backendUrl
+    backendUrl,
+    assetUrl
   }
 
   // guarantee to initalize all widgets on the page
   document.addEventListener('DOMContentLoaded', async () => {
-    await loadScriptAsync(`${backendUrl}/nice2/javascript/nice2-react.release.js`)
+    await loadScriptAsync(`${assetUrl}/nice2/javascript/nice2-react.release.js`)
     await bootstrapWidgets(params)
   })
 })()
