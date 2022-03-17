c16afa49840ce6bb52105388fc503b2808a2840e
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2022-03-17 17:30:00 +0100

    refactor(resource-scheduler): add collapse feature
    Refs: TOCDEV-5187
    Changelog: add collapse feature to resource-scheduler

--- packages/actions/resource-scheduler/src/components/ResourceScheduler/StyledComponents.js
+++ packages/actions/resource-scheduler/src/components/ResourceScheduler/StyledComponents.js
@@ -1,9 +1,19 @@
 import Split from 'react-split'
-import styled from 'styled-components'
-import {scale, StyledScrollbar, theme} from 'tocco-ui'
+import styled, {css} from 'styled-components'
+import {Button, scale, StyledScrollbar, theme} from 'tocco-ui'
+
+const drawerSymbol =
+  // eslint-disable-next-line max-len
+  'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAeCAYAAADkftS9AAAAIklEQVQoU2M4c+bMfxAGAgYYmwGrIIiDjrELjpo5aiZeMwF+yNnOs5KSvgAAAABJRU5ErkJggg=='
+
+export const StyledResourceSchedulerWrapper = styled.div`
+  display: flex;
+  height: 100%;
+`
 
 export const StyledSplitPanelWrapperLeft = styled.div`
   && {
+    display: ${({isCollapsed}) => (isCollapsed ? 'none' : 'block')};
     background-color: ${theme.color('paper')};
     height: 100%;
     overflow: auto;
@@ -25,15 +35,29 @@ export const StyledSplitPanelWrapperRight = styled.div`
   }
 `
 
-export const StyledSplitPane = styled(Split)`
+export const StyledToggleCollapse = styled.div`
   display: flex;
-  flex-direction: row;
-  height: 100%;
+  background-color: ${theme.color('paper')};
+  padding: 0 ${scale.space(-0.53)} ${scale.space(-0.53)} ${scale.space(-1.7)};
 `
 
-const drawerSymbol =
-  // eslint-disable-next-line max-len
-  'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAeCAYAAADkftS9AAAAIklEQVQoU2M4c+bMfxAGAgYYmwGrIIiDjrELjpo5aiZeMwF+yNnOs5KSvgAAAABJRU5ErkJggg=='
+export const StyledPlaceHolder = styled.div`
+  display: ${({isCollapsed}) => (isCollapsed ? 'flex' : 'none')};
+  flex: 1;
+  height: 100%;
+  width: 25px;
+  background: ${theme.color('paper')};
+  padding-top: ${scale.space(-0.53)};
+  align-items: flex-start;
+
+  &:hover {
+    cursor: pointer;
+  }
+
+  ${StyledToggleCollapse} {
+    padding: 0 0 ${scale.space(-0.53)} ${scale.space(-1.7)};
+  }
+`
 
 export const StyledGutter = styled.div`
   background-color: ${theme.color('backgroundBody')};
@@ -44,5 +68,33 @@ export const StyledGutter = styled.div`
 
   &:hover {
     cursor: col-resize;
+  }
+`
+
+export const StyledSplitPane = styled(Split)`
+  display: flex;
+  flex-direction: row;
+  height: 100%;
+
+  ${StyledGutter} {
+    display: ${({isCollapsed}) => (isCollapsed ? 'none' : 'block')};
+  }
+`
+
+export const StyledToggleCollapseButton = styled(Button)`
+  font-size: ${scale.font(0)};
+  padding: 0;
+  ${({isCollapsed}) =>
+    !isCollapsed &&
+    css`
+      position: relative;
+      left: -4px;
+      top: 2px;
+    `}
+  &:hover,
+  &:focus,
+  ${/* sc-selector */ StyledPlaceHolder}:hover & {
+    background-color: transparent;
+    color: ${theme.color('secondaryLight')};
   }
 `
