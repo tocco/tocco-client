70d10099baa057cb9809d8e5ace7004fb117a32f
Isabel Boos <iboos@tocco.ch>
2022-03-22 10:35:44 +0100

    chore: deploy storybook to seperate repo
    - to decrease tocco-client repo size
      storybook deployment is deployed in a separate repo
    - push builded storybook pages to tocco-storybook repo
    
    Cherry-pick: Up
    Refs: TOCDEV-5165

---
+++ scripts/git.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+function setupGithub() {
+  # add ssh key of user
+  eval $(ssh-agent -s)
+  ssh-add <(echo "$GITHUB_DEPLOY_PRIVATE_KEY" | base64 -d)
+
+  # add github as known host
+  mkdir -p ~/.ssh
+  echo "github.com ssh-rsa ${GITHUB_PUBLIC_KEY}" >>~/.ssh/known_hosts
+}
+
+function setupGitlab() {
+  # add ssh key of user
+  eval $(ssh-agent -s)
+  ssh-add <(echo "$GITLAB_SSH_PRIVATE_KEY" | base64 -d)
+
+  # add github as known host
+  mkdir -p ~/.ssh
+  echo "gitlab.com ssh-rsa ${GITLAB_PUBLIC_KEY}" >>~/.ssh/known_hosts
+}
+
+function setupGithubUser() {
+  git config user.email "tocco.github.bot@gmail.com"
+  git config user.name "ToccoBot"
+}
+
+function setupGitlabUser() {
+  git config user.email "admin@tocco.ch"
+  git config user.name "Tocco Admin"
+}
