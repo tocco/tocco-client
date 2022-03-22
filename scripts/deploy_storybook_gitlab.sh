fc68787c40c24eb2f0d41a15b7f776ffda1f95e2
Isabel Boos <iboos@tocco.ch>
2022-03-22 10:35:44 +0100

    chore: deploy storybook to seperate repo
    - to decrease tocco-client repo size
      storybook deployment is deployed in a separate repo
    - push builded storybook pages to tocco-storybook repo
    
    Cherry-pick: Up
    Refs: TOCDEV-5165

--- scripts/deploy_storybook_gitlab.sh
+++ scripts/deploy_storybook_gitlab.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 set -eu # Exit with nonzero exit code if anything fails
-PAGES_BRANCH="gh-pages"
 
-source ./scripts/github.sh
-setupGithub
+PAGES_BRANCH="master"
+REPO_NAME="tocco-storybook"
 
-echo "Clone ${PAGES_BRANCH} branch"
+source ./scripts/git.sh
+setupGitlab
+
 cd dist
-git clone https://github.com/tocco/tocco-client.git $PAGES_BRANCH --single-branch --branch $PAGES_BRANCH --depth 1
 
-echo "Copy storybook to folder in repo"
+echo "Clone ${REPO_NAME}"
+git clone git@gitlab.com:toccoag/tocco-storybook.git --single-branch --branch $PAGES_BRANCH --depth 1
+
+echo "Copy storybook pages to ${REPO_NAME}"
 BRANCH_FOLDER_NAME="${CI_COMMIT_BRANCH//\//_}"
 echo "folder name: ${BRANCH_FOLDER_NAME}"
-rm -rf $PAGES_BRANCH/$BRANCH_FOLDER_NAME
-mkdir $PAGES_BRANCH/$BRANCH_FOLDER_NAME
-mv storybook/* $PAGES_BRANCH/$BRANCH_FOLDER_NAME
-cd $PAGES_BRANCH
+rm -rf $REPO_NAME/$BRANCH_FOLDER_NAME
+mkdir $REPO_NAME/$BRANCH_FOLDER_NAME
+mv storybook/* $REPO_NAME/$BRANCH_FOLDER_NAME
+cd $REPO_NAME
 
 echo "Commit and push"
-setupGitUser
+setupGitlabUser
 
 git add -A
 rev=$(git rev-parse --short HEAD)
 git diff-index --quiet HEAD || git commit -m "rebuild storybook at ${rev}"
 
 git pull --rebase origin $PAGES_BRANCH
-git push -q git@github.com:tocco/tocco-client.git HEAD:$PAGES_BRANCH
+git push -q git@gitlab.com:toccoag/tocco-storybook.git HEAD:$PAGES_BRANCH
