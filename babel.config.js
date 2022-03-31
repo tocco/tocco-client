39961eed28bb1e7463c8f744115570c3e4c85561
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2022-03-31 14:29:46 +0200

    chore: introduce react linting rules
    Refs: TOCDEV-5310
    Changelog: introduce new linting rules to remove obsolete React import which is not necessary from React v17 onwards

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,7 @@ module.exports = function (api) {
   api.cache(true)
 
   return {
-    presets: ['@babel/preset-env', '@babel/preset-react'],
+    presets: ['@babel/preset-env', ['@babel/preset-react', {runtime: 'automatic'}]],
     plugins: [
       '@babel/plugin-proposal-class-properties',
       '@babel/plugin-proposal-private-methods',
