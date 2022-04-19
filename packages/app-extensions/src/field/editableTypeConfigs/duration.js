5de51f1731d469281fa2ef23f9bc8d8a22c13242
Roland Züger <rzueger@tocco.ch>
2022-04-19 11:45:55 +0200

    feat(tocco-ui): allow duration values to be negative
    - `options.allowNegative` will be set to true, if there is no number
      range validation or if `fromIncluding` is less than 0
    - if negative values are allowed, the minus sign "-" will be displayed
      in the biggest unit that's not 0 (so, in the minutes field, if there are
      no hours, and in the hours field, if there are hours)
    - other improvements:
    -- hours can be greater than 24 now
    -- minutes can also be greater or equal to 60 now -> value is converted
       to hours and minutes then (old hours value will be deleted)
    -- fixed automatic growing (unit "px" was missing in styling rule) and
       added a padding of 5px
    -- removed automatic rewriting of minutes to 0 or to 59 in certain
       conditions as it was rather irritating than helpful
    - needs to be cherry picked to newer branches by hand, as there
      will be some merge conflicts
    
    Changelog: allow duration values to be negative
    Refs: TOCDEV-5338

--- packages/app-extensions/src/field/editableTypeConfigs/duration.js
+++ packages/app-extensions/src/field/editableTypeConfigs/duration.js
@@ -1,6 +1,13 @@
+const allowNegative = formField =>
+  !formField.validation
+  || !formField.validation.numberRange
+  || typeof formField.validation.numberRange.fromIncluding !== 'number'
+  || formField.validation.numberRange.fromIncluding < 0
+
 export default {
-  getOptions: ({formData}) => ({
+  getOptions: ({formField, formData}) => ({
     hoursLabel: formData.intl.formatMessage({id: 'client.component.duration.hoursLabel'}),
-    minutesLabel: formData.intl.formatMessage({id: 'client.component.duration.minutesLabel'})
+    minutesLabel: formData.intl.formatMessage({id: 'client.component.duration.minutesLabel'}),
+    allowNegative: allowNegative(formField)
   })
 }
