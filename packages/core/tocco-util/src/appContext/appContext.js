9f67744fba6dd65adb35180dd6f3ef48302048ed
Isabel Boos <iboos@tocco.ch>
2022-04-19 11:17:13 +0200

    feat(widget-utils): add widgetConfigKey to appContext

--- packages/core/tocco-util/src/appContext/appContext.js
+++ packages/core/tocco-util/src/appContext/appContext.js
@@ -3,5 +3,11 @@ import PropTypes from 'prop-types'
 import {ALLOWED_EMBED_TYPES} from '../env/env'
 
 export const propTypes = PropTypes.shape({
-  embedType: PropTypes.oneOf(ALLOWED_EMBED_TYPES)
+  embedType: PropTypes.oneOf(ALLOWED_EMBED_TYPES),
+  widgetConfigKey: (props, propName, componentName) => {
+    // widgetConfigKey is only required for widgets
+    if (props.embedType === 'widget' && !props[propName]) {
+      return new Error('Prop `' + propName + '` is missing for ' + ' `' + componentName + '`. Validation failed.')
+    }
+  }
 })
