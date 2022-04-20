9d66def59eff0b460690c47d5891e09f71eee724
Stefan Würsten <swuersten@tocco.ch>
2022-04-20 13:18:28 +0200

    chore: adjust plop package template

--- plop/templates/package/src/main.js
+++ plop/templates/package/src/main.js
@@ -1,6 +1,5 @@
-import React from 'react'
-import {reducer as reducerUtil} from 'tocco-util'
 import {appFactory} from 'tocco-app-extensions'
+import {reducer as reducerUtil} from 'tocco-util'
 
 import reducers, {sagas} from './modules/reducers'
 
@@ -11,27 +10,22 @@ const initApp = (id, input, events, publicPath) => {
 
   const store = appFactory.createStore(reducers, sagas, input, packageName)
 
-  return appFactory.createApp(
-    packageName,
-    content,
-    store,
-    {
-      input,
-      events,
-      actions: [],
-      publicPath,
-      textResourceModules: ['component', 'common', packageName]
-    }
-  )
+  return appFactory.createApp(packageName, content, store, {
+    input,
+    events,
+    actions: [],
+    publicPath,
+    textResourceModules: ['component', 'common', packageName]
+  })
 }
 
-(() => {
+;(() => {
   if (__PACKAGE_NAME__ === packageName) {
     appFactory.registerAppInRegistry(packageName, initApp)
 
     if (__DEV__) {
       const input = require('./dev/input.json')
-  
+
       if (!__NO_MOCK__) {
         const fetchMock = require('fetch-mock').default
         fetchMock.config.overwriteRoutes = false
@@ -39,17 +33,17 @@ const initApp = (id, input, events, publicPath) => {
         setupFetchMocks(packageName, fetchMock)
         fetchMock.spy()
       }
-  
+
       const app = initApp(packageName, input)
-  
+
       if (module.hot) {
         module.hot.accept('./modules/reducers', () => {
           const reducers = require('./modules/reducers').default
           reducerUtil.hotReloadReducers(app.store, reducers)
         })
       }
-  
+
       appFactory.renderApp(app.component)
-    } 
+    }
   }
 })()
