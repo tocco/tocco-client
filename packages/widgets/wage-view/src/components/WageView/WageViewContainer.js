0a97906841b54a0c37a0c1ee52f6c140f048a4ef
Stefan Würsten <swuersten@tocco.ch>
2022-04-20 13:07:25 +0200

    feat(wage-view): create wage view widget
    Changelog: create wage view widget
    Refs: TOCDEV-5254

---
+++ packages/widgets/wage-view/src/components/WageView/WageViewContainer.js
@@ -0,0 +1,21 @@
+import {injectIntl} from 'react-intl'
+import {connect} from 'react-redux'
+import {reports} from 'tocco-app-extensions'
+
+import WageView from './WageView'
+
+const mapActionCreators = {
+  loadReports: reports.loadReports
+}
+
+const mapStateToProps = state => ({
+  reports: state.reports.reports,
+  reportIds: state.input.reportIds,
+  allowCreate: state.input.allowCreate,
+  searchFilters: state.input.searchFilters,
+  backendUrl: state.input.backendUrl,
+  businessUnit: state.input.businessUnit,
+  appContext: state.input.appContext
+})
+
+export default connect(mapStateToProps, mapActionCreators)(injectIntl(WageView))
---
+++ packages/widgets/wage-view/src/main.js
@@ -0,0 +1,62 @@
+import PropTypes from 'prop-types'
+import {appFactory, reports} from 'tocco-app-extensions'
+import {env, appContext} from 'tocco-util'
+
+import WageView from './components/WageView'
+
+const packageName = 'wage-view'
+
+const initApp = (id, input, events, publicPath) => {
+  env.setInputEnvs(input)
+
+  const content = <WageView />
+
+  const store = appFactory.createStore({}, null, input, packageName)
+  reports.addToStore(store)
+
+  return appFactory.createApp(packageName, content, store, {
+    input,
+    events,
+    actions: [],
+    publicPath,
+    textResourceModules: ['component', 'common', 'actions', packageName]
+  })
+}
+
+;(() => {
+  if (__PACKAGE_NAME__ === packageName) {
+    appFactory.registerAppInRegistry(packageName, initApp)
+
+    if (__DEV__) {
+      const input = require('./dev/input.json')
+
+      if (!__NO_MOCK__) {
+        const fetchMock = require('fetch-mock').default
+        fetchMock.config.overwriteRoutes = false
+        const setupFetchMocks = require('./dev/fetchMocks').default
+        setupFetchMocks(packageName, fetchMock)
+        fetchMock.spy()
+      }
+
+      const app = initApp(packageName, input)
+
+      appFactory.renderApp(app.component)
+    }
+  }
+})()
+
+const WageViewApp = props => {
+  const {component} = appFactory.useApp({initApp, props, packageName})
+  return component
+}
+
+WageViewApp.propTypes = {
+  allowCreate: PropTypes.bool,
+  reports: PropTypes.arrayOf(PropTypes.string).isRequired,
+  searchFilters: PropTypes.arrayOf(PropTypes.string),
+  backendUrl: PropTypes.string,
+  businessUnit: PropTypes.string,
+  appContext: appContext.propTypes.isRequired
+}
+
+export default WageViewApp
