768f0c4936988c20d60a4a974d1c0b2ab9ec5958
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2022-04-22 10:32:49 +0200

    chore: fix tests
    Refs: TOCDEV-5341

--- packages/core/tocco-ui/src/Table/SortingState.spec.js
+++ packages/core/tocco-ui/src/Table/SortingState.spec.js
@@ -1,4 +1,5 @@
 import {mount} from 'enzyme'
+import {IntlProvider} from 'react-intl'
 
 import {Icon} from '../'
 import SortingState from './SortingState'
@@ -7,30 +8,50 @@ describe('tocco-ui', () => {
   describe('Table', () => {
     describe('SortingState', () => {
       test('should render arrow up', () => {
-        const wrapper = mount(<SortingState column={{id: 'firstname', sorting: {sortRank: 1, order: 'asc'}}} />)
+        const wrapper = mount(
+          <IntlProvider locale="de">
+            <SortingState column={{id: 'firstname', sorting: {sortRank: 1, order: 'asc'}}} />
+          </IntlProvider>
+        )
         expect(wrapper.find(Icon)).to.have.prop('icon', 'sort-up')
       })
 
       test('should render arrow down', () => {
-        const wrapper = mount(<SortingState column={{id: 'firstname', sorting: {sortRank: 1, order: 'desc'}}} />)
+        const wrapper = mount(
+          <IntlProvider locale="de">
+            <SortingState column={{id: 'firstname', sorting: {sortRank: 1, order: 'desc'}}} />
+          </IntlProvider>
+        )
         expect(wrapper.find(Icon)).to.have.prop('icon', 'sort-down')
       })
 
       test('should render nothing if no sorting set', () => {
-        const wrapper = mount(<SortingState column={{id: 'firstname'}} />)
-        expect(wrapper).to.be.empty
+        const wrapper = mount(
+          <IntlProvider locale="de">
+            <SortingState column={{id: 'firstname'}} />
+          </IntlProvider>
+        )
+        expect(wrapper.find(Icon)).to.have.length(0)
       })
 
       test('should render sup if second sorting is set', () => {
-        const wrapper = mount(<SortingState column={{id: 'firstname', sorting: {sortRank: 2, order: 'desc'}}} />)
+        const wrapper = mount(
+          <IntlProvider locale="de">
+            <SortingState column={{id: 'firstname', sorting: {sortRank: 2, order: 'desc'}}} />
+          </IntlProvider>
+        )
         expect(wrapper.find(Icon).prop('icon')).to.eql('sort-down')
-        expect(wrapper.html()).contains('<span>2</span>')
+        expect(wrapper.html()).to.be.contains('2')
       })
 
       test('should render sup if third sorting is set', () => {
-        const wrapper = mount(<SortingState column={{id: 'firstname', sorting: {sortRank: 3, order: 'asc'}}} />)
+        const wrapper = mount(
+          <IntlProvider locale="de">
+            <SortingState column={{id: 'firstname', sorting: {sortRank: 3, order: 'asc'}}} />
+          </IntlProvider>
+        )
         expect(wrapper.find(Icon).prop('icon')).to.eql('sort-up')
-        expect(wrapper.html()).contains('<span>3</span>')
+        expect(wrapper.html()).to.be.contains('3')
       })
     })
   })
