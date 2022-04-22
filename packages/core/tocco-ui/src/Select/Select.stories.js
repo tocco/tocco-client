4555075caec0b79583fdae3a8c25b01ddc0e5838
Isabel Boos <iboos@tocco.ch>
2022-04-22 08:16:33 +0200

    docs(tocco-ui): improve select story
    Refs: TOCDEV-5318

--- packages/core/tocco-ui/src/Select/Select.stories.js
+++ packages/core/tocco-ui/src/Select/Select.stories.js
@@ -1,10 +1,10 @@
 import {action} from '@storybook/addon-actions'
 import PropTypes from 'prop-types'
-import React from 'react'
+import {useState} from 'react'
 
 import Select from './'
 
-const options = [
+const availableOptions = [
   {key: 1, display: 'Option 1'},
   {key: 2, display: 'Option 2'},
   {key: 3, display: 'Option 3'},
@@ -14,106 +14,197 @@ const options = [
 
 export default {
   title: 'Tocco-UI/Select',
-  component: Select
+  component: Select,
+  argTypes: {
+    hasAdvancedSearch: {type: 'boolean', defaultValue: false}
+  }
 }
 
-class SelectStory extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      isLoading: false,
-      options: null,
-      value: [{key: 3, display: 'Option 3'}],
-      valueMulti: [{key: 2, display: 'Option 2'}],
-      moreOptionsAvailable: false,
-      tooltips: {}
-    }
-  }
+const useSelectStoryHepers = delay => {
+  const [isLoading, setIsLoading] = useState(false)
+  const [options, setOptions] = useState(null)
+  const [value, setValue] = useState([availableOptions[2]])
+  const [valueMulti, setValueMulti] = useState([availableOptions[0], availableOptions[3]])
+  const [moreOptionsAvailable, setMoreOptionsAvailable] = useState(false)
+  const [tooltips, setTooltips] = useState({})
 
-  fetchOptions = () => {
-    if (this.state.options === null) {
-      this.setState({...this.state, options: null, isLoading: true})
+  const fetchOptions = () => {
+    if (options === null) {
+      setOptions(null)
+      setIsLoading(true)
       setTimeout(() => {
-        this.setState({...this.state, options, isLoading: false})
-      }, this.props.delay || 0)
+        setOptions(availableOptions)
+        setIsLoading(false)
+      }, delay || 0)
     }
   }
 
-  searchOptions = () => {
-    this.setState({...this.state, options: null, isLoading: true})
+  const searchOptions = () => {
+    setOptions(null)
+    setIsLoading(true)
     setTimeout(() => {
-      this.setState({
-        ...this.state,
-        options: options.filter((e, idx) => idx % 2 === 0),
-        isLoading: false,
-        moreOptionsAvailable: true
-      })
-    }, this.props.delay || 0)
+      setOptions(availableOptions.filter((e, idx) => idx % 2 === 0))
+      setIsLoading(false)
+      setMoreOptionsAvailable(true)
+    }, delay || 0)
   }
 
-  setValue = value => {
-    this.setState({...this.state, value})
-  }
-
-  setValueMulti = valueMulti => {
-    this.setState({...this.state, valueMulti})
-  }
-
-  loadToolTip = key => {
-    if (!this.state.tooltips[key]) {
-      this.setState({...this.state, tooltips: {...this.state.tooltips, [key]: `TOOL TIP ${key}`}})
+  const loadToolTip = key => {
+    if (!tooltips[key]) {
+      setTooltips({...tooltips, [key]: `TOOL TIP ${key}`})
     }
   }
 
-  render() {
-    return (
-      <div style={{maxWidth: '400px'}}>
-        <Select
-          fetchOptions={this.fetchOptions}
-          isLoading={this.state.isLoading}
-          isMulti={false}
-          loadTooltip={this.loadToolTip}
-          moreOptionsAvailable={this.state.moreOptionsAvailable}
-          moreOptionsAvailableText="More options available"
-          noResultsText="No more options."
-          onChange={this.setValue}
-          openAdvancedSearch={action('open advanced search')}
-          options={this.state.options}
-          immutable={this.props.immutable}
-          searchOptions={this.searchOptions}
-          tooltips={this.state.tooltips}
-          value={this.state.value}
-        />
-        <Select
-          fetchOptions={this.fetchOptions}
-          isLoading={this.state.isLoading}
-          isMulti={true}
-          loadTooltip={this.loadToolTip}
-          moreOptionsAvailable={this.state.moreOptionsAvailable}
-          moreOptionsAvailableText="More options available"
-          noResultsText="No more options."
-          onChange={this.setValueMulti}
-          openAdvancedSearch={action('open advanced search')}
-          options={this.state.options}
-          immutable={this.props.immutable}
-          searchOptions={this.searchOptions}
-          tooltips={this.state.tooltips}
-          value={this.state.valueMulti}
-          DetailLink={({entityKey, children}) => (
-            <a href={`/${entityKey}`} target="_blank" rel="noopener noreferrer">
-              {children}
-            </a>
-          )}
-        />
-      </div>
-    )
+  return {
+    isLoading,
+    options,
+    value,
+    valueMulti,
+    moreOptionsAvailable,
+    tooltips,
+    fetchOptions,
+    searchOptions,
+    loadToolTip,
+    setValue,
+    setValueMulti
   }
 }
 
-SelectStory.propTypes = {
-  delay: PropTypes.number,
-  isMulti: PropTypes.bool,
-  immutable: PropTypes.bool
+// export const Overview = () => <SelectStory action={action} delay={2000} />
+export const Overview = props => {
+  const {
+    isLoading,
+    options,
+    value,
+    valueMulti,
+    moreOptionsAvailable,
+    tooltips,
+    fetchOptions,
+    searchOptions,
+    loadToolTip,
+    setValue,
+    setValueMulti
+  } = useSelectStoryHepers(props.delay || 2000)
+
+  const basicProps = {
+    fetchOptions: fetchOptions,
+    isLoading: isLoading,
+    isMulti: false,
+    loadTooltip: loadToolTip,
+    moreOptionsAvailable: moreOptionsAvailable,
+    moreOptionsAvailableText: 'More options available',
+    noResultsText: 'No more options.',
+    onChange: setValue,
+    options: options,
+    searchOptions: searchOptions,
+    tooltips: tooltips,
+    value: value
+  }
+
+  return (
+    <div style={{maxWidth: '400px'}}>
+      <h2>Single Select</h2>
+      <Select {...basicProps} />
+
+      <h2>Multi Select</h2>
+      <Select
+        {...basicProps}
+        isMulti={true}
+        onChange={setValueMulti}
+        value={valueMulti}
+        DetailLink={({entityKey, children}) => (
+          <a href={`/${entityKey}`} target="_blank" rel="noopener noreferrer">
+            {children}
+          </a>
+        )}
+      />
+
+      <h2>Multi Select immutable</h2>
+      <Select
+        {...basicProps}
+        isMulti={true}
+        onChange={setValueMulti}
+        value={valueMulti}
+        immutable
+        DetailLink={({entityKey, children}) => (
+          <a href={`/${entityKey}`} target="_blank" rel="noopener noreferrer">
+            {children}
+          </a>
+        )}
+      />
+
+      <h2>Multi Remote Select</h2>
+      <Select
+        {...basicProps}
+        isMulti={true}
+        onChange={setValueMulti}
+        value={valueMulti}
+        DetailLink={({entityKey, children}) => (
+          <a href={`/${entityKey}`} target="_blank" rel="noopener noreferrer">
+            {children}
+          </a>
+        )}
+        openAdvancedSearch={action('open advanced search')}
+      />
+
+      <h2>Multi Remote Select immutable</h2>
+      <Select
+        {...basicProps}
+        isMulti={true}
+        onChange={setValueMulti}
+        value={valueMulti}
+        immutable
+        DetailLink={({entityKey, children}) => (
+          <a href={`/${entityKey}`} target="_blank" rel="noopener noreferrer">
+            {children}
+          </a>
+        )}
+        openAdvancedSearch={action('open advanced search')}
+      />
+    </div>
+  )
 }
 
-export const Basic = () => <SelectStory action={action} delay={2000} />
+Overview.propTypes = {
+  delay: PropTypes.number
+}
+
+export const Basic = ({delay, hasAdvancedSearch, ...props}) => {
+  const {
+    isLoading,
+    options,
+    value,
+    moreOptionsAvailable,
+    tooltips,
+    fetchOptions,
+    searchOptions,
+    loadToolTip,
+    setValue
+  } = useSelectStoryHepers(props.delay || 2000)
+
+  return (
+    <div style={{maxWidth: '400px'}}>
+      <Select
+        fetchOptions={fetchOptions}
+        isLoading={isLoading}
+        isMulti={false}
+        loadTooltip={loadToolTip}
+        moreOptionsAvailable={moreOptionsAvailable}
+        moreOptionsAvailableText="More options available"
+        noResultsText="No more options."
+        onChange={setValue}
+        options={options}
+        searchOptions={searchOptions}
+        tooltips={tooltips}
+        value={value}
+        openAdvancedSearch={hasAdvancedSearch ? action('open advanced search') : undefined}
+        {...props}
+      />
+    </div>
+  )
+}
+
+Basic.propTypes = {
+  delay: PropTypes.number,
+  hasAdvancedSearch: PropTypes.bool
+}
