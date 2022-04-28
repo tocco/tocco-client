ad291b6b17fe9b682df4e70a616e7fc524728a70
Isabel Boos <iboos@tocco.ch>
2022-04-28 11:22:38 +0200

    chore: upgrade dev-dependencies
    Refs: TOCDEV-4976

--- build/jest.config.base.js
+++ build/jest.config.base.js
@@ -1,6 +1,6 @@
 module.exports = {
   setupFilesAfterEnv: ['<rootDir>/build/jest-setup.js'],
-  transformIgnorePatterns: ['node_modules/(?!redux-form|lodash-es|redux-saga)'],
+  transformIgnorePatterns: ['node_modules/(?!redux-form|lodash-es|redux-saga|uuid)'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
--- packages/widgets/sso-login/build/webpack.js
+++ packages/widgets/sso-login/build/webpack.js
@@ -1,10 +1,10 @@
-import CopyWebpackPlugin from 'copy-webpack-plugin'
+import CopyWebpackPlugin from 'copy-webpack-plugin' // eslint-disable-line import/default
 
 export const adjustConfig = (webpackConfig, config) => {
   const {__DEV__} = config.globals
 
   if (__DEV__) {
-    webpackConfig.plugins.push(new CopyWebpackPlugin(['./packages/sso-login/src/dev/test.html']))
+    webpackConfig.plugins.push(new CopyWebpackPlugin({patterns: ['./packages/widgets/sso-login/src/dev/test.html']}))
   }
 
   return webpackConfig
