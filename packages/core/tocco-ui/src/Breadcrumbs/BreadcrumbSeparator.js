600001353abd05d489958bd041fbfbb3db5a330d
Isabel Boos <iboos@tocco.ch>
2022-05-12 17:29:43 +0200

    fix(tocco-ui): render html escape characters in breadcrumbs
    Changelog: render html escape characters in breadcrumbs
    Refs: TOCDEV-5492
    Cherry-pick: Up

---
+++ packages/core/tocco-ui/src/Breadcrumbs/BreadcrumbSeparator.js
@@ -0,0 +1,10 @@
+import Icon from '../Icon'
+import {StyledBreadcrumbSeparator} from './StyledBreadcrumbs'
+
+const BreadcrumbSeparator = () => (
+  <StyledBreadcrumbSeparator breakWords>
+    <Icon icon="chevron-right" />
+  </StyledBreadcrumbSeparator>
+)
+
+export default BreadcrumbSeparator
