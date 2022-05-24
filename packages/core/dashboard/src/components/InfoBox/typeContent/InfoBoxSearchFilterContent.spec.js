c95470228c18e930d6dd0483a2dfce954c57e3a9
Luca Neukom <lneukom@tocco.ch>
2022-05-24 15:20:29 +0200

    refactor(dashboard): fit backend infobox orderBy and add test
    Refs: TOCDEV-5308
    Cherry-pick: Up

--- packages/core/dashboard/src/components/InfoBox/typeContent/InfoBoxSearchFilterContent.spec.js
+++ packages/core/dashboard/src/components/InfoBox/typeContent/InfoBoxSearchFilterContent.spec.js
@@ -13,7 +13,8 @@ describe('dashboard', () => {
               searchFilterUniqueId: 'birthdays',
               entityName: 'User',
               scope: 'infobox',
-              limit: 25
+              limit: 25,
+              orderBy: 'field'
             }
             const wrapper = shallow(<InfoBoxSearchFilterContent id="infobox" content={content} />)
 
@@ -22,8 +23,32 @@ describe('dashboard', () => {
             expect(wrapper.find(EntityListApp).prop('entityName')).to.deep.equal(content.entityName)
             expect(wrapper.find(EntityListApp).prop('formName')).to.deep.equal(content.entityName)
             expect(wrapper.find(EntityListApp).prop('limit')).to.deep.equal(content.limit)
+            expect(wrapper.find(EntityListApp).prop('sorting')).to.deep.equal([{field: 'field', order: 'asc'}])
 
             expect(wrapper.find(EntityListApp).prop('searchFormType')).to.deep.equal('fulltext')
+          })
+
+          test('should handle desc order', () => {
+            const content = {
+              orderBy: 'field desc'
+            }
+
+            const wrapper = shallow(<InfoBoxSearchFilterContent id="infobox" content={content} />)
+
+            expect(wrapper.find(EntityListApp).prop('sorting')).to.deep.equal([{field: 'field', order: 'desc'}])
+          })
+
+          test('should handle multiple sortings', () => {
+            const content = {
+              orderBy: 'field desc, other asc'
+            }
+
+            const wrapper = shallow(<InfoBoxSearchFilterContent id="infobox" content={content} />)
+
+            expect(wrapper.find(EntityListApp).prop('sorting')).to.deep.equal([
+              {field: 'field', order: 'desc'},
+              {field: 'other', order: 'asc'}
+            ])
           })
         })
       })
