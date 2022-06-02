4986f6735a1c621a5c9918095bfca122be84d3fc
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2022-06-02 16:47:12 +0200

    refactor(tocco-ui): enhance colunm picker
    Cherry-pick: Up
    Refs: TOCDEV-5534
    Changelog: enhance column picker ux by adding numbering, mass selection and sorting features

--- packages/core/tocco-ui/src/ColumnPicker/ColumnPicker.js
+++ packages/core/tocco-ui/src/ColumnPicker/ColumnPicker.js
@@ -5,10 +5,20 @@ import {dragAndDrop} from 'tocco-util'
 import Button from '../Button'
 import SearchBox from '../SearchBox'
 import Typography from '../Typography'
-import {StyledCheckbox, StyledUl, StyledButtonWrapper, StyledId, StyledItem} from './StyledColumnPicker'
+import {
+  StyledColumnPickerWrapper,
+  StyledControlsWrapper,
+  StyledCheckbox,
+  StyledUl,
+  StyledButtonWrapper,
+  StyledId,
+  StyledItem,
+  StyledNumber
+} from './StyledColumnPicker'
 
 const ColumnPicker = ({onOk, dndEnabled, initialColumns, intl, buttonLabel}) => {
   const [columns, setColumns] = useState([])
+  const someUnchecked = columns.some(column => column.hidden)
   useEffect(() => setColumns(initialColumns), [initialColumns])
   const [searchTerm, setSearchTerm] = useState(null)
   const changeColumnPosition = useCallback(
@@ -26,53 +36,76 @@ const ColumnPicker = ({onOk, dndEnabled, initialColumns, intl, buttonLabel}) => 
   )
   const {dndEvents, dndState} = dragAndDrop.useDnD(changeColumnPosition)
 
-  const items = useMemo(
-    () =>
-      columns
-        .filter(column => searchTerm === null || column.label.match(new RegExp(searchTerm, 'i')))
-        .map(column => (
-          <StyledItem
-            key={column.id}
-            draggable={dndEnabled}
-            isDraggedOver={dndState.currentlyDragOver === column.id && dndState.currentlyDragging !== column.id}
-            {...(dndEnabled && {...dndEvents(column.id)})}
-          >
-            <StyledCheckbox
-              type={'checkbox'}
-              id={column.id}
-              checked={!column.hidden}
-              onChange={value =>
-                setColumns(
-                  columns.map(c =>
-                    c.id === column.id
-                      ? {
-                          ...c,
-                          hidden: !value.target.checked
-                        }
-                      : c
-                  )
-                )
-              }
-            />
-            <Typography.Label for={column.id}>{column.label || <StyledId>{column.id}</StyledId>}</Typography.Label>
-          </StyledItem>
-        )),
-    [searchTerm, columns, dndState, dndEnabled, dndEvents]
-  )
+  const listItems = useMemo(() => {
+    const filteredColumns = columns.filter(
+      column => searchTerm === null || column.label.match(new RegExp(searchTerm, 'i'))
+    )
+
+    return filteredColumns.map((column, idx) => {
+      const handleChange = value => {
+        setColumns(
+          columns
+            .map(c =>
+              c.id === column.id
+                ? {
+                    ...c,
+                    hidden: !value.target.checked
+                  }
+                : c
+            )
+            .sort((a, b) => a.hidden - b.hidden)
+        )
+      }
+
+      return (
+        <StyledItem
+          key={column.id}
+          draggable={dndEnabled}
+          isDraggedOver={dndState.currentlyDragOver === column.id && dndState.currentlyDragging !== column.id}
+          {...(dndEnabled && {...dndEvents(column.id)})}
+        >
+          <StyledCheckbox type={'checkbox'} id={column.id} checked={!column.hidden} onChange={handleChange} />
+          <Typography.Label for={column.id}>
+            <StyledNumber>{idx + 1}.</StyledNumber>
+            {column.label || <StyledId>{column.id}</StyledId>}
+          </Typography.Label>
+        </StyledItem>
+      )
+    })
+  }, [searchTerm, columns, dndState, dndEnabled, dndEvents])
+
+  const toggleAllCheckBoxes = e => {
+    const isChecked = e.target.checked
+
+    const toggledColumns = [
+      ...columns.map(column => ({
+        ...column,
+        hidden: !isChecked
+      }))
+    ]
+
+    setColumns(toggledColumns)
+  }
 
   return (
-    <div>
+    <StyledColumnPickerWrapper>
       <SearchBox
         placeholder={intl.formatMessage({id: 'client.entity-list.preferences.columns.search'})}
         onSearch={setSearchTerm}
       />
-      <StyledUl>{items}</StyledUl>
+      <StyledControlsWrapper>
+        <StyledCheckbox type={'checkbox'} onChange={toggleAllCheckBoxes} checked={!someUnchecked} />
+        <Typography.Span>
+          {intl.formatMessage({id: 'client.entity-list.preferences.columns.selectDeselectAllCheckbox'})}
+        </Typography.Span>
+      </StyledControlsWrapper>
+      <StyledUl>{listItems}</StyledUl>
       <StyledButtonWrapper>
         <Button onClick={() => onOk(columns)} look={'raised'}>
           {buttonLabel || intl.formatMessage({id: 'client.entity-list.preferences.columns.okButton'})}
         </Button>
       </StyledButtonWrapper>
-    </div>
+    </StyledColumnPickerWrapper>
   )
 }
 
