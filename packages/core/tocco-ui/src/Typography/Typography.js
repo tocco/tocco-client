1f23d77060ad8d5ce366b142e89babd9b3efedd2
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2022-06-09 13:51:34 +0200

    refactor(tocco-ui): convert typography
    Refs: TOCPROD-41
    Changelog: convert typography to functional component

--- packages/core/tocco-ui/src/Typography/Typography.js
+++ packages/core/tocco-ui/src/Typography/Typography.js
@@ -1,10 +1,4 @@
-import React from 'react'
-
 // This is a dummy component only for showcase.
-export class Typography extends React.Component {
-  render() {
-    return <React.Fragment />
-  }
-}
+export const Typography = () => <></>
 
 export default Typography
