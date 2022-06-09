6b31f5e09fc65a592b3de1f099c1f2fc1dd83e9c
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2022-06-09 11:31:25 +0200

    refactor(tocco-ui): refactor phoneEdit
    Refs: TOCPROD-41
    Changelog: convert phoneEdit to functional component

--- packages/core/tocco-ui/src/EditableValue/editors/PhoneEdit.js
+++ packages/core/tocco-ui/src/EditableValue/editors/PhoneEdit.js
@@ -1,47 +1,42 @@
 import _get from 'lodash/get'
 import _isEmpty from 'lodash/isEmpty'
 import PropTypes from 'prop-types'
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 import Link from '../../Link'
 import {StyledEditableControl, StyledEditableWrapper} from '../StyledEditableValue'
 import StyledPhoneEdit from './StyledPhoneEdit'
 
-class PhoneEdit extends React.Component {
-  componentIsUnmounted = false
-  FALLBACK_DEFAULT_COUNTRY = 'CH'
+const PhoneEdit = props => {
+  const {onChange, value: valueProp, immutable, id} = props
+  const FALLBACK_DEFAULT_COUNTRY = 'CH'
+  const defaultCountry = _get(props, 'options.defaultCountry') || FALLBACK_DEFAULT_COUNTRY
+  const [phoneImportState, setPhoneImportState] = useState({libPhoneImport: null, defaultCountry})
+  const inputElement = React.createRef()
+  const phoneImportDefaultCountry = phoneImportState.defaultCountry
 
-  constructor(props) {
-    super(props)
-    const defaultCountry = _get(props, 'options.defaultCountry') || this.FALLBACK_DEFAULT_COUNTRY
-    this.state = {libPhoneImport: null, defaultCountry}
-    this.importLibPhoneNumber()
-    this.inputElement = React.createRef()
-  }
+  useEffect(() => {
+    ;(async () => {
+      const libPhoneImport = await import(/* webpackChunkName: "vendor-libphonenumber-js" */ 'libphonenumber-js')
+      setPhoneImportState({...phoneImportState, libPhoneImport})
+    })()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
-  async importLibPhoneNumber() {
-    const libPhoneImport = await import(/* webpackChunkName: "vendor-libphonenumber-js" */ 'libphonenumber-js')
-    if (!this.componentIsUnmounted) {
-      this.setState({...this.state, libPhoneImport})
-    }
-  }
-
-  componentWillUnmount() {
-    this.componentIsUnmounted = true
-  }
-
-  amountOfSpacesBeforeCaret = (str, caretPosition) => {
+  const amountOfSpacesBeforeCaret = (str, caretPosition) => {
     const spaces = str.substring(0, caretPosition - 1).match(/\s/g)
-    return spaces ? spaces.length : 0
+    return spaces?.length || 0
   }
 
-  repositionCaret = (value, previousValue, caretPosition) => {
+  const repositionCaret = (value, previousValue, caretPosition) => {
     if (caretPosition && value.length !== caretPosition) {
-      const previousFormatted = new this.state.libPhoneImport.AsYouType(this.state.defaultCountry).input(previousValue)
-      const currentFormatted = new this.state.libPhoneImport.AsYouType(this.state.defaultCountry).input(value)
+      const previousFormatted = new phoneImportState.libPhoneImport.AsYouType(phoneImportDefaultCountry).input(
+        previousValue
+      )
+      const currentFormatted = new phoneImportState.libPhoneImport.AsYouType(phoneImportDefaultCountry).input(value)
 
-      const spacesPrevious = this.amountOfSpacesBeforeCaret(previousFormatted, caretPosition)
-      const spacesCurrent = this.amountOfSpacesBeforeCaret(currentFormatted, caretPosition)
+      const spacesPrevious = amountOfSpacesBeforeCaret(previousFormatted, caretPosition)
+      const spacesCurrent = amountOfSpacesBeforeCaret(currentFormatted, caretPosition)
 
       const offset = spacesCurrent - spacesPrevious
 
@@ -49,58 +44,56 @@ class PhoneEdit extends React.Component {
         function () {
           const start = caretPosition + offset
           this.setSelectionRange(start, start)
-        }.bind(this.inputElement.current)
+        }.bind(inputElement.current)
       )
     }
   }
 
-  removeSpaces = str => str.replace(/\s/g, '')
+  const removeSpaces = str => str.replace(/\s/g, '')
 
-  handleChange = e => {
+  const handleChange = e => {
     const newValue = e.target.value
-    const {libPhoneImport} = this.state
-    const parsedNumber = libPhoneImport ? libPhoneImport.parseNumber(newValue, this.state.defaultCountry) : newValue
+    const {libPhoneImport} = phoneImportState
+    const parsedNumber = libPhoneImport ? libPhoneImport.parseNumber(newValue, phoneImportDefaultCountry) : newValue
 
     const valueNormalized = _isEmpty(parsedNumber)
-      ? this.removeSpaces(new this.state.libPhoneImport.AsYouType(this.state.defaultCountry).input(newValue))
+      ? removeSpaces(new phoneImportState.libPhoneImport.AsYouType(phoneImportDefaultCountry).input(newValue))
       : libPhoneImport.formatNumber(parsedNumber, 'E.164')
 
-    this.props.onChange(valueNormalized)
+    onChange(valueNormalized)
 
     const currentCaretPosition = e.target.selectionStart
-    this.repositionCaret(newValue, this.props.value, currentCaretPosition)
+    repositionCaret(newValue, valueProp, currentCaretPosition)
   }
 
-  determineDisplayValue = () => {
-    if (this.props.value && this.state.libPhoneImport && this.state.libPhoneImport.AsYouType) {
-      return new this.state.libPhoneImport.AsYouType(this.state.defaultCountry).input(this.props.value)
+  const determineDisplayValue = () => {
+    if (valueProp && phoneImportState.libPhoneImport?.AsYouType) {
+      return new phoneImportState.libPhoneImport.AsYouType(phoneImportDefaultCountry).input(valueProp)
     }
 
-    return this.props.value || ''
+    return valueProp || ''
   }
 
-  render() {
-    const displayValue = this.determineDisplayValue()
+  const displayValue = determineDisplayValue()
 
-    return (
-      <StyledEditableWrapper immutable={this.props.immutable}>
-        <StyledPhoneEdit
-          disabled={this.props.immutable || !this.state.libPhoneImport}
-          id={this.props.id}
-          name={name}
-          onChange={this.handleChange}
-          immutable={this.props.immutable || !this.state.libPhoneImport}
-          ref={this.inputElement}
-          value={displayValue}
-        />
-        {displayValue && (
-          <StyledEditableControl>
-            <Link href={`tel:${this.props.value}`} icon="phone" tabIndex={-1} neutral />
-          </StyledEditableControl>
-        )}
-      </StyledEditableWrapper>
-    )
-  }
+  return (
+    <StyledEditableWrapper immutable={immutable}>
+      <StyledPhoneEdit
+        disabled={immutable || !phoneImportState.libPhoneImport}
+        id={id}
+        name={name}
+        onChange={handleChange}
+        immutable={immutable || !phoneImportState.libPhoneImport}
+        ref={inputElement}
+        value={displayValue}
+      />
+      {displayValue && (
+        <StyledEditableControl>
+          <Link href={`tel:${valueProp}`} icon="phone" tabIndex={-1} neutral />
+        </StyledEditableControl>
+      )}
+    </StyledEditableWrapper>
+  )
 }
 
 PhoneEdit.propTypes = {
