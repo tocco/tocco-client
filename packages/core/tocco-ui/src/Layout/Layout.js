61f72d48669a52ad8896c4491cd5a818f7246090
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2022-06-09 13:39:47 +0200

    refactor(tocco-ui): convert layout component
    Refs: TOCPROD-41
    Changelog: convert layout to functional component

--- packages/core/tocco-ui/src/Layout/Layout.js
+++ packages/core/tocco-ui/src/Layout/Layout.js
@@ -1,15 +1,9 @@
-import React from 'react'
-
 /**
  * Use <Layout.Container/> to apply grid and gutter on <Layout.Box>.
  * Define how many cells are displayed per row and breakpoint.
  */
 
 // This is a dummy component only for showcase.
-export class Layout extends React.Component {
-  render() {
-    return <React.Fragment />
-  }
-}
+export const Layout = () => <></>
 
 export default Layout
