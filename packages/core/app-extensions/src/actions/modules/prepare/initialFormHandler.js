2a0324a3c70ecf7f7fd9c0a7ac4842e76d854fb5
Stefan Würsten <swuersten@tocco.ch>
2022-06-14 17:01:25 +0200

    feat(app-extensions): simple actions add conditions
    the prepare request of a simple action can now return conditions which are applied to a specific form field
    
    Refs: TOCDEV-5584
    Changelog: simple actions add conditions

--- packages/core/app-extensions/src/actions/modules/prepare/initialFormHandler.js
+++ packages/core/app-extensions/src/actions/modules/prepare/initialFormHandler.js
@@ -1,5 +1,6 @@
 import {channel} from 'redux-saga'
 import {call, put, take} from 'redux-saga/effects'
+import {api} from 'tocco-util'
 
 import notification from '../../../notification'
 import simpleFormConnector from '../../containers/simpleFormConnector'
@@ -20,17 +21,18 @@ export default function* initialFormHandler(preparationResponse, params, definit
 
 export const formValues = formValues => ({formValues})
 
-export function* handleInitialForm({formDefinition, defaultValues, formTitle, formMessage}, config) {
+export function* handleInitialForm({formDefinition, defaultValues, conditions, formTitle, formMessage}, config) {
   const answerChannel = yield call(channel)
 
   const id = new Date().valueOf()
   const onSend = ({values}) => answerChannel.put(formValues(values))
   const onCancel = () => answerChannel.put(formValues(null))
   const SimpleFormContainer = simpleFormConnector(config.formApp)
+  const modifiedForm = addConditionsToFormDefinition(formDefinition.form, conditions)
   yield put(
     notification.modal(id, formTitle, formMessage, () => (
       <SimpleFormContainer
-        form={formDefinition.form}
+        form={modifiedForm}
         listApp={config.listApp}
         onSubmit={onSend}
         onCancel={onCancel}
@@ -43,4 +45,20 @@ export function* handleInitialForm({formDefinition, defaultValues, formTitle, fo
   const response = yield take(answerChannel)
   yield put(notification.removeModal(id))
   return response.formValues ? {model: formDefinition.model, paths: response.formValues} : null
+}
+
+export const addConditionsToFormDefinition = (formDefinition, conditions) => {
+  if (!conditions || Object.keys(conditions).length === 0) {
+    return formDefinition
+  }
+
+  return {
+    ...formDefinition,
+    ...(api.relationFieldTypes.includes(formDefinition.dataType) && conditions[formDefinition.id]
+      ? {condition: conditions[formDefinition.id]}
+      : {}),
+    ...(formDefinition.children
+      ? {children: formDefinition.children.map(c => addConditionsToFormDefinition(c, conditions))}
+      : {})
+  }
 }
--- packages/core/app-extensions/src/actions/modules/prepare/initialFormHandler.spec.js
+++ packages/core/app-extensions/src/actions/modules/prepare/initialFormHandler.spec.js
@@ -2,7 +2,7 @@ import {channel} from 'redux-saga'
 import {expectSaga} from 'redux-saga-test-plan'
 
 import {MODAL} from '../../../notification/modules/modal/actions'
-import initialFormHandler from './initialFormHandler'
+import initialFormHandler, {addConditionsToFormDefinition} from './initialFormHandler'
 
 describe('app-extensions', () => {
   describe('actions', () => {
@@ -11,7 +11,10 @@ describe('app-extensions', () => {
         test('should dispatch confirm and return abort eql false in case of positive answer', () => {
           const response = {
             initialFormValues: {
-              formDefinition: {model: 'User'},
+              formDefinition: {
+                model: 'User',
+                form: {}
+              },
               formData: {},
               formTitle: 'title',
               formMessage: 'message'
@@ -40,7 +43,7 @@ describe('app-extensions', () => {
         test('should return abort in case of negative answer', () => {
           const response = {
             initialFormValues: {
-              formDefinition: {},
+              formDefinition: {form: {}},
               formData: {},
               formTitle: 'title',
               formMessage: 'message'
@@ -74,6 +77,58 @@ describe('app-extensions', () => {
               abort: false
             })
             .run()
+        })
+      })
+
+      describe('addConditionsToFormDefinition', () => {
+        const formDefinition = {
+          componentType: 'form',
+          children: [
+            {
+              componentType: 'layout',
+              children: [
+                {
+                  id: 'relEvent_status',
+                  componentType: 'field-set',
+                  children: [
+                    {
+                      id: 'relEvent_status',
+                      dataType: 'single-remote-field'
+                    }
+                  ]
+                },
+                {
+                  id: 'description',
+                  componentType: 'field-set',
+                  children: [
+                    {
+                      id: 'description',
+                      dataType: 'string'
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        }
+
+        test('if no conditions are set do not modify form', () => {
+          const conditions = {}
+          const modifiedForm = addConditionsToFormDefinition(formDefinition, conditions)
+          expect(modifiedForm).to.equal(formDefinition)
+        })
+
+        test('if no condition matches a field do not modify form', () => {
+          const conditions = {relEvent_type: 'system_entity'}
+          const modifiedForm = addConditionsToFormDefinition(formDefinition, conditions)
+          expect(modifiedForm).to.deep.equal(formDefinition)
+        })
+
+        test('if condition matches a field do modify form', () => {
+          const conditions = {relEvent_status: 'evaluation_allowed'}
+          const modifiedForm = addConditionsToFormDefinition(formDefinition, conditions)
+          expect(modifiedForm).to.not.deep.equal(formDefinition)
+          expect(modifiedForm.children[0].children[0].children[0].condition).to.equal('evaluation_allowed')
         })
       })
     })
