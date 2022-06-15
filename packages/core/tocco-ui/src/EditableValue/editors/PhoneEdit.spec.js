18c6e4e60f4800daafdca77ba825251a75d731d5
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2022-06-15 14:46:25 +0200

    chore: fix phoneEdit tests
    Refs: TOCPROD-41

--- packages/core/tocco-ui/src/EditableValue/editors/PhoneEdit.spec.js
+++ packages/core/tocco-ui/src/EditableValue/editors/PhoneEdit.spec.js
@@ -6,17 +6,16 @@ describe('tocco-ui', () => {
   describe('EditableValue', () => {
     describe('editors', () => {
       describe('PhoneEdit ', () => {
-        test('should call on change with phone number in e.164 format', () => {
+        test('should call on change with phone number in e.164 format', done => {
           const onChangeSpy = sinon.spy()
-          const wrapper = mount(<PhoneEdit value="0792345678" onChange={onChangeSpy} />)
-          return wrapper
-            .find(PhoneEdit)
-            .instance()
-            .importLibPhoneNumber()
-            .then(() => {
-              wrapper.find('input').simulate('change', {target: {value: '0793456789'}})
-              return expect(onChangeSpy).to.have.been.calledWith('+41793456789')
-            })
+
+          const executeChange = () => {
+            wrapper.find('input').simulate('change', {target: {value: '0793456789'}})
+            expect(onChangeSpy).to.have.been.calledWith('+41793456789')
+            done()
+          }
+
+          const wrapper = mount(<PhoneEdit value="0792345678" onChange={onChangeSpy} onLibLoaded={executeChange} />)
         })
       })
     })
