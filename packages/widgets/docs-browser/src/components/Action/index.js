ba19c98b3a5445f555f7f0881f99683baa2bfa5c
Isabel Boos <iboos@tocco.ch>
2022-06-22 09:08:40 +0200

    feat(docs-browser): prepare for customer actions
    Refs: TOCDEV-5465
    Changelog: prepare for customer actions

--- packages/widgets/docs-browser/src/components/Action/index.js
+++ packages/widgets/docs-browser/src/components/Action/index.js
@@ -1,1 +1,14 @@
-export {default} from './LazyAction'
+import {lazy} from 'react'
+import {actions} from 'tocco-app-extensions'
+
+const actionMap = {
+  'dms-create-folder': lazy(() => import(/* webpackChunkName: "docs-actions" */ './actions/CreateFolder')),
+  'dms-create-domain': lazy(() => import(/* webpackChunkName: "docs-actions" */ './actions/CreateDomain')),
+  'dms-edit': lazy(() => import(/* webpackChunkName: "docs-actions" */ './actions/Edit')),
+  'dms-move': lazy(() => import(/* webpackChunkName: "docs-actions" */ './actions/MoveContainer')),
+  delete: lazy(() => import(/* webpackChunkName: "docs-actions" */ './actions/Delete'))
+}
+
+const LazyAction = actions.actionFactory(actionMap)
+
+export default LazyAction
