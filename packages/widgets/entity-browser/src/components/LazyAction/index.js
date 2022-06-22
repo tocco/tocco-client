c7bfd5b1991034e94d7bf21bc44423cb0cacc9a1
Isabel Boos <iboos@tocco.ch>
2022-06-22 09:09:03 +0200

    feat(entity-browser): prepare for customer actions
    Refs: TOCDEV-5465
    Changelog: prepare for customer actions

--- packages/widgets/entity-browser/src/components/LazyAction/index.js
+++ packages/widgets/entity-browser/src/components/LazyAction/index.js
@@ -1,1 +1,12 @@
-export {default} from './LazyAction'
+import {lazy} from 'react'
+import {actions} from 'tocco-app-extensions'
+
+const actionMap = {
+  delete: lazy(() => import(/* webpackChunkName: "actions" */ './actions/Delete')),
+  'input-edit': lazy(() => import(/* webpackChunkName: "actions" */ './actions/InputEdit')),
+  'subscribe-calendar': lazy(() => import(/* webpackChunkName: "actions" */ './actions/SubscribeCalendar'))
+}
+
+const LazyAction = actions.actionFactory(actionMap)
+
+export default LazyAction
