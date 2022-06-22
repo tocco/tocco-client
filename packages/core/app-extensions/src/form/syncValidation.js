0468e6800b5863b72bb7e86a3286293d409df9f7
PadPalon <luca.neukom@gmail.com>
2022-06-22 09:25:32 +0200

    fix(app-extensions): do not crash syncValidation when no values exist

--- packages/core/app-extensions/src/form/syncValidation.js
+++ packages/core/app-extensions/src/form/syncValidation.js
@@ -1,12 +1,13 @@
 import {syncTypeValidateField, syncValidateField} from './validators/syncValidation'
 
-export default (fieldDefinitions, formDefinition) => values =>
-  fieldDefinitions.reduce((acc, fieldDefinition) => {
-    const value = values[fieldDefinition.path]
+export default (fieldDefinitions, formDefinition) =>
+  (values = {}) =>
+    fieldDefinitions.reduce((acc, fieldDefinition) => {
+      const value = values[fieldDefinition.path]
 
-    return {
-      ...acc,
-      ...syncValidateField(fieldDefinition, value),
-      ...syncTypeValidateField(values, formDefinition, value, fieldDefinition)
-    }
-  }, {})
+      return {
+        ...acc,
+        ...syncValidateField(fieldDefinition, value),
+        ...syncTypeValidateField(values, formDefinition, value, fieldDefinition)
+      }
+    }, {})
