61b90e8960ce2f525206f7070e9f4dbc81361700
PadPalon <luca.neukom@gmail.com>
2022-06-22 13:51:14 +0200

    fix(mailing-list): correct loading of publish_detail
    awaiting a generator actually doesn't do anything, but it also doesn't crash

--- packages/widgets/mailing-list/src/components/MailingList/MailingList.js
+++ packages/widgets/mailing-list/src/components/MailingList/MailingList.js
@@ -7,54 +7,48 @@ import {searchFormTypePropTypes} from 'tocco-entity-list/src/main'
 import {LoadMask} from 'tocco-ui'
 import {appContext as appContextPropType} from 'tocco-util'
 
-const MailingList = ({
-  searchFilters,
-  searchFormType,
-  limit,
-  backendUrl,
-  businessUnit,
-  appContext,
-  loadReports,
-  reportIds,
-  reports,
-  intl,
-  showEmailAction
-}) => {
+function* modifyFormDefinition(
+  formDefinition,
+  {parent, entityName, entityId},
+  {reports, intl, showEmailAction, appContext}
+) {
+  if (formDefinition.id === 'Mailing_list_detail_relUser_list') {
+    let userListFormDefinition = formDefinition
+    if (reports?.length > 0) {
+      userListFormDefinition = form.addReports(userListFormDefinition, reports, intl)
+    }
+    if (!showEmailAction) {
+      userListFormDefinition = form.removeActions(userListFormDefinition, ['mailingListMailAction'])
+    } else {
+      userListFormDefinition = form.adjustAction(userListFormDefinition, 'mailingListMailAction', action => ({
+        ...action,
+        properties: {
+          ...action.properties,
+          widgetKey: appContext.widgetConfigKey,
+          eventKey: parent.key
+        }
+      }))
+    }
+    return userListFormDefinition
+  }
+
+  if (entityId && entityName === 'User') {
+    const user = yield rest.fetchEntity(entityName, entityId, {paths: ['publish_detail']})
+    const publishDetail = _get(user, ['paths', 'publish_detail', 'value'])
+    if (!publishDetail) {
+      return form.removeBoxes(formDefinition, ['address_information', 'communication_information'])
+    }
+  }
+
+  return formDefinition
+}
+
+const MailingList = props => {
+  const {searchFilters, searchFormType, limit, backendUrl, businessUnit, appContext, loadReports, reportIds, reports} =
+    props
   useEffect(() => {
     loadReports(reportIds)
   }, [loadReports, reportIds])
-
-  const modifyFormDefinition = async (formDefinition, {parent, entityName, entityId}) => {
-    if (formDefinition.id === 'Mailing_list_detail_relUser_list') {
-      let userListFormDefinition = formDefinition
-      if (reports?.length > 0) {
-        userListFormDefinition = form.addReports(userListFormDefinition, reports, intl)
-      }
-      if (!showEmailAction) {
-        userListFormDefinition = form.removeActions(userListFormDefinition, ['mailingListMailAction'])
-      } else {
-        userListFormDefinition = form.adjustAction(userListFormDefinition, 'mailingListMailAction', action => ({
-          ...action,
-          properties: {
-            ...action.properties,
-            widgetKey: appContext.widgetConfigKey,
-            eventKey: parent.key
-          }
-        }))
-      }
-      return userListFormDefinition
-    }
-
-    if (entityId && entityName === 'User') {
-      const user = await rest.fetchEntity(entityName, entityId, {paths: ['publish_detail']})
-      const publishDetail = _get(user, ['paths', 'publish_detail', 'value'])
-      if (!publishDetail) {
-        return form.removeBoxes(formDefinition, ['address_information', 'communication_information'])
-      }
-    }
-
-    return formDefinition
-  }
 
   return (
     <LoadMask required={[reports]}>
@@ -63,7 +57,7 @@ const MailingList = ({
         formBase="Mailing_list"
         searchFilters={searchFilters}
         limit={limit}
-        modifyFormDefinition={modifyFormDefinition}
+        modifyFormDefinition={(formDefinition, context) => modifyFormDefinition(formDefinition, context, props)}
         backendUrl={backendUrl}
         businessUnit={businessUnit}
         appContext={appContext}
