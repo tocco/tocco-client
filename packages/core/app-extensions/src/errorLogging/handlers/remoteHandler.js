b53d3669458e2e3e90659e47653e442d5aa74585
Isabel Boos <iboos@tocco.ch>
2022-06-28 09:58:22 +0200

    fix(app-extensions): fix remote error handler
    Changelog: fix remote error handler

--- packages/core/app-extensions/src/errorLogging/handlers/remoteHandler.js
+++ packages/core/app-extensions/src/errorLogging/handlers/remoteHandler.js
@@ -2,5 +2,5 @@ import {call} from 'redux-saga/effects'
 import {remoteLogger} from 'tocco-util'
 
 export default function* remoteLoggerSaga(title, description, error) {
-  yield call(remoteLogger.logException(error))
+  yield call(remoteLogger.logException, error)
 }
