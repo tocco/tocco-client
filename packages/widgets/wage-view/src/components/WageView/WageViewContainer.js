0664ed4c6b2542e356987b87f1c5c716fcdf60b9
PadPalon <luca.neukom@gmail.com>
2022-07-01 09:31:19 +0200

    refactor(wage-view): remove reportIds handling
    - entity-browser now supports these natively
    
    Refs: TOCDEV-5622

--- packages/widgets/wage-view/src/components/WageView/WageViewContainer.js
+++ packages/widgets/wage-view/src/components/WageView/WageViewContainer.js
@@ -1,15 +1,11 @@
 import {injectIntl} from 'react-intl'
 import {connect} from 'react-redux'
-import {reports} from 'tocco-app-extensions'
 
 import WageView from './WageView'
 
-const mapActionCreators = {
-  loadReports: reports.loadReports
-}
+const mapActionCreators = {}
 
 const mapStateToProps = state => ({
-  reports: state.reports.reports,
   reportIds: state.input.reportIds,
   allowCreate: state.input.allowCreate,
   searchFilters: state.input.searchFilters,
