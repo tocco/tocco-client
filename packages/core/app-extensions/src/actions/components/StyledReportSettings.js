2aeafee3484dc6f81af831b7a130383574cf85e2
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2022-07-01 13:43:41 +0200

    refactor(app-extenions): fix modal footer
    Cherry-pick: Up
    
    Refs: TOCDEV-5684
    Changelog: fix modal footer when opening multiselect fields

--- packages/core/app-extensions/src/actions/components/StyledReportSettings.js
+++ packages/core/app-extensions/src/actions/components/StyledReportSettings.js
@@ -2,6 +2,8 @@ import styled from 'styled-components'
 import {scale, theme, StyledButton} from 'tocco-ui'
 
 export const StyledReportSettings = styled.div`
+  margin-bottom: ${scale.space(1)};
+
   && {
     > div:first-of-type {
       margin-bottom: 10px;
@@ -10,9 +12,10 @@ export const StyledReportSettings = styled.div`
 `
 
 export const StyledStickyButtons = styled.div`
-  position: sticky;
+  position: absolute;
   bottom: 0;
-  padding-top: ${scale.space(0)};
+  width: calc(100% - 2 * ${scale.space(1)});
+  padding: ${scale.space(0)};
   background-color: ${theme.color('paper')};
   z-index: 1;
   display: flex;
