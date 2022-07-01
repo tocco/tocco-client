e77ed8ef1c092f37ba7849914064a52fb49ef04e
PadPalon <luca.neukom@gmail.com>
2022-07-01 09:31:19 +0200

    refactor(wage-view): remove reportIds handling
    - entity-browser now supports these natively
    
    Refs: TOCDEV-5622

--- packages/widgets/wage-view/src/components/WageView/WageView.js
+++ packages/widgets/wage-view/src/components/WageView/WageView.js
@@ -1,57 +1,33 @@
 import PropTypes from 'prop-types'
-import {useEffect} from 'react'
 import {form} from 'tocco-app-extensions'
 import EntityBrowserApp from 'tocco-entity-browser/src/main'
-import {LoadMask} from 'tocco-ui'
 import {appContext as appContextPropType} from 'tocco-util'
 
-const WageView = ({
-  loadReports,
-  reports,
-  allowCreate,
-  reportIds,
-  searchFilters,
-  limit,
-  intl,
-  backendUrl,
-  businessUnit,
-  appContext
-}) => {
-  useEffect(() => {
-    loadReports(reportIds)
-  }, [loadReports, reportIds])
-
+const WageView = ({allowCreate, reportIds, searchFilters, limit, backendUrl, businessUnit, appContext}) => {
   const modifyFormDefinition = formDefinition => {
     if (!allowCreate) {
       formDefinition = form.removeCreate(formDefinition)
-    }
-
-    if (reports.length > 0) {
-      formDefinition = form.addReports(formDefinition, reports, intl)
     }
 
     return formDefinition
   }
 
   return (
-    <LoadMask required={[reports]}>
-      <EntityBrowserApp
-        entityName="Wage"
-        formBase="Wage_view"
-        searchFilters={searchFilters}
-        limit={limit}
-        modifyFormDefinition={modifyFormDefinition}
-        backendUrl={backendUrl}
-        businessUnit={businessUnit}
-        appContext={appContext}
-      />
-    </LoadMask>
+    <EntityBrowserApp
+      entityName="Wage"
+      formBase="Wage_view"
+      searchFilters={searchFilters}
+      limit={limit}
+      modifyFormDefinition={modifyFormDefinition}
+      backendUrl={backendUrl}
+      businessUnit={businessUnit}
+      appContext={appContext}
+      reportIds={reportIds}
+    />
   )
 }
 
 WageView.propTypes = {
-  loadReports: PropTypes.func.isRequired,
-  reports: PropTypes.arrayOf(PropTypes.object),
   allowCreate: PropTypes.bool,
   reportIds: PropTypes.arrayOf(PropTypes.string).isRequired,
   searchFilters: PropTypes.arrayOf(PropTypes.string),
