72d8f63d7a297ef02b6bb034e8a6b0c6d91ce9d1
PadPalon <luca.neukom@gmail.com>
2022-07-04 15:52:43 +0200

    feat(app-extensions): load icons for reports
    Refs: TOCDEV-5583
    Changelog: report definitions now contain an icon as defined by the backend

--- packages/core/app-extensions/src/reports/reducer.js
+++ packages/core/app-extensions/src/reports/reducer.js
@@ -3,11 +3,13 @@ import {reducer as reducerUtil} from 'tocco-util'
 import * as actions from './actions'
 
 const ACTION_HANDLERS = {
-  [actions.SET_REPORTS]: reducerUtil.singleTransferReducer('reports')
+  [actions.SET_REPORTS]: reducerUtil.singleTransferReducer('reports'),
+  [actions.SET_REPORT_ICONS]: reducerUtil.singleTransferReducer('reportIcons')
 }
 
 const initialState = {
-  reports: null
+  reports: null,
+  reportIcons: null
 }
 
 export default function reducer(state = initialState, action) {
--- packages/core/app-extensions/src/reports/sagas.js
+++ packages/core/app-extensions/src/reports/sagas.js
@@ -1,9 +1,11 @@
-import {all, call, put, takeLatest} from 'redux-saga/effects'
+import {all, call, put, select, takeLatest} from 'redux-saga/effects'
+import {api} from 'tocco-util'
 
 import rest from '../rest'
 import * as actions from './actions'
 
 export const inputSelector = state => state.input
+export const reportsSelector = state => state.reports
 
 // same threshold as in ReportActionFormInterceptor
 export const DEFAULT_CONFIRMATION_THRESHOLD = 100
@@ -22,22 +24,46 @@ export function* loadReport({payload: {reportIds, entityName, scope}}) {
 
   const query = {
     where: reportConditions,
-    paths: 'unique_id,label,ignore_selection'
+    paths: 'unique_id,label,ignore_selection,relOutput_template.relReport_file_format.unique_id'
   }
 
   const result = yield call(rest.fetchEntities, 'Report', query, {method: 'GET'})
 
-  const reports = result.map(r => ({
-    actionType: 'report',
-    componentType: 'report',
-    confirmationThreshold: r.paths.ignore_selection.value ? null : DEFAULT_CONFIRMATION_THRESHOLD,
-    id: r.paths.unique_id.value,
-    label: r.paths.label.value,
-    reportId: r.paths.unique_id.value,
-    showConfirmation: !r.paths.ignore_selection.value
-  }))
+  const reports = yield all(
+    result.map(entity => api.getFlattenEntity(entity)).map(report => call(buildReportDefinition, report))
+  )
 
   yield put(actions.setReports(reports))
+}
+
+function* buildReportDefinition(report) {
+  return {
+    actionType: 'report',
+    componentType: 'report',
+    confirmationThreshold: report.ignore_selection ? null : DEFAULT_CONFIRMATION_THRESHOLD,
+    id: report.unique_id,
+    label: report.label,
+    reportId: report.unique_id,
+    showConfirmation: !report.ignore_selection,
+    icon: yield call(getReportIcon, report)
+  }
+}
+
+function* getReportIcon(report) {
+  const reportIcons = yield call(loadReportIcons)
+  const fileType = report['relOutput_template.relReport_file_format.unique_id']
+  return reportIcons[fileType]
+}
+
+function* loadReportIcons() {
+  const {reportIcons} = yield select(reportsSelector)
+  if (!reportIcons) {
+    const {body} = yield call(rest.requestSaga, '/client/report-icons', {method: 'GET'})
+    yield put(actions.setReportIcons(body))
+    return body
+  } else {
+    return reportIcons
+  }
 }
 
 const getReportConditions = (reportIds, entityName, scope) => {
