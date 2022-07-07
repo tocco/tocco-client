f188cfcd50da2578058597e7e182dbb368c6dbed
PadPalon <luca.neukom@gmail.com>
2022-07-07 14:08:25 +0200

    feat(export): handle additional paths from templates
    - paths in templates now get converted to columns and added to the column picker
    - split column lists in two with one containing all columns that are always visible and the other containing the columns of the current template
    - use new initialized flag from templateValues and LoadMasks to only display action after defaults have been handled
    
    Refs: TOCDEV-5686
    Cherry-pick: Up
    Changelog: export action now handles additional paths from templates again

--- packages/actions/export/src/modules/export/reducer.js
+++ packages/actions/export/src/modules/export/reducer.js
@@ -4,11 +4,15 @@ import * as actions from './actions'
 
 const ACTION_HANDLERS = {
   [actions.SET_AVAILABLE_COLUMNS]: reducerUtil.singleTransferReducer('availableColumns'),
+  [actions.SET_DEFAULT_COLUMNS]: reducerUtil.singleTransferReducer('defaultColumns'),
+  [actions.SET_TEMPLATE_COLUMNS]: reducerUtil.singleTransferReducer('templateColumns'),
   [actions.SET_DEFAULT_VALUES]: reducerUtil.singleTransferReducer('defaultValues')
 }
 
 const initialState = {
   availableColumns: null,
+  defaultColumns: null,
+  templateColumns: null,
   defaultValues: null
 }
 
