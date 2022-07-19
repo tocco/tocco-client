42b6d342daaaf76ab5db99d2c0ba34db8d2757bc
Stefan Würsten <swuersten@tocco.ch>
2022-07-19 12:59:02 +0200

    fix(tocco-ui): fix breakwords for email and phone formatter
    Changelog: fix breakwords for email and phone formatter
    Cherry-pick: Up
    Refs: TOCDEV-5765

--- packages/core/tocco-ui/src/FormattedValue/typeFormatters/EmailFormatter.js
+++ packages/core/tocco-ui/src/FormattedValue/typeFormatters/EmailFormatter.js
@@ -1,14 +1,15 @@
 import PropTypes from 'prop-types'
 
 import Link from '../../Link'
-import Typography from '../../Typography'
 
 const EmailFormatter = props => (
-  <Typography.Span breakWords={props.breakWords}>
-    <Link href={`mailto:${props.value.toString()}`} target="_blank" onClick={e => e.stopPropagation()}>
-      {props.value.toString()}
-    </Link>
-  </Typography.Span>
+  <Link
+    href={`mailto:${props.value.toString()}`}
+    target="_blank"
+    onClick={e => e.stopPropagation()}
+    label={props.value.toString()}
+    breakWords={props.breakWords}
+  />
 )
 
 EmailFormatter.propTypes = {
