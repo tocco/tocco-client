df9f6fa518cad84ee3c4548ffe9c5c7068eedc12
Stefan Würsten <swuersten@tocco.ch>
2022-07-19 14:22:36 +0200

    feat(admin): add build time to about tocco
    Changelog: add build time to about tocco
    Refs: TOCDEV-5698

--- packages/apps/admin/src/components/AboutTocco/AboutToccoContainer.js
+++ packages/apps/admin/src/components/AboutTocco/AboutToccoContainer.js
@@ -5,7 +5,8 @@ import AboutTocco from './AboutTocco'
 
 const mapStateToProps = state => ({
   niceVersion: state.preferences.serverSettings.niceVersion,
-  niceRevision: state.preferences.serverSettings.niceRevision
+  niceRevision: state.preferences.serverSettings.niceRevision,
+  buildTimestamp: state.preferences.serverSettings.buildTimestamp
 })
 
 export default connect(mapStateToProps, null)(injectIntl(AboutTocco))
