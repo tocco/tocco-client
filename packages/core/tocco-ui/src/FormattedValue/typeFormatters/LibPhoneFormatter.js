113160a0e8b5667438ce83ef70a7f85ec5416f1e
Stefan Würsten <swuersten@tocco.ch>
2022-07-19 12:59:02 +0200

    fix(tocco-ui): fix breakwords for email and phone formatter
    Changelog: fix breakwords for email and phone formatter
    Cherry-pick: Up
    Refs: TOCDEV-5765

--- packages/core/tocco-ui/src/FormattedValue/typeFormatters/LibPhoneFormatter.js
+++ packages/core/tocco-ui/src/FormattedValue/typeFormatters/LibPhoneFormatter.js
@@ -3,18 +3,19 @@ import _isEmpty from 'lodash/isEmpty'
 import PropTypes from 'prop-types'
 
 import Link from '../../Link'
-import Typography from '../../Typography'
 
 const LibPhoneFormatter = ({value, breakWords}) => {
   const parsed = parsePhoneNumber(value)
   const formattedInput = _isEmpty(parsed) ? value : parsed.formatInternational()
 
   return (
-    <Typography.Span breakWords={breakWords}>
-      <Link href={`tel:${value}`} target="_blank" onClick={e => e.stopPropagation()}>
-        {formattedInput}
-      </Link>
-    </Typography.Span>
+    <Link
+      href={`tel:${value}`}
+      target="_blank"
+      onClick={e => e.stopPropagation()}
+      label={formattedInput}
+      breakWords={breakWords}
+    />
   )
 }
 
