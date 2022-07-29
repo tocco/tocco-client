5e3550270e1d53e45a3cc6d821640b48a988ebbb
Isabel Boos <iboos@tocco.ch>
2022-07-29 11:42:11 +0200

    chore: fix webpack config
    - ckeditor translation files needs to be inserted into a output file
    - preferably into a chunk related to the ckeditor
    - MinChunkSizePlugin did not create always a html-editor chunk
    - creating an explict chunk with all the @ckeditor modules provides an existing ckeditor chunk
    - using this chunk to use it as output file for the compiled translation files
    - remove ckeditor plugin for packages which do not have the ckeditor bundled
    
    Cherry-pick: Up

--- packages/widgets/sso-login/build/webpack.js
+++ packages/widgets/sso-login/build/webpack.js
@@ -1,11 +1,15 @@
 import CopyWebpackPlugin from 'copy-webpack-plugin' // eslint-disable-line import/default
 
+import {removeCKEditor} from '../../../../build/lib/webpack'
+
 export const adjustConfig = (webpackConfig, config) => {
   const {__DEV__} = config.globals
 
   if (__DEV__) {
     webpackConfig.plugins.push(new CopyWebpackPlugin({patterns: ['./packages/widgets/sso-login/src/dev/test.html']}))
   }
+
+  webpackConfig = removeCKEditor(webpackConfig)
 
   return webpackConfig
 }
