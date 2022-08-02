1ab3fbbd2912c648ca9e40d995fa7eb86ea51b0c
Isabel Boos <iboos@tocco.ch>
2022-08-02 15:31:53 +0200

    fix(tocco-util): fix datetime search field with default value
    - defaultValue for datetime-field is a datetime (no date-only)
      - tql builder created a where-clause with `==` instead of range over whole day
    - when selecting a single value manually a date is set (without time)
      - tql builder creates a where-clause for a range over whole the whole day
    - always use ranges for datetime-fields, also for any single value
    - ignore time for single values and search over whole day
      (otherwise UI component is out of sync)
    
    Cherry-pick: Up
    Changelog: fix datetime search field with default value
    Refs: TOCDEV-5780, TOCDEV-5498

--- packages/core/tocco-util/src/tqlBuilder/utils.js
+++ packages/core/tocco-util/src/tqlBuilder/utils.js
@@ -1,4 +1,4 @@
-import {format, parse, isMatch} from 'date-fns'
+import {format, parse, isMatch, startOfDay} from 'date-fns'
 import {formatInTimeZone} from 'date-fns-tz'
 
 const DateOnlyFormat = 'yyyy-MM-dd'
@@ -6,20 +6,36 @@ const TQLDateTimeFormat = 'yyyy-MM-dd HH:mm'
 const TQLTimeFormat = 'HH:mm:ss.SSS'
 
 /**
- * Parse date string always as local time.
+ * Parse date only string always as local time.
  * Also date-only string `yyyy-MM-dd` as local time instead of UTC.
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/Date#timestamp_string
  * @param {String} value
  * @returns Date
  *
  * ```
- * const localDate = parseInLocalTime('2022-06-05')
+ * // 2022-06-04 22:00
+ * const localDate = parseDateOnlyInLocalTime('2022-06-05')
  * ```
  */
 export const parseDateOnlyInLocalTime = value => parse(value, DateOnlyFormat, new Date())
 
 /**
- * Parse date string always as local time.
+ * Parse date or datetime string as local time and return start of day.
+ * @param {String} value
+ * @returns Date
+ *
+ * ```
+ * // 2022-06-04 22:00
+ * const localDate = parseAsStartOfDayInLocalTime('2022-06-05')
+ *
+ * // 2022-06-04 22:00
+ * const localDate = parseAsStartOfDayInLocalTime('2022-06-05T14:30:00.000+02:00')
+ * ```
+ */
+export const parseAsStartOfDayInLocalTime = value => startOfDay(parseInLocalTime(value))
+
+/**
+ * Parse date or datetime string always as local time.
  * Also date-only string `yyyy-MM-dd` as local time instead of UTC.
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/Date#timestamp_string
  * @param {String} value
@@ -45,6 +61,8 @@ export const parseInLocalTime = value => {
   if (isMatch(value, "yyyy-MM-dd'T'HH:mm:ss.SSSxxx")) {
     return new Date(value)
   }
+
+  return NaN
 }
 
 /**
