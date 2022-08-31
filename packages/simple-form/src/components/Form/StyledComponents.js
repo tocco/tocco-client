ec7d6fecb6721cbafb529d67c08362d06b9b56c2
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2022-08-31 14:38:38 +0200

    refactor(simple-form): implement sticky buttons in modal
    Cherry-pick: Up
    Refs: TOCDEV-5943
    Changelog: implement sticky buttons for simple-form modal

--- packages/simple-form/src/components/Form/StyledComponents.js
+++ packages/simple-form/src/components/Form/StyledComponents.js
@@ -1,9 +1,13 @@
 import styled from 'styled-components'
-import {StyledButton} from 'tocco-ui'
+import {StyledButton, theme} from 'tocco-ui'
 
 export const StyledButtonsWrapper = styled.div`
+  position: sticky;
+  bottom: 0;
   display: flex;
   justify-content: flex-end;
+  background-color: ${theme.color('paper')};
+  z-index: 2; // higher than StyledIndicatorsContainerWrapper
 
   ${StyledButton} {
     margin-right: 0;
