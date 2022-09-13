11b9cd77c6845f123be6b185d6ef3d799f9879fc
Stefan Würsten <swuersten@tocco.ch>
2022-09-13 12:51:40 +0200

    fix(admin): remove locale from documentation url
    Changelog: remove locale from documentation url
    Cherry-pick: Up

--- packages/apps/admin/src/utils/docsUtils.spec.js
+++ packages/apps/admin/src/utils/docsUtils.spec.js
@@ -7,25 +7,25 @@ describe('admin', () => {
         test('should handle 2.29', () => {
           const url = getDocsUrl('2.29')
 
-          expect(url).to.equal('https://229.docs.tocco.ch/de/')
+          expect(url).to.equal('https://229.docs.tocco.ch/')
         })
 
         test('should handle 3.0', () => {
           const url = getDocsUrl('3.0')
 
-          expect(url).to.equal('https://300.docs.tocco.ch/de/')
+          expect(url).to.equal('https://300.docs.tocco.ch/')
         })
 
         test('should handle 3.1', () => {
           const url = getDocsUrl('3.1')
 
-          expect(url).to.equal('https://301.docs.tocco.ch/de/')
+          expect(url).to.equal('https://301.docs.tocco.ch/')
         })
 
         test('should handle 3.10', () => {
           const url = getDocsUrl('3.10')
 
-          expect(url).to.equal('https://310.docs.tocco.ch/de/')
+          expect(url).to.equal('https://310.docs.tocco.ch/')
         })
       })
     })
