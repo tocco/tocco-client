80b9d76080096015cd731e6f7a1b3ecf558a17dd
Isabel Boos <iboos@tocco.ch>
2022-10-06 10:33:23 +0200

    fix(entity-browser): add export action to entity browser
    Refs: TOCDEV-5888
    Cherry-pick: Up
    Changelog: add export action to entity-browser

--- packages/widgets/entity-browser/src/components/LazyAction/index.js
+++ packages/widgets/entity-browser/src/components/LazyAction/index.js
@@ -3,6 +3,7 @@ import {actions} from 'tocco-app-extensions'
 
 const actionMap = {
   delete: lazy(() => import(/* webpackChunkName: "actions" */ './actions/Delete')),
+  export: lazy(() => import(/* webpackChunkName: "actions" */ './actions/Export')),
   'input-edit': lazy(() => import(/* webpackChunkName: "actions" */ './actions/InputEdit')),
   'subscribe-calendar': lazy(() => import(/* webpackChunkName: "actions" */ './actions/SubscribeCalendar')),
   'mailing-list-mail-action': lazy(() => import(/* webpackChunkName: "actions" */ './actions/MailingListMailAction'))
