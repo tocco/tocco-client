76fe17cb1486801d16079437a72f342e6064e4ad
Stefan Würsten <swuersten@tocco.ch>
2022-10-14 16:44:38 +0200

    feat(app-extensions): disable removing outputjob toasters in widgets
    Changelog: disable removing outputjob toasters in widgets
    Refs: TOCDEV-5957

--- packages/core/app-extensions/src/notification/modules/socket/socket.spec.js
+++ packages/core/app-extensions/src/notification/modules/socket/socket.spec.js
@@ -1,5 +1,7 @@
+import {env} from 'tocco-util'
+
 import {TYPES} from '../../types'
-import {notificationToToaster, TOASTER_KEY_PREFIX} from './socket'
+import {hasOutputjobInWidget, notificationToToaster, TOASTER_KEY_PREFIX} from './socket'
 
 describe('app-extensions', () => {
   describe('notification', () => {
@@ -25,6 +27,40 @@ describe('app-extensions', () => {
             expect(toaster.time).to.be.eql(new Date(timeString))
             expect(toaster.title).to.be.eql(title)
             expect(toaster.message).to.be.eql(message)
+          })
+        })
+
+        describe('hasOutputjobInWidget', () => {
+          const notificationWithoutResult = {}
+          const notificationWithEnityResult = {
+            result: {
+              type: 'ENTITIES'
+            }
+          }
+          const notificationWithOutputjob = {
+            result: {
+              type: 'OUTPUTJOB'
+            }
+          }
+
+          test('has output job but embed type is admin', () => {
+            env.setEmbedType('admin')
+            expect(hasOutputjobInWidget(notificationWithOutputjob)).to.be.eql(false)
+          })
+
+          test('has output job and embed type is widget', () => {
+            env.setEmbedType('widget')
+            expect(hasOutputjobInWidget(notificationWithOutputjob)).to.be.eql(true)
+          })
+
+          test('has entity result and embed type is widget', () => {
+            env.setEmbedType('widget')
+            expect(hasOutputjobInWidget(notificationWithEnityResult)).to.be.eql(false)
+          })
+
+          test('without result and embed type is widget', () => {
+            env.setEmbedType('widget')
+            expect(hasOutputjobInWidget(notificationWithoutResult)).to.be.eql(false)
           })
         })
       })
