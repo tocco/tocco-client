95414a50de63a8875dff77a05c19592f782c9e57
PadPalon <luca.neukom@gmail.com>
2022-10-19 11:45:07 +0200

    feat(tocco-ui): customize PercentFormatter
    - instead of using NumberFormatter and adding a % by hand, use FormattedNumber directly in percent mode
    
    Refs: TOCDEV-5759
    Changelog: use FormattedNumber in percent mode in PercentFormatter

--- packages/core/tocco-ui/src/FormattedValue/formatters/PercentFormatter.js
+++ packages/core/tocco-ui/src/FormattedValue/formatters/PercentFormatter.js
@@ -1,16 +1,20 @@
 import PropTypes from 'prop-types'
+import {FormattedNumber} from 'react-intl'
 
 import Typography from '../../Typography'
-import NumberFormatter from './NumberFormatter'
 
 const PercentFormatter = props => (
-  <Typography.Span>
-    <NumberFormatter value={props.value} />%
+  <Typography.Span breakWords={props.breakWords}>
+    <FormattedNumber value={props.value} style="percent" minimumFractionDigits={props?.options?.postPointDigits} />
   </Typography.Span>
 )
 
 PercentFormatter.propTypes = {
-  value: PropTypes.number.isRequired
+  value: PropTypes.number.isRequired,
+  options: PropTypes.shape({
+    postPointDigits: PropTypes.number
+  }),
+  breakWords: PropTypes.bool
 }
 
 export default PercentFormatter
--- packages/core/tocco-ui/src/FormattedValue/formatters/PercentFormatter.spec.js
+++ packages/core/tocco-ui/src/FormattedValue/formatters/PercentFormatter.spec.js
@@ -10,19 +10,28 @@ describe('tocco-ui', () => {
         test('should format value', () => {
           const wrapper = mount(
             <IntlProvider locale="en">
-              <PercentFormatter value={2.41} />
+              <PercentFormatter value={0.0241} options={{postPointDigits: 2}} />
             </IntlProvider>
           )
           expect(wrapper.text()).to.equal('2.41%')
         })
 
-        test('should format value accorind to locale', () => {
+        test('should respect postDecimalDigits', () => {
           const wrapper = mount(
-            <IntlProvider locale="de">
-              <PercentFormatter value={99.9} />
+            <IntlProvider locale="en">
+              <PercentFormatter value={0.0241} options={{postPointDigits: 5}} />
             </IntlProvider>
           )
-          expect(wrapper.text()).to.equal('99,90%')
+          expect(wrapper.text()).to.equal('2.41000%')
+        })
+
+        test('should format value according to locale', () => {
+          const wrapper = mount(
+            <IntlProvider locale="de">
+              <PercentFormatter value={0.999} options={{postPointDigits: 2}} />
+            </IntlProvider>
+          )
+          expect(wrapper.text()).to.equal('99,90\xa0%') // FormattedNumber uses a non-breaking space
         })
       })
     })
