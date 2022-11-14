fc2fef3b31bb8ec239fa21735084f119a37220aa
Isabel Boos <iboos@tocco.ch>
2022-11-14 09:28:28 +0100

    feat(widget-code-copy): simplify widget code
    Refs: TOCDEV-6223
    Changelog: simplify widget code

--- packages/actions/widget-code-copy/src/utils/widgetCode.spec.js
+++ packages/actions/widget-code-copy/src/utils/widgetCode.spec.js
@@ -6,53 +6,12 @@ describe('widget-code-copy', () => {
   describe('utils', () => {
     describe('widgetCode', () => {
       describe('generateWidgetCode', () => {
-        test('should consider localhost', () => {
+        test('should set correct widget-key', () => {
           const widgetConfig = {
-            key: 1,
-            paths: {domain: {value: 'localhost'}}
+            key: 1
           }
           const expectedWidgetCode = trim(`
             <div data-tocco-widget-key="1"></div>
-            <script src="http://localhost:8080/js/tocco-widget-utils/dist/bootstrap.js"></script>
-          `)
-
-          expect(trim(generateWidgetCode(widgetConfig))).to.equal(expectedWidgetCode)
-        })
-
-        test('should consider tocco domains', () => {
-          const widgetConfig = {
-            key: 1,
-            paths: {domain: {value: 'abc.tocco.ch'}}
-          }
-          const expectedWidgetCode = trim(`
-            <div data-tocco-widget-key="1"></div>
-            <script src="https://abc.tocco.ch/js/tocco-widget-utils/dist/bootstrap.js"></script>
-          `)
-
-          expect(trim(generateWidgetCode(widgetConfig))).to.equal(expectedWidgetCode)
-        })
-
-        test('should use tocco backend url instead foreign domain', () => {
-          const widgetConfig = {
-            key: 1,
-            paths: {domain: {value: 'abc.ch'}}
-          }
-          const expectedWidgetCode = trim(`
-            <div data-tocco-widget-key="1"></div>
-            <script src="http://localhost/js/tocco-widget-utils/dist/bootstrap.js"></script>
-          `)
-
-          expect(trim(generateWidgetCode(widgetConfig))).to.equal(expectedWidgetCode)
-        })
-
-        test('should use tocco backend url instead foreign www domain', () => {
-          const widgetConfig = {
-            key: 1,
-            paths: {domain: {value: 'www.abc.ch'}}
-          }
-          const expectedWidgetCode = trim(`
-            <div data-tocco-widget-key="1"></div>
-            <script src="http://localhost/js/tocco-widget-utils/dist/bootstrap.js"></script>
           `)
 
           expect(trim(generateWidgetCode(widgetConfig))).to.equal(expectedWidgetCode)
