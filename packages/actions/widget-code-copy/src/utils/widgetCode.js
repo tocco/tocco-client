0ad6ee79feee8a5f8e5f730121827221dafec2fd
Isabel Boos <iboos@tocco.ch>
2022-11-14 09:28:28 +0100

    feat(widget-code-copy): simplify widget code
    Refs: TOCDEV-6223
    Changelog: simplify widget code

--- packages/actions/widget-code-copy/src/utils/widgetCode.js
+++ packages/actions/widget-code-copy/src/utils/widgetCode.js
@@ -1,23 +1,6 @@
-const getBaseUrl = domain => {
-  if (domain === 'localhost') {
-    return 'http://localhost:8080'
-  }
-
-  // customer.tocco.ch => widget will be added in legacy cms
-  if (domain.includes('.tocco.ch')) {
-    return `https://${domain}`
-  }
-
-  // use either cms tocco backend proxy url or tocco backend url
-  return window.location.origin
-}
-
 export const generateWidgetCode = widgetConfig => {
-  const {key, paths} = widgetConfig
-  const domain = paths?.domain?.value
+  const {key} = widgetConfig
 
-  const widgetCode = `<div data-tocco-widget-key="${key}"></div>
-<script src="${getBaseUrl(domain)}/js/tocco-widget-utils/dist/bootstrap.js"></script>`
-
+  const widgetCode = `<div data-tocco-widget-key="${key}"></div>`
   return widgetCode
 }
