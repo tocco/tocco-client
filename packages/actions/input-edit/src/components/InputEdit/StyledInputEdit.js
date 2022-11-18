98e88c0931032343dc982053f582e69cb3b7ad88
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2022-11-18 15:29:12 +0100

    refactor(input-edit): harmonize spacing
    Refs: TOCDEV-6323
    Changelog: harmonize spacing of input edit action wrapper

--- packages/actions/input-edit/src/components/InputEdit/StyledInputEdit.js
+++ packages/actions/input-edit/src/components/InputEdit/StyledInputEdit.js
@@ -19,13 +19,9 @@ export const StyledActionsWrapper = styled.div`
   background-color: ${theme.color('paper')};
   margin-bottom: 3px;
   /* remove left padding on widget */
-  padding: ${({env}) => (env === 'widget' ? '0 0 8px 0' : '0 8px 8px')};
+  padding: ${({env}) => (env === 'widget' ? '0 0 8px 0' : '8px')};
   grid-row-start: action-start;
   flex-wrap: wrap;
-
-  & > * {
-    margin-top: 8px;
-  }
 `
 export const StyledListWrapper = styled.div`
   grid-row-start: table-start;
