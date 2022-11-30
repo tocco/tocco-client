c9aab588f20787b94bb7fd63f26cb159df4dfd43
Isabel Boos <iboos@tocco.ch>
2022-11-30 11:50:10 +0100

    feat(export): disable submit button for invalid form
    Refs: TOCDEV-6315
    Cherry-pick: Up
    Changelog: disable submit button for invalid form

--- packages/actions/export/src/components/ExportContainer.js
+++ packages/actions/export/src/components/ExportContainer.js
@@ -1,5 +1,7 @@
 import {injectIntl} from 'react-intl'
 import {connect} from 'react-redux'
+import {isValid} from 'redux-form'
+import {templateValues} from 'tocco-app-extensions'
 
 import {loadFormData, runExport, handleTemplateChange} from '../modules/export/actions'
 import Export from './Export'
@@ -14,7 +16,8 @@ const mapStateToProps = state => ({
   availableColumns: state.exportAction.availableColumns,
   defaultValues: state.exportAction.defaultValues,
   selection: state.input.selection,
-  templatesInitialized: state.templateValues.initialized
+  templatesInitialized: state.templateValues.initialized,
+  formValid: isValid(templateValues.reduxFormName)(state)
 })
 
 export default connect(mapStateToProps, mapActionCreators)(injectIntl(Export))
