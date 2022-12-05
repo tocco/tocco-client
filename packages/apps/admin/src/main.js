8b1fa1f99d8f008a84536e74a8a258baca0eb587
PadPalon <luca.neukom@gmail.com>
2022-12-05 09:36:12 +0100

    feat(admin): display non-fullscreen custom actions in menu
    - legacy action are ignored until we actually need them
    
    Refs: TOCDEV-6222
    Changelog: custom actions that should open in a modal are now displayed in menu

--- packages/apps/admin/src/main.js
+++ packages/apps/admin/src/main.js
@@ -16,6 +16,8 @@ import {chooseDocument} from 'tocco-docs-browser/src/main'
 import {reducer as reducerUtil} from 'tocco-util'
 
 import reducers, {sagas} from './modules/reducers'
+import Action from './routes/entities/components/Action'
+import navigationStrategy from './routes/entities/utils/navigationStrategy'
 import shortcuts from './shortcuts'
 
 const packageName = 'admin'
@@ -39,6 +41,10 @@ const initApp = (id, input, events, publicPath) => {
   const store = appFactory.createStore(reducers, sagas, input, packageName)
   externalEvents.addToStore(store, () => ({}))
   actionEmitter.addToStore(store)
+  actions.addToStore(store, () => ({
+    appComponent: Action,
+    navigationStrategy
+  }))
   actions.dynamicActionsAddToStore(store)
   errorLogging.addToStore(store, true, ['console', 'remote', 'notification'])
   notification.addToStore(store, true, {withNotificationCenter: true})
