01a160d3dff5290554c6d65d2fdaf73f895eced2
Isabel Boos <iboos@tocco.ch>
2022-12-06 11:14:24 +0100

    chore: remove unused plops

--- plopfile.js
+++ plopfile.js
@@ -1,7 +1,5 @@
 module.exports = function (plop) {
   plop.addHelper('ifIn', require('./plop/helpers/ifIn').default)
-  plop.setGenerator('Component', require('./plop/generators/component').default)
-  plop.setGenerator('Action', require('./plop/generators/action').default)
   plop.setGenerator('Package', require('./plop/generators/package').default)
   plop.setGenerator('Env', require('./plop/generators/env').default)
   plop.setGenerator('Bundle app', require('./plop/generators/bundle-app').default)
