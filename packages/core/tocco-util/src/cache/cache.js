d4520e9869d80afc93659feeb9cae87caa704fab
Isabel Boos <iboos@tocco.ch>
2022-12-08 16:34:25 +0100

    feat(tocco-util): add object cache
    - add generic object cache which can be cleared
    
    Refs: TOCDEV-6276
    Changelog: add object cache
    Cherry-pick: Up

--- packages/core/tocco-util/src/cache/cache.js
+++ packages/core/tocco-util/src/cache/cache.js
@@ -1,10 +1,15 @@
+import _get from 'lodash/get'
+import _set from 'lodash/set'
+import _unset from 'lodash/unset'
+
 import nice from '../nice'
 
 /*
+ * Object cache is per browser tab, but clears on reload.
  * Short term caching (sessionStorage) is per browser tab.
  * Long term cache (localStorage) is shared across tabs and browser sessions.
  *
- * short term cache gets cleared
+ * short term / object cache gets cleared
  *  - individually (e.g. displays on change entity)
  *  - whenever overall cache gets cleared
  *
@@ -18,21 +23,28 @@ import nice from '../nice'
 const prefix = `tocco.cache`
 const getKey = (type, id) => `${prefix}.${type}.${id}`
 
-export const addShortTerm = (type, id, value) => add(type, id, value, sessionStorage)
-export const addLongTerm = (type, id, value) => add(type, id, value, localStorage)
+const objectCache = {}
 
-const add = (type, id, value, storage) => {
+const objectWriter = (key, value) => _set(objectCache, key, value)
+const storageWriter = storage => (key, value) => storage.setItem(key, JSON.stringify(value))
+
+export const addObjectCache = (type, id, value) => add(type, id, value, objectWriter)
+export const addShortTerm = (type, id, value) => add(type, id, value, storageWriter(sessionStorage))
+export const addLongTerm = (type, id, value) => add(type, id, value, storageWriter(localStorage))
+
+const add = (type, id, value, write) => {
   if (__DEV__ || nice.getRunEnv() !== 'PRODUCTION') {
     return null
   }
 
-  return storage.setItem(getKey(type, id), JSON.stringify(value))
+  return write(getKey(type, id), value)
 }
 
-export const getShortTerm = (type, id) => get(type, id, sessionStorage)
-export const getLongTerm = (type, id) => get(type, id, localStorage)
+export const getObjectCache = (type, id) => _get(objectCache, getKey(type, id))
+export const getShortTerm = (type, id) => getFromStorage(type, id, sessionStorage)
+export const getLongTerm = (type, id) => getFromStorage(type, id, localStorage)
 
-const get = (type, id, storage) => {
+const getFromStorage = (type, id, storage) => {
   const cachedValue = storage.getItem(getKey(type, id))
 
   if (!cachedValue) {
@@ -42,18 +54,21 @@ const get = (type, id, storage) => {
   return JSON.parse(cachedValue)
 }
 
-export const removeShortTerm = (type, id) => remove(type, id, sessionStorage)
-export const removeLongTerm = (type, id) => remove(type, id, localStorage)
-const remove = (type, id, storage) => storage.removeItem(getKey(type, id))
+export const removeObjectCache = (type, id) => _unset(objectCache, getKey(type, id))
+export const removeShortTerm = (type, id) => removeFromStorage(type, id, sessionStorage)
+export const removeLongTerm = (type, id) => removeFromStorage(type, id, localStorage)
+const removeFromStorage = (type, id, storage) => storage.removeItem(getKey(type, id))
 
 export const clearAll = () => {
+  clearObjectCache()
   clearShortTerm()
   clearLongTerm()
 }
 
-export const clearShortTerm = () => clear(sessionStorage)
-export const clearLongTerm = () => clear(localStorage)
-const clear = storage => {
+export const clearObjectCache = () => Object.keys(objectCache).forEach(key => _unset(objectCache, key))
+export const clearShortTerm = () => clearStorage(sessionStorage)
+export const clearLongTerm = () => clearStorage(localStorage)
+const clearStorage = storage => {
   const keys = Object.keys(storage)
   keys.filter(key => key.startsWith(prefix)).forEach(key => storage.removeItem(key))
 }
